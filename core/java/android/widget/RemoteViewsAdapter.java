109a799f584be3be1d3fd6510dc531c1260955b4
Jet.Z <jetzhow@gmail.com>
2012-10-26 10:26:34 +0800

    fix bug about view cache in RemoteViews
    clear the data in cache before reloading new items,
    & the method 'getCount()' returns tmpMetaData.count.
    
    Change-Id: I0d37d7a99408f60b1d63fa5ca0c0a0f265dd0a8a
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- core/java/android/widget/RemoteViewsAdapter.java
+++ core/java/android/widget/RemoteViewsAdapter.java
@@ -1080,9 +1080,9 @@ public class RemoteViewsAdapter extends BaseAdapter implements Handler.Callback 
     }
 
     public int getCount() {
-        final RemoteViewsMetaData metaData = mCache.getMetaData();
-        synchronized (metaData) {
-            return metaData.count;
+        final RemoteViewsMetaData tmpMetaData = mCache.getTemporaryMetaData();
+        synchronized (tmpMetaData) {
+            return tmpMetaData.count;
         }
     }
 
@@ -1268,6 +1268,11 @@ public class RemoteViewsAdapter extends BaseAdapter implements Handler.Callback 
             return;
         }
 
+        // Clear the data in cache
+        final RemoteViewsMetaData metaData = mCache.getMetaData();
+        synchronized (metaData) {
+            metaData.reset();
+        }
         // Flush the cache so that we can reload new items from the service
         synchronized (mCache) {
             mCache.reset();
