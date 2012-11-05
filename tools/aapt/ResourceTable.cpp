f7bcd62e360db1b6d3fa3fd019826a2a783a4118
JBirdVegas <jbirdvegas@gmail.com>
2012-11-04 23:30:52 -0500

    Suppress String resources build warnings
    warnings suppressed are:
    aapt: warning: **** string '%s' has no default or required localization for
    aapt: warning: string '%s' has no default translation in
    
    Patch Set 2: move ifdef and only suppress warnings for packages that
                 define NO_DEFAULT_TRANSLATION_WARNINGS and/or
                 NO_LOCALIZATION_WARNING
    
    Patch Set 3: by default do not show warnings unless
                 SHOW_DEFAULT_TRANSLATION_WARNINGS and/or
                 SHOW_LOCALIZATION_WARNINGS
    
    Change-Id: I50ef2a623959a1cc53053b446328df8e34cc99e8
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- tools/aapt/ResourceTable.cpp
+++ tools/aapt/ResourceTable.cpp
@@ -2744,6 +2744,7 @@ ResourceTable::validateLocalizations(void)
     for (const auto& nameIter : mLocalizations) {
         const std::map<String8, SourcePos>& configSrcMap = nameIter.second;
 
+#ifdef SHOW_DEFAULT_TRANSLATION_WARNINGS
         // Look for strings with no default localization
         if (configSrcMap.count(defaultLocale) == 0) {
             SourcePos().warning("string '%s' has no default translation.",
@@ -2755,7 +2756,8 @@ ResourceTable::validateLocalizations(void)
             }
             // !!! TODO: throw an error here in some circumstances
         }
-
+#endif
+#ifdef SHOW_LOCALIZATION_WARNINGS
         // Check that all requested localizations are present for this string
         if (mBundle->getConfigurations().size() > 0 && mBundle->getRequireLocalization()) {
             const char* allConfigs = mBundle->getConfigurations().string();
@@ -2805,6 +2807,7 @@ ResourceTable::validateLocalizations(void)
                         configStr.string());
             }
         }
+#endif
     }
 
     return err;
