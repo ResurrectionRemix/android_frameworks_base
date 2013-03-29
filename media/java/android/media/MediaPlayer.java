1459b7d69eaef2d1a2ef9a5debc0146342eb712f
Andrew Neal <andrewdneal@gmail.com>
2013-03-28 23:52:07 -0500

    Remove unnecessary updateSurfaceScreenOn call.
    This change stops updateSurfaceScreenOn() from being called one time
    right after another. It's called at the end of stayAwake(boolean awake),
    which is called right before updateSurfaceScreenOn() in release().
    
    Change-Id: I3e9db322f0abce0cd37320a103015ba72dffa64f

--- media/java/android/media/MediaPlayer.java
+++ media/java/android/media/MediaPlayer.java
@@ -1455,7 +1455,6 @@ public class MediaPlayer implements SubtitleController.Listener
      */
     public void release() {
         stayAwake(false);
-        updateSurfaceScreenOn();
         mOnPreparedListener = null;
         mOnBufferingUpdateListener = null;
         mOnCompletionListener = null;
