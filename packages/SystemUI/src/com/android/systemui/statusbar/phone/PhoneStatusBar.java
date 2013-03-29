652b349a3390a03d7c43103b015c4b6ac8b82ff0
Danny Baumann <dannybaumann@web.de>
2013-03-29 12:30:15 +0100

    Fix pie controls being disabled by launcher gestures.
    Some launchers have gestures for expanding/collapsing the status bar
    from swipe gestures which call the respective StatusBarService methods
    even if the status bar isn't visible due to expanded desktop. Because of
    that, the status bar code internally thinks the expansion is carried out
    (even if invisible), thus disabling the bottom pie controls in the
    process.
    Fix that by making PhoneStatusBar aware of it being hidden by expanded
    desktop and ignoring the expand call in that case.
    
    Change-Id: If386ca953517d9b5efcb6cb9054f5c5e6a9e0bc4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1689,6 +1689,11 @@ public class PhoneStatusBar extends BaseStatusBar {
         if ((mDisabled & StatusBarManager.DISABLE_EXPAND) != 0) {
             return ;
         }
+        // don't allow expanding via e.g. service call while status bar is hidden
+        // due to expanded desktop
+        if (mExpandedDesktopState == 2) {
+            return;
+        }
 
         mNotificationPanel.expand();
         if (mHasFlipSettings && mScrollView.getVisibility() != View.VISIBLE) {
@@ -1743,6 +1748,11 @@ public class PhoneStatusBar extends BaseStatusBar {
     public void animateExpandSettingsPanel() {
         if (SPEW) Slog.d(TAG, "animateExpand: mExpandedVisible=" + mExpandedVisible);
         if ((mDisabled & StatusBarManager.DISABLE_EXPAND) != 0) {
+            return;
+        }
+        // don't allow expanding via e.g. service call while status bar is hidden
+        // due to expanded desktop
+        if (mExpandedDesktopState == 2) {
             return;
         }
 
