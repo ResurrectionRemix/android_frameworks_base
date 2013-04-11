1c13f79be67aa2719c00e88ec2baf12d0fc355fc
Tom Marshall <tdm.code@gmail.com>
2013-04-11 02:45:56 +0000

    Allow custom density setting
    Use system property persist.lcd_density to set custom density.  The custom
    setting affects the entire UI.  It is independent of ro.sf.lcd_density and
    has no effect on play store compatibility.
    
    Code distilled from PA project via PAC project.
    
    Change-Id: I8d26405d5d33bdf2890a0e9f67f113a4dc3e763b

--- core/java/android/view/DisplayInfo.java
+++ core/java/android/view/DisplayInfo.java
@@ -306,6 +306,7 @@ public final class DisplayInfo implements Parcelable {
         outMetrics.scaledDensity = outMetrics.noncompatScaledDensity = outMetrics.density;
         outMetrics.xdpi = outMetrics.noncompatXdpi = physicalXDpi;
         outMetrics.ydpi = outMetrics.noncompatYdpi = physicalYDpi;
+        outMetrics.updateDensity();
 
         if (cih != null) {
             CompatibilityInfo ci = cih.getIfNeeded();
