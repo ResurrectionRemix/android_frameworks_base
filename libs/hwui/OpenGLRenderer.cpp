ce374d20442ed349b3e4ce745438b0c0236982b2
altan <korukaltan@gmail.com>
2013-05-27 21:55:45 +0300

    Remove opaque check in preparing dirty region
    Change-Id: I80f9e3d1de19e85b9d339369a11d49e41f383017

--- libs/hwui/OpenGLRenderer.cpp
+++ libs/hwui/OpenGLRenderer.cpp
@@ -208,12 +208,19 @@ status_t OpenGLRenderer::prepareDirty(float left, float top, float right, float 
 }
 
 status_t OpenGLRenderer::clear(float left, float top, float right, float bottom, bool opaque) {
-#ifdef QCOM_BSP
+#ifdef QCOM_HARDWARE
     mCaches.enableScissor();
     mCaches.setScissor(left, mSnapshot->height - bottom, right - left, bottom - top);
     glClear(GL_COLOR_BUFFER_BIT);
     if(opaque)
+    {
         mCaches.resetScissor();
+        return DrawGlInfo::kStatusDone;
+    }
+    else
+    {
+        return DrawGlInfo::kStatusDrew;
+    }
 #else
     if (!opaque) {
         mCaches.enableScissor();
@@ -221,10 +228,10 @@ status_t OpenGLRenderer::clear(float left, float top, float right, float bottom,
         glClear(GL_COLOR_BUFFER_BIT);
         return DrawGlInfo::kStatusDrew;
     }
-    mCaches.resetScissor();
-#endif
 
+    mCaches.resetScissor();
     return DrawGlInfo::kStatusDone;
+#endif
 }
 
 void OpenGLRenderer::syncState() {
