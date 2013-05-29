d3e6f6e881d456816b3e6002d223dca395ad1b68
altan <korukaltan@gmail.com>
2013-05-29 22:32:59 +0300

    UPDATE1
    Change-Id: I95b41076fdce77330c4aee7c5297e021be73d63e

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -26,10 +26,12 @@ import android.util.Slog;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.aokp.StatusBarHelpers;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.policy.NetworkController;
 
@@ -58,6 +60,9 @@ public class SignalClusterView
 
     private boolean showingSignalText = false;
     private boolean showingWiFiText = false;
+    private int mStockFontSize;
+    private int mFontSize;
+
     private boolean showingAltCluster = false;
 
     ViewGroup mWifiGroup, mMobileGroup;
@@ -105,6 +110,7 @@ public class SignalClusterView
 
         mHandler = new Handler();
 
+        mStockFontSize = StatusBarHelpers.pixelsToSp(mContext,mMobileText.getTextSize());
         mSettingsObserver.observe();
 
         apply();
@@ -271,6 +277,8 @@ public class SignalClusterView
     protected void updateSettings() {
         ContentResolver resolver = mContext.getContentResolver();
 
+        int fontSize = Settings.System.getInt(resolver,
+                Settings.System.STATUSBAR_FONT_SIZE,mStockFontSize);
         showingSignalText = (Settings.System.getInt(resolver,
                 Settings.System.STATUSBAR_SIGNAL_TEXT,STYLE_HIDE) > 0);
         showingWiFiText = Settings.System.getInt(resolver,
@@ -278,6 +286,19 @@ public class SignalClusterView
         boolean clustdefault = getResources().getBoolean(R.bool.statusbar_alt_signal_layout);
         showingAltCluster = Settings.System.getBoolean(resolver,
                 Settings.System.STATUSBAR_SIGNAL_CLUSTER_ALT, clustdefault);
+        if (fontSize != mFontSize) {
+            mFontSize = fontSize;
+            mWiFiText.setTextSize(mFontSize);
+            mMobileText.setTextSize(mFontSize);
+            
+            int width = StatusBarHelpers.getIconWidth(mContext, mFontSize);
+            mWifi.getLayoutParams().width = width;
+            mMobile.getLayoutParams().width = width;
+            mAirplane.getLayoutParams().width = width;
+
+
+        }
         apply();
     }
 }
+
