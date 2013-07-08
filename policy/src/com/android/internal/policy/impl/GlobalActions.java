935807d6468438947e4ca2d121e9675dcdbfd752
altan <korukaltan@gmail.com>
2013-07-09 02:49:25 +0300

    fix for power menu torch toggle
    Change-Id: I230541b96af63c78e7a5c0f71386f891d6da0753

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -411,12 +411,11 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             mItems.add(new SinglePressAction(com.android.internal.R.drawable.ic_lock_torch,
                     R.string.global_action_torch) {
                 public void onPress() {
-                    mContext.sendBroadcast(new Intent("net.cactii.flash2.TOGGLE_FLASHLIGHT"));
-                    /*Intent intent = new Intent("android.intent.action.MAIN");
+                    Intent intent = new Intent("android.intent.action.MAIN");
                     intent.setComponent(ComponentName.unflattenFromString("com.aokp.Torch/.TorchActivity"));
                     intent.addCategory("android.intent.category.LAUNCHER");
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    mContext.startActivity(intent);*/
+                    mContext.startActivity(intent);
                 }
 
                 public boolean showDuringKeyguard() {
