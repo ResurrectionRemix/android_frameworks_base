d5cb17b3d41556ea5615db135be27ad996d76cbb
altan <korukaltan@gmail.com>
2013-07-28 20:58:00 +0300

    HALO first_start tutorial & gesture dead-zone

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -110,6 +110,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
     public static final String TAG = "HaloLauncher";
 
     enum State {
+		FIRST_START,
         IDLE,
         HIDDEN,
         SILENT,
@@ -185,7 +186,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
     SharedPreferences preferences;
     private String KEY_HALO_POSITION_Y = "halo_position_y";
     private String KEY_HALO_POSITION_X = "halo_position_x";
-
+    private String KEY_HALO_FIRST_START = "halo_first_start";
 
     private final class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {
@@ -323,7 +324,12 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
         preferences = mContext.getSharedPreferences("Halo", 0);
         int msavePositionX = preferences.getInt(KEY_HALO_POSITION_X, 0);
         int msavePositionY = preferences.getInt(KEY_HALO_POSITION_Y, mScreenHeight / 2 - mIconHalfSize);
-        
+
+        if (preferences.getBoolean(KEY_HALO_FIRST_START, true)) {
+            mState = State.FIRST_START;
+            preferences.edit().putBoolean(KEY_HALO_FIRST_START, false).apply();
+        }
+       
         mKillX = mScreenWidth / 2;
         mKillY = mIconHalfSize;
 
@@ -355,7 +361,28 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
             mEffect.setHaloX(msavePositionX);
             mEffect.setHaloY(msavePositionY);
             mEffect.nap(500);
-            if (mHideTicker) mEffect.sleep(HaloEffect.SNAP_TIME + HaloEffect.NAP_TIME + 2500, HaloEffect.SLEEP_TIME, false);
+            
+            if (mState == State.FIRST_START) {
+                mEffect.ticker(mContext.getResources().getString(R.string.halo_tutorial1), 0, 1000);
+                mEffect.updateResources();
+                mHandler.postDelayed(new Runnable() {
+                    public void run() {
+                        mEffect.ticker(mContext.getResources().getString(R.string.halo_tutorial2), 0, 4000);
+                        mEffect.updateResources();
+                        mHandler.postDelayed(new Runnable() {
+                            public void run() {
+                                mEffect.ticker(mContext.getResources().getString(R.string.halo_tutorial3), 0, 3000);
+                                mEffect.updateResources();
+                                mHandler.postDelayed(new Runnable() {
+                                    public void run() {
+                                        mState = State.IDLE;
+                                        if (mHideTicker) mEffect.sleep(HaloEffect.SNAP_TIME + HaloEffect.NAP_TIME + 2500, HaloEffect.SLEEP_TIME, false);
+                                    }}, 3000);
+                            }}, 7000);
+                    }}, 5000);
+            } else {
+                if (mHideTicker) mEffect.sleep(HaloEffect.SNAP_TIME + HaloEffect.NAP_TIME + 2500, HaloEffect.SLEEP_TIME, false);
+            }
         }
     }
     
@@ -504,6 +531,10 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+		
+        // Prevent any kind of interaction while HALO explains itself
+        if (mState == State.FIRST_START) return true;
+        
         mGestureDetector.onTouchEvent(event);
 
         final int action = event.getAction();
@@ -757,13 +788,14 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
 
                         boolean gestureChanged = false;
                         final int deltaIndex = (Math.abs(deltaY) - verticalThreshold) / verticalSteps;
-                        if (deltaY > 0) {                           
-                            if (deltaIndex < 2 && mGesture != Gesture.UP1) {
-                                mGesture = Gesture.UP1;
-                                gestureChanged = true;
-                                mEffect.setHaloOverlay(HaloProperties.Overlay.DISMISS, 1f);
-                                gestureText = mContext.getResources().getString(R.string.halo_dismiss);
-                            } else if (deltaIndex > 1 && mGesture != Gesture.UP2) {
+                        if (deltaIndex < 1 && mGesture != Gesture.NONE) {
+                            // Dead zone buffer to prevent accidental notifiction dismissal
+                            mGesture = Gesture.NONE;
+                            gestureChanged = true;
+                            mEffect.setHaloOverlay(HaloProperties.Overlay.NONE, 0f);
+                            gestureText = "";
+                        } else if (deltaY > 0) { 
+                            if (deltaIndex == 1 && mGesture != Gesture.UP1) {
                                 mGesture = Gesture.UP2;
                                 gestureChanged = true;
                                 mEffect.setHaloOverlay(HaloProperties.Overlay.CLEAR_ALL, 1f);
@@ -771,7 +803,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
                             }
 
                         } else {
-                            if (deltaIndex < 2 && mGesture != Gesture.DOWN1) {
+                          if (deltaIndex == 1 && mGesture != Gesture.DOWN1) {
                                 mGesture = Gesture.DOWN1;
                                 gestureChanged = true;
                                 mEffect.setHaloOverlay(mTickerLeft ? HaloProperties.Overlay.BACK_LEFT
@@ -1212,8 +1244,9 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
         }
 
         // Set text
-        if (mState != State.SILENT) mEffect.ticker(text, delay, duration);
 
+        if (mState != State.SILENT && mState != State.FIRST_START) mEffect.ticker(text, delay, duration);
+        
         mEffect.updateResources();
         mEffect.invalidate();
     }
