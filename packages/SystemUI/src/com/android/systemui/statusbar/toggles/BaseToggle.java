03327ebb2e8a31782d7b080f72cd0c1ed013806e
altan <korukaltan@gmail.com>
2013-07-31 02:16:18 +0300

    hata

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/BaseToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/BaseToggle.java
@@ -1,4 +1,3 @@
-
 package com.android.systemui.statusbar.toggles;
 
 import android.app.ActivityManagerNative;
@@ -72,10 +71,17 @@ public abstract class BaseToggle
         scheduleViewUpdate();
     }
 
+<<<<<<< HEAD
+=======
+    protected final void setTextColor(int cl) {
+        mTextColor = cl;
+    }
+
     public final int getTextColor() {
         return mTextColor;
     }
 
+>>>>>>> ce3869b... add support for textsize and -color
     protected final void setTextSize(int s) {
         mTextSize = s;
     }
