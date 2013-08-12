0a114aad443099948a49111db9a2e86218b7e64d
Danesh Mondegarian <daneshm90@gmail.com>
2013-08-12 00:07:13 -0700

    PreferenceFragment : Set title if specified
    Set the title of the preference activity if specified by the
    preference screen.
    
    Example of where it is broken :
    
    Settings -> Accessibility
    Title says "Accessibility", but preferencescreen's android:title attribute
    specified "Accessibility settings"
    
    Change-Id: Icc89cf6bc37e3f6fc98c6bc05a0ef47185daa52c
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- core/java/android/preference/PreferenceFragment.java
+++ core/java/android/preference/PreferenceFragment.java
@@ -266,6 +266,11 @@ public abstract class PreferenceFragment extends Fragment implements
                 postBindPreferences();
             }
         }
+        CharSequence title = preferenceScreen.getTitle();
+        // Set the title of the activity
+        if (title != null) {
+            getActivity().setTitle(title);
+        }
     }
 
     /**
