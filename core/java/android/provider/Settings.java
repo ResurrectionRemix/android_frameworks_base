ad4a25c6e2c9014dd832b4a2c331988ad1223a2e
cristianomatos <cristianobmatos@gmail.com>
2013-09-24 07:28:46 -0300

    AOKP custom system animations (1/2)
    Ported from here: http://gerrit.aokp.co/#/c/12304/
    
    Thanks to Steve Spear
    
    PS2: Clean tabs
    
    Change-Id: I190044f3fdb166beb8a620bc4b5f91784d6398b8
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3749,7 +3749,27 @@ public final class Settings {
          * @hide
          */
          public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
-         
+
+         /**
+          * Custom System Animations
+          *
+          * @hide
+          */
+         public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                 "activity_open",
+                 "activity_close",
+                 "task_open",
+                 "task_close",
+                 "task_to_front",
+                 "task_to_back",
+                 "wallpaper_open",
+                 "wallpaper_close",
+                 "wallpaper_intra_open",
+                 "wallpaper_intra_close",
+         };
+         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+         public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
+                  
          /**
          * Width and height of output vide expressed in WxH
          * @hide
