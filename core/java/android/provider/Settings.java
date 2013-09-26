69819b94f6a5e3f6288b5793715b1e04f24bc4b9
htchoi1995 <htchoi1995@gmail.com>
2013-09-26 13:01:58 +0200

    [1/2] SystemUI: Network speed indicator
    Change-Id: I88e7cdfe20342c695a33383db36bf80bb4ffe009
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3813,6 +3813,11 @@ public final class Settings {
         public static final String HIDE_FROM_RECENTS_LIST = "hide_from_recents_list";
 
 
+        /* Network speed indicator
+         * @hide
+         */
+        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
