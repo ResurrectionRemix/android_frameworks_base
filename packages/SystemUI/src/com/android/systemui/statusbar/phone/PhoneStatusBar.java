7332e65dd85250ac1ba88d03ebe683d1ed015a7a
htchoi1995 <htchoi1995@gmail.com>
2013-09-26 13:01:58 +0200

    [1/2] SystemUI: Network speed indicator
    Change-Id: I88e7cdfe20342c695a33383db36bf80bb4ffe009
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2320,7 +2320,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         @Override
         public void run() {
             checkBarModes();
-        }};
+        }
+    };
 
     @Override
     public void setInteracting(int barWindow, boolean interacting) {
