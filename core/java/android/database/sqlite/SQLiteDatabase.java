a6e727ee400e83b3e852bd3dad9d1fc3a6bf6eea
muzbit.kim <muzbit.kim@lge.com>
2013-11-07 14:18:23 +0900

    SQLiteDatabase: Catch corrupt exception during transaction
    If corruption occurs in a transaction running, DB file should be deleted.
    
    Change-Id: I5b3a122ebe51770966c334021ab6717d5ff081c3
    Signed-off-by: muzbit.kim <muzbit.kim@lge.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- core/java/android/database/sqlite/SQLiteDatabase.java
+++ core/java/android/database/sqlite/SQLiteDatabase.java
@@ -551,7 +551,10 @@ public final class SQLiteDatabase extends SQLiteClosable {
                             SQLiteSession.TRANSACTION_MODE_IMMEDIATE,
                     transactionListener,
                     getThreadDefaultConnectionFlags(false /*readOnly*/), null);
-        } finally {
+        } catch (SQLiteDatabaseCorruptException ex) {
+            onCorruption();
+            throw ex;
+        }finally {
             releaseReference();
         }
     }
@@ -564,6 +567,9 @@ public final class SQLiteDatabase extends SQLiteClosable {
         acquireReference();
         try {
             getThreadSession().endTransaction(null);
+        } catch (SQLiteDatabaseCorruptException ex) {
+            onCorruption();
+            throw ex;
         } finally {
             releaseReference();
         }
@@ -681,6 +687,9 @@ public final class SQLiteDatabase extends SQLiteClosable {
         acquireReference();
         try {
             return getThreadSession().yieldTransaction(sleepAfterYieldDelay, throwIfUnsafe, null);
+        } catch (SQLiteDatabaseCorruptException ex) {
+            onCorruption();
+            throw ex;
         } finally {
             releaseReference();
         }
