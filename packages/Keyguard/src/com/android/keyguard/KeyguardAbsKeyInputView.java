e9bc9b2eea67ed60bd5d0287b6d003e129456864
DvTonder <david.vantonder@gmail.com>
2013-11-19 12:15:32 +0530

    [2/2] frameworks/base: Enable quick unlock
    Automatically unlock the lockscreen when the correct pin/password is keyed.
    
    Settings: https://gerrit.omnirom.org/#/c/2605/
    
    Change-Id: I3c1df4b573321a9343d1c339055577d5ca737f14
    
    Conflicts:
    
    	packages/Keyguard/src/com/android/keyguard/KeyguardAbsKeyInputView.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardAbsKeyInputView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardAbsKeyInputView.java
@@ -119,6 +119,9 @@ public abstract class KeyguardAbsKeyInputView extends LinearLayout
             }
         });
 
+        mQuickUnlock = (Settings.System.getInt(mContext.getContentResolver(),
+            Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL, 0) == 1);
+
         mPasswordEntry.addTextChangedListener(new TextWatcher() {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
@@ -132,8 +135,13 @@ public abstract class KeyguardAbsKeyInputView extends LinearLayout
                 }
                 if (mQuickUnlock) {
                     String entry = mPasswordEntry.getText().toString();
+<<<<<<< HEAD
                     if (entry.length() > MINIMUM_PASSWORD_LENGTH_BEFORE_REPORT
                             && mLockPatternUtils.checkPassword(entry)) {
+=======
+                    if (entry.length() > MINIMUM_PASSWORD_LENGTH_BEFORE_REPORT &&
+                            mLockPatternUtils.checkPassword(entry)) {
+>>>>>>> f6b74ff... [2/2] frameworks/base: Enable quick unlock
                         mCallback.reportSuccessfulUnlockAttempt();
                         mCallback.dismiss(true);
                     }
