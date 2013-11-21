c928efd28a3fe07af71525ac38bc121110d26d01
Tom Marshall <tdm.code@gmail.com>
2013-11-21 19:03:26 +0000

    Allow custom density setting
    Use system property persist.sys.lcd_density to set custom density.  The
    custom setting affects the entire UI.  It is independent of
    ro.sf.lcd_density and has no effect on play store compatibility.
    
    Code distilled from PA project via PAC project.
    
    Change-Id: I8d26405d5d33bdf2890a0e9f67f113a4dc3e763b

--- core/java/android/content/res/Resources.java
+++ core/java/android/content/res/Resources.java
@@ -247,6 +247,7 @@ public class Resources {
             CompatibilityInfo compatInfo, IBinder token) {
         mAssets = assets;
         mMetrics.setToDefaults();
+        mMetrics.updateDensity();
         if (compatInfo != null) {
             mCompatibilityInfo = compatInfo;
         }
@@ -1906,6 +1907,7 @@ public class Resources {
             if (mConfiguration.densityDpi != Configuration.DENSITY_DPI_UNDEFINED) {
                 mMetrics.densityDpi = mConfiguration.densityDpi;
                 mMetrics.density = mConfiguration.densityDpi * DisplayMetrics.DENSITY_DEFAULT_SCALE;
+                mMetrics.updateDensity();
             }
             mMetrics.scaledDensity = mMetrics.density * mConfiguration.fontScale;
 
@@ -2353,7 +2355,7 @@ public class Resources {
             }
             sPreloaded = true;
             mPreloading = true;
-            sPreloadedDensity = DisplayMetrics.DENSITY_DEVICE;
+            sPreloadedDensity = DisplayMetrics.getDeviceDensity();
             mConfiguration.densityDpi = sPreloadedDensity;
             updateConfiguration(null, null);
         }
