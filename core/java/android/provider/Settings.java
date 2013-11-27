f2134ec6b74322e7ab8fde6e1a4f3d0997de06b5
jkl5616 <jkl5616@gmail.com>
2013-11-27 22:04:19 +0100

    Frameworks: Listanimation views and interpolator (1/2)
    thanks team ThinkingBridge for it. Difference to original implementation
    
    - code cleanup
    - some smaller code fixes
    - remove of rotation animation due it is to laggy
    - multiuser support
    
    Change-Id: I255b60dd963c616fb5c0aa583c23d52a7a5108f8
    
    Patchset: 1
    https://gerrit.omnirom.org/#/c/2862/

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3410,6 +3410,35 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
 
         /**
+         * ListView Animations
+         * 0 == None
+         * 1 == Wave (Left)
+         * 2 == Wave (Right)
+         * 3 == Scale
+         * 4 == Alpha
+         * 5 == Stack (Top)
+         * 6 == Stack (Bottom)
+         * 7 == Translate (Left)
+         * 8 == Translate (Right)
+         * @hide
+         */
+         public static final String LISTVIEW_ANIMATION = "listview_animation";
+
+        /**
+         * ListView Interpolators
+         * 0 == None
+         * 1 == accelerate_interpolator
+         * 2 == decelerate_interpolator
+         * 3 == accelerate_decelerate_interpolator
+         * 4 == anticipate_interpolator
+         * 5 == overshoot_interpolator
+         * 6 == anticipate_overshoot_interpolator
+         * 7 == bounce_interpolator
+         * @hide
+         */
+         public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
