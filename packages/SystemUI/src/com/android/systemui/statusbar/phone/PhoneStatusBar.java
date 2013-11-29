74cab600c7e481f0ec04bf7d96846d05617e73af
Lars Greiss <kufikugel@googlemail.com>
2013-11-29 02:03:44 +0100

    fb: enable navbar for hw key devices
    we moved this part into PhoneWindowManager which controlls the navbar.
    Due that we allow on all devices a navbar the check on PhoneStatusBar
    is not needed and prevend the navbar on comming up on hw key devices
    
    Change-Id: Ia3aef15389007cba1b3905b5de9f084578b4aca3
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -590,25 +590,29 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
 
         updateShowSearchHoldoff();
 
+<<<<<<< HEAD
         try {
             boolean showNav = mWindowManagerService.hasNavigationBar();
             if (DEBUG) Log.v(TAG, "hasNavigationBar=" + showNav);
             if (showNav && !mRecreating) {
                 mNavigationBarView =
                     (NavigationBarView) View.inflate(context, R.layout.navigation_bar, null);
-
-                mNavigationBarView.setDisabledFlags(mDisabled);
-                mNavigationBarView.setBar(this);
-                mNavigationBarView.setOnTouchListener(new View.OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View v, MotionEvent event) {
-                        checkUserAutohide(v, event);
-                        return false;
-                    }});
-            }
-        } catch (RemoteException ex) {
-            // no window manager? good luck with that
+=======
+        if (mNavigationBarView == null) {
+            mNavigationBarView =
+                (NavigationBarView) View.inflate(context, R.layout.navigation_bar, null);
         }
+>>>>>>> 2c6ef8f... fb: enable navbar for hw key devices
+
+        mNavigationBarView.setDisabledFlags(mDisabled);
+        mNavigationBarView.setBar(this);
+        mNavigationBarView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                checkUserAutohide(v, event);
+                return false;
+            }
+        });
 
         if (mRecreating) {
 	    removeSidebarView();
