1dbbbcb16d373b7dfeb022461c467da57895ab26
Danny Baumann <dannybaumann@web.de>
2013-12-05 13:13:56 +0100

    Make ImageView drawable update more efficient in some cases.
    When updating the view's drawable via setImageResource or setImageURI,
    make sure to skip the layout pass if the old and new drawable have equal
    sizes. This follows what is already done for setDrawable().
    
    Change-Id: If73d225e38d0a2f7c616c4aeb33f67bdaed390f1

--- core/java/android/widget/ImageView.java
+++ core/java/android/widget/ImageView.java
@@ -390,7 +390,6 @@ public class ImageView extends View {
             final int oldWidth = mDrawableWidth;
             final int oldHeight = mDrawableHeight;
 
-            updateDrawable(null);
             mResource = resId;
             mUri = null;
 
@@ -419,7 +418,6 @@ public class ImageView extends View {
         if (mResource != 0 ||
                 (mUri != uri &&
                  (uri == null || mUri == null || !uri.equals(mUri)))) {
-            updateDrawable(null);
             mResource = 0;
             mUri = uri;
 
@@ -724,25 +722,26 @@ public class ImageView extends View {
         }
     }
 
+    private void ensureResolvedUri() {
+        if (mDrawable == null) {
+            resolveUri();
+        }
+    }
+
     private void resolveUri() {
-        if (mDrawable != null) {
-            return;
-        }
-
-        Resources rsrc = getResources();
-        if (rsrc == null) {
-            return;
-        }
-
         Drawable d = null;
+        Resources rsrc = getResources();
 
-        if (mResource != 0) {
+        if (rsrc == null) {
+            // Can't resolve any drawable, fall through to clearing the drawable
+        } else if (mResource != 0) {
             try {
                 d = mContext.getDrawable(mResource);
             } catch (Exception e) {
                 Log.w("ImageView", "Unable to find resource: " + mResource, e);
                 // Don't try again.
                 mUri = null;
+                mResource = 0;
             }
         } else if (mUri != null) {
             String scheme = mUri.getScheme();
@@ -772,7 +771,7 @@ public class ImageView extends View {
                         }
                     }
                 }
-        } else {
+            } else {
                 d = Drawable.createFromPath(mUri.toString());
             }
     
@@ -782,7 +781,7 @@ public class ImageView extends View {
                 mUri = null;
             }
         } else {
-            return;
+            // No drawable to resolve
         }
 
         updateDrawable(d);
@@ -864,7 +863,7 @@ public class ImageView extends View {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        resolveUri();
+        ensureResolvedUri();
         int w;
         int h;
         
