d92115edee4300ba9b7801f726aa4989c86dec7b
Jorge Ruesga <jorge@ruesga.com>
2013-12-17 23:13:31 +0100

    systemui: Add text plain battery style
    Patchset 2: Fixed issues
                Shared anim offset between battery views (uptaded by the master view)
    Patchset 3: More fixes
                Revert shared anim (is not viable)
    Patchset 4: Fixed coding style
                Rebased
    Patchset 5: Use system clock font
                Fix vertical aligment with system clock
                Rebased
    
    Settings Change-Id: http://review.cyanogenmod.org/#/c/56039/
    
    Change-Id: Ie288e58d5ce4f5c4d68d9ace31b333e82b4bff59
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3104,6 +3104,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
                 mode = BatteryMeterMode.BATTERY_METER_ICON_LANDSCAPE;
                 break;
 
+            case 6:
+                mode = BatteryMeterMode.BATTERY_METER_TEXT;
+                break;
+
             default:
                 break;
         }
