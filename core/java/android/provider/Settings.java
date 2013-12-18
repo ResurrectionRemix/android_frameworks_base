861e1640920a26b1749232c117a61d16a072df56
westcripp <korukaltan@gmail.com>
2013-12-18 07:43:34 +0200

    Revert "weste time"
    This reverts commit 3f0bcb2b18e8e4a9a067b66b76a0f0f6b6717032.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4479,6 +4479,17 @@ public final class Settings {
         public static final String LOCK_PATTERN_VISIBLE = "lock_pattern_visible_pattern";
 
         /**
+<<<<<<< HEAD
+=======
+         * Whether the NumKeyPad will change the orders of numbers
+         * in a PIN locked lockscreen
+         * 0 = off | 1 = always | 2 = only on request
+         * @hide
+          */
+
+        public static final String LOCK_NUMPAD_RANDOM = "lock_numpad_random";
+
+        /**
          * Colorize custom lock icon true/false
          * @hide
          */
@@ -4509,6 +4520,19 @@ public final class Settings {
         public static final String LOCKSCREEN_FRAME_COLOR = "lockscreen_frame_color";
 
         /**
+         *Whether lock before unlock is enabled or disabled
+         * @hide
+         */
+        public static final String LOCK_BEFORE_UNLOCK = "lock_before_unlock";
+
+        /**
+         * Determines the width and height of the LockPatternView widget
+         * @hide
+         */
+        public static final String LOCK_PATTERN_SIZE = "lock_pattern_size";
+
+        /**
+>>>>>>> 13b1b3a... Frameworks: Lockscreen Colors and Lock Image [1/2]
          * Whether lock pattern will vibrate as user enters (0 = false, 1 =
          * true)
          *
