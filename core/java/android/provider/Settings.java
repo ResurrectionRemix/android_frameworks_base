06e633429fff65b87137ab41cfcb7284a12ae773
westcripp <korukaltan@gmail.com>
2013-12-19 23:41:19 +0200

    Screen recorder: add ability to record audio from mic [1/3]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3466,8 +3466,14 @@ public final class Settings {
          * Screen recorder framerate in bits per second
          * @hide
          */
+         
         public static final String SCREEN_RECORDER_BITRATE = "screen_recorder_bitrate";
-
+         /**
+          * Whether to include audio when recording a video
+          * @hide
+          */
+          
+         public static final String SCREEN_RECORDER_RECORD_AUDIO = "screen_recorder_record_audio";
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
