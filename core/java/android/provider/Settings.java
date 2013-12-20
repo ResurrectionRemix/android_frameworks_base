41c9972df04b7fe88c65e59b52abe1179b740830
westcripp <korukaltan@gmail.com>
2013-12-20 08:45:49 +0200

    Revert "Frameworks: Statusbar clock and date customizations"
    This reverts commit 21c0ac608dab2e575f3403fd52c8cb0be7bd1fdc.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2731,7 +2731,7 @@ public final class Settings {
          */
         public static final String EGG_MODE = "egg_mode";
 
-	/**
+        /**
          * Quick Settings Panel Tiles to Use
          *
          * @hide
@@ -3015,54 +3015,6 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_CLOCK = "status_bar_clock";
-
-	/**
-         * AM/PM Style for clock options
-         * 0 - Normal AM/PM
-         * 1 - Small AM/PM
-         * 2 - No AM/PM
-         * @hide
-         */
-        public static final String STATUSBAR_CLOCK_AM_PM_STYLE = "statusbar_clock_am_pm_style";
-
-        /**
-         * Style of clock
-         * 0 - Hide Clock
-         * 1 - Right Clock
-         * 2 - Center Clock
-         * @hide
-         */
-        public static final String STATUSBAR_CLOCK_STYLE = "statusbar_clock_style";
-
-        /**
-         * Setting for clock color
-         * @hide
-         */
-        public static final String STATUSBAR_CLOCK_COLOR = "statusbar_clock_color";
-
-        /**
-         * @hide
-         * Shows custom date before clock time
-         * 0 - No Date
-         * 1 - Small Date
-         * 2 - Normal Date
-         */
-        public static final String STATUSBAR_CLOCK_DATE_DISPLAY = "statusbar_clock_date_display";
-
-        /**
-         * @hide
-         * Sets the date string style
-         * 0 - Regular style
-         * 1 - Lowercase
-         * 2 - Uppercase
-         */
-        public static final String STATUSBAR_CLOCK_DATE_STYLE = "statusbar_clock_date_style";
-
-        /**
-         * @hide
-         * Stores the java DateFormat string for the date
-         */
-        public static final String STATUSBAR_CLOCK_DATE_FORMAT = "statusbar_clock_date_format";
 
         /**
          * Whether to show the signal text or signal bars.
