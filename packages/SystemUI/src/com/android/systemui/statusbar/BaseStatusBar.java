7292e455e715c7e334b87572f6aed71de6bf5dd2
westcripp <korukaltan@gmail.com>
2014-01-03 04:05:01 +0200

    Revert "Active display [squashed] (1/2)"
    This reverts commit 1feff1233c2ea63550636614ea2becb3d8adb449.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -16,24 +16,6 @@
 
 package com.android.systemui.statusbar;
 
-<<<<<<< HEAD
-=======
-import android.service.notification.StatusBarNotification;
-import android.content.res.Configuration;
-import com.android.internal.statusbar.IStatusBarService;
-import com.android.internal.statusbar.StatusBarIcon;
-import com.android.internal.statusbar.StatusBarIconList;
-import com.android.internal.widget.SizeAdaptiveLayout;
-import com.android.systemui.R;
-import com.android.systemui.SearchPanelView;
-import com.android.systemui.SystemUI;
-import com.android.systemui.recent.RecentTasksLoader;
-import com.android.systemui.recent.RecentsActivity;
-import com.android.systemui.recent.TaskDescription;
-import com.android.systemui.statusbar.policy.NotificationRowLayout;
-import com.android.systemui.statusbar.policy.activedisplay.ActiveDisplayView;
-import android.app.Activity;
->>>>>>> cf802dd... Active display [squashed] (1/2)
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.Notification;
@@ -56,13 +38,9 @@ import android.graphics.PixelFormat;
 import android.content.res.Resources;
 import android.content.ServiceConnection;
 import android.database.ContentObserver;
-<<<<<<< HEAD
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-=======
-import android.graphics.PixelFormat;
->>>>>>> cf802dd... Active display [squashed] (1/2)
 import android.graphics.Rect;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuffXfermode;
@@ -116,7 +94,6 @@ import com.android.systemui.statusbar.AppSidebar;
 import com.android.systemui.statusbar.phone.KeyguardTouchDelegate;
 import com.android.systemui.statusbar.halo.Halo;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
-import com.android.systemui.statusbar.policy.activedisplay.ActiveDisplayView;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -217,7 +194,6 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     private RecentsComponent mRecents;
 
-<<<<<<< HEAD
     private int mExpandedDesktopStyle = 0;
 
     public Ticker getTicker() {
@@ -236,9 +212,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected AppSidebar mAppSidebar;
     protected int mSidebarPosition;
 
-=======
-    protected ActiveDisplayView mActiveDisplayView;
->>>>>>> cf802dd... Active display [squashed] (1/2)
 
     public IStatusBarService getStatusBarService() {
         return mBarService;
@@ -1551,7 +1524,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         mWindowManager.addView(mAppSidebar, getAppSidebarLayoutParams(mSidebarPosition));
     }
 
-<<<<<<< HEAD
     protected void removeSidebarView() {
         if (mAppSidebar != null)
             mWindowManager.removeView(mAppSidebar);
@@ -1562,50 +1534,17 @@ public abstract class BaseStatusBar extends SystemUI implements
                 LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.TYPE_STATUS_BAR_SUB_PANEL,
-=======
-    protected static void setSystemUIVisibility(View v, int visibility) {
-        v.setSystemUiVisibility(visibility);
-    }
-
-    protected void addActiveDisplayView() {
-        mActiveDisplayView = (ActiveDisplayView)View.inflate(mContext, R.layout.active_display, null);
-        int activeDisplayVis = View.SYSTEM_UI_FLAG_LOW_PROFILE
-                             | View.SYSTEM_UI_FLAG_FULLSCREEN
-                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-        setSystemUIVisibility(mActiveDisplayView, activeDisplayVis);
-        mActiveDisplayView.setStatusBar(this);
-        mWindowManager.addView(mActiveDisplayView, getActiveDisplayViewLayoutParams());
-    }
-
-    protected void removeActiveDisplayView() {
-        if (mActiveDisplayView != null)
-            mWindowManager.removeView(mActiveDisplayView);
-    }
-
-    protected WindowManager.LayoutParams getActiveDisplayViewLayoutParams() {
-        WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
-                LayoutParams.MATCH_PARENT,
-                LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.TYPE_STATUS_BAR_PANEL,
->>>>>>> cf802dd... Active display [squashed] (1/2)
                 0
                 | WindowManager.LayoutParams.FLAG_TOUCHABLE_WHEN_WAKING
                 | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                 | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
                 | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH,
-<<<<<<< HEAD
                 PixelFormat.TRANSLUCENT);
         lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_NO_MOVE_ANIMATION;
         lp.gravity = Gravity.TOP;// | Gravity.FILL_VERTICAL;
         lp.gravity |= position == AppSidebar.SIDEBAR_POSITION_LEFT ? Gravity.LEFT : Gravity.RIGHT;
         lp.setTitle("AppSidebar");
-=======
-                PixelFormat.OPAQUE);
-        lp.gravity = Gravity.TOP | Gravity.FILL_VERTICAL | Gravity.FILL_HORIZONTAL;
-        lp.setTitle("ActiveDisplayView");
->>>>>>> cf802dd... Active display [squashed] (1/2)
 
         return lp;
     }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -94,8 +94,6 @@ import android.widget.ImageView;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
-import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
@@ -300,15 +298,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     // last theme that was applied in order to detect theme change (as opposed
     // to some other configuration change).
     CustomTheme mCurrentTheme;
-<<<<<<< HEAD
     private boolean mRecreating = false;
 	private StatusHeaderMachine mStatusHeaderMachine;
     private Runnable mStatusHeaderUpdater;
     
-=======
-    
-    // status bar brightness control
->>>>>>> cf802dd... Active display [squashed] (1/2)
     private boolean mBrightnessControl;
     private float mScreenWidth;
     private int mMinBrightness;
@@ -317,8 +310,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     int mLinger;
     int mInitialTouchX;
     int mInitialTouchY;
-
-    private boolean mRecreating = false;
 
     // for disabling the status bar
     int mDisabled = 0;
@@ -623,15 +614,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         }
 
         if (mRecreating) {
-<<<<<<< HEAD
 	    removeSidebarView();
         }
 	addSidebarView();
-=======
-        } else {
-            addActiveDisplayView();
-        }
->>>>>>> cf802dd... Active display [squashed] (1/2)
 
         // figure out which pixel-format to use for the status bar.
         mPixelFormat = PixelFormat.OPAQUE;
@@ -2537,7 +2522,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         mHandler.postDelayed(mUserAutohide, 350); // longer than app gesture -> flag clear
     }
 
-    public boolean areLightsOn() {
+    private boolean areLightsOn() {
         return 0 == (mSystemUiVisibility & View.SYSTEM_UI_FLAG_LOW_PROFILE);
     }
 
@@ -2557,7 +2542,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         }
     }
 
-    @Override
     public void topAppWindowChanged(boolean showMenu) {
         if (DEBUG) {
             Log.d(TAG, (showMenu?"showing":"hiding") + " the MENU button");
