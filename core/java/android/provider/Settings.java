7b3d26d4e1743007925ba8d513d24f1d510f6ac6
westcripp <korukaltan@gmail.com>
2014-01-09 06:51:28 +0200

    Revert "error"
    This reverts commit 052d30f3ab602f72274ccd5170221e9b2993b008.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3356,6 +3356,10 @@ public final class Settings {
         public static final String VOLUME_KEYS_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
 
         /**
+<<<<<<< HEAD
+         * Whether to enable custom rebindings of the actions performed on
+         * certain key press events.
+=======
          * Action for long-pressing back button on lock screen
          * @hide
          */
@@ -3375,8 +3379,7 @@ public final class Settings {
 
         /**
          * Expanded desktop on/off state
-         * Whether to enable custom rebindings of the actions performed on
-         * certain key press events.
+>>>>>>> e6649cc... LockScreen: allow long-press actions from hardware keys (1/2)
          * @hide
          */
         public static final String HARDWARE_KEY_REBINDING = "hardware_key_rebinding";
