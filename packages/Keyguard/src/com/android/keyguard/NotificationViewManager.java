031151fb74c3e8a7220d985a7d094d91cdcdf3a8
Der-Schubi <schubi@erlangen.cc.de>
2014-01-11 18:41:17 +0100

    [1/2] LS Notification dinamic width & blacklist options
    - Lockscreen Notifications: disable dynamic width for devices <= xhdpi
    
    - [1/2] Base: Lockscreen notifications: Option to disable dynamic width
    from: https://github.com/OmniKang/android_frameworks_base/commit/87d6598b63a1b83e93631b0fcb0822365cf7563b
    
    -  [1/2] Base: Lockscreen notifications: Allow apps to be excluded from being displayed
    from: https://github.com/OmniKang/android_frameworks_base/commit/8631349b965f507eeff4b5616c1f581bc4e6e615
    
     Credits to 0xD34D / ChameleonOS for the original implementation for Active Display
    
    PS3/4: fixes
    
    Change-Id: I796b08cb94185ea17139bd74f1b41d287ba65155

--- packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -34,7 +34,12 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.service.notification.INotificationListener;
 import android.service.notification.StatusBarNotification;
+import android.text.TextUtils;
 import android.util.Log;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class NotificationViewManager {
     private final static String TAG = "Keyguard:NotificationViewManager";
@@ -56,6 +61,8 @@ public class NotificationViewManager {
     private PowerManager mPowerManager;
     private NotificationHostView mHostView;
 
+    private Set<String> mExcludedApps = new HashSet<String>();
+
     public static Configuration config;
 
     class Configuration extends ContentObserver {
@@ -71,6 +78,7 @@ public class NotificationViewManager {
         public int notificationsHeight = 4;
         public float offsetTop = 0.3f;
         public boolean privacyMode = false;
+        public boolean dynamicWidth = true;
 
         public Configuration(Handler handler) {
             super(handler);
@@ -101,6 +109,10 @@ public class NotificationViewManager {
                 Settings.System.LOCKSCREEN_NOTIFICATIONS_OFFSET_TOP), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                 Settings.System.LOCKSCREEN_NOTIFICATIONS_PRIVACY_MODE), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS_DYNAMIC_WIDTH), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS_EXCLUDED_APPS), false, this);
         }
 
         @Override
@@ -133,6 +145,12 @@ public class NotificationViewManager {
                 Settings.System.LOCKSCREEN_NOTIFICATIONS_HEIGHT, notificationsHeight);
             offsetTop = Settings.System.getFloat(mContext.getContentResolver(),
                 Settings.System.LOCKSCREEN_NOTIFICATIONS_OFFSET_TOP, offsetTop);
+            dynamicWidth = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS_DYNAMIC_WIDTH, dynamicWidth ? 1 : 0) == 1;
+            String excludedApps = Settings.System.getString(mContext.getContentResolver(),
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS_EXCLUDED_APPS);
+
+            createExcludedAppsSet(excludedApps);
             }
         }
 
@@ -141,8 +159,8 @@ public class NotificationViewManager {
             if (event.sensor.equals(ProximitySensor)) {
                 if (!mIsScreenOn) {
                     if (event.values[0] >= ProximitySensor.getMaximumRange()) {
-                        if (config.pocketMode && mTimeCovered != 0 && (config.showAlways || mHostView.getNotificationCount() > 0) &&
-                                System.currentTimeMillis() - mTimeCovered > MIN_TIME_COVERED) {
+                        if (config.pocketMode && mTimeCovered != 0 && (config.showAlways || mHostView.getNotificationCount() > 0)
+                                && System.currentTimeMillis() - mTimeCovered > MIN_TIME_COVERED) {
                             wakeDevice();
                             mWokenByPocketMode = true;
                             mHostView.showAllNotifications();
@@ -176,6 +194,10 @@ public class NotificationViewManager {
         @Override
         public void onNotificationRemoved(final StatusBarNotification sbn) {
             mHostView.removeNotification(sbn, false);
+        }
+
+        public boolean isValidNotification(final StatusBarNotification sbn) {
+            return (!mExcludedApps.contains(sbn.getPackageName()));
         }
     }
 
@@ -275,5 +297,16 @@ public class NotificationViewManager {
                 unregisterListeners();
             }
         }, ANIMATION_MAX_DURATION);
+    }
+
+    /**
+     * Create the set of excluded apps given a string of packages delimited with '|'.
+     * @param excludedApps
+     */
+    private void createExcludedAppsSet(String excludedApps) {
+        if (TextUtils.isEmpty(excludedApps))
+            return;
+        String[] appsToExclude = excludedApps.split("\\|");
+        mExcludedApps = new HashSet<String>(Arrays.asList(appsToExclude));
     }
 }
