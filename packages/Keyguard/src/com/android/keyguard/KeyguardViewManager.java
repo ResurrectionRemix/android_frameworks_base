ec2a72c39afdced8fbc6bfd2c69dce7a638d1c11
David L.-W <david.leppla.weber@gmail.com>
2014-01-11 17:34:46 +0100

    [1/2] Lockscreen Notifications fixes
    - Lockscreen Notifications: make animations a bit faster
    from: https://github.com/AOSPAL/android_frameworks_base/commit/5b6e143c4edeb9454650dbc05e597fbbae6b5c26
    
    - Fix ongoing notifications reappearing when hidden & some small animation tweaks again.
    from: https://github.com/AOSPAL/android_frameworks_base/commit/ac9bf6a36a7275d1e98e2ae6750723b1e006dd4d
    
    -  Lockscreen Notifications: fix notifications disappearing on orientation change
    from: https://github.com/AOSPAL/android_frameworks_base/commit/e9dcb99f0bd3e74a45ee7d15ebc19a99db7a09a2
    
    Change-Id: Id72ec7c6c9e61fc7502299e434e0ba0d12957ec7

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -676,6 +676,7 @@ public class KeyguardViewManager {
             mNotificationView = (NotificationHostView)mKeyguardView.findViewById(R.id.notification_host_view);
             mNotificationViewManager.setHostView(mNotificationView);
             mNotificationViewManager.onScreenTurnedOff();
+            mNotificationView.addNotifications();
         }
 
         // HACK
