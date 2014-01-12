254de61aaff70ab5fe6c1929fe5b797a9d216f85
David96 <xd_e-mail@gmx.de>
2014-01-12 19:50:13 +0100

    Lockscreen Notifications: fix phone turning on in pocket
    Change-Id: Ia0385280510f1d684c5dcf537dd2049c6916ddec

--- packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -269,7 +269,9 @@ public class NotificationViewManager {
     }
 
     private void wakeDevice() {
-        mPowerManager.wakeUp(SystemClock.uptimeMillis());
+        if (mTimeCovered == 0) {
+            mPowerManager.wakeUp(SystemClock.uptimeMillis());
+        }
     }
 
     public void onScreenTurnedOff() {
