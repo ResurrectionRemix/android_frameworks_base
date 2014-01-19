8871dc602eae35d9c43fd3fd9c29d7a47aa5ebf9
westcripp <korukaltan@gmail.com>
2014-01-19 17:09:10 +0200

    Revert "clear"
    This reverts commit 627a091bfff41ed8e2313542fd49476a8bacac7e.

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -17,14 +17,16 @@
 
 package com.android.keyguard;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.*;
 import android.provider.Settings;
 import android.view.*;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+
 import com.android.internal.policy.IKeyguardShowCallback;
 import com.android.internal.widget.LockPatternUtils;
 
@@ -85,8 +87,6 @@ public class KeyguardViewManager {
     public final static String IS_SWITCHING_USER = "is_switching_user";
 
     private final int BLUR_RADIUS = 14;
-    private final int MAX_BLUR_WIDTH = 900;
-    private final int MAX_BLUR_HEIGHT = 1600;
 
     // Delay dismissing keyguard to allow animations to complete.
     private static final int HIDE_KEYGUARD_DELAY = 500;
@@ -105,10 +105,23 @@ public class KeyguardViewManager {
     private KeyguardHostView mKeyguardView;
 
     private boolean mScreenOn = false;
+    private boolean mSeeThroughEnabled = false;
     private LockPatternUtils mLockPatternUtils;
     private Drawable mCustomBackground = null;
 
     private boolean mUnlockKeyDown = false;
+<<<<<<< HEAD
+=======
+    private NotificationHostView mNotificationView;
+    private NotificationViewManager mNotificationViewManager;
+<<<<<<< HEAD
+    private boolean mLockscreenNotifications = true;
+=======
+    private boolean mLockscreenNotifications = false;
+>>>>>>> 4c6df9f... [1/2] FW: LockScreen Notifications by AOSPAL
+>>>>>>> parent of 6a50448... clear conflicts
+=======
+>>>>>>> parent of 3b685e2... [1/2] FW: LockScreen Notifications by AOSPAL
 
     private KeyguardUpdateMonitorCallback mBackgroundChanger = new KeyguardUpdateMonitorCallback() {
         @Override
@@ -137,11 +150,63 @@ public class KeyguardViewManager {
         @Override
         public void onChange(boolean selfChange) {
             setKeyguardParams();
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 3b685e2... [1/2] FW: LockScreen Notifications by AOSPAL
             if(!isSeeThroughEnabled()) mCustomBackground = null;
             mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
         }
     }
 
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 6a50448... clear conflicts
+            updateSettings();
+            try {
+                mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
+            } catch(IllegalArgumentException e) {
+                // Call yo mom call yo dad!
+            }
+            
+        }
+    }
+    
+    private void updateSettings() {
+        mLockscreenNotifications = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_NOTIFICATIONS, 0) == 1;
+        if(mLockscreenNotifications && mNotificationViewManager == null) {
+            mNotificationViewManager = new NotificationViewManager(mContext, this);
+        } else if(!mLockscreenNotifications && mNotificationViewManager != null) {
+            mNotificationViewManager.unregisterListeners();
+            mNotificationViewManager = null;
+        }
+
+<<<<<<< HEAD
+=======
+    private void updateSettings() {
+        mLockscreenNotifications = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_NOTIFICATIONS, 0) == 1;
+        if(mLockscreenNotifications && mNotificationViewManager == null) {
+            mNotificationViewManager = new NotificationViewManager(mContext, this);
+        } else if(!mLockscreenNotifications && mNotificationViewManager != null) {
+            mNotificationViewManager.unregisterListeners();
+            mNotificationViewManager = null;
+        }
+
+>>>>>>> parent of 6a50448... clear conflicts
+        mSeeThroughEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                            Settings.System.LOCKSCREEN_SEE_THROUGH, 0) == 1;
+        if(!mSeeThroughEnabled) mCustomBackground = null;
+    }
+<<<<<<< HEAD
+=======
+>>>>>>> 4c6df9f... [1/2] FW: LockScreen Notifications by AOSPAL
+>>>>>>> parent of 6a50448... clear conflicts
+=======
+>>>>>>> parent of 3b685e2... [1/2] FW: LockScreen Notifications by AOSPAL
     /**
      * @param context Used to create views.
      * @param viewManager Keyguard will be attached to this.
@@ -158,7 +223,7 @@ public class KeyguardViewManager {
 
         SettingsObserver observer = new SettingsObserver(new Handler());
         observer.observe();
-        if(!isSeeThroughEnabled()) mCustomBackground = null;
+        updateSettings();
     }
 
     /**
@@ -193,12 +258,12 @@ public class KeyguardViewManager {
 
     public void setKeyguardParams() {
         boolean enableScreenRotation = shouldEnableScreenRotation();
-
+        updateSettings();
         int flags = WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
                     | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR
                     | WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN;
 
-            if (!isSeeThroughEnabled()) {
+            if (!mSeeThroughEnabled) {
                 flags |= WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
             }
 
@@ -242,24 +307,18 @@ public class KeyguardViewManager {
     }
 
     public void setBackgroundBitmap(Bitmap bmp) {
-        if (isSeeThroughEnabled()) {
+        if (mSeeThroughEnabled) {
                 bmp = blurBitmap(bmp, BLUR_RADIUS);
         }
         mCustomBackground = new BitmapDrawable(mContext.getResources(), bmp);
     }
 
     private Bitmap blurBitmap(Bitmap bmp, int radius) {
-        Bitmap tmpBmp = bmp;
-
-        // scale image if its too large
-        if (bmp.getWidth() > MAX_BLUR_WIDTH)
-                    tmpBmp = bmp.createScaledBitmap(bmp, MAX_BLUR_WIDTH, MAX_BLUR_HEIGHT, false);
-
-        Bitmap out = Bitmap.createBitmap(tmpBmp);
+        Bitmap out = Bitmap.createBitmap(bmp);
         RenderScript rs = RenderScript.create(mContext);
 
         Allocation input = Allocation.createFromBitmap(
-                rs, tmpBmp, MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
+                rs, bmp, MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
         Allocation output = Allocation.createTyped(rs, input.getType());
 
         ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
@@ -270,7 +329,6 @@ public class KeyguardViewManager {
         output.copyTo(out);
 
         rs.destroy();
-
         return out;
     }
 
@@ -374,7 +432,8 @@ public class KeyguardViewManager {
             if (bgAspect > vAspect) {
                 background.setBounds(0, 0, (int) (vHeight * bgAspect), vHeight);
             } else {
-                background.setBounds(0, 0, vWidth, (int) (vWidth * (vAspect >= 1 ? bgAspect : (1 / bgAspect))));
+                mCustomBackground.setBounds(0, 0, vWidth,
+                        (int) (vWidth * (vAspect >= 1 ? bgAspect : (1 / bgAspect))));
             }
         }
 
@@ -422,6 +481,14 @@ public class KeyguardViewManager {
     public boolean handleKeyDown(int keyCode, KeyEvent event) {
         if (event.getRepeatCount() == 0) {
             mUnlockKeyDown = true;
+            // We check for Camera key press in handleKeyDown, because
+            // it gives us "instant" unlock, when user depresses
+            // the button.
+            if (keyCode == KeyEvent.KEYCODE_CAMERA) {
+                if (mKeyguardView.handleCameraKey()) {
+                    return true;
+                }
+            }
         }
         if (event.isLongPress()) {
             String action = null;
@@ -603,14 +670,14 @@ public class KeyguardViewManager {
             KeyguardUpdateMonitor.getInstance(mContext).registerCallback(mBackgroundChanger);
         }
 
-            if (force || mKeyguardView == null) {
+        if (force || mKeyguardView == null) {
                 mKeyguardHost.setCustomBackground(null);
                 mKeyguardHost.removeAllViews();
                 inflateKeyguardView(options);
                 mKeyguardView.requestFocus();
         }
 
-            if(mCustomBackground != null) {
+        if(mCustomBackground != null) {
                 mKeyguardHost.setCustomBackground(mCustomBackground);
         }
 
@@ -685,23 +752,16 @@ public class KeyguardViewManager {
         mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
     }
 
-    private boolean isSeeThroughEnabled() {
-        return Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.LOCKSCREEN_SEE_THROUGH, 0) == 1;
-    }
-
     void updateShowWallpaper(boolean show) {
-        if (isSeeThroughEnabled()) {
-            return;
-        } else {
-            if (show) {
-                mWindowLayoutParams.flags |= WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
-            } else {
-                mWindowLayoutParams.flags &= ~WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
-            }
+        if (mSeeThroughEnabled) show = false;
 
-            mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
+        if (show) {
+            mWindowLayoutParams.flags |= WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
+        } else {
+            mWindowLayoutParams.flags &= ~WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
         }
+
+        mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
     }
 
     public void setNeedsInput(boolean needsInput) {
