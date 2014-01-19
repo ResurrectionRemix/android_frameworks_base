65063cd023cc2b9e8084fb142dad0ff8c96b8b74
Jun Su <juns@nvidia.com>
2014-01-19 12:43:58 +0800

    Disable statusbar time refresh when screen off
    When screen is off, don't refresh the status bar to save power.
    When screen is on, refresh immediately for user experience.
    
    PS: (updateClock(); => getHandler().post(() -> updateClock());)
    updated for android 8.0
    
    Change-Id: Ie250e20bab57c096a16886680a53263c635bf6f4
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
@@ -42,11 +42,18 @@ public class DateView extends TextView {
     private DateFormat mDateFormat;
     private String mLastText;
     private String mDatePattern;
+    private boolean mScreenOn = true;
 
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
+            if (action.equals(Intent.ACTION_SCREEN_ON)) {
+                mScreenOn = true;
+            } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                mScreenOn = false;
+            }
+
             if (Intent.ACTION_TIME_TICK.equals(action)
                     || Intent.ACTION_TIME_CHANGED.equals(action)
                     || Intent.ACTION_TIMEZONE_CHANGED.equals(action)
@@ -56,7 +63,9 @@ public class DateView extends TextView {
                     // need to get a fresh date format
                     getHandler().post(() -> mDateFormat = null);
                 }
-                getHandler().post(() -> updateClock());
+                if (mScreenOn) {
+                    getHandler().post(() -> updateClock());
+                }
             }
         }
     };
@@ -83,6 +92,8 @@ public class DateView extends TextView {
         super.onAttachedToWindow();
 
         IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_TIME_TICK);
         filter.addAction(Intent.ACTION_TIME_CHANGED);
         filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
