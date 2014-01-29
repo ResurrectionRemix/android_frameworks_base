30db52660edd1dddfd5587e36d08f687ac0e73b6
westcripp <korukaltan@gmail.com>
2014-01-29 14:46:19 +0200

    Revert "Set Settings.Global.WIFI_SUSPEND_OPTIMIZATIONS_ENABLED default value"
    This reverts commit 08f65ff4ae952547bf410ff56ef2b4889da5d4e8.

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2363,9 +2363,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             loadIntegerSetting(stmt, Settings.Global.WIFI_SCAN_ALWAYS_AVAILABLE,
                     R.integer.def_wifi_scan_always_available);
 
-            loadIntegerSetting(stmt, Settings.Global.WIFI_SUSPEND_OPTIMIZATIONS_ENABLED,
-                    R.integer.def_wifi_suspend_optimizations_enabled);
-
             // --- New global settings start here
         } finally {
             if (stmt != null) stmt.close();
