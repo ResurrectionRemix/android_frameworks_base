b348b3f6947e026b0404f10ff9a759bd45bee2e0
Shubhang <shubhang92@gmail.com>
2014-01-31 00:26:55 +0530

    fix f-ing status bar icon update issues with center clock

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3261,6 +3261,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         mStatusBarContainer.addView(mStatusBarWindow);
 
         updateExpandedViewPos(EXPANDED_LEAVE_ALONE);
+	updateSettings();
         mRecreating = false;
     }
 
