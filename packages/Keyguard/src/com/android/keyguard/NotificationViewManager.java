30a63dfc67837bd18327b9bcf1906a154dd3c46a
David96 <xd_e-mail@gmx.de>
2014-02-03 14:39:02 +0100

    FW: LS Notifications Rewrite add notification logic & fix pokect mode
    Original commit message:
    
     There were some problems with notifications getting replaced while the user was currently dragging them.
     This commit should fix that by always waiting until animations and user input are finished before replacing a notification view.
    
    and I added some missing line for pocket mode fixes
    
    PS 2&3: Update Author & message
    
    Change-Id: I73e3337e953c1fb8f17597efae8782a4d2878c40
    
    Conflicts:
    
    	packages/Keyguard/src/com/android/keyguard/NotificationHostView.java

--- packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -189,10 +189,10 @@ public class NotificationViewManager {
         @Override
         public void onNotificationPosted(final StatusBarNotification sbn) {
             boolean screenOffAndNotCovered = !mIsScreenOn && mTimeCovered == 0;
-            boolean ongoingAndReposted = sbn.isOngoing() && mHostView.containsNotification(sbn);
-            if (mHostView.addNotification(sbn, (screenOffAndNotCovered || mIsScreenOn) && !ongoingAndReposted,
+            boolean showNotification = !mHostView.containsNotification(sbn) || mHostView.getNotification(sbn).when != sbn.getNotification().when;
+            if (mHostView.addNotification(sbn, (screenOffAndNotCovered || mIsScreenOn) && showNotification,
                         config.forceExpandedView) && config.wakeOnNotification && screenOffAndNotCovered
-                        && !ongoingAndReposted) {
+                        && showNotification && mTimeCovered == 0) {
                 wakeDevice();
             }
         }
@@ -274,9 +274,7 @@ public class NotificationViewManager {
     }
 
     private void wakeDevice() {
-        if (mTimeCovered == 0) {
-            mPowerManager.wakeUp(SystemClock.uptimeMillis());
-        }
+        mPowerManager.wakeUp(SystemClock.uptimeMillis());
     }
 
     public void onScreenTurnedOff() {
