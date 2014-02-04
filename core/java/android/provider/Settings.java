b5e677cf9cee0fc9769f528f95579f2b84b65d96
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-02-04 23:01:28 +0100

    Frameworks: Open source google dialer and more (4/5)
    This is an open source variant of the well known google Dialer which gives the
    ability to show nearby places and revese lookup. It uses default the Google
    engine to do this. As well you have the ability to use other providers.
    
    Providers which are currently available:
    
    lookup/nearby places:
    Google
    OpenStreetMap
    
    Reverse lookup:
    Google
    OpenCnam (US-only)
    WhitePages (US-only)
    YellowPages (US-only)
    ZabaSearch (US-only)
    
    Change-Id: I86cc221c0afac83d59f1b06a4cf89b8bd7f3ec30
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3294,7 +3294,33 @@ public final class Settings {
          */
 
         public static final String QUIET_HOURS_DIM = "quiet_hours_dim";
-        
+         /**
+          * Enable looking up of phone numbers of nearby places
+          *
+          * @hide
+          */
+         public static final String ENABLE_FORWARD_LOOKUP = "enable_forward_lookup";
+ 
+         /**
+          * Enable looking up of information of phone numbers not in the contacts
+          *
+          * @hide
+          */
+         public static final String ENABLE_REVERSE_LOOKUP = "enable_reverse_lookup";
+ 
+         /**
+          * The forward lookup provider
+          *
+          * @hide
+          */
+         public static final String FORWARD_LOOKUP_PROVIDER = "forward_lookup_provider";
+ 
+         /**
+          * The reverse lookup provider
+          *
+          * @hide
+          */
+         public static final String REVERSE_LOOKUP_PROVIDER = "reverse_lookup_provider";        
          /**
           * Network speed indicator
           * 0 = disabled
