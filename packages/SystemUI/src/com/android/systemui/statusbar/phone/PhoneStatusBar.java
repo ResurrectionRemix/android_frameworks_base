d300fd44f6f03c37435d788a421bf0fba6208d37
westcripp <korukaltan@gmail.com>
2014-02-05 19:50:09 +0200

    Revert "re order buld fix"
    This reverts commit 1b2e29577309cfd08972370a52caf7de166b6b45.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1859,6 +1859,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     final int FLIP_DURATION = (FLIP_DURATION_IN + FLIP_DURATION_OUT);
 
     Animator mScrollViewAnim, mFlipSettingsViewAnim, mNotificationButtonAnim,
+
         mSettingsButtonAnim, mHaloButtonAnim, mClearButtonAnim, mRibbonViewAnim, mAddTileButtonAnim;
 
     @Override
@@ -3029,18 +3030,19 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         public void onClick(View v) {
             Intent intent=Intent.makeMainSelectorActivity(Intent.ACTION_MAIN, Intent.CATEGORY_APP_CALENDAR);
             startActivityDismissingKeyguard(intent,true);
-         }
-      };
-      
-    private View.OnClickListener mAddTileButtonListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            Intent intent = new Intent();
-            intent.setClassName("com.android.settings",
-                    "com.android.settings.Settings$QuickSettingsConfigActivity");
-            startActivityDismissingKeyguard(intent, true);
+            
         }
     };
-
+    
+    private View.OnClickListener mAddTileButtonListener = new View.OnClickListener() {
+         public void onClick(View v) {
+             Intent intent = new Intent();
+             intent.setClassName("com.android.settings",
+                     "com.android.settings.Settings$QuickSettingsConfigActivity");
+             startActivityDismissingKeyguard(intent, true);
+         }
+     };
+     
     private View.OnClickListener mNotificationButtonListener = new View.OnClickListener() {
         public void onClick(View v) {
             animateExpandNotificationsPanel();
