212bdd33d40279584d0bb531bdb8a4e28460fce2
Shubhang <shubhang92@gmail.com>
2014-02-09 22:38:52 +0530

    Active Display: more fixes
    fix http://jira.omnirom.org/browse/OMNI-352
    fix http://jira.omnirom.org/browse/OMNI-357

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -587,14 +587,14 @@ public class ActiveDisplayView extends FrameLayout {
                 }
 		KeyguardTouchDelegate.getInstance(mContext).dismiss();
             }
-            if (mNotification.isClearable()) {
-                try {
+		try {
+                 if (mNotification.isClearable()) {
                      mNM.cancelNotificationFromSystemListener(mNotificationListener,
-                             mNotification.getPackageName(), mNotification.getTag(),
-                             mNotification.getId());
-                } catch (RemoteException e) {
-                } catch (NullPointerException npe) {
-                }
+                         mNotification.getPackageName(), mNotification.getTag(),
+                         mNotification.getId());
+                 }
+            } catch (RemoteException e) {
+            } catch (NullPointerException npe) {
             }
             mNotification = null;
         }
@@ -747,8 +747,9 @@ public class ActiveDisplayView extends FrameLayout {
         mNotification = getNextAvailableNotification();
         if (mNotification != null) {
             setActiveNotification(mNotification, true);
-                invalidate();
-                mGlowPadView.ping();
+	    inflateRemoteView(mNotification);
+            invalidate();
+            mGlowPadView.ping();
             isUserActivity();
             return;
         }
@@ -1109,19 +1110,20 @@ public class ActiveDisplayView extends FrameLayout {
         } catch (Resources.NotFoundException nfe) {
             mNotificationDrawable = null;
         }
-        if (mNotificationDrawable != null) {
-            post(new Runnable() {
-                 @Override
-                 public void run() {
+        post(new Runnable() {
+             @Override
+             public void run() {
+                 if (mNotificationDrawable != null) {
                      mCurrentNotificationIcon.setImageDrawable(mNotificationDrawable);
                      setHandleText(sbn);
                      mNotification = sbn;
-                     updateResources();
-                     mGlowPadView.invalidate();
-                     if (updateOthers) updateOtherNotifications();
                  }
-            });
-        }
+                 updateResources();
+                 mGlowPadView.invalidate();
+                 if (updateOthers) updateOtherNotifications();
+             }
+        });
+
     }
 
     /**
