2e2427562a0d66afd964e7dd99acbf4e3c0a3945
rodman01 <email4joern@arcor.de>
2014-02-27 23:35:10 +0100

    LocationTile cherry picked from SlimRoms and modified/Ported GPSTile.java from X-Roms

--- packages/SystemUI/src/com/android/systemui/quicksettings/GPSTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/GPSTile.java
@@ -1,11 +1,9 @@
 package com.android.systemui.quicksettings;
 
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.provider.Settings;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
@@ -16,19 +14,21 @@ import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
 import com.android.systemui.statusbar.policy.LocationController;
 import com.android.systemui.statusbar.policy.LocationController.LocationSettingsChangeCallback;
 
-
 public class GPSTile extends QuickSettingsTile implements LocationSettingsChangeCallback {
 
-    ContentResolver mContentResolver;
+    private QuickSettingsController mQsc;
     private LocationController mLocationController;
     private boolean mLocationEnabled;
+    private int mLocationMode;
 
     public GPSTile(Context context, QuickSettingsController qsc) {
         super(context, qsc);
 
-        mContentResolver = mContext.getContentResolver();
+        mQsc = qsc;
         mLocationController = new LocationController(mContext);
         mLocationController.addSettingsChangedCallback(this);
+        mLocationMode = mLocationController.getLocationMode();
+        mLocationEnabled = mLocationController.isLocationEnabled();
 
         mOnClick = new OnClickListener() {
             @Override
@@ -43,7 +43,9 @@ public class GPSTile extends QuickSettingsTile implements LocationSettingsChange
         mOnLongClick = new OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                startSettingsActivity(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                if (mLocationEnabled) {
+                    mLocationController.switchLocationMode(mLocationMode);
+                }
                 return true;
             }
         };
@@ -62,15 +64,36 @@ public class GPSTile extends QuickSettingsTile implements LocationSettingsChange
     }
 
     private synchronized void updateTile() {
+        switch (mLocationMode) {
+            case Settings.Secure.LOCATION_MODE_SENSORS_ONLY:
+                mDrawable = R.drawable.ic_qs_location_on_gps;
+                break;
+            case Settings.Secure.LOCATION_MODE_BATTERY_SAVING:
+                mDrawable = R.drawable.ic_qs_location_on_wifi;
+                break;
+            case Settings.Secure.LOCATION_MODE_HIGH_ACCURACY:
+                mDrawable = R.drawable.ic_qs_location_on;
+                break;
+            case Settings.Secure.LOCATION_MODE_OFF:
+                mDrawable = R.drawable.ic_qs_location_off;
+                break;
+        }
         int textResId = mLocationEnabled ? R.string.quick_settings_location_label
                 : R.string.quick_settings_location_off_label;
         mLabel = mContext.getText(textResId).toString();
-        mDrawable = mLocationEnabled
-                ? R.drawable.ic_qs_location_on : R.drawable.ic_qs_location_off;
     }
 
     @Override
-    public void onLocationSettingsChanged(boolean locationEnabled) {
+    public void onLocationSettingsChanged(boolean locationEnabled, int locationMode) {
+        // collapse all panels in case the confirmation dialog needs to show up
+        if ((mLocationMode == Settings.Secure.LOCATION_MODE_SENSORS_ONLY
+                        && locationMode == Settings.Secure.LOCATION_MODE_HIGH_ACCURACY)
+                || (!mLocationEnabled && locationEnabled
+                        && (locationMode == Settings.Secure.LOCATION_MODE_HIGH_ACCURACY
+                        || locationMode == Settings.Secure.LOCATION_MODE_BATTERY_SAVING))) {
+            mQsc.mBar.collapseAllPanels(true);
+        }
+        mLocationMode = locationMode;
         mLocationEnabled = locationEnabled;
         updateResources();
     }
