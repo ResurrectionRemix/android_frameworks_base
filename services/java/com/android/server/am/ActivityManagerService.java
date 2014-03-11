dc014e9b1327f3d93a161a60304d880f31217720
Fabien DUVOUX <fabien.duvoux@gmail.com>
2014-03-11 17:41:24 +0700

    Base: Check ProcessesReady before broadcast
    Most of the time testIsSystemReady is used to know if we are allowed to broadcast intents.
    But in broadcast intent method we check mProcessesReady flag (not mSystemReady).
    If we broadcast after systemReady flag but before ProcessesReady flag, system will crash
    
    Adding condition to check if system & processes are ready fix that race condition
    Signed-off-by: Fabien DUVOUX <fabien.duvoux@gmail.com>
    
    Change-Id: I2eec1c4f280daf29f4741919a7636b02da341e32

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -9246,7 +9246,7 @@ public final class ActivityManagerService extends ActivityManagerNative
 
     public boolean testIsSystemReady() {
         // no need to synchronize(this) just to read & return the value
-        return mSystemReady;
+        return mSystemReady && mProcessesReady;
     }
 
     private static File getCalledPreBootReceiversFile() {
