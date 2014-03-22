98419e6a29dcce0a1612d8d7fee364623e05750f
Scott Weber <scott.d.weber@gmail.com>
2014-03-21 22:41:55 -0400

    Prevent erroneous calls to OnCheckedChangedListener
    This change updates the CheckedStateTracker so that it only
    updates the RadioGroup's checkedId when a RadioButton is
    checked. Previously, the CheckedStateTracker would call
    setCheckedId() even if it was responding to an uncheck
    event (e.g. when calling clearCheck() to uncheck all radio
    buttons). This would in turn cause a bogus call to the
    RadioGroup's OnCheckedChangedListener, falsely indicating
    to the listener that the unchecked RadioButton had just
    been checked.
    
    This issue is reported on b.android.com: Issue 4785
    
    Change-Id: Ida4d4be478db38b8972359055119b5e6d285c20e
    Signed-off-by: Scott Weber <scott.d.weber@gmail.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- core/java/android/widget/RadioGroup.java
+++ core/java/android/widget/RadioGroup.java
@@ -347,8 +347,10 @@ public class RadioGroup extends LinearLayout {
             }
             mProtectFromCheckedChange = false;
 
-            int id = buttonView.getId();
-            setCheckedId(id);
+            if (isChecked) {
+                int id = buttonView.getId();
+                setCheckedId(id);
+            }
         }
     }
 
