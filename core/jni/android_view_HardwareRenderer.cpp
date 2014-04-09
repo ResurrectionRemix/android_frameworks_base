1fa6ea0ddbab9c8e3d9e469749ca7160b174ce50
westcripp <korukaltan@gmail.com>
2014-04-10 01:10:42 +0300

    Revert "Fix STE OMX"
    This reverts commit cd35a0a49d63753cdf021368afafc2f42b9bbe41.

--- core/jni/android_view_HardwareRenderer.cpp
+++ core/jni/android_view_HardwareRenderer.cpp
@@ -125,11 +125,9 @@ static void android_view_HardwareRenderer_beginFrame(JNIEnv* env, jobject clazz,
 }
 
 static jlong android_view_HardwareRenderer_getSystemTime(JNIEnv* env, jobject clazz) {
-#ifndef STE_HARDWARE
     if (uirenderer::Extensions::getInstance().hasNvSystemTime()) {
         return eglGetSystemTimeNV();
     }
-#endif
     return systemTime(SYSTEM_TIME_MONOTONIC);
 }
 
