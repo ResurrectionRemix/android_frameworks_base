130fc2be9d32f5bd14c73e77e52d7ca5e0721972
Flamefire <alex@grundis.de>
2014-04-27 15:19:26 +0200

    Fix a NPE when putting a null-Bundle in an Intent
    Happens frequently e.g. with GoogleAccountAuthenticator
    
    Change-Id: I284bf153f40ee9cc3e70bce6efcc3b9e6706361b
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- core/java/android/content/Intent.java
+++ core/java/android/content/Intent.java
@@ -7029,6 +7029,9 @@ public class Intent implements Parcelable, Cloneable {
      * @see #removeExtra
      */
     public Intent putExtras(Bundle extras) {
+        if (extras == null) {
+            return this;
+        }
         if (mExtras == null) {
             mExtras = new Bundle();
         }
