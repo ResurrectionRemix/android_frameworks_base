6e32c8c62b4976214a57a21140cb198ef6b96565
Felix Ableitner <me@nutomic.com>
2014-05-04 16:20:11 +0200

    DocumentsUI: Remove catch-all statements
    This removes several "catch (Exception)" statements which violates the Android code
    style guidelines and hides problems.
    
    I left some catch-all statements in, as they seem to be intended like this (grep for
    "catch (Exception").
    
    I suggest we do not merge this before we are sure that we fixed any problems. One problem
    is a crash when deleting items (I'm currently working to fix that).
    
    PS 2: Add exception to log.
    Change-Id: I483c320c7ee256acd993dd2a509c031fa5e3f09e

--- packages/DocumentsUI/src/com/android/documentsui/DocumentsActivity.java
+++ packages/DocumentsUI/src/com/android/documentsui/DocumentsActivity.java
@@ -51,6 +51,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.os.RemoteException;
 import android.os.storage.StorageManager;
 import android.os.storage.StorageVolume;
 import android.provider.DocumentsContract;
@@ -1263,7 +1264,7 @@ public class DocumentsActivity extends Activity {
                         resolver, cwd.derivedUri.getAuthority());
                 childUri = DocumentsContract.createDocument(
                         client, cwd.derivedUri, mMimeType, mDisplayName);
-            } catch (Exception e) {
+            } catch (RemoteException e) {
                 Log.w(TAG, "Failed to create document", e);
             } finally {
                 ContentProviderClient.releaseQuietly(client);
@@ -1343,7 +1344,11 @@ public class DocumentsActivity extends Activity {
 
                     count++;
                     publishProgress((Integer) count);
-                } catch (Exception e) {
+                } catch (RemoteException e) {
+                    Log.w(TAG, "Failed to copy " + doc, e);
+                } catch (FileNotFoundException e) {
+                    Log.w(TAG, "Failed to copy " + doc, e);
+                }catch (IOException e) {
                     Log.w(TAG, "Failed to copy " + doc, e);
                 }
             }
