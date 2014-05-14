a95c0020d10b91faf81ccacc49d29eee54b97099
namagi <br.mladen@gmail.com>
2014-05-14 12:53:09 +0200

    SystemUI: prevent NPE on clear recents double press
    Pressing Clear all button twice fast or while the list
    is clearing but not yet fully cleared causes sysui to crash due to
    mRecentTaskDescriptions being null: (http://pastebin.com/2gRFvf8k)
    
    Workaround it by hiding clear all recents button.
    
    Change-Id: I23d10edfd336041739fe80352ee01987e671dbd7

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -512,6 +512,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 @Override
                 public void onClick(View v) {
                     mRecentsContainer.removeAllViewsInLayout();
+                    mClearRecents.setVisibility(View.INVISIBLE);
                 }
             });
         }
