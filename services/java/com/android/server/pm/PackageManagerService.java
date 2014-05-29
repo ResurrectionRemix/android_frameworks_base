9176ec2482b4c2c409497203457c845ebaf29288
Andy Mast <andy@cyngn.com>
2014-05-29 12:40:09 -0700

    Cleanup app->{theme1,theme2...} mapping on uninstall
    Change-Id: I24b43e8817db1927d5b43fcf7ad3340269d046ea

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -5612,6 +5612,10 @@ public class PackageManagerService extends IPackageManager.Stub {
                 map.remove(opkg.packageName);
             }
 
+            if (map.isEmpty()) {
+                mOverlays.remove(target);
+            }
+
             PackageParser.Package targetPkg = mPackages.get(target);
             if (targetPkg != null) {
                 String idmapPath = getIdmapPath(targetPkg, opkg);
@@ -5632,6 +5636,7 @@ public class PackageManagerService extends IPackageManager.Stub {
            String idmapPath = getIdmapPath(appPkg, opkg);
            new File(idmapPath).delete();
         }
+        mOverlays.remove(appPkg.packageName);
     }
 
     private void recursiveDelete(File f) {
