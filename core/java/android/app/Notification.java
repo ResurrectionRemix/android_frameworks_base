253d196f0610a645684d9d30eebbea275edc7c9f
Sean Barbeau <sjbarbeau@gmail.com>
2014-07-19 21:31:41 -0700

    Keep deleteIntent when copying Notification attributes
    Ensures that any previously registered deleteIntent is retained after a call
    to Notification.setLatestEventInfo().
    
    Currently, a call to setLatestEventInfo() effectively erases a previously
    registered Notification.deleteIntent, which results in the app never being
    notified when the user dismisses the notification.
    
    This issue is described in detail in #73720:
    https://code.google.com/p/android/issues/detail?id=73720
    
    Change-Id: Ie58bf6a310058d40beb0d1af6506e9e1f4324b37
    Signed-off-by: Sean Barbeau <sjbarbeau@gmail.com>

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -1668,6 +1668,7 @@ public class Notification implements Parcelable
         builder.setSound(this.sound, this.audioStreamType);
         builder.setDefaults(this.defaults);
         builder.setVibrate(this.vibrate);
+        builder.setDeleteIntent(this.deleteIntent);
 
         // now apply the latestEventInfo fields
         if (contentTitle != null) {
