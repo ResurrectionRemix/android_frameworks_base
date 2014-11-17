b64cbe03649626adc53c41945943453bc17ffed6
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-11-17 13:56:05 +0200

    Implement expanded desktop feature
    Author: Kyrylo Mikos <kiril.mik.os@gmail.com>
    Date:   Mon Nov 17 13:56:05 2014 +0200
    
        Bringup expanded desktop and implement per-user configuration.
    
        Change-Id: I90d834d788b3050a3f4c11dda9b6d1a7d79546cc
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Wed Feb 4 01:00:06 2015 -0800
    
        Create configurable default expanded desktop style.
    
        Change-Id: I90c7f8544cda9b91fb9b700fcf146f4e58b15a26
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Wed Jan 21 21:52:35 2015 -0800
    
        Remove unused POLICY_CONTROL_SELECTED. Unbreak public api.
    
    Change-Id: Ib0900349115442274fb08eb172de2567a8b00295

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12022,7 +12022,7 @@ public final class Settings {
         /**
          * Defines global runtime overrides to window policy.
          *
-         * See {@link com.android.server.policy.PolicyControl} for value format.
+         * See {@link android.view.WindowManagerPolicyControl} for value format.
          *
          * @hide
          */
@@ -12044,6 +12044,15 @@ public final class Settings {
          * @hide
          */
         public static final String BLOCKED_SLICES = "blocked_slices";
+
+        /**
+         * Defines global runtime overrides to window policy style.
+         *
+         * See {@link android.view.WindowManagerPolicyControl} for value definitions.
+         *
+         * @hide
+         */
+        public static final String POLICY_CONTROL_STYLE = "policy_control_style";
 
         /**
          * Defines global zen mode.  ZEN_MODE_OFF, ZEN_MODE_IMPORTANT_INTERRUPTIONS,
