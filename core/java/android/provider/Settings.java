a06f143ed0d4dc37679b018feef94f04b1f8e8cc
Danesh M <daneshm90@gmail.com>
2014-11-27 14:17:19 +0100

    BatteryMeterDrawable: add battery styles [1/2]
     - Portrait
     - Landscape
     - Circle
    -----
    
    DU port by elginsk8r
    
    Change-Id: Icfc14d989eebde9014538291c24aa76d328df234

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9200,6 +9200,16 @@ public final class Settings {
         public static final String FACE_AUTO_UNLOCK = "face_auto_unlock";
 
         /**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 1: Display the battery an icon in landscape mode
+         * 2: Display the battery as a circle
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
