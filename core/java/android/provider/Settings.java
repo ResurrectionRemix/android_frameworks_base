85785659a8821b925093d50cf6f3f7860bbf0bde
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-12-01 22:08:24 +0200

    Add option to disable searchbar in recents [2/2]
    Coded by yashade2001 <yashade2001@gmail.com>
    Thanks to TheMrcool212 for his help.
    Cherry-picked from Project D.I.S.C.O.
    
    NEEDS THIS PATCH: https://github.com/CyanogenMod/android_frameworks_base/commit/d7a236b5485fc09c4c1d44deaeae214bd705f8a2
    
    Signed-off-by: Yashar Ahmedizadeh <yashade2001@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
    
    Change-Id: I3a9bbb12d55f314d9e7dce82b8d50555c7e18337

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3148,6 +3148,12 @@ public final class Settings {
          */
         public static final String EGG_MODE = "egg_mode";
 
+         /**
+         * Recents Show/Hide Search Bar
+         * @hide
+         */
+        public static final String RECENTS_SHOW_HIDE_SEARCH_BAR = "recents_show_hide_search_bar";
+
         /**
          * Status bar battery %
          * @hide
