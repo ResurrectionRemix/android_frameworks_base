eff63f5527313c26f4a98699a35758836d0f3e6a
westcripp <korukaltan@gmail.com>
2014-12-04 17:27:31 +0200

    Revert "Add CM PlatLogo (1/2)"
    This reverts commit 6ba3e9bfc3b9b88a3b78070b2d01a2c12e4b4a19.

--- packages/SystemUI/src/com/android/systemui/egg/LLand.java
+++ packages/SystemUI/src/com/android/systemui/egg/LLand.java
@@ -699,9 +699,9 @@ public class LLand extends FrameLayout {
         public Player(Context context) {
             super(context);
 
-            setBackgroundResource(getEggPlayer());
+            setBackgroundResource(R.drawable.android);
             getBackground().setTintMode(PorterDuff.Mode.SRC_ATOP);
-            getBackground().setTint(getEggPlayerColor());
+            getBackground().setTint(0xFF00FF00);
             setOutlineProvider(new ViewOutlineProvider() {
                 @Override
                 public void getOutline(View view, Outline outline) {
@@ -945,13 +945,5 @@ public class LLand extends FrameLayout {
             w = h = irand(PARAMS.STAR_SIZE_MIN, PARAMS.STAR_SIZE_MAX);
             v = z = 0;
         }
-    }
-
-    protected int getEggPlayer() {
-        return R.drawable.android;
-    }
-
-    protected int getEggPlayerColor() {
-        return 0xFF00FF00;
     }
 }
--- packages/SystemUI/src/com/android/systemui/egg/LLandActivity.java
+++ packages/SystemUI/src/com/android/systemui/egg/LLandActivity.java
@@ -27,19 +27,10 @@ public class LLandActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final boolean isCM = getIntent().getBooleanExtra("is_cm", false);
-        if (isCM) {
-            setContentView(R.layout.cmland);
-            CMLand world = (CMLand) findViewById(R.id.world);
-            world.setScoreField((TextView) findViewById(R.id.score));
-            world.setSplash(findViewById(R.id.welcome));
-            Log.v(CMLand.TAG, "focus: " + world.requestFocus());
-        } else {
-            setContentView(R.layout.lland);
-            LLand world = (LLand) findViewById(R.id.world);
-            world.setScoreField((TextView) findViewById(R.id.score));
-            world.setSplash(findViewById(R.id.welcome));
-            Log.v(LLand.TAG, "focus: " + world.requestFocus());
-        }
+        setContentView(R.layout.lland);
+        LLand world = (LLand) findViewById(R.id.world);
+        world.setScoreField((TextView) findViewById(R.id.score));
+        world.setSplash(findViewById(R.id.welcome));
+        Log.v(LLand.TAG, "focus: " + world.requestFocus());
     }
 }
