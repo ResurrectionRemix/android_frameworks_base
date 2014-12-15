bb9d5f20e337f83cce9f241eee5864165ac294d3
westcripp <korukaltan@gmail.com>
2014-12-15 05:10:54 +0200

    ImageView drawable update
    This reverts commit 1dbbbcb16d373b7dfeb022461c467da57895ab26.

--- core/java/android/widget/ImageView.java
+++ core/java/android/widget/ImageView.java
@@ -390,6 +390,7 @@ public class ImageView extends View {
             final int oldWidth = mDrawableWidth;
             final int oldHeight = mDrawableHeight;
 
+            updateDrawable(null);
             mResource = resId;
             mUri = null;
 
@@ -418,6 +419,7 @@ public class ImageView extends View {
         if (mResource != 0 ||
                 (mUri != uri &&
                  (uri == null || mUri == null || !uri.equals(mUri)))) {
+            updateDrawable(null);
             mResource = 0;
             mUri = uri;
 
@@ -722,26 +724,25 @@ public class ImageView extends View {
         }
     }
 
-    private void ensureResolvedUri() {
-        if (mDrawable == null) {
-            resolveUri();
-        }
-    }
-
     private void resolveUri() {
-        Drawable d = null;
-        Resources rsrc = getResources();
+        if (mDrawable != null) {
+            return;
+        }
 
+        Resources rsrc = getResources();
         if (rsrc == null) {
-            // Can't resolve any drawable, fall through to clearing the drawable
-        } else if (mResource != 0) {
+            return;
+        }
+
+        Drawable d = null;
+
+        if (mResource != 0) {
             try {
                 d = mContext.getDrawable(mResource);
             } catch (Exception e) {
                 Log.w("ImageView", "Unable to find resource: " + mResource, e);
                 // Don't try again.
                 mUri = null;
-                mResource = 0;
             }
         } else if (mUri != null) {
             String scheme = mUri.getScheme();
@@ -771,7 +772,7 @@ public class ImageView extends View {
                         }
                     }
                 }
-            } else {
+        } else {
                 d = Drawable.createFromPath(mUri.toString());
             }
     
@@ -781,7 +782,7 @@ public class ImageView extends View {
                 mUri = null;
             }
         } else {
-            // No drawable to resolve
+            return;
         }
 
         updateDrawable(d);
@@ -863,7 +864,7 @@ public class ImageView extends View {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        ensureResolvedUri();
+        resolveUri();
         int w;
         int h;
         
