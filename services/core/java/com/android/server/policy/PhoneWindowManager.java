04879ee0dc03e165adfe9781f8836fd861e6f033
maxwen <max.weninger@gmail.com>
2014-12-28 22:11:38 +0100

    base: bring back screenrecord
    *******
    
    abc edit: remove PhoneWindowManager power button part,
    we already have power+VolUp for partial screenshot so
    we're gonna use a tile for this
    
    ******
    
    - adapt for O
    
    Squashed commit of the following:
    
    commit 06f9b5a7ebefb8ff477241269a005b9afae2be87
    Author: maxwen <max.weninger@gmail.com>
    Date:   Sun Dec 28 21:31:46 2014 +0100
    
        base: screenrecord: use different notification image
    
        Change-Id: I252144a4f5c3bdaa84974b996348885ba8dee942
    
    commit 6f9f2d83e5c86b6259bccfdfadb0e4243872f7f9
    Author: XpLoDWilD <xplod@ouverta.fr>
    Date:   Sun Jul 13 21:43:28 2014 +0200
    
        Screenrecord: Fix SystemUI loop-crash
    
        Change-Id: I25045820465d21b7d1f0e933e5e4f3324cb044b6
    
    commit b6f20490332f293df050db46f9a8ced9394b9641
    Author: maxwen <max.weninger@gmail.com>
    Date:   Tue Apr 29 23:28:06 2014 +0200
    
        base: screenrecord: update notification
    
        Update show/hide pointer button for actual state
    
        Change-Id: I02090f05b06896248483f3f9ff2eb22924a22476
    
    commit aa23ce799167f97a6e9f7beaf57d23027b5c32ed
    Author: maxwen <max.weninger@gmail.com>
    Date:   Tue Dec 23 01:04:35 2014 +0100
    
        Screenrecord: correct multi-user acces for settings
    
        Change-Id: I0bee907d190b2dc0df1c12d136f31ed379a59866
    
    commit 1fc7464c6d3efcb13e243aae7ed798d67af86ede
    Author: xplodwild <me@xplod.fr>
    Date:   Wed Nov 13 08:16:57 2013 +0100
    
        Screenrecord: Create the Screenshots directory if it doesn't exist
    
        Change-Id: I2b0458eed024dbda97e9e1ea3426c12d29add781
    
    commit 7c08a37a28bcf7b44e9de2b67c502077c0f4fb47
    Author: xplodwild <me@xplod.fr>
    Date:   Tue Nov 5 20:30:09 2013 +0100
    
        base: Add a key combo to start/stop video recording
    
        Press volume up and power to start recording a video of your screen.
        Press this key combo again to stop it.
    
        Change-Id: Id0b13ba1092a1d6e0bf7bff5f14d89764cdf370c
    
    Change-Id: Ida1ee4a5c8d334c72d549752d02bd7848932dc7c
    
    base: bring back screen record shortcut
    
    Change-Id: Iad561a2dc7f6c68b49e40537c4d5152d93dcdf09
    
    SystemUI: screenrecord: make sure to always delete tmp file
    
    tell media scanner that the tmp file got deleted
    
    Change-Id: I2974969fa25a01c9422f13316d188b65db591156
    
    commit 6690cbf35c4b03bbba5c79a9c13fecc303697cc3
    Author: Alexander Martinz <eviscerationls@gmail.com>
    Date:   Tue Jan 19 21:23:57 2016 +0100
    
    Save screenrecord in "Screenrecords" directory
    This was previously saved in "Screenshots"
    
    - Also refactor source a bit, remove warnings
    
    Change-Id: I685558e9c5f4bde1c771bee7c3b8e951fd122a64
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>
    
     Conflicts:
    	packages/SystemUI/AndroidManifest.xml
    	services/core/java/com/android/server/policy/PhoneWindowManager.java
    
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -171,10 +171,12 @@ import android.app.StatusBarManager;
 import android.app.UiModeManager;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
@@ -2030,6 +2032,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private final ScreenshotRunnable mScreenshotRunnable = new ScreenshotRunnable();
+
+    private final Runnable mScreenrecordRunnable = new Runnable() {
+        @Override
+        public void run() {
+            takeScreenrecord();
+        }
+    };
 
     Runnable mBackLongPress = new Runnable() {
         public void run() {
@@ -6593,6 +6602,67 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         setHdmiPlugged(!mHdmiPlugged);
     }
 
+    final Object mScreenrecordLock = new Object();
+    ServiceConnection mScreenrecordConnection = null;
+    final Runnable mScreenrecordTimeout = new Runnable() {
+        @Override public void run() {
+            synchronized (mScreenrecordLock) {
+                if (mScreenrecordConnection != null) {
+                    mContext.unbindService(mScreenrecordConnection);
+                    mScreenrecordConnection = null;
+                }
+            }
+        }
+    };
+    // Assume this is called from the Handler thread.
+    private void takeScreenrecord() {
+        synchronized (mScreenrecordLock) {
+            if (mScreenrecordConnection != null) {
+                return;
+            }
+            ComponentName cn = new ComponentName("com.android.systemui",
+                    "com.android.systemui.screenrecord.TakeScreenrecordService");
+            Intent intent = new Intent();
+            intent.setComponent(cn);
+            ServiceConnection conn = new ServiceConnection() {
+                @Override
+                public void onServiceConnected(ComponentName name, IBinder service) {
+                    synchronized (mScreenrecordLock) {
+                        Messenger messenger = new Messenger(service);
+                        Message msg = Message.obtain(null, 1);
+                        final ServiceConnection myConn = this;
+                        Handler h = new Handler(mHandler.getLooper()) {
+                            @Override
+                            public void handleMessage(Message msg) {
+                                synchronized (mScreenrecordLock) {
+                                    if (mScreenrecordConnection == myConn) {
+                                        mContext.unbindService(mScreenrecordConnection);
+                                        mScreenrecordConnection = null;
+                                        mHandler.removeCallbacks(mScreenrecordTimeout);
+                                    }
+                                }
+                            }
+                        };
+                        msg.replyTo = new Messenger(h);
+                        msg.arg1 = msg.arg2 = 0;
+                        try {
+                            messenger.send(msg);
+                        } catch (RemoteException e) {
+                        }
+                    }
+                }
+                @Override
+                public void onServiceDisconnected(ComponentName name) {}
+            };
+            if (mContext.bindServiceAsUser(
+                    intent, conn, Context.BIND_AUTO_CREATE, UserHandle.CURRENT)) {
+                mScreenrecordConnection = conn;
+                // Screenrecord max duration is 30 minutes. Allow 31 minutes before killing
+                // the service.
+                mHandler.postDelayed(mScreenrecordTimeout, 31 * 60 * 1000);
+            }
+        }
+    }
 
     /** {@inheritDoc} */
     @Override
