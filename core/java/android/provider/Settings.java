e3df5a0ebba4908aa714402fd984ae2efc14a0af
cristianomatos <cristianobmatos@gmail.com>
2014-12-29 16:04:53 -0200

    AOKP animations: Add an entry for TRANSIT_TASK_OPEN_BEHIND [1/2]
    Change-Id: I306c932c79d32e727f15fe1645ffa723d3169bec
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3719,17 +3719,18 @@ public final class Settings {
          * AOKP Custom System Animations
          * @hide
          */  
-        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
                 "activity_open",
                 "activity_close",
-                "task_open",  
-                "task_close",  
+                "task_open",
+                "task_close",
                 "task_to_front",
-                "task_to_back",  
-                "wallpaper_open",  
-                "wallpaper_close",  
-                "wallpaper_intra_open",  
-                "wallpaper_intra_close",  
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+                "task_open_behind",
         };  
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
 
--- services/core/java/com/android/server/wm/AppTransition.java
+++ services/core/java/com/android/server/wm/AppTransition.java
@@ -229,7 +229,7 @@ public class AppTransition implements Dump {
     private final int mClipRevealTranslationY;
 
     private SettingsObserver mSettingsObserver;
-    private int[] mActivityAnimations = new int[10];
+    private int[] mActivityAnimations = new int[11];
     private int mAnimationDuration;
     private boolean mIsResId = false;
 
@@ -1664,9 +1664,17 @@ public class AppTransition implements Dump {
                     }
                     break;
                 case TRANSIT_TASK_OPEN_BEHIND:
-                    animAttr = enter
-                            ? WindowAnimation_launchTaskBehindSourceAnimation
-                            : WindowAnimation_launchTaskBehindTargetAnimation;
+                    if (mActivityAnimations[10] != 0) {
+                        mIsResId = true;
+                        int[] animArray = AwesomeAnimationHelper.getAnimations(mActivityAnimations[10]);
+                        animAttr = enter
+                                ? animArray[1]
+                                : animArray[0];
+                    } else {
+                        animAttr = enter
+                                ? WindowAnimation_launchTaskBehindSourceAnimation
+                                : WindowAnimation_launchTaskBehindTargetAnimation;
+                    }
             }
             a = animAttr != 0 ? loadAnimationAttr(lp, animAttr) : null;
             if (a != null) {
@@ -2062,7 +2070,7 @@ public class AppTransition implements Dump {
             ContentResolver resolver = mContext.getContentResolver();
             resolver.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.ANIMATION_CONTROLS_DURATION), false, this);
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 11; i++) {
 	            resolver.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.ACTIVITY_ANIMATION_CONTROLS[i]), false, this);
             }
@@ -2075,7 +2083,7 @@ public class AppTransition implements Dump {
 
     private void updateSettings() {
         ContentResolver resolver = mContext.getContentResolver();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 11; i++) {
             mActivityAnimations[i] = Settings.System.getInt(resolver, Settings.System.ACTIVITY_ANIMATION_CONTROLS[i], 0);
         }
 
