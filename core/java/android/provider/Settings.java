67cda589d0e5f09119420934014231fc92c55012
dankoman <dankoman30@gmail.com>
2015-01-02 16:13:34 +0100

    Fw_base - Proximity speaker [1/3]
    Patchset: add time delay preference
    Change-Id: I8846e1d54b7f26f4322b91fc1ba20f3e5da465ec
    Signed-off-by: Janson Kang <temasek71@gmail.com>
    Signed-off-by: Varun <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4775,6 +4775,26 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_MAX_NOTIF_CONFIG = "lockscreen_max_notif_cofig";
 
+	/**
+         * Whether the proximity sensor will adjust call to speaker
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER = "proximity_auto_speaker";
+
+        /**
+         * Time delay to activate speaker after proximity sensor triggered
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_DELAY = "proximity_auto_speaker_delay";
+
+        /**
+         * Whether the proximity sensor will adjust call to speaker,
+         * only while in call (not while ringing on outgoing call)
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_INCALL_ONLY =
+                "proximity_auto_speaker_incall_only";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
