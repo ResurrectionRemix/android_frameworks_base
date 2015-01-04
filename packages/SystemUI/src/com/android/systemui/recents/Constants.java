174bfc4d50d736bf440e5a16f4abafffa57da871
westcripp <korukaltan@gmail.com>
2015-01-04 10:17:58 +0200

    Update popups animation & add top bottom clear recent button and fix layout
    Change-Id: I6bb7fca0c8e1eb8716614d544db3bac6e5dbf2d8

--- packages/SystemUI/src/com/android/systemui/recents/Constants.java
+++ packages/SystemUI/src/com/android/systemui/recents/Constants.java
@@ -58,9 +58,10 @@ public class Constants {
             // clear button location
             public static final int RECENTS_CLEAR_ALL_TOP_RIGHT    = 0;
             public static final int RECENTS_CLEAR_ALL_TOP_LEFT     = 1;
-            public static final int RECENTS_CLEAR_ALL_BOTTOM_RIGHT = 2;
-            public static final int RECENTS_CLEAR_ALL_BOTTOM_LEFT  = 3;
-            public static final int RECENTS_CLEAR_ALL_BOTTOM_CENTER  = 4;
+            public static final int RECENTS_CLEAR_ALL_TOP_CENTER   = 2;
+            public static final int RECENTS_CLEAR_ALL_BOTTOM_RIGHT = 3;
+            public static final int RECENTS_CLEAR_ALL_BOTTOM_LEFT  = 4;
+            public static final int RECENTS_CLEAR_ALL_BOTTOM_CENTER  = 5;
         }
     }
 
