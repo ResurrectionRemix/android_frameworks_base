c612d9c7f9486f6a4e82156a1923263681d4b714
Lars Greiss <kufikugel@googlemail.com>
2015-01-14 19:47:21 +0000

    Privacy guard: option to disable notification (1/2)
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    
    Change-Id: I52b11f2fefaf9a31f4153cfbe5eeb476b636fd49

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6044,6 +6044,12 @@ public final class Settings {
         public static final String THEME_PREV_BOOT_API_LEVEL = "theme_prev_boot_api_level";
 
         /**
+         * Whether privacy guard notification should show.
+         * @hide
+         */
+        public static final String PRIVACY_GUARD_NOTIFICATION = "privacy_guard_notification";
+
+        /**
          * Whether to include options in power menu for rebooting into recovery or bootloader
          * @hide
          */
@@ -6135,6 +6141,7 @@ public final class Settings {
             SLEEP_TIMEOUT,
             PRIVACY_GUARD_DEFAULT,
             LOCKSCREEN_QUICK_UNLOCK_CONTROL,
+            PRIVACY_GUARD_NOTIFICATION,
             ADVANCED_REBOOT,
             DEVELOPMENT_SHORTCUT,
             STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD
--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -1979,6 +1979,11 @@ final class ActivityStack {
     }
 
     private final void updatePrivacyGuardNotificationLocked(ActivityRecord next) {
+        if (android.provider.Settings.Secure.getIntForUser(mService.mContext.getContentResolver(),
+            android.provider.Settings.Secure.PRIVACY_GUARD_NOTIFICATION,
+            1, UserHandle.USER_CURRENT) == 0) {
+            return;
+        }
 
         String privacyGuardPackageName = mStackSupervisor.mPrivacyGuardPackageName;
         if (privacyGuardPackageName != null && privacyGuardPackageName.equals(next.packageName)) {
