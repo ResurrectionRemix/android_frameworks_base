e93369ebd2d6f6a5d48f341bb475b5a2f0a6f5e7
BigBrother1984 <carlosavignano@aospa.co>
2015-01-17 16:38:37 +0100

    SystemUI: Materialize volume panel in/out animation
    * Thanks to @David96 that had the original idea, material
      oriented, already during kitkat times.
    
    AICPfy
    PS2: FIx expanded panel not hiding derp
    
    Signed-off-by: BigBrother1984 <carlosavignano@aospa.co>
    Change-Id: Ic79d404c4a0d5bc0a83d20cd4e0d7d5ad7b7c90f
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -105,6 +105,7 @@ public class VolumePanel extends Handler implements DemoMode {
     private static final int TIMEOUT_DELAY_COLLAPSED = 4500;
     private static final int TIMEOUT_DELAY_SAFETY_WARNING = 5000;
     private static final int TIMEOUT_DELAY_EXPANDED = 10000;
+    private static final int ANIMATION_DURATION = 300; // ms
 
     private static final int MSG_VOLUME_CHANGED = 0;
     private static final int MSG_FREE_RESOURCES = 1;
@@ -1301,6 +1302,27 @@ public class VolumePanel extends Handler implements DemoMode {
             mAudioManager.forceVolumeControlStream(stream);
             if (mDialog != null) {
                 mDialog.show();
+                Runnable r = new Runnable() {
+                    public void run() {
+                        mView.setY(-mView.getHeight());
+                        mView.animate().y(0).setDuration(ANIMATION_DURATION)
+                            .withEndAction(new Runnable() {
+                                public void run() {
+                                    if (mCallback != null) {
+                                        mCallback.onVisible(true);
+                                    }
+                                    announceDialogShown();
+                                }
+                        });
+                    }
+                };
+                if (mView.getHeight() == 0) {
+                    new Handler().post(r);
+                } else {
+                    r.run();
+                }
+            } else {
+                Log.d(mTag, "Bad, mDialog is null...");
             }
             if (stream != STREAM_MASTER) {
                 mAudioManager.forceVolumeControlStream(stream);
@@ -1565,12 +1587,18 @@ public class VolumePanel extends Handler implements DemoMode {
                 if (isShowing()) {
                     hideVolumePanel();
                     if (mDialog != null) {
-                        mDialog.dismiss();
-                        clearRemoteStreamController();
-                        mActiveStreamType = -1;
-                        if (mCallback != null) {
-                            mCallback.onVisible(false);
-                        }
+                        mView.animate().y(-mView.getHeight())
+                                .setDuration(ANIMATION_DURATION)
+                                .withEndAction(new Runnable() {
+                            public void run() {
+                                mDialog.dismiss();
+                                clearRemoteStreamController();
+                                mActiveStreamType = -1;
+                                if (mCallback != null) {
+                                    mCallback.onVisible(false);
+                                }
+                            }
+                        });
                     }
                 }
                 synchronized (sSafetyWarningLock) {
