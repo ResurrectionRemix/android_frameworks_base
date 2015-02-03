9ad09af1587e0fe90b1c8e7cd1564d71f255f9f3
westcripp <korukaltan@gmail.com>
2015-02-03 21:57:11 +0200

    Recreate center clock after apply new theme
    Change-Id: I60b02d5e9a6cda78662ab23c1dcddee9d902bc3c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3820,7 +3820,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             showKeyguard();
             // The following views need to be invisible if the keyguard is showing
             // These views were hidden but re-inflating the status bar changed them back to visible
-            mClockView.setVisibility(View.INVISIBLE);
+            mCenterClockLayout.setVisibility(View.INVISIBLE);
             mNotificationIconArea.setVisibility(View.INVISIBLE);
             mSystemIconArea.setVisibility(View.INVISIBLE);
         }
