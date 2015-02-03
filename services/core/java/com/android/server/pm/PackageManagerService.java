7c569a19efc6b1e6482339aec5181175f2cf81d6
d34d <clark@cyngn.com>
2015-02-03 11:53:10 -0800

    Themes: Get boot theme config from ThemeUtils
    mBootThemeConfig is always null right now because we ever assign
    it using ThemeUtils.getBootThemeDirty().  This patch makes sure we
    take care of that now.
    
    Change-Id: I3bb8a3361f0bea8f31d1a03762378bc7ff316127

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -1658,6 +1658,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                 }
             }
 
+            mBootThemeConfig = ThemeUtils.getBootThemeDirty();
+
             // Collect vendor overlay packages.
             // (Do this before scanning any apps.)
             // For security and version matching reason, only consider
