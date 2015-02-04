a6bf38a280fc4c8002f99d7243ad4405ed435675
Danesh M <daneshm90@gmail.com>
2015-02-03 23:34:54 -0800

    SystemUI : Fix Rtl for lockscreen shortcuts
    Change-Id: I2c3c72bb6e1b06402b882b4f8b2d8d250d813bcd

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1252,7 +1252,9 @@ public class NotificationPanelView extends PanelView implements
                 && y >= header.getTop() && y <= header.getBottom();
 
         final float w = getMeasuredWidth();
-        float region = (w * (1.f/4.f)); // TODO overlay region fraction?
+        float region = (w * (1.f/3.f)); // TODO overlay region fraction?
+        boolean showQsOverride = isLayoutRtl() ? (x < region) : (w - region < x);
+        showQsOverride = showQsOverride && mStatusBarState == StatusBarState.SHADE;
 
         boolean showQsOverride = false;
 
