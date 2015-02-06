265feb2bb2709ffd680305f885c182555eed149a
snak3ater <saeedyasir91@gmail.com>
2015-02-06 14:27:20 +0500

    Fix make updateapi for custom density setting

--- core/java/android/util/DisplayMetrics.java
+++ core/java/android/util/DisplayMetrics.java
@@ -117,6 +117,10 @@ public class DisplayMetrics {
     @Deprecated
     public static int DENSITY_DEVICE;
 
+    /**
+     * Allow custom density setting
+     * @hide
+     */
     public static int DENSITY_CURRENT;
 
     static {
@@ -214,6 +218,10 @@ public class DisplayMetrics {
      */
     public float noncompatYdpi;
 
+    /**
+     * Allow custom density setting
+     * @hide
+     */
     public void updateDensity() {
         density = DENSITY_CURRENT / (float) DENSITY_DEFAULT;
         densityDpi = DENSITY_CURRENT;
@@ -318,7 +326,11 @@ public class DisplayMetrics {
             ", height=" + heightPixels + ", scaledDensity=" + scaledDensity +
             ", xdpi=" + xdpi + ", ydpi=" + ydpi + "}";
     }
-
+    
+    /**
+     * Allow custom density setting
+     * @hide
+     */
     public static int getDeviceDensity() {
         return DENSITY_CURRENT;
     }
