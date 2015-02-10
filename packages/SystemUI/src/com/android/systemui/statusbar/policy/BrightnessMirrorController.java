12f455be034d60b7972f1fe92666ddc917161415
Liu Xiaowen <xiaowen.liu@freescale.com>
2015-02-10 13:44:54 +0800

    Fix notification panel can't hide in statusbar
    The root cause is when do notification panel animate,
    it enable LAYER_TYPE_HARDWARE to do optimization.
    When device doesn't support GPU, the draw cache created
    by LAYER_TYPE_HARDWARE will not be destroyed.
    So, destroy draw cache to correct it.
    
    Change-Id: Ic0c4d2ccf5d45427d996256d01a0bfa745384afb

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
@@ -71,6 +71,7 @@ public class BrightnessMirrorController {
             @Override
             public void run() {
                 mBrightnessMirror.setVisibility(View.INVISIBLE);
+                mPanelHolder.destroyDrawingCache();
             }
         });
     }
