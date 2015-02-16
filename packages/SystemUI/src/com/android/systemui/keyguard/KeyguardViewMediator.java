dec336954e4c9ab076ae71bf738a7f82182ae65e
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-16 12:13:39 +0000

    Profiles: Disable lock screen after boot
    This brings it inline with stock behavior and also fixes a couple of issues.
    If lock screen was disabled via profiles, on boot you would end up with no home or recents buttons
    and if you had a simpin set you would be faced with a black screen with no way to access the phone unless you
    popped the sim out.
    
    Ref: CYNGNOS-1930
    
    Change-Id: I9f1f9f467647e31525a1179dc1a0ce7b9dc3285f

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -921,9 +921,9 @@ public class KeyguardViewMediator extends SystemUI {
                 if (DEBUG) Log.d(TAG, "isKeyguardDisabled: keyguard is disabled by profile");
                 return true;
             }
-         }
+        }
         return false;
-     }
+    }
 
     /**
      * A dream started.  We should lock after the usual screen-off lock timeout but only
@@ -1215,6 +1215,11 @@ public class KeyguardViewMediator extends SystemUI {
         if (isKeyguardDisabled(KeyguardUpdateMonitor.getCurrentUser())
                 && !lockedOrMissing) {
             if (DEBUG) Log.d(TAG, "doKeyguard: not showing because lockscreen is off");
+            // update state
+            mShowing = false;
+            updateActivityLockScreenState();
+            adjustStatusBarLocked();
+            userActivity();
             return;
         }
 
