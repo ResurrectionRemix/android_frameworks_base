82009d6b514a5fc835c272f0c7f20d8d15618caa
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-03-11 14:04:02 +0100

    Lock screen: Weather panel improvements, (1/2):
    - Move weather panel to the bottom the clock panel
    - Add wind
    - Add humidity
    - Add Condition
    - Add Timestamp
    - Add condition icons (colored and VClouds)
    - Rearrange the weather panel:
      - Left side:
        - Location
        - Wind
      - Center:
        - Condiditon icon
      - Right side:
        - Current temperature
        - Humidity
      - Bottom:
        - Condition
        - Timestamp
    - Option to show/hide the timestamp
    - Option to colorize the monochrome icons
    
    Change-Id: I986975c13c5d29bd8103424e020ad775a8f03804
    
    This also includes:
    FWB: Proper LS weather switch from XXMrHyde
    https://github.com/AICP/frameworks_base/commit/ff1ceb40e268dda12906b4bfffd78df849430de1
    
    Weather: Handle no weather information available, (1/2)
    https://github.com/AICP/frameworks_base/commit/b1406dc8561ff1043bf357dcef9962e18a33ad63
    
    Lock screen: Update Handle no weather information available
    https://github.com/AICP/frameworks_base/commit/3691a5875edddc824e687a905ac413b3b2677160
    
    Change-Id: I72b477db49e7a6b3f2f358e8f9aa23bca542a054

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3949,6 +3949,33 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_WEATHER_FONT_STYLE = "status_bar_weather_font_style";
+        /*
+         * Whether to show the weather info on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER = "lock_screen_show_weather";
+
+        /**
+         * Whether to show the weather location lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER_LOCATION = "lock_screen_show_weather_location";
+
+        /**
+         * Whether to show the weather update timestamp on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER_TIMESTAMP = "lock_screen_show_weather_timestamp";
+
+        /**
+         * Weather condition icon on the lock screen
+         * 0: monochrome
+         * 1: colored
+         * 2: vclouds
+         * default: 0
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_CONDITION_ICON = "lock_screen_weather_condition_icon";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
