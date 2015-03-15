206e189c56dd5a7d59b9a85a10a76ba3a7723ec5
westcripp <korukaltan@gmail.com>
2015-03-15 23:11:21 +0200

    No need it
    Change-Id: Id6ad12fe667a0e245897d6662c1935a8d3b07c3d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6269,12 +6269,6 @@ public final class Settings {
         public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
 
         /**
-         * Timeout of long-pressing the Back button
-         * @hide
-         */
-        public static final String KILL_APP_LONGPRESS_TIMEOUT = "kill_app_longpress_timeout";
-
-        /**
          * The default NFC payment component
          * @hide
          */
