0fa2e6b5b10ccbcfdab76da603ba463f2c39c6c4
Paramananda <parama@codeaurora.org>
2015-03-24 14:34:16 +0530

    DocumentsUi : Drm lock icon can be shown on documnets picker view.
    - Drm lock icon added on Documents provider view where the
      documents picker view can display a lock icon for drm content.
    
    CRs-fixed : 804180
    
    Change-Id: I3adfbc0cb6eddc53c018de39f2d378e4d735e7a4

--- packages/DocumentsUI/src/com/android/documentsui/DirectoryFragment.java
+++ packages/DocumentsUI/src/com/android/documentsui/DirectoryFragment.java
@@ -71,6 +71,7 @@ import android.os.Parcelable;
 import android.os.RemoteException;
 import android.provider.DocumentsContract;
 import android.provider.DocumentsContract.Document;
+import android.text.TextUtils;
 import android.text.format.DateUtils;
 import android.text.format.Formatter;
 import android.text.format.Time;
@@ -111,6 +112,7 @@ import java.util.concurrent.Executor;
 
 
 import android.content.ContentValues;
+import android.drm.DrmHelper;
 import android.drm.DrmManagerClient;
 import android.drm.DrmStore.DrmDeliveryType;
 import android.drm.DrmStore.RightsStatus;
@@ -966,8 +968,13 @@ public class DirectoryFragment extends Fragment {
                     iconMime.setImageDrawable(
                             IconUtils.loadPackageIcon(context, docAuthority, docIcon));
                 } else {
-                    iconMime.setImageDrawable(IconUtils.loadMimeIcon(
-                            context, docMimeType, docAuthority, docId, state.derivedMode));
+                    if (DrmHelper.isDrmFile(docDisplayName)) {
+                        iconMime.setImageDrawable(context
+                                .getDrawable(R.drawable.ic_doc_drm));
+                    } else {
+                        iconMime.setImageDrawable(IconUtils.loadMimeIcon(
+                                context, docMimeType, docAuthority, docId, state.derivedMode));
+                    }
                 }
             }
 
