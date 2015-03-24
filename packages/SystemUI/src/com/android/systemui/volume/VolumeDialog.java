e9346004252464f9b10316e955d1db66a79b065e
Meticulus <theonejohnnyd@gmail.com>
2015-03-24 15:14:17 +0100

    Live Volume Steps [1/2]
    -Volume steps changes are instant, no need to reboot!
    
    **** Modifications for Marshmallow by DirtyUnicorns ****
    
    -- Broadcast a new intent from AudioService when steps change
    -- Receive broadcast in VolumeController triggering a state
       refresh

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
@@ -700,7 +700,8 @@ public class VolumeDialog {
 
         // update slider max
         final int max = ss.levelMax * 100;
-        if (max != row.slider.getMax()) {
+        final boolean maxChanged = max != row.slider.getMax();
+        if (maxChanged) {
             row.slider.setMax(max);
         }
 
@@ -756,7 +757,7 @@ public class VolumeDialog {
         final boolean enableSlider = !zenMuted;
         final int vlevel = row.ss.muted && (isRingVibrate || !isRingStream && !zenMuted) ? 0
                 : row.ss.level;
-        updateVolumeRowSliderH(row, enableSlider, vlevel);
+        updateVolumeRowSliderH(row, enableSlider, vlevel, maxChanged);
     }
 
     private void updateVolumeRowHeaderVisibleH(VolumeRow row) {
@@ -780,7 +781,7 @@ public class VolumeDialog {
         row.slider.setThumbTintList(tint);
     }
 
-    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel) {
+    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel, boolean maxChanged) {
         row.slider.setEnabled(enable);
         updateVolumeRowSliderTintH(row, row.stream == mActiveStream);
         if (row.tracking) {
@@ -804,7 +805,7 @@ public class VolumeDialog {
             }
         }
         final int newProgress = vlevel * 100;
-        if (progress != newProgress) {
+        if (progress != newProgress || maxChanged) {
             if (mShowing && rowVisible) {
                 // animate!
                 if (row.anim != null && row.anim.isRunning()
