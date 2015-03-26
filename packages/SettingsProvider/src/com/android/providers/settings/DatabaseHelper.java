43234962a586ecdc1763bd3a7b9dd5bcc13d7e27
Michael Bestas <mikeioannina@gmail.com>
2015-03-26 02:51:30 +0200

    Make forward/reverse lookup default overlay
    Change-Id: Ic0b9ebbc11edc661ec9c9563a8eabeaf7e014a46
    
    Conflicts:
    
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2481,8 +2481,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                     
             loadHeadsUpSetting(stmt);
 
+            loadIntegerSetting(stmt, Settings.System.ENABLE_FORWARD_LOOKUP,
+                    R.integer.def_forward_lookup);
+
             loadIntegerSetting(stmt, Settings.System.ENABLE_PEOPLE_LOOKUP,
                     R.integer.def_people_lookup);
+
+            loadIntegerSetting(stmt, Settings.System.ENABLE_REVERSE_LOOKUP,
+                    R.integer.def_reverse_lookup);
 
         } finally {
             if (stmt != null) stmt.close();
