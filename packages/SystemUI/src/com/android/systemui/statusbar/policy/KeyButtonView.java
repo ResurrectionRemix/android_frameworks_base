f15578c952445803bd2a45bed12235257ab672cc
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-04-03 16:11:06 +0100

    Revert "SystemUI: fix nav button ripple getting stuck"
    This reverts commit 19040260713a8367a00d1d679843a2cf7f353098.
    
    Change-Id: I839c3cbbcbbe6bef11e099c62e6e5cc60320c41d

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -75,7 +75,6 @@ public class KeyButtonView extends ImageView {
     private boolean mInEditMode;
     private AudioManager mAudioManager;
     private Animator mAnimateToQuiescent = new ObjectAnimator();
-    private KeyButtonRipple mRipple;
 
     private PowerManager mPm;
     private boolean mPerformedLongClick;
@@ -126,7 +125,7 @@ public class KeyButtonView extends ImageView {
         setClickable(true);
         mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
         mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        setBackground(mRipple = new KeyButtonRipple(context, this));
+        setBackground(new KeyButtonRipple(context, this));
         mPm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 
         SettingsObserver settingsObserver = new SettingsObserver(new Handler());
@@ -287,9 +286,6 @@ public class KeyButtonView extends ImageView {
                 break;
             case MotionEvent.ACTION_CANCEL:
                 setPressed(false);
-                // hack to fix ripple getting stuck. exitHardware() starts an animation,
-                // but sometimes does not finish it.
-                mRipple.exitSoftware();
                 if (mCode != 0) {
                     sendEvent(KeyEvent.ACTION_UP, KeyEvent.FLAG_CANCELED);
                 }
