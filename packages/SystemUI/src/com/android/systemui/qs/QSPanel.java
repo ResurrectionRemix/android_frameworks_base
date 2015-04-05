9d608e6a04868fd7e007a214988ddb80d45def94
westcripp <korukaltan@gmail.com>
2015-04-05 04:43:05 +0300

    QS :Make tiles visible behind of transparent detail view
    Change-Id: I3a59080bf0ec5c229f06582222ea1d57f4d16dd4

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -696,7 +696,7 @@ public class QSPanel extends ViewGroup {
         public void onAnimationEnd(Animator animation) {
             // Only hide content if still in detail state.
             if (mDetailRecord != null) {
-                setGridContentVisibility(false);
+                setGridContentVisibility(true);
             }
         }
     };
