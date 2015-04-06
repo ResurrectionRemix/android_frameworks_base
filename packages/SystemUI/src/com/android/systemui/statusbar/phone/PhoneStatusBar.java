92c857b9caa96211dabbdb1843f353cf0d41f4ab
Roman Birg <roman@cyngn.com>
2015-04-06 15:44:44 -0700

    SystemUI: move keyguard visualizer behind notifications
    Also, handle hiding and showing the visualizer when dragging
    notifications, or tapping on them.
    
    Change-Id: I9402dded1cd76676887caae5f3aa075b03a6e7ea
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1076,6 +1076,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mExpandedContents = mStackScroller;
 
         mBackdrop = (BackDropView) mStatusBarWindowContent.findViewById(R.id.backdrop);
+        mBackdrop.setService(this);
         mBackdropFront = (ImageView) mBackdrop.findViewById(R.id.backdrop_front);
         mBackdropBack = (ImageView) mBackdrop.findViewById(R.id.backdrop_back);
 
@@ -5291,6 +5292,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     public boolean hasActiveNotifications() {
         return !mNotificationData.getActiveNotifications().isEmpty();
+    }
+
+    public void requestVisualizer(boolean show, int delay) {
+        mBackdrop.requestVisualizer(show, delay);
     }
 
     public void wakeUpIfDozing(long time, MotionEvent event) {
