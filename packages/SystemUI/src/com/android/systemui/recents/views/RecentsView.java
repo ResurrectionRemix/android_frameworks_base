276c6d483537c84bb39d5693d73dc7d633268873
Altan KRK <korukaltan@gmail.com>
2015-04-14 20:39:18 +0300

    Update default clear all button location

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -363,7 +363,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         if (mFloatingButton != null && showClearAllRecents) {
             int clearRecentsLocation = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.RECENTS_CLEAR_ALL_LOCATION,
-            Constants.DebugFlags.App.RECENTS_CLEAR_ALL_BOTTOM_RIGHT, UserHandle.USER_CURRENT);
+            Constants.DebugFlags.App.RECENTS_CLEAR_ALL_BOTTOM_CENTER, UserHandle.USER_CURRENT);
 
             FrameLayout.LayoutParams params = (FrameLayout.LayoutParams)
                     mFloatingButton.getLayoutParams();
