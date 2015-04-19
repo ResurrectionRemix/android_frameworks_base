d6edcb944d2ac2e6f5a038484cfe9986595b7597
westcripp <korukaltan@gmail.com>
2015-04-19 15:55:20 +0300

    Clear Conflicts
    Change-Id: I9034bf39fc3708ba90a04c947d3d644b97b68776

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -7266,14 +7266,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // overridden by qemu.hw.mainkeys in the emulator.
     @Override
     public boolean hasNavigationBar() {
-<<<<<<< HEAD
-        return mOverWriteHasNavigationBar
-            ? mHasNavigationBar
-            : mContext.getResources().getBoolean(
-                    com.android.internal.R.bool.config_showNavigationBar);
-=======
     return mHasNavigationBar;
->>>>>>> ca4adb3... PhoneWindowManager: Fix screen pinning averting soft reboot
     }
 
     @Override
