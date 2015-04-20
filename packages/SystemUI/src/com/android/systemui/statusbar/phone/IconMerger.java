56889d6fae63d4d03d914ee94998f0142c821281
Danilo Krummrich <danilo.krummrich@esolutions.de>
2015-04-20 13:44:04 +0200

    Fixed: Wrong status bar notification layout width.
    The layout of the status bar which surrounds the notification icons
    should be a multiple of the notification icon width. Previous calculation
    ensured to be a multiple of the notification icon height.
    
    Change-Id: I35faf50b75f7521116065568b9f9142f16fbae2d
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/IconMerger.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/IconMerger.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/IconMerger.java
@@ -27,22 +27,37 @@ import com.android.systemui.statusbar.policy.Clock;
 public class IconMerger extends LinearLayout {
     private static final String TAG = "IconMerger";
     private static final boolean DEBUG = false;
-
     private int mIconSize;
     private int mClockAndDateWidth;
     private boolean mCenterClock;
     private boolean mLeftClock;    
+    private int mIconWidth;
+    private int mClockLocation;
     private View mMoreView;
 
     public IconMerger(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        mIconSize = context.getResources().getDimensionPixelSize(
-                R.dimen.status_bar_icon_size);
+        mIconWidth = calculateIconWidth(context);
 
         if (DEBUG) {
             setBackgroundColor(0x800099FF);
         }
+    }
+
+    /**
+     * Considering the padding, this method calculates the effective icon width
+     * of the notification icons.
+     *
+     * @param context
+     * @return The effective icon width which is expected by the {@link IconMerger}.
+     */
+    public static int calculateIconWidth(final Context context) {
+        int iconSize = context.getResources().getDimensionPixelSize(
+                R.dimen.status_bar_icon_size);
+        int iconHPadding = context.getResources().getDimensionPixelSize(
+                R.dimen.status_bar_icon_padding);
+        return iconSize + 2 * iconHPadding;
     }
 
     public void setOverflowIndicator(View v) {
@@ -54,14 +69,12 @@ public class IconMerger extends LinearLayout {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         // we need to constrain this to an integral multiple of our children
         int width = getMeasuredWidth();
-        if (mCenterClock) {
-            final int totalWidth = mContext.getResources().getDisplayMetrics().widthPixels;
-            final int usableWidth = (totalWidth - mClockAndDateWidth - 2 * mIconSize) / 2;
-            if (width > usableWidth) {
-                width = usableWidth;
-            }
+        if (mClockLocation == Clock.STYLE_CLOCK_CENTER) {
+            int totalWidth = mContext.getResources().getDisplayMetrics().widthPixels;
+            width = totalWidth / 2 - mIconWidth * 2;
+
         }
-        setMeasuredDimension(width - (width % mIconSize), getMeasuredHeight());
+        setMeasuredDimension(width - (width % mIconWidth), getMeasuredHeight());
     }
 
     @Override
@@ -80,8 +93,15 @@ public class IconMerger extends LinearLayout {
         }
         final boolean overflowShown = (mMoreView.getVisibility() == View.VISIBLE);
         // let's assume we have one more slot if the more icon is already showing
-        if ((!mCenterClock || !mLeftClock) && overflowShown) visibleChildren --;
-        final boolean moreRequired = visibleChildren * mIconSize > width;
+        if (overflowShown) {
+            int totalWidth = mContext.getResources().getDisplayMetrics().widthPixels;
+            if ((mClockLocation != Clock.STYLE_CLOCK_CENTER &&
+                    mClockLocation != Clock.STYLE_CLOCK_LEFT) ||
+                    (visibleChildren > (totalWidth / mIconWidth / 2 + 1))) {
+                visibleChildren--;
+            }
+        }
+        final boolean moreRequired = visibleChildren * mIconWidth > width;
         if (moreRequired != overflowShown) {
             post(new Runnable() {
                 @Override
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1204,7 +1204,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateDisplaySize(); // populates mDisplayMetrics
         updateResources(null);
 
-        mIconSize = res.getDimensionPixelSize(com.android.internal.R.dimen.status_bar_icon_size);
+        mIconSize = res.getDimensionPixelSize(R.dimen.status_bar_icon_size);
 
         mStatusBarWindowContent = (FrameLayout) View.inflate(context,
                 R.layout.super_status_bar, null);
@@ -2432,9 +2432,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private void updateNotificationIcons() {
         if (mNotificationIcons == null) return;
-
+        final int effectiveIconWidth = IconMerger.calculateIconWidth(mContext);
         final LinearLayout.LayoutParams params
-            = new LinearLayout.LayoutParams(mIconSize + 2*mIconHPadding, mNaturalBarHeight);
+            = new LinearLayout.LayoutParams(effectiveIconWidth, mNaturalBarHeight);
 
         ArrayList<Entry> activeNotifications = mNotificationData.getActiveNotifications();
         final int N = activeNotifications.size();
@@ -4698,7 +4698,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 com.android.internal.R.dimen.status_bar_height);
 
         int newIconSize = res.getDimensionPixelSize(
-            com.android.internal.R.dimen.status_bar_icon_size);
+            R.dimen.status_bar_icon_size);
         int newIconHPadding = res.getDimensionPixelSize(
             R.dimen.status_bar_icon_padding);
 
