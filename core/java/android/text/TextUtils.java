7b1d983a98c690b880fb7ac81f5dbe4f0004bc17
Ralph Bergmann <ralph@the4thfloor.eu>
2015-04-20 21:24:29 +0200

    fix NPE in TextUtils
    return false if String is null or empty. Fix NPE too.
    
    see https://code.google.com/p/android/issues/detail?id=24965
    
    Change-Id: I7ce8a98f1e0753e905410ec469cd76a8b542d484
    Signed-off-by: Ralph Bergmann <ralph@the4thfloor.eu>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- core/java/android/text/TextUtils.java
+++ core/java/android/text/TextUtils.java
@@ -1473,6 +1473,9 @@ public class TextUtils {
      * Returns whether the given CharSequence contains only digits.
      */
     public static boolean isDigitsOnly(CharSequence str) {
+        if (isEmpty(str)) {
+            return false;
+        }
         final int len = str.length();
         for (int i = 0; i < len; i++) {
             if (!Character.isDigit(str.charAt(i))) {
