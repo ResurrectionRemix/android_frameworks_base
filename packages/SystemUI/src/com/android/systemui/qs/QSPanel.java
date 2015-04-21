a833ec1cc2a773d6ebe06d1269822ab7b1f95df4
Roman Birg <roman@cyngn.com>
2015-04-21 13:13:06 -0700

    SystemUI: add a disabled state for Quick Tiles
    Some tiles may want to be visible, but not allow any user interaction
    due to security reasons. Add a visual disabled state for tiles which are
    not enabled.
    
    With this patch, the Profiles tile and the Lockscreen toggle tile
    disable themselves on a secure lock screen.
    
    Change-Id: I65ec7837661483d238d8dc3fa18419f76c8029dd
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -315,6 +315,15 @@ public class QSPanel extends ViewGroup {
         v.setVisibility(visibility);
     }
 
+    private void setTileEnabled(View v, boolean enabled) {
+        mHandler.obtainMessage(H.SET_TILE_ENABLED, enabled ? 1 : 0, 0, v).sendToTarget();
+    }
+
+    private void handleSetTileEnabled(View v, boolean enabled) {
+        if (enabled == v.isEnabled()) return;
+        v.setEnabled(enabled);
+    }
+
     public void setTiles(Collection<QSTile<?>> tiles) {
         for (TileRecord record : mRecords) {
             removeView(record.tileView);
@@ -344,6 +353,7 @@ public class QSPanel extends ViewGroup {
                     visibility = INVISIBLE;
                 }
                 setTileVisibility(r.tileView, visibility);
+                setTileEnabled(r.tileView, state.enabled);
                 r.tileView.onStateChanged(state);
             }
             @Override
@@ -651,12 +661,15 @@ public class QSPanel extends ViewGroup {
     private class H extends Handler {
         private static final int SHOW_DETAIL = 1;
         private static final int SET_TILE_VISIBILITY = 2;
+        private static final int SET_TILE_ENABLED = 3;
         @Override
         public void handleMessage(Message msg) {
             if (msg.what == SHOW_DETAIL) {
                 handleShowDetail((Record)msg.obj, msg.arg1 != 0);
             } else if (msg.what == SET_TILE_VISIBILITY) {
                 handleSetTileVisibility((View)msg.obj, msg.arg1);
+            } else if (msg.what == SET_TILE_ENABLED) {
+                handleSetTileEnabled((View)msg.obj, msg.arg1 == 1);
             }
         }
     }
