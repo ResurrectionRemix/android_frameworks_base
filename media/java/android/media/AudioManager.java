4e2c38b1460c8d92ad3e9300671b86bbf10690da
westcripp <korukaltan@gmail.com>
2015-04-23 03:55:15 +0300

    Revert AudioManager: update references to application context
    This reverts commit 085c29e1e3d892a24d7c22ea3f2447ce4cc0c2a8.

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -1389,7 +1389,7 @@ public class AudioManager {
      * current ringer mode that can be queried via {@link #getRingerMode()}.
      */
     public boolean shouldVibrate(int vibrateType) {
-        String packageName = mApplicationContext.getPackageName();
+        String packageName = mContext.getPackageName();
         // Don't apply profiles for "android" context, as these could
         // come from the NotificationManager, and originate from a real package.
         if (!packageName.equals("android")) {
