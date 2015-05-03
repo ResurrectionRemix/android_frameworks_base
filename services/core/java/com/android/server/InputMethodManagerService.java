1b0fca1a0b7328010228d12b47e71b2b3860969a
Adnan <adnan@cyngn.com>
2015-05-03 22:56:10 +0900

    SystemUI: HeadsUp blacklists (2/3)
    Change-Id: I5bb9ead502d71182a689a3cb63f7ba9f8414d5b6

--- services/core/java/com/android/server/InputMethodManagerService.java
+++ services/core/java/com/android/server/InputMethodManagerService.java
@@ -2363,6 +2363,10 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
         return res;
     }
 
+    public boolean isImeShowing() {
+        return mInputShown;
+    }
+
     @Override
     public void showInputMethodPickerFromClient(
             IInputMethodClient client, int auxiliarySubtypeMode) {
