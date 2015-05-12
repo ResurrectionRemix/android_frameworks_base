544e08ab7edb31ee8a8f48f6b7b41ae095b7b641
UtkarshGupta <utkarsh.eminem@gmail.com>
2015-05-12 21:41:05 +0200

    FWB: Battery light: 100% charged level (1/2)
    Change-Id: Ie6b116532cc1906337cf5a4bf47ee34befde4e3a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3081,6 +3081,12 @@ public final class Settings {
          */
         public static final String BATTERY_LIGHT_REALLY_FULL_COLOR = "battery_light_really_full_color";
 
+         /**
+         * What color to use for the battery LED while charging - really full (100%)
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_REALLY_FULL_COLOR = "battery_light_really_full_color";
+
         /**
          * Sprint MWI Quirk: Show message wait indicator notifications
          * @hide
