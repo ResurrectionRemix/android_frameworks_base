0daad672e88ed9710aa870ab31a9e28d00541509
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-05-13 20:27:27 +0300

    Weather: Handle no weather information available, (1/2)
    Change-Id: Ie3cd7d8c95b13fd1c224e13ced7bb22a2825b749

--- packages/SystemUI/src/com/android/systemui/statusbar/DismissViewButton.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/DismissViewButton.java
@@ -21,6 +21,7 @@ import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff.Mode;
 import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.view.Choreographer;
@@ -133,4 +134,10 @@ public class DismissViewButton extends Button {
     public boolean isButtonStatic() {
         return mActiveDrawable == mStaticDismissDrawable;
     }
+
+    public void setIconColor(int color) {
+        mStaticDismissDrawable.setColorFilter(color, Mode.MULTIPLY);
+        mAnimatedDismissDrawable.setColorFilter(color, Mode.MULTIPLY);
+    }
+
 }
