ac54a050f448fa6699185b2b57c03224ac921f14
dhacker29 <dhackerdvm@gmail.com>
2015-05-21 20:59:02 -0400

    SystemServer: Skip TorchService construction if no flash feature detected
    * If a device doesn't have a flash, the torch service is unneeded
         and most likely will crash
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    Change-Id: Ideea6931979b87725fdd321d5a2ab447aa1e231a

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -662,7 +662,8 @@ public final class SystemServer {
                 }
             }
 
-            if (!disableNonCoreServices) {
+            if (!disableNonCoreServices &&
+                    mPackageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
                 try {
                     Slog.i(TAG, "TorchService");
                     ServiceManager.addService(Context.TORCH_SERVICE, new TorchService(context));
