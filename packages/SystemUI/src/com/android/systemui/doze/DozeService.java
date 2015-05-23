61f6da607ff8094c1ebb4311c56183ddcc271e60
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2015-05-23 00:05:45 -0400

    SystemUI: DozeService: Prevent NPE
    Prevent an npe when exiting ambient mode due to battery saver being
    enabled. The NPE only happens when not using the Accelerometer overlay.
    
    Change-Id: Ic5008c9d9e45ad88926af9b9d22357f568b2e9cc
    Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

--- packages/SystemUI/src/com/android/systemui/doze/DozeService.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeService.java
@@ -389,7 +389,9 @@ public class DozeService extends DreamService implements ProximitySensorManager.
     }
 
     private void finishNow() {
-        listenForSignalsSensor(false);
+        if (mUseAccelerometer)
+            listenForSignalsSensor(false);
+
         finish();
     }
 
