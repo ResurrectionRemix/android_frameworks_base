bb1c351ffc9c04b5f903f0a3206665476216decd
Kra1o5 <kra1o5x@gmail.com>
2015-05-25 12:22:48 +0200

    Buttons: Allow setting default long-press app switch behavior
    Enables the ability to assign different default actions to longpress on
    the app switch button.
    
    Change-Id: I6fa124d4ad97d1014f9f71052abf5c6081cf6ef0

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1880,16 +1880,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
         mPressOnAssistBehavior = KEY_ACTION_SEARCH;
         mLongPressOnAssistBehavior = KEY_ACTION_VOICE_SEARCH;
-
         mPressOnAppSwitchBehavior = KEY_ACTION_APP_SWITCH;
-
         mLongPressOnAppSwitchBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_longPressOnAppSwitchBehavior);
-
-        if (mLongPressOnAppSwitchBehavior == KEY_ACTION_NOTHING &&
-                (hasAppSwitch && !hasMenu)) {
-            mLongPressOnAppSwitchBehavior = KEY_ACTION_MENU;
-        }
 
         mLongPressOnHomeBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_longPressOnHomeBehavior);
