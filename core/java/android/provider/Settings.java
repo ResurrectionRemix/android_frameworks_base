88a196b69c40d8994ed7a8c9803a2fdf82a4b6d5
HazouPH <jgrharbers@gmail.com>
2015-06-07 20:58:52 +0200

    [1/2] Camera button support
    Add support for camera button
    
    Based on commit http://review.cyanogenmod.org/#/c/51487/
    
    This patch adds:
    - Use camera button as wake key
    - Use focus button as peek and wake key
    - Use camera button to launch (secure) camera
    
    Depends on:
    [2/2] Camera button support in packages/apps/Settings
    
    Change-Id: Ia515c04cca098bf0d20b077ebffc079ee4008f21

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4385,6 +4385,24 @@ public final class Settings {
         public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
 
         /**
+         * Whether to wake the screen with the camera key half-press.
+         * @hide
+         */
+        public static final String CAMERA_WAKE_SCREEN = "camera_wake_screen";
+
+        /**
+         * Whether or not to send device back to sleep if Camera button is released ("Peek")
+         * @hide
+         */
+        public static final String CAMERA_SLEEP_ON_RELEASE = "camera_sleep_on_release";
+
+        /**
+         * Whether to launch secure camera app when key is longpressed
+         * @hide
+         */
+        public static final String CAMERA_LAUNCH = "camera_launch";
+
+        /**
          * Whether the notification light will be allowed when in zen mode during downtime
          * @hide
          */
