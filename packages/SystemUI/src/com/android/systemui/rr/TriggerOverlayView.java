90432658e8bd2f24ac7b7efe000d09e02a709aef
riddle_hsu <riddle_hsu@htc.com>
2015-06-08 20:47:41 +0800

    RR rependencies

--- packages/SystemUI/src/com/android/systemui/rr/TriggerOverlayView.java
+++ packages/SystemUI/src/com/android/systemui/rr/TriggerOverlayView.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.systemui.benzo;
+package com.android.systemui.rr;
 
 import android.app.KeyguardManager;
 import android.content.Context;
