3b98a2fa31c41a09d4d905fbb9c883fe2bcbc8e7
Shuhao Wu <shuhao@shuhaowu.com>
2015-06-22 20:21:14 -0400

    Allow toggling animations off [1/2]
    Allow for toggling the animations that comes by default with Android L,
    such as the open app animation, or other transition animations. This
    effect is the same as when it goes into lower power mode, except nothing
    else is affected.
    
    A change in packages/app/Settings is also required.
    
    Change-Id: I9b19e043386898a75f1ff02941ecdddbf6116711
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3771,6 +3771,12 @@ public final class Settings {
         public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
 
         /**
+         * Disabled Transition animations of the android system
+         * @hide
+         */
+        public static final String DISABLE_TRANSITION_ANIMATIONS = "disable_transition_animations";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -659,9 +659,14 @@ public class WindowManagerService extends IWindowManager.Stub
         private final Uri mAnimationDurationScaleUri =
                 Settings.Global.getUriFor(Settings.Global.ANIMATOR_DURATION_SCALE);
 
+        private final Uri mDisableAnimationsUri =
+                Settings.System.getUriFor(Settings.System.DISABLE_TRANSITION_ANIMATIONS);
+
         public SettingsObserver() {
             super(new Handler());
             ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(mDisableAnimationsUri, false, this,
+                    UserHandle.USER_ALL);
             resolver.registerContentObserver(mDisplayInversionEnabledUri, false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(mWindowAnimationScaleUri, false, this,
@@ -680,6 +685,8 @@ public class WindowManagerService extends IWindowManager.Stub
 
             if (mDisplayInversionEnabledUri.equals(uri)) {
                 updateCircularDisplayMaskIfNeeded();
+            } else if (mDisableAnimationsUri.equals(uri))  {
+                updateAnimationsDisabledSetting(getAnimationsDisabledByUser());
             } else {
                 @UpdateAnimationScaleMode
                 final int mode;
@@ -941,6 +948,23 @@ public class WindowManagerService extends IWindowManager.Stub
         }, 0);
     }
 
+    private void updateAnimationsDisabledSetting(boolean enabled) {
+        synchronized (mWindowMap) {
+            if (mAnimationsDisabled != enabled) {
+                mAnimationsDisabled = enabled;
+                dispatchNewAnimatorScaleLocked(null);
+            }
+        }
+    }
+
+    private boolean getAnimationsDisabledByUser() {
+        return Settings.System.getIntForUser(
+                mContext.getContentResolver(),
+                Settings.System.DISABLE_TRANSITION_ANIMATIONS,
+                0,
+                mCurrentUserId) == 1;
+    }
+
     private WindowManagerService(Context context, InputManagerService inputManager,
             boolean haveInputMethods, boolean showBootMsgs, boolean onlyCore) {
         mContext = context;
@@ -985,15 +1009,10 @@ public class WindowManagerService extends IWindowManager.Stub
                 new PowerManagerInternal.LowPowerModeListener() {
             @Override
             public void onLowPowerModeChanged(boolean enabled) {
-                synchronized (mWindowMap) {
-                    if (mAnimationsDisabled != enabled && !mAllowAnimationsInLowPowerMode) {
-                        mAnimationsDisabled = enabled;
-                        dispatchNewAnimatorScaleLocked(null);
-                    }
-                }
+                updateAnimationsDisabledSetting(enabled);
             }
         });
-        mAnimationsDisabled = mPowerManagerInternal.getLowPowerModeEnabled();
+        mAnimationsDisabled = getAnimationsDisabledByUser() || mPowerManagerInternal.getLowPowerModeEnabled();
         mScreenFrozenLock = mPowerManager.newWakeLock(
                 PowerManager.PARTIAL_WAKE_LOCK, "SCREEN_FROZEN");
         mScreenFrozenLock.setReferenceCounted(false);
