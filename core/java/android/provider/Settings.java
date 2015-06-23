dab9b6fd573753ea598aeed17ab53ee63f9b9daa
Shuhao Wu <shuhao@shuhaowu.com>
2015-06-22 20:21:14 -0400

    Allow toggling animations off [1/2]
    @neobuddyy89: Rewrite code with below changes
    * Adapt for Android O
    * Move settings to Global with other animation settings
    * Fixed disabling logic
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I75cf9d742128228384cf761c63ef690ef76c4bcd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -15557,7 +15557,12 @@ public final class Settings {
         public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
 
         /**
+         * Disabled Transition animations of the android system
+         * @hide
+         */
+        public static final String DISABLE_TRANSITION_ANIMATIONS = "disable_transition_animations";
 
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -702,6 +702,8 @@ public class WindowManagerService extends IWindowManager.Stub
                 Settings.Global.getUriFor(Settings.Global.POLICY_CONTROL);
         private final Uri mPointerLocationUri =
                 Settings.System.getUriFor(Settings.System.POINTER_LOCATION);
+        private final Uri mDisableAnimationsUri =
+                Settings.Global.getUriFor(Settings.Global.DISABLE_TRANSITION_ANIMATIONS);
 
         public SettingsObserver() {
             super(new Handler());
@@ -718,6 +720,8 @@ public class WindowManagerService extends IWindowManager.Stub
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(mPolicyControlUri, false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(mPointerLocationUri, false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(mDisableAnimationsUri, false, this,
+                    UserHandle.USER_ALL);
         }
 
         @Override
@@ -739,6 +743,14 @@ public class WindowManagerService extends IWindowManager.Stub
             if (mPointerLocationUri.equals(uri)) {
                 updatePointerLocation();
                 return;
+            }
+
+            if (mDisableAnimationsUri.equals(uri))  {
+                mAnimationsForceDisabled = Settings.Global.getInt(
+                    mContext.getContentResolver(), Settings.Global.DISABLE_TRANSITION_ANIMATIONS, 0) != 0;
+                synchronized (mWindowMap) {
+                    dispatchNewAnimatorScaleLocked(null);
+                }
             }
 
             @UpdateAnimationScaleMode
@@ -793,6 +805,7 @@ public class WindowManagerService extends IWindowManager.Stub
     private float mAnimatorDurationScaleSetting = 1.0f;
     private boolean mAnimationsDisabled = false;
     boolean mPointerLocationEnabled = false;
+    private boolean mAnimationsForceDisabled = false;
 
     final InputManagerService mInputManager;
     final DisplayManagerInternal mDisplayManagerInternal;
@@ -3042,11 +3055,15 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     public float getWindowAnimationScaleLocked() {
-        return mAnimationsDisabled ? 0 : mWindowAnimationScaleSetting;
+        if (mAnimationsDisabled || mAnimationsForceDisabled)
+            return 0;
+        return mWindowAnimationScaleSetting;
     }
 
     public float getTransitionAnimationScaleLocked() {
-        return mAnimationsDisabled ? 0 : mTransitionAnimationScaleSetting;
+        if (mAnimationsDisabled || mAnimationsForceDisabled)
+            return 0;
+        return mTransitionAnimationScaleSetting;
     }
 
     @Override
@@ -3068,7 +3085,9 @@ public class WindowManagerService extends IWindowManager.Stub
     @Override
     public float getCurrentAnimatorScale() {
         synchronized (mGlobalLock) {
-            return mAnimationsDisabled ? 0 : mAnimatorDurationScaleSetting;
+            if (mAnimationsDisabled || mAnimationsForceDisabled)
+                return 0;
+            return mAnimatorDurationScaleSetting;
         }
     }
 
