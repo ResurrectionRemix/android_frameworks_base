8b26057b9299f5f313b5be759f082218a2e54080
westcripp <korukaltan@gmail.com>
2015-06-26 13:04:09 +0300

    Fix derp
    Change-Id: I8732b093880979b89e86358df8000c171341a887

--- core/java/android/util/DisplayMetrics.java
+++ core/java/android/util/DisplayMetrics.java
@@ -257,7 +257,6 @@ public class DisplayMetrics {
         noncompatScaledDensity = o.noncompatScaledDensity;
         noncompatXdpi = o.noncompatXdpi;
         noncompatYdpi = o.noncompatYdpi;
-        updateDensity();
     }
     
     public void setToDefaults() {
