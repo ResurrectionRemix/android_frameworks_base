5b6aa0c69aafd66090afe55af95bf350d20b9a5b
Roman Birg <roman@cyngn.com>
2015-07-08 22:51:10 -0700

    SystemUI: more keyguard visualizer tweaks
    - link and unlink in the background. this is an expensive operation and
      we should avoid running it on the main thread
    - remove leftover getSystemService call
    - keep track of whether QS is expanded to avoid starting visualizer when
      it is visible
    - prefer light vibrant color from palette
    - run palette generation async
    - don't apply keyguard bitmap while visualizer is shown
    
    Change-Id: I222a27df9438466423c63555041275e4bf153c64
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2478,13 +2478,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
         }
 
-        WallpaperManager wm = (WallpaperManager) mContext.getSystemService(
-                Context.WALLPAPER_SERVICE);
         boolean keyguardVisible = (mState != StatusBarState.SHADE);
         boolean visualizerVisible = mVisualizerEnabled && keyguardVisible
                 && (mMediaController != null);
 
-        if (backdropBitmap == null && (mMediaMetadata == null || visualizerVisible)) {
+        if (backdropBitmap == null && mMediaMetadata == null) {
             backdropBitmap = mKeyguardWallpaper;
         }
 
@@ -2833,6 +2831,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     public void setQsExpanded(boolean expanded) {
         mStatusBarWindowManager.setQsExpanded(expanded);
+        if (mVisualizerEnabled && mState != StatusBarState.SHADE) {
+            mBackdrop.setQsExpanded(expanded);
+        }
     }
 
     public boolean isGoingToNotificationShade() {
