527fa0887041e0e498d811982301dfd3139c9788
Jorge Ruesga <jorge@ruesga.com>
2015-07-10 18:29:51 +0200

    systemui: fix battery text level visibility
    Use present flag instead of plug-in flag to determine the battery present (only hide the
    text level view if the battery is not present)
    
    Change-Id: I8e20e107bbeeecdc846833dc80c1a2754d82a039
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1228,7 +1228,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
 
                 @Override
-                public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
+                public void onBatteryLevelChanged(boolean present, int level,
+                        boolean pluggedIn, boolean charging) {
                     // noop
                 }
 
