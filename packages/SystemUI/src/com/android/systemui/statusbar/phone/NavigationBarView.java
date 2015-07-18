730eb1b5d474c2db9fb70e2715ef60a27b739b6c
Daniel Koman <dankoman30@gmail.com>
2015-07-18 19:58:09 +0200

    SlimDim: don't dim navbar on lockscreen
    Change-Id: I0bd50d48c415be98937b265ca98814f92a6d298e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -224,14 +224,17 @@ public class NavigationBarView extends LinearLayout {
 
     public void onNavButtonTouched() {
         mHandler.removeCallbacks(mNavButtonDimmer);
+        final boolean keyguardProbablyEnabled =
+                (mDisabledFlags & View.STATUS_BAR_DISABLE_HOME) != 0;
         if (getNavButtons() != null) {
+            // restore alpha to previous state first
             if (mIsDim || mIsAnimating) {
                 mIsAnimating = false;
                 getNavButtons().clearAnimation();
                 mIsDim = false;
                 getNavButtons().setAlpha(mOriginalAlpha);
             }
-            if (mDimNavButtons) {
+            if (mDimNavButtons && !keyguardProbablyEnabled) {
                 mHandler.postDelayed(mNavButtonDimmer, mDimNavButtonsTimeout);
             }
         }
