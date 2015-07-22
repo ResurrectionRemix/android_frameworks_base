96676a289e5ccf68ca259cab720198c438a0d4a8
Steve Kondik <steve@cyngn.com>
2015-07-22 12:45:39 -0700

    systemui: Fix various issues with transparent bars
     * You current can't enable lights-out with transparency/translucency-
       the code is only half-way there but it's sequenced in a strange way
       and the translucent combo isn't implemented at all. Fix it.
     * Enabling a transparent navbar when the bar is vertical (landscape)
       has bugs that make the behavior unpredictable for some apps. We can
       still allow it, so add a new View flag to signal this.
    
    Change-Id: Iec98c960d6c13a294d8f9f0dd9f9e2fc1f8f9b52

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -344,6 +344,10 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
         notifyVerticalChangedListener(mVertical);
     }
 
+    public void setTransparencyAllowedWhenVertical(boolean allowed) {
+        mBarTransitions.setTransparencyAllowedWhenVertical(allowed);
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         initDownStates(event);
