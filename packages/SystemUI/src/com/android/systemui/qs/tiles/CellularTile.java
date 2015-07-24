644c909260c6022513a299ca8194315f58ee4b1a
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-07-23 22:01:33 -0600

    CellularTile: Fix secondary button doesn't work and change single cli…
    …ck behavior
    
    Single click: Enable or disable mobile data
    Secondary click: Show detail
    Long click: Mobile network settings
    
    Change-Id: Ie338f865f81fa501069cfd11a10c38a6ad71c616

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -90,7 +90,7 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
     @Override
     protected void handleClick() {
         if (mDataController.isMobileDataSupported()) {
-            showDetail(true);
+            mDataController.setMobileDataEnabled(!mDataController.isMobileDataEnabled());
         } else {
             mHost.startSettingsActivity(DATA_USAGE_SETTINGS);
         }
@@ -98,7 +98,15 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
 
     @Override
     protected void handleSecondaryClick() {
-        mHost.startSettingsActivity(new Intent(Settings.ACTION_DATA_ROAMING_SETTINGS));
+        if (mDataController.isMobileDataSupported()) {
+            showDetail(true);
+        } else {
+            if (mTelephonyManager.getDefault().getPhoneCount() > 1) {
+                mHost.startSettingsActivity(MOBILE_NETWORK_SETTINGS_MSIM);
+            } else {
+                mHost.startSettingsActivity(MOBILE_NETWORK_SETTINGS);
+            }
+        }
     }
 
     @Override
