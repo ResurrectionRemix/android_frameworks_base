af2f68d4b8d8e7908d5b82221abb3903b9d5981d
Akhil Narang <akhilnarang.1999@gmail.com>
2015-08-03 16:43:20 +0530

    Revert "AdBlock_2/3"
    This reverts commit df5de668c0ea0479d63c6a9e52926fee11c3a94b.
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4072,14 +4072,7 @@ public final class Settings {
          * Whether to show or hide search bar in recents view
          * @hide
          */
-        public static final String RECENTS_SEARCH_BAR = "recents_search_bar";
-
-        /**
-         * Whether to mute annoying notifications
-		 * Disable ads (HFM)
-		 * @hide
-		 */
-		public static final String HFM_DISABLE_ADS = "hfm_disable_ads";        
+        public static final String RECENTS_SEARCH_BAR = "recents_search_bar";        
         
          /**
          * Whether to show a fullscreen keyboard in landscape
