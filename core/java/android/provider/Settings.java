762577c8bcb08ed9f035e045671df9c6d2a26ede
Daniel Koman <dankoman30@gmail.com>
2015-08-14 20:12:11 -0400

    Proximity Speaker 2/3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2497,6 +2497,25 @@ public final class Settings {
         public static final String APPEND_FOR_LAST_AUDIBLE = "_last_audible";
 
         /**
+         * Whether the proximity sensor will adjust call to speaker
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER = "proximity_auto_speaker";
+        /**
+         * Time delay to activate speaker after proximity sensor triggered
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_DELAY = "proximity_auto_speaker_delay";
+
+        /**
+         * Whether the proximity sensor will adjust call to speaker,
+         * only while in call (not while ringing on outgoing call)
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_INCALL_ONLY =
+                "proximity_auto_speaker_incall_only";
+
+        /**
          * Persistent store for the static default system ringtone URI.
          * This setting is initialized within media scanner and read only afterwards.
          * It provides the persistent URI of default system ringtone.
