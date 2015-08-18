cf743b4f1030498bdf63a92cd83c00ed64e4155c
AdrianDC <radian.dc@gmail.com>
2015-08-18 19:33:18 +0200

    LEDs Brightness [2/2]: Lights notifications brightness support
    Implement the support of an overall brightness control for the LEDs.
    
    The setting is deactivated by default
    and will be ignored by the unimplemented phones.
    Current LibLights will simply not use the new variable.
    
    Changes includes :
      frameworks/base
      packages/Apps/Settings
    
    Change-Id: I1c0de01b1c6a2a2cf1432028a2e69f90b2373b2c
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3001,6 +3001,13 @@ public final class Settings {
         public static final String SHOW_WEB_SUGGESTIONS = "show_web_suggestions";
 
         /**
+         * Contains the notifications light maximum brightness to use.
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL =
+                "notification_light_brightness_level";
+
+        /**
          * Whether to allow notifications with the screen on or DayDreams.
          * The value is boolean (1 or 0). Default will always be false.
          * @hide
@@ -4811,6 +4818,7 @@ public final class Settings {
             LIVE_DISPLAY_HINTED,
             NONE_IS_SILENT,
             ALLOW_LIGHTS,
+            NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL,
             NOTIFICATION_LIGHT_SCREEN_ON
         };
 
