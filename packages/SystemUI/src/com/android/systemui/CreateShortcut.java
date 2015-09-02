7016679c397db803ca806e8ec89125202f3b0fcb
LorDClockaN <davor@losinj.com>
2015-09-02 22:24:53 +0200

    SlimShortcuts: Restart SystemUI
    PS2: commit name fix
    
    Change-Id: I4c32e7bec6a33730a57d87985d49f49ed9dd7286

--- packages/SystemUI/src/com/android/systemui/CreateShortcut.java
+++ packages/SystemUI/src/com/android/systemui/CreateShortcut.java
@@ -121,6 +121,8 @@ public class CreateShortcut extends LauncherActivity {
             return R.drawable.ic_qs_reboot_recovery;
         } else if (c.equals("VolumePanel")) {
             return R.drawable.ic_qs_volume;
+        } else if (c.equals("RestartUI")) {
+            return R.drawable.ic_qs_systemui_restart;
         } else if (c.equals("ChamberOfSecrets")) {
             return R.drawable.ic_qs_slim_action;
         } else {
