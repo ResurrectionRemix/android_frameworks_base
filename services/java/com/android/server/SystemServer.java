fb8f07c42eb0b0c92fd98b9308596c7d4d454c15
Jorge Ruesga <jorge@ruesga.com>
2015-09-06 11:12:03 +0200

    base: report wtf for external cm services
    Now that system relies in more functionallity from cmsdk, its services are now required
    on boot. Make system crash at boot it one of this service isn't available.
    
    Change-Id: I0f683ed70b298eb52141c9d048505797fa5a623b
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1120,7 +1120,7 @@ public final class SystemServer {
                 Slog.i(TAG, service);
                 mSystemServiceManager.startService(service);
             } catch (Throwable e) {
-                Slog.e(TAG, "Failure starting " + service , e);
+                reportWtf("starting " + service , e);
             }
         }
 
