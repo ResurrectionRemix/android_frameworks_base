aa5c43e86441b812da4b83fd85bec848b4cacb0a
Connor Tumbleson <connor.tumbleson@gmail.com>
2015-10-06 07:49:15 -0500

    return from functions early that modify apk for compat purposes
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    
    Conflicts:
    	tools/aapt/ResourceTable.cpp
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- tools/aapt/AaptConfig.cpp
+++ tools/aapt/AaptConfig.cpp
@@ -255,6 +255,7 @@ bool parseCommaSeparatedList(const String8& str, std::set<ConfigDescription>* ou
 }
 
 void applyVersionForCompatibility(ConfigDescription* config) {
+    return;
     if (config == NULL) {
         return;
     }
