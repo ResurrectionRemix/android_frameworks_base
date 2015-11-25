6bb1fe4f233a12dc9c12f38f2ca5f942bedc21f7
westcripp <korukaltan@gmail.com>
2015-11-25 15:11:16 +0200

    Revert "Update default tiles"
    This reverts commit b53787ef571b221af00020196ddb1254050260b0.

--- packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
@@ -129,18 +129,16 @@ public class QSTileView extends ViewGroup {
         Log.d(TAG, "recreateLabel() called with " + "");
         CharSequence labelText = null;
         CharSequence labelDescription = null;
-        if (mDualLabel != null) {
-            labelText = mDualLabel.getText();
-            if (mLabel != null) {
-                labelDescription = mDualLabel.getContentDescription();
-            }
-            removeView(mDualLabel);
-            mDualLabel = null;
-        }
         if (mLabel != null) {
             labelText = mLabel.getText();
             removeView(mLabel);
             mLabel = null;
+        }
+        if (mDualLabel != null) {
+            labelText = mDualLabel.getText();
+            labelDescription = mDualLabel.getContentDescription();
+            removeView(mDualLabel);
+            mDualLabel = null;
         }
         final Resources res = mContext.getResources();
         if (mDual) {
