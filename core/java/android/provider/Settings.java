0f04cdb628aecda76239e3eb1cbab537f9e52f60
Varun <date.varun123@gmail.com>
2015-11-30 20:45:00 +0530

    Base:Add Missing Traffic Colors Setting

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3715,6 +3715,13 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
+	 /**
+         * Network stats Color style
+         * @hide
+         */
+
+	public static final String NETWORK_TRAFFIC_COLOR = "network_traffic_color";
+
         /**
          * Alternative recent apps integration using OmniSwitch
          * @hide
