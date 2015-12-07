4c0f7f5ba1198061c15a4fd7519a86b0e18a0f11
Varun <date.varun123@gmail.com>
2015-12-07 17:43:06 +0530

    Base: Fix Conflicts after Adding Pie
    Signed-off-by: Varun <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -66,9 +66,9 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_START_ASSIST               = 23 << MSG_SHIFT;
     private static final int MSG_CAMERA_LAUNCH_GESTURE      = 24 << MSG_SHIFT;
     private static final int MSG_SET_AUTOROTATE_STATUS      = 25 << MSG_SHIFT;
-    private static final int MSG_TOGGLE_LAST_APP            = 25 << MSG_SHIFT;
-    private static final int MSG_TOGGLE_KILL_APP            = 26 << MSG_SHIFT;
-    private static final int MSG_TOGGLE_SCREENSHOT          = 27 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_LAST_APP            = 26 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_KILL_APP            = 27 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_SCREENSHOT          = 28 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -119,6 +119,7 @@ public class CommandQueue extends IStatusBar.Stub {
         public void setAutoRotate(boolean enabled);
         public void toggleLastApp();
         public void toggleKillApp();
+	 public void toggleScreenshot();
         public void toggleOrientationListener(boolean enable);
     }
 
@@ -460,6 +461,7 @@ public class CommandQueue extends IStatusBar.Stub {
                     break;
                 case MSG_SET_AUTOROTATE_STATUS:
                     mCallbacks.setAutoRotate(msg.arg1 != 0);
+		    break;	
                 case MSG_TOGGLE_LAST_APP:
                     mCallbacks.toggleLastApp();
                     break;
