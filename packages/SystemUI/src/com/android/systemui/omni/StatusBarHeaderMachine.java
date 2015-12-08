fdc96df47f3d788a3b5dac049b06637b5edc02be
Randall Rushing <randall.rushing@gmail.com>
2015-12-08 00:14:38 -0800

    statusbar: fix poly/original contextual header drawable style change
    Change-Id: I64a5c9255adde89b9a270e49786f06bf7a731ce0

--- packages/SystemUI/src/com/android/systemui/omni/StatusBarHeaderMachine.java
+++ packages/SystemUI/src/com/android/systemui/omni/StatusBarHeaderMachine.java
@@ -107,6 +107,7 @@ public class StatusBarHeaderMachine {
 
         @Override
         public void onChange(boolean selfChange) {
+            doUpdateStatusHeaderObservers(true); // simplest way to update poly headers without clobbering API
             updateEnablement();
         }
     }
