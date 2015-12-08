9dddd3c5a3f751a6ed5b21d68e279e18c7a88d98
Varun <date.varun123@gmail.com>
2015-12-08 22:24:31 +0530

    Revert "SystemUI:Add Missing Parts for CAF Task Manager"
    This reverts commit 23741080e5fb15d8f26f293ffe09cb4a5d846b83.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1550,12 +1550,6 @@ public class NotificationPanelView extends PanelView implements
         }
     }
 
-  private void cancelAnimation() {
-        if (mQsExpansionAnimator != null) {
-            mQsExpansionAnimator.cancel();
-        }
-    }
-
     @Override
     protected boolean isScrolledToBottom() {
         if (!isInSettings()) {
