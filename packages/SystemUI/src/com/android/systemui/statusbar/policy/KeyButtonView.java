f417a6b39c0259fbb0d5d91e601583771a4775f6
Carlo Savignano <carlosavignano@aospa.co>
2015-12-08 20:06:26 +0800

    base: Introduce new navigation bar key event source
    * When generating virtual navigation bar KeyEvent, set
      a specific source fort it. There is no precise way to
      recognize if the KeyEvent is coming from KeyButtonView.
      Another way is passing another flag.
    
    Change-Id: I243c9f91afcbf4c430e2d526e8c814a5a7975e51
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -324,7 +324,7 @@ public class KeyButtonView extends ImageView implements ButtonInterface {
         final KeyEvent ev = new KeyEvent(mDownTime, when, action, mCode, repeatCount,
                 0, KeyCharacterMap.VIRTUAL_KEYBOARD, 0,
                 flags,
-                InputDevice.SOURCE_KEYBOARD);
+                InputDevice.SOURCE_NAVIGATION_BAR);
         InputManager.getInstance().injectInputEvent(ev,
                 InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
     }
