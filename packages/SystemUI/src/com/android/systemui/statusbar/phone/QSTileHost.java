9e5d286caeb92deb9e7ef2b9c85fb3aae9fe2506
Danesh M <daneshm90@gmail.com>
2015-12-14 11:18:15 -0800

    SystemUI : Port LiveDisplay tile from 12.1
    Change-Id: I2d37b2023dc1a5474cd47cc36709433a5b4fefe5
    Signed-off-by: Varun <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -50,6 +50,7 @@ import com.android.systemui.qs.tiles.EditTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
+import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.PerfProfileTile;
@@ -354,6 +355,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("screen_timeout")) return new ScreenTimeoutTile(this);
         else if (tileSpec.equals("performance")) return new PerfProfileTile(this);
         else if (tileSpec.equals("ambient_display")) return new AmbientDisplayTile(this);
+        else if (tileSpec.equals("live_display")) return new LiveDisplayTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
@@ -434,6 +436,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("screen_timeout")) return R.string.quick_settings_screen_timeout_detail_title;
         else if (spec.equals("performance")) return R.string.qs_tile_performance;
         else if (spec.equals("ambient_display")) return R.string.quick_settings_ambient_display_label;
+        else if (spec.equals("live_display")) return R.string.live_display_title;
         return 0;
     }
 
