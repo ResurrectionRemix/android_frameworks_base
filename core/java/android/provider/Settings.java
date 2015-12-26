412c025a0e431a5ea2db1dd33cd56bc84ae55bc1
GWX <kagwind@gmail.com>
2015-12-26 17:39:08 +0100

    Flip to Mute/Reject Call [1/3]
    saving flip action value to Setings container

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5553,6 +5553,14 @@ public final class Settings {
             Secure.WIFI_WATCHDOG_PING_TIMEOUT_MS;
 
         /**
+         * Whether flip action during incomming call should mute or dismiss
+         * the call (mute = 0, dismiss = 1, nothing = 2 (default))
+         *
+         * @hide
+         */
+        public static final String CALL_FLIP_ACTION_KEY = "call_flip_action_key";
+
+        /**
          * An app can use this method to check if it is currently allowed to write or modify system
          * settings. In order to gain write access to the system settings, an app must declare the
          * {@link android.Manifest.permission#WRITE_SETTINGS} permission in its manifest. If it is
