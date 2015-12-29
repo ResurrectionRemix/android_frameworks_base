cf60006e9b6975f960d1718fad890be36b9bd909
mochangming <mochangming@xiaomi.com>
2015-12-29 13:55:24 +0800

    Animate backlight quickly when display state request to change or power mode changed
      * mAppliedDimming should be false when power request policy is not dim.
      * mAppliedLowPower should be false when low power mode is disabled.
      * Backlight brightness changes quickly.
    
    Change-Id: I44bf9c216554f8dce5abd0520f198824df5e8dcf
    Signed-off-by: mochangming <mochangming@xiaomi.com>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -817,6 +817,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 slowChange = false;
             }
             mAppliedDimming = true;
+        } else if (mAppliedDimming) {
+            slowChange = false;
+            mAppliedDimming = false;
         }
 
         // If low power mode is enabled, cut the brightness level by half
@@ -829,6 +832,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 slowChange = false;
             }
             mAppliedLowPower = true;
+        } else if (mAppliedLowPower) {
+            slowChange = false;
+            mAppliedLowPower = false;
         }
 
         // Animate the screen brightness when the screen is on or dozing.
