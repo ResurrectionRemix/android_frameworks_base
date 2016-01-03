03c7e20a2c462f37d248bc9d8214e388813e10d7
LorDClockaN <davor@losinj.com>
2016-01-03 20:00:11 +0100

    FWB: Add clock/date in recents while in fullscreen mode (1/2)
    Change-Id: I9ecc5890269997453056dc3a41b583022db7bd6a
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4478,6 +4478,18 @@ public final class Settings {
 
        // PIE COLORS EVERYWHERE! //
 
+	/**
+         * Whether to display clock while recents are in full screen
+         * @hide
+         */
+        public static final String RECENTS_FULL_SCREEN_CLOCK = "recents_full_screen_clock";
+
+        /**
+         * Whether to display clock while recents are in full screen
+         * @hide
+         */
+        public static final String RECENTS_FULL_SCREEN_DATE = "recents_full_screen_date";
+
         /**
          * @hide
          */
