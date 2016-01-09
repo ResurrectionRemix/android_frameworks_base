6dc8f2cf9ceb0dddaefdd1b273cd7885495d74d5
Varun <date.varun123@gmail.com>
2016-01-09 20:30:13 +0530

    QS:Add App SideBar Tile [1/2] Credits to 0xD34D for implementing app sidebar
    original App Sidebar Commit here : https://github.com/ResurrectionRemix/android_frameworks_base/commit/045b22302666ee5e4c8c83e1ecd74affd9c729ea
    
    Signed-off-by: Varun <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.AppCircleBarTile;
+import com.android.systemui.qs.tiles.AppsidebarTile;
 import com.android.systemui.qs.tiles.KernelAdiutorTile;
 import com.android.systemui.qs.tiles.NavBarTile;
 import com.android.systemui.qs.tiles.PieTile;
@@ -404,6 +405,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
 	else if (tileSpec.equals("kernel_adiutor")) return new KernelAdiutorTile(this);
 	else if (tileSpec.equals("screenrecord")) return new ScreenrecordTile(this);
 	else if (tileSpec.equals("pie")) return new PieTile(this);
+	else if (tileSpec.equals("appsidebar")) return new AppsidebarTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
@@ -505,6 +507,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
 	else if (spec.equals("kernel_adiutor")) return R.string.quick_settings_kernel_title;
 	else if (spec.equals("screenrecord")) return R.string.quick_settings_screenrecord;
 	else if (spec.equals("pie")) return R.string.quick_settings_pie;
+	else if (spec.equals("appsidebar")) return R.string.quick_settings_app_sidebar;
         return 0;
     }
 
@@ -548,6 +551,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
 	else if (spec.equals("kernel_adiutor")) return R.drawable.ic_qs_kernel_adiutor;	
 	else if (spec.equals("screenrecord")) return R.drawable.ic_qs_screenrecord;	
 	else if (spec.equals("pie")) return R.drawable.ic_qs_pie_on;	
+	else if (spec.equals("appsidebar")) return R.drawable.ic_qs_appsidebar_on;
         return 0;
     }
 
