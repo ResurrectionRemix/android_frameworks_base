2807904bae56ba6d6a84adf6a5ec3ddca6c5da5f
Varun <date.varun123@gmail.com>
2016-01-09 15:09:47 +0530

    SystemUI: Add Five New Tiles[1/2]
    New Tiles:
    -Navbar Tile
    -App Circle Sidebar Tile
    -Kernel Audiutor Tile
    -Pie Tile
    -Screen Record Tile
    
    Ported From Lollipop
    adapted For Marshmallow By Varun Date (varund7726)
    
    Credits:
    
    Thanks To Alex Cruz (Mazda) for Navbar and AppcircleBar Tile
    Original Navbar commit here : http://gerrit.dirtyunicorns.com/#/c/16762/
    Original RR commit here : https://github.com/ResurrectionRemix/android_frameworks_base/commit/95d9f1e15196f6008c00e2edcf2308a5cd502e47
    
    Thanks to rogersb11 For The Kernel Auditor Tile
    Original RR Commit Here :
    https://github.com/ResurrectionRemix/android_frameworks_base/commit/8c80e13d6e90d36769334ca080ef9ff2e6b3a83f
    
    Thanks to Nico60 for Pie Tile
    Original RR Commit Here :
    https://github.com/ResurrectionRemix/android_frameworks_base/commit/eb2af9a4ba2023e3e45f1ecbe5984b985fea094a
    
    Thanks to eyosen For ScreenRecord Tile
    Original RR Commit Here:
    https://github.com/ResurrectionRemix/android_frameworks_base/commit/3ba406b0376c84ff65651fe299254b641b708ca2
    
    Signed-off-by: Varun <date.varun123@gmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/NavBarTile.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2015 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.content.ComponentName;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ListView;
+
+import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
+import com.android.systemui.qs.QSTileView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import com.android.internal.logging.MetricsLogger;
+import cyanogenmod.providers.CMSettings;
+
+public class NavBarTile extends QSTile<QSTile.BooleanState> {
+    private boolean mListening;
+    private NavbarObserver mObserver;
+private static final Intent NAVBAR_Settings = new Intent().setComponent(new ComponentName(
+            "com.android.settings", "com.android.settings.Settings$ButtonSettingsActivity"));
+
+    public NavBarTile(Host host) {
+        super(host);
+        mObserver = new NavbarObserver(mHandler);
+    }
+
+    @Override
+    protected BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        toggleState();
+        refreshState();
+    }
+
+   @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.DONT_TRACK_ME_BRO;
+    }
+
+     @Override
+    protected void handleSecondaryClick() {
+	mHost.startActivityDismissingKeyguard(NAVBAR_Settings);
+    }
+
+    @Override
+    public void handleLongClick() {
+	mHost.startActivityDismissingKeyguard(NAVBAR_Settings);
+    }
+
+ protected void toggleState() {
+
+	CMSettings.Secure.putInt(mContext.getContentResolver(),
+                    CMSettings.Secure.DEV_FORCE_SHOW_NAVBAR, !HwkeysDisabled() ? 1 : 0);
+         Settings.System.putInt(mContext.getContentResolver(),
+                        Settings.System.NAVIGATION_BAR_SHOW, !navbarEnabled() ? 1 : 0);
+    }
+
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.visible = true;
+	if (navbarEnabled()) {
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_navbar_on);
+        state.label = mContext.getString(R.string.quick_settings_navbar_on);
+	} else {
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_navbar_off);
+	state.label = mContext.getString(R.string.quick_settings_navbar_off);
+	    }
+	}
+
+    private boolean navbarEnabled() {
+
+        return Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.NAVIGATION_BAR_SHOW, 1) == 1;
+
+    }
+
+	private boolean HwkeysDisabled()
+	{
+	return CMSettings.Secure.getInt(mContext.getContentResolver(),
+                CMSettings.Secure.DEV_FORCE_SHOW_NAVBAR, 1) == 1;
+	}
+
+    @Override
+    public void setListening(boolean listening) {
+        if (mListening == listening) return;
+        mListening = listening;
+        if (listening) {
+            mObserver.startObserving();
+        } else {
+            mObserver.endObserving();
+        }
+    }
+
+    private class NavbarObserver extends ContentObserver {
+        public NavbarObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            refreshState();
+        }
+
+        public void startObserving() {
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.NAVIGATION_BAR_SHOW),
+                    false, this);
+        }
+
+        public void endObserving() {
+            mContext.getContentResolver().unregisterContentObserver(this);
+        }
+    }
+}
+
