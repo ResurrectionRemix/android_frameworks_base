e7dfd1a5e1f4c1982558c6cbb227c600d64d3f0d
Nico60 <ndhouailly@gmail.com>
2016-01-10 14:55:13 +0100

    Improve Navbar Tile

--- core/java/com/android/internal/logging/MetricsConstants.java
+++ core/java/com/android/internal/logging/MetricsConstants.java
@@ -263,6 +263,7 @@ public interface MetricsConstants {
     public static final int TUNER_DEMO_MODE_ON = 236;
     public static final int TUNER_BATTERY_PERCENTAGE = 237;
     public static final int FUELGAUGE_INACTIVE_APPS = 238;
+    public static final int QS_NAVBAR_TOGGLE = 239;
 
 
 
