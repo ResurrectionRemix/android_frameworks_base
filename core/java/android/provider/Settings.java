d3762a5eca9c7bc58db96e63bd8c17c82ae2d5e0
beanstown106 <nbenis106@gmail.com>
2016-01-14 05:56:50 -0500

    Fingerprint authentication vibration [1/2]
    @xyyx: adapted to 8.1
    
    Change-Id: Ieed52af28462da7507bcae01e13744f7dc5ed32c
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3993,6 +3993,13 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_TICKER = "status_bar_show_ticker";
 
         /**
+         * whether to enable or disable vibration on succesful fingerprint auth
+         *
+         * @hide
+         */
+        public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- services/core/java/com/android/server/fingerprint/ClientMonitor.java
+++ services/core/java/com/android/server/fingerprint/ClientMonitor.java
@@ -23,8 +23,10 @@ import android.hardware.fingerprint.FingerprintManager;
 import android.hardware.fingerprint.IFingerprintServiceReceiver;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.os.UserHandle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.provider.Settings;
 import android.util.Slog;
 
 import java.util.NoSuchElementException;
@@ -222,7 +224,10 @@ public abstract class ClientMonitor implements IBinder.DeathRecipient {
 
     public final void vibrateSuccess() {
         Vibrator vibrator = mContext.getSystemService(Vibrator.class);
-        if (vibrator != null) {
+
+        boolean FingerprintVib = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.FINGERPRINT_SUCCESS_VIB, 1, UserHandle.USER_CURRENT) == 1;
+        if (vibrator != null && FingerprintVib) {
             vibrator.vibrate(mSuccessVibrationEffect);
         }
     }
