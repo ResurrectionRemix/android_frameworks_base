6c1f6773a6163cb6a561206fb196f4b9b3f18093
Varun <date.varun123@gmail.com>
2016-01-18 23:14:09 +0530

    Fix Derp

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -563,7 +563,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
 	else if (spec.equals("appsidebar")) return R.drawable.ic_qs_appsidebar_on;
 	else if (spec.equals("restartui")) return R.drawable.ic_qs_systemui_restart;
 	else if (spec.equals("gesture_anywhere")) return R.drawable.ic_qs_gestures_on;
-        else if (spec.equals("heads_up")) return R.drawable.ic_qs_battery_saver_on;
+        else if (spec.equals("battery_saver")) return R.drawable.ic_qs_battery_saver_on;
         return 0;
     }
 
