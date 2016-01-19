33dc64c50999016d248cef224abf814d9e1f2d07
Alex Cruz <mazdarider23@gmail.com>
2016-01-19 18:27:57 -0500

    Immersive Recents [1/2] This adds on to previous mod called FULL SCREEN RECENTS but now adds options to show recents with statusbar only or navbar only as well as fullscreen.
    - Renamed the mod to immersive recents to avoid confusion moving forward
    
    0 - Default AOSP
    1 - Fullscreen
    2 - Statusbar only
    3 - NavBar only

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4962,6 +4962,16 @@ public final class Settings {
         public static final String KEYGUARD_TOGGLE_TORCH = "keyguard_toggle_torch";
 
         /**
+         * Immersive recents options
+         *
+         * 0 = Default AOSP look
+         * 1 = Statusbar only
+         * 2 = Navbar only
+         * 3 = Fullscreen
+         */ 
+        public static final String IMMERSIVE_RECENTS = "immersive_recents";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
