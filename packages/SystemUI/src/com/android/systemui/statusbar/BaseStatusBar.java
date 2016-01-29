a4b08509cfa378b1044d1696a5333acfc6f6eec5
xyyx <xyyx@mail.ru>
2016-01-29 11:22:41 +0900

    SystemUI: HeadsUp whitelists (2/3)
    All notifications from app in whitelists will be posted to headsup.
    
    Change-Id: I12a5c506b45893a3a534b2262c4d7e36a83ee671

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -347,6 +347,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     private ArrayList<String> mDndList = new ArrayList<String>();
     private ArrayList<String> mBlacklist = new ArrayList<String>();
+    private ArrayList<String> mWhitelist = new ArrayList<String>();
 
     @Override  // NotificationData.Environment
     public boolean isDeviceProvisioned() {
@@ -392,6 +393,8 @@ public abstract class BaseStatusBar extends SystemUI implements
                     CMSettings.System.HEADS_UP_CUSTOM_VALUES), false, this);
             resolver.registerContentObserver(CMSettings.System.getUriFor(
                     CMSettings.System.HEADS_UP_BLACKLIST_VALUES), false, this);
+            resolver.registerContentObserver(CMSettings.System.getUriFor(
+                    CMSettings.System.HEADS_UP_WHITELIST_VALUES), false, this);
             update();
         }
 
@@ -407,8 +410,11 @@ public abstract class BaseStatusBar extends SystemUI implements
                     CMSettings.System.HEADS_UP_CUSTOM_VALUES);
             final String blackString = CMSettings.System.getString(resolver,
                     CMSettings.System.HEADS_UP_BLACKLIST_VALUES);
+            final String whiteString = CMSettings.System.getString(resolver,
+                    CMSettings.System.HEADS_UP_WHITELIST_VALUES);
             splitAndAddToArrayList(mDndList, dndString, "\\|");
             splitAndAddToArrayList(mBlacklist, blackString, "\\|");
+            splitAndAddToArrayList(mWhitelist, whiteString, "\\|");
         }
     };
 
@@ -2497,6 +2503,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         Notification notification = sbn.getNotification();
         // some predicates to make the boolean logic legible
+        boolean whiteListed = isPackageWhitelisted(sbn.getPackageName());
         boolean isNoisy = (notification.defaults & Notification.DEFAULT_SOUND) != 0
                 || (notification.defaults & Notification.DEFAULT_VIBRATE) != 0
                 || notification.sound != null
@@ -2515,7 +2522,8 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         boolean isIMEShowing = inputMethodManager.isImeShowing();
 
-        boolean interrupt = (isFullscreen || (isHighPriority && (isNoisy || hasTicker)))
+        boolean interrupt = (whiteListed
+                || (isFullscreen || (isHighPriority && (isNoisy || hasTicker))))
                 && isAllowed
                 && !accessibilityForcesLaunch
                 && !justLaunchedFullScreenIntent
@@ -2549,6 +2557,10 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     private boolean isPackageBlacklisted(String packageName) {
         return mBlacklist.contains(packageName);
+    }
+
+    private boolean isPackageWhitelisted(String packageName) {
+        return mWhitelist.contains(packageName);
     }
 
     private void splitAndAddToArrayList(ArrayList<String> arrayList,
