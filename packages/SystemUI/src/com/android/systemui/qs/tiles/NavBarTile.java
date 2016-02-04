0d4713a673d898677699ddee8e16e10f519fe47a
Varun <date.varun123@gmail.com>
2016-02-04 23:10:31 +0530

    NavbarTile: Fix Copyright

--- packages/SystemUI/src/com/android/systemui/qs/tiles/NavBarTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/NavBarTile.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 RR 
+ * Copyright (C) 2016 The Dirty Unicorns Project
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
