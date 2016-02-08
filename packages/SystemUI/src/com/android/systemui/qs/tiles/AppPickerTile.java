8e0961831c59e68a2c8e106a17811276f41e5166
LorDClockaN <davor@losinj.com>
2016-02-08 15:08:02 +0100

    FWB: App picker tile (1/2)
    RR Edits: Use Icon from M
    
    Change-Id: Ic7a8729749ecdcbbe4e7b5744d64b758645bb34a
    Signed-off-by: Varun Date <date.varun123@gmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AppPickerTile.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2016 Resurrection Remix
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.ComponentName;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ListView;
+
+import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
+import com.android.systemui.qs.QSTileView;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+
+public class AppPickerTile extends QSTile<QSTile.BooleanState> {
+    private boolean mListening;
+
+    private static final Intent APP_PICKER = new Intent().setComponent(new ComponentName(
+            "com.android.systemui", "com.android.systemui.rr.apppicker.AppPickerActivity"));
+
+    public AppPickerTile(Host host) {
+        super(host);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.QUICK_SETTINGS;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.qs_app_picker_title);
+    }
+
+    @Override
+    protected void handleClick() {
+        mHost.collapsePanels();
+        startAppPicker();
+        refreshState();
+    }
+
+    @Override
+    protected void handleSecondaryClick() {
+	    handleClick();
+    }
+
+    @Override
+    public void handleLongClick() {
+        handleClick();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    protected void startAppPicker() {
+        mHost.startActivityDismissingKeyguard(APP_PICKER);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_app_picker);
+        state.label = mContext.getString(R.string.qs_app_picker_title);
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        if (mListening == listening) return;
+        mListening = listening;
+    }
+}
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -42,6 +42,7 @@ import com.android.systemui.qs.external.TileServices;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.BatteryTile;
+import com.android.systemui.qs.tiles.AppPickerTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.CastTile;
@@ -465,6 +466,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         //else if (tileSpec.equals("expanded_desktop")) return new ExpandedDesktopTile(this);
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(this);
         else if (tileSpec.equals("music")) return new MusicTile(this);
+        else if (tileSpec.equals("app_picker")) return new AppPickerTile(this);
         else if (tileSpec.equals("ime")) return new ImeTile(this);
         else if (tileSpec.equals("configurations")) return new RRTile(this);
         else if (tileSpec.equals("caffeine")) return new CaffeineTile(this);
