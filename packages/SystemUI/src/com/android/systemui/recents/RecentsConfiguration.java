de3ed658f1de98428f0aca913a7b72aea51474e7
LorDClockaN <davor@losinj.com>
2016-02-09 14:58:20 +0100

    Fix build by removing part of the old patch
    Change-Id: I94f5e243e4a402e4cbe1eb12d279db2c4b83988c

--- packages/SystemUI/src/com/android/systemui/recents/RecentsConfiguration.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsConfiguration.java
@@ -292,8 +292,6 @@ public class RecentsConfiguration {
         altTabKeyDelay = res.getInteger(R.integer.recents_alt_tab_key_delay);
         fakeShadows = res.getBoolean(R.bool.config_recents_fake_shadows);
         svelteLevel = res.getInteger(R.integer.recents_svelte_level);
-
-        updateShowSearch(context);
     }
 
     public boolean updateShowSearch(Context context) {
@@ -316,14 +314,6 @@ public class RecentsConfiguration {
         lockToAppEnabled = ssp.getSystemSetting(context,
                 Settings.System.LOCK_TO_APP_ENABLED) != 0;
         multiStackEnabled = "true".equals(ssp.getSystemProperty("persist.sys.debug.multi_window"));
-        updateShowSearch(context);
-    }
-
-    private void updateShowSearch(Context context) {
-        boolean showSearchBar = CMSettings.System.getIntForUser(context.getContentResolver(),
-                CMSettings.System.RECENTS_SHOW_SEARCH_BAR, 1, UserHandle.USER_CURRENT) == 1;
-        searchBarSpaceHeightPx = showSearchBar ? context.getResources().getDimensionPixelSize(
-                R.dimen.recents_search_bar_space_height): 0;
     }
 
     /** Called when the configuration has changed, and we want to reset any configuration specific
