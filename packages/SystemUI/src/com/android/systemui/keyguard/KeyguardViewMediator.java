0b51a2d713c08bbd5d64f85b5713b0251dd5760f
Roman Birg <roman@cyngn.com>
2016-02-08 18:56:33 -0800

    SystemUI: respect profile after unlocking SIM PIN
    We should hide the keyguard after the user has input their SIM PIN if
    the profile (or ourselves) say so.
    
    Ref: CYNGNOS-1930
    
    Change-Id: I6cb1478eebd4442346c4c0970729fbf7450b156c
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -517,7 +517,8 @@ public class KeyguardViewMediator extends SystemUI {
                     break;
                 case READY:
                     synchronized (this) {
-                        if (mInternallyDisabled && !mUpdateMonitor.isSimPinSecure()) {
+                        if ((mInternallyDisabled || isProfileDisablingKeyguard())
+                                && !mUpdateMonitor.isSimPinSecure()) {
                             hideLocked();
                         } else if (mShowing) {
                             resetStateLocked();
