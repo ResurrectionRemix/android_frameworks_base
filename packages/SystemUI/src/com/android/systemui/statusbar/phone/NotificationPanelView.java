960b875c31cff7453c5df6afba64b7ee5e84b79b
d34d <clark@cyngn.com>
2016-02-09 15:12:05 -0800

    SysUI: Show LLS when state is SHADE_LOCKED
    Continue to show the LLS when the status bar state is SHADE_LOCKED
    
    Change-Id: I3f676bec9a036ea629e47f600f58d53d3cfa079d
    TICKET: CYNGNOS-1964

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1159,7 +1159,8 @@ public class NotificationPanelView extends PanelView implements
                 mAfforanceHelper.updatePreviews();
             }
         }
-        if (keyguardShowing) {
+        if (statusBarState == StatusBarState.KEYGUARD ||
+                statusBarState == StatusBarState.SHADE_LOCKED) {
             updateDozingVisibilities(false /* animate */);
             if (mThirdPartyKeyguardViewComponent != null) {
                 if (mKeyguardExternalView == null) {
