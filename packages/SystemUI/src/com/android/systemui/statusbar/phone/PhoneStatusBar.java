3885f4c9482808556c1e1102f4ea8542703bc7f1
d34d <clark@cyngn.com>
2016-02-09 14:59:44 -0800

    SysUI: Don't show LS wallpaper if LLS is available
    If the user has a live lock screen enabled we should not show the
    static lock screen wallpaper.
    
    Change-Id: I7e509924b49c8acbc31d076f251f470da1eed8a6
    TICKET: CYNGNOS-1963
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3076,11 +3076,6 @@ private final View.OnClickListener mKillClickListener = new View.OnClickListener
             // might still be null
         }
 
-        // apply user lockscreen image
-        if (mMediaMetadata == null && backdropBitmap == null) {
-            backdropBitmap = mKeyguardWallpaper;
-        }
-
         // HACK: Consider keyguard as visible if showing sim pin security screen
         KeyguardUpdateMonitor updateMonitor = KeyguardUpdateMonitor.getInstance(mContext);
         boolean keyguardVisible = mState != StatusBarState.SHADE || updateMonitor.isSimPinSecure();
@@ -3094,7 +3089,9 @@ private final View.OnClickListener mKillClickListener = new View.OnClickListener
                             == PlaybackState.STATE_PLAYING);
         }
 
-        if (backdropBitmap == null && mMediaMetadata == null) {
+        // apply user lockscreen image
+        if (backdropBitmap == null && mMediaMetadata == null &&
+                !mNotificationPanel.hasExternalKeyguardView()) {
             backdropBitmap = mKeyguardWallpaper;
         }
 
