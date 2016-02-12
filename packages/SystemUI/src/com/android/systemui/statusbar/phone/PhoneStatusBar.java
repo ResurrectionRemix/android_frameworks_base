d568551cb68cfa84a2c967af7e3ec9e8e7a9ba87
Matthias Yzusqui <myzb.dev@gmail.com>
2016-02-12 10:47:38 -0800

    Revert "SystemUI: survive notification update spam"
    This reverts commit 6d24aa43398a9888bc93d3d4afe0eccd24211356.
    
    Fixes janky headsup animation when a (new) headsup is displayed.
    
    RM-208
    Change-Id: Icf2e620fec696b55ac611cba47321a23acf0120c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -270,7 +270,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private static final int MSG_CLOSE_PANELS = 1001;
     private static final int MSG_OPEN_SETTINGS_PANEL = 1002;
     private static final int MSG_LAUNCH_TRANSITION_TIMEOUT = 1003;
-    private static final int MSG_UPDATE_NOTIFICATIONS = 1004;
     // 1020-1040 reserved for BaseStatusBar
 
     // Time after we abort the launch transition.
@@ -2773,18 +2772,12 @@ private final View.OnClickListener mKillClickListener = new View.OnClickListener
         return entry.row.getParent() instanceof NotificationStackScrollLayout;
     }
 
-    private void handleUpdateNotifications() {
+    @Override
+    protected void updateNotifications() {
         mNotificationData.filterAndSort();
 
         updateNotificationShade();
         mIconController.updateNotificationIcons(mNotificationData);
-    }
-
-    @Override
-    protected void updateNotifications() {
-        if (!mHandler.hasMessages(MSG_UPDATE_NOTIFICATIONS)) {
-            mHandler.sendEmptyMessage(MSG_UPDATE_NOTIFICATIONS);
-        }
     }
 
     @Override
@@ -3461,9 +3454,6 @@ private final View.OnClickListener mKillClickListener = new View.OnClickListener
                     break;
                 case MSG_LAUNCH_TRANSITION_TIMEOUT:
                     onLaunchTransitionTimeout();
-                    break;
-                case MSG_UPDATE_NOTIFICATIONS:
-                    handleUpdateNotifications();
                     break;
             }
         }
