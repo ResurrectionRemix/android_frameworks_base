1591e204010e5b30105d1d7a303499fa7b27dc16
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2016-02-26 13:19:27 +0100

    fix crash on boot
    Instead of moving "sendStateChangeBroadcast(true)" as in
    https://github.com/AOKP/frameworks_base/commit/3e1b86ec70076eb7e6a8889cfe38fdb04b161558
    check in Recents.java if booting is complete.
    
    The crash actually stems from trying to get the size of the search widget that
    is shown in the recents view.
    
    Change-Id: I97737fc706f3fdd2ed6475386873edc29d95a25c
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>
    Signed-off-by: Varun <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/Recents.java
+++ packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -312,6 +312,7 @@ public class Recents extends SystemUI
     @Override
     public void onBootCompleted() {
         mBootCompleted = true;
+        reloadHeaderBarLayout();
     }
 
     /** Shows the Recents. */
@@ -592,10 +593,12 @@ public class Recents extends SystemUI
         // Try and pre-emptively bind the search widget on startup to ensure that we
         // have the right thumbnail bounds to animate to.
         // Note: We have to reload the widget id before we get the task stack bounds below
+	if (mBootCompleted == true) {
         if (mConfig.searchBarEnabled &&
                 mSystemServicesProxy.getOrBindSearchAppWidget(mContext, mAppWidgetHost) != null) {
             mConfig.getSearchBarBounds(mWindowRect.width(), mWindowRect.height(),
                     mStatusBarHeight, searchBarBounds);
+		}
         }
         mConfig.getAvailableTaskStackBounds(mWindowRect.width(), mWindowRect.height(),
                 mStatusBarHeight, (mConfig.hasTransposedNavBar ? mNavBarWidth : 0), searchBarBounds,
