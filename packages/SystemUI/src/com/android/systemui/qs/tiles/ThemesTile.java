a6f591a8ff8921912f4c2a46be87200057661a7e
westcripp <korukaltan@gmail.com>
2016-03-05 08:22:37 +0200

    Revert "ThemesTile: properly get theme config from our context"
    This reverts commit 50630be12710e8c3b86169e3b08aa3d481628f6b.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ThemesTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ThemesTile.java
@@ -313,14 +313,24 @@ public class ThemesTile extends QSTile<QSTile.BooleanState> implements ThemeMana
 
         }
 
+        //TODO: Maybe there is better way?
         private String getCurrentTheme(String app) {
-            ThemeConfig themeConfig = getHost().getContext().getResources().getConfiguration().themeConfig;
+
+            ThemeConfig themeConfig;
+
+            try {
+                themeConfig = ActivityManagerNative.getDefault().getConfiguration().themeConfig;
+            } catch (RemoteException e) {
+                throw new RuntimeException(e);
+            }
 
             if (themeConfig.getAppThemes().get(app) != null) {
                 return themeConfig.getAppThemes().get(app).getOverlayPkgName();
             } else {
                 return "default";
             }
+
+
         }
 
         private String getCurrentTheme() {
