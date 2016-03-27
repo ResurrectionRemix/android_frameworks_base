0b149fa5cb79cb87d23c37da010a6e7041909f83
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    HeadsUp: add timeout option (1/2)
    Forward ported to marshmallow By: @BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: Ic0d899879dcf6b324c270e7a33c3c62a664c7df8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4840,6 +4840,12 @@ public final class Settings {
         public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
 
         /**
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * Whether to use slim recents
          * @hide
          */
