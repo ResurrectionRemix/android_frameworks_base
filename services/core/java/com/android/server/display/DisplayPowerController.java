13bc9bba4ddf53136f50714f17a63215f15c6e63
Steve Kondik <steve@cyngn.com>
2016-03-31 11:25:53 -0700

    livedisplay: Moving LiveDisplay to CMSDK
     * Remove all LiveDisplay-related code from the framework, and
       update the QS tile for the new API.
    
    Change-Id: I41c04a254afa91c0ed26c050362b63049d489fba
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -267,9 +267,6 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     // The controller for the automatic brightness level.
     private AutomaticBrightnessController mAutomaticBrightnessController;
 
-    // The controller for LiveDisplay
-    private final LiveDisplayController mLiveDisplayController;
-
     // Animators.
     private ObjectAnimator mColorFadeOnAnimator;
     private ObjectAnimator mColorFadeOffAnimator;
@@ -310,8 +307,6 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         mWindowManagerPolicy = LocalServices.getService(WindowManagerPolicy.class);
         mBlanker = blanker;
         mContext = context;
-
-        mLiveDisplayController = new LiveDisplayController(context, handler.getLooper());
 
         final Resources resources = context.getResources();
         final int screenBrightnessSettingMinimum = clampAbsoluteBrightness(resources.getInteger(
@@ -408,7 +403,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                         mScreenBrightnessRangeMaximum, dozeScaleFactor, lightSensorRate,
                         brighteningLightDebounce, brighteningLightFastDebounce,
                         darkeningLightDebounce, autoBrightnessResetAmbientLuxAfterWarmUp,
-                        ambientLightHorizon, mLiveDisplayController);
+                        ambientLightHorizon);
             }
         }
 
@@ -827,9 +822,6 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 animateScreenBrightness(brightness, 0);
             }
         }
-
-        // Update LiveDisplay now
-        mLiveDisplayController.updateLiveDisplay();
 
         // Determine whether the display is ready for use in the newly requested state.
         // Note that we do not wait for the brightness ramp animation to complete before
@@ -1269,8 +1261,6 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         if (mAutomaticBrightnessController != null) {
             mAutomaticBrightnessController.dump(pw);
         }
-
-        mLiveDisplayController.dump(pw);
     }
 
     private static String proximityToString(int state) {
@@ -1330,10 +1320,6 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
 
     private static int clampAbsoluteBrightness(int value) {
         return MathUtils.constrain(value, PowerManager.BRIGHTNESS_OFF, PowerManager.BRIGHTNESS_ON);
-    }
-
-    void systemReady() {
-        mLiveDisplayController.systemReady();
     }
 
     private final class DisplayControllerHandler extends Handler {
