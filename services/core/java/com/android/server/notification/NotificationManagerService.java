dea8bfb37e090c1c911759251c902c6414721cf8
westcripp <korukaltan@gmail.com>
2016-04-04 22:26:53 +0300

    Revert "base: correctly save app notification config"
    This reverts commit 7f9f2995a39bee65aca6aa53a50103c856efb937.

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1543,8 +1543,8 @@ public class NotificationManagerService extends SystemService {
         @Override
         public void setPackagePeekable(String pkg, int uid, boolean peekable) {
             checkCallerIsSystem();
+
             mRankingHelper.setPackagePeekable(pkg, uid, peekable);
-            savePolicyFile();
         }
 
         @Override
@@ -1556,8 +1556,8 @@ public class NotificationManagerService extends SystemService {
         @Override
         public void setPackageKeyguard(String pkg, int uid, boolean keguard) {
             checkCallerIsSystem();
+
             mRankingHelper.setPackageKeyguard(pkg, uid, keguard);
-            savePolicyFile();
         }
 
         @Override
