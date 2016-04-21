91af71db8e3a80f8d9ec48fcbbe3cbda937df2f9
Ethan Chen <intervigil@gmail.com>
2016-04-20 19:00:20 -0700

    SystemUI: Fix mCanDismissKeyguard state issue
    * Setting it to keyguardShowing causes issues when notifications are
      expanded from the lockscreen if a fingerprint is used to unlock the
      device.
    
    REF: CYNGNOS-2400, NIGHTLIES-2767
    Change-Id: Id973c2cc011e56f516a34b69df0851ee3307af24
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1375,8 +1375,9 @@ public class NotificationPanelView extends PanelView implements
 
         mStatusBarState = statusBarState;
         mKeyguardShowing = keyguardShowing;
-        mKeyguardOrShadeShowing = keyguardOrShadeShowing;
-        mCanDismissKeyguard = keyguardShowing;
+        if (oldState != statusBarState && statusBarState == StatusBarState.KEYGUARD) {
+            mCanDismissKeyguard = true;
+        }
 
         if (goingToFullShade || (oldState == StatusBarState.KEYGUARD
                 && statusBarState == StatusBarState.SHADE_LOCKED)) {
