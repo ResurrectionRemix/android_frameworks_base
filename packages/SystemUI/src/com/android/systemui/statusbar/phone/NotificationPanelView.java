2f2bd9e9bb9f624bafb5e74701257021cee7124c
westcripp <korukaltan@gmail.com>
2016-04-22 13:30:57 +0300

    Improve TM animation & use stock detail background

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1845,7 +1845,7 @@ public class NotificationPanelView extends PanelView implements
             mTaskManagerShowing = taskManagerShowing;
             cancelAnimation();
             int x = mQsPanel.getLeft() + mQsPanel.getWidth() / 2;
-            int y = mQsPanel.getTop() + mQsPanel.getHeight() / 2;
+            int y = mQsPanel.getTop() / 2;
             if (!taskManagerShowing) {
                 mQsPanel.setVisibility(View.VISIBLE);
             }
@@ -1856,7 +1856,8 @@ public class NotificationPanelView extends PanelView implements
    private final AnimatorListenerAdapter mHideQsPanelWhenDone = new AnimatorListenerAdapter() {
         public void onAnimationEnd(Animator animation) {
             if (mTaskManagerShowing) {
-                mQsPanel.setVisibility(View.GONE);
+			mTaskManagerPanel.setVisibility(View.VISIBLE);
+			mQsPanel.setVisibility(View.GONE);
             }
         };
     };
