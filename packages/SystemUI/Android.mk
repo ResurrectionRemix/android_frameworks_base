de990e9e9015eebdda08f5b0154b9772913132d0
d34d <clark@cyngn.com>
2016-04-25 08:16:03 -0700

    SysUI: Use VectorDrawable's for rendering battery
    The current implementation of BatteryMeterView uses arrays of points
    that are drawn programmatically.  This limits the ability to overlay
    or theme the battery.  By utilizing VectorDrawables and
    AnimatedVectorDrawables we can achieve the same look and feel of
    stock while allowing for more unique battery meters.
    
    Change-Id: I16a8a949a92827a9e8c1b528981031b6830e97cd
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -14,7 +14,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := Keyguard \
     android-support-v4 \
     android-opt-cards \
     trail-drawing \
-    rebound
+    rebound \
+    uicommon
 
 LOCAL_JAVA_LIBRARIES := telephony-common org.dirtyunicorns.utils
 LOCAL_FULL_LIBS_MANIFEST_FILES := $(LOCAL_PATH)/AndroidManifest_cm.xml
