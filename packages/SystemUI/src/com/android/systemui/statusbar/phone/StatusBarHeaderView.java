8b63b1ffbb26249c735e731f3cc17e9b9024c954
Luis Vidal <lvidal@cyngn.com>
2016-05-02 10:48:56 -0700

    SystemUI: Update cached weather data when temperature unit changes
    Register an observer to keep track of the temperature unit
    selected in settings to update the cached data accordingly
    
    TICKET: CYNGNOS-2694
    
    Change-Id: I92adec20835bcd6d6476d793564300611bcba4a1
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -92,6 +92,7 @@ import java.text.NumberFormat;
 
 import cyanogenmod.app.StatusBarPanelCustomTile;
 import cyanogenmod.providers.CMSettings;
+import cyanogenmod.weather.util.WeatherUtils;
 import org.cyanogenmod.internal.logging.CMMetricsLogger;
 
 /**
@@ -777,16 +778,16 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
 
     @Override
     public void onWeatherChanged(WeatherController.WeatherInfo info) {
-        if (info.temp == null || info.condition == null) {
+        if (Double.isNaN(info.temp) || info.condition == null) {
 			mWeatherimage = (ImageButton) findViewById(R.id.no_weather_image);
 			mWeatherimage.setVisibility(View.VISIBLE);
-            mWeatherLine1.setText(null);
-            mWeatherLine2.setText(null);
+			mWeatherLine1.setText(null);
+            		mWeatherLine2.setText(null);
         } else {
             mWeatherimage.setVisibility(View.GONE);
             mWeatherLine1.setText(mContext.getString(
                     R.string.status_bar_expanded_header_weather_format,
-                    info.temp,
+                    WeatherUtils.formatTemperature(info.temp, info.tempUnit),
                     info.condition));
         }
             mWeatherLine2.setText(info.city);
