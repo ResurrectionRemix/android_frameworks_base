f03428491f352741f05b3eddbc23e541224d884d
Nico60 <ndhouailly@gmail.com>
2016-05-16 21:48:30 +0200

    Add animations to QS tiles [1/2]
    Change-Id: Idbe80d2b56d30a1f3675c19b81ac47ecc52c3c27
    Signed-off-by: varund7726 <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4428,6 +4428,18 @@ public final class Settings {
                BOOLEAN_VALIDATOR;
 
         /**
+         * Change quick settings tiles animation style
+         * @hide
+         */
+        public static final String ANIM_TILE_STYLE = "anim_tile_style";
+
+         /**
+         * Change quick settings tiles animation duration
+         * @hide
+         */
+        public static final String ANIM_TILE_DURATION = "anim_tile_duration";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -13876,3 +13888,4 @@ public final class Settings {
         return packages[0];
     }
 }
+
