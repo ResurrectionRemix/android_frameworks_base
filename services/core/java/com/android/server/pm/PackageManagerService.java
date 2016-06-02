3d4fb7ff2f95aaae5128fb0fbafbc887cde945cc
Mårten Kongstad <marten.kongstad@sonymobile.com>
2016-06-02 09:35:31 +0200

    OMS7-N: Integrate OverlayManagerService into framework [3/11]
    Hand over ownership of overlays to OverlayManagerService.
    
    Changes to a package's overlays are propagated using the activity life
    cycle. Affected activities will be recreated as needed. This provides a
    well-defined point to modify an application's assets while the
    application is paused.
    
    Consolidate how overlays targeting the system and overlays targeting
    regular applications are handled. Previously, system overlays were
    handled as a special case. Now, everything is handled identically. As a
    side effect, the call to idmap --scan during Zygote boot has become
    obsolete and is removed.
    
    Deprecate and remove use of SCAN_TRUSTED_OVERLAY from
    PackageManagerService. Previously, the flag was used to restrict what
    paths overlays were allowed to be installed in. Now, overlay packages
    are first class packages and it is up to the OverlayManagerService to
    decide whether to use the overlay or not.
    
    Information on what overlays to use is recorded in
    ApplicationInfo.resourceDirs. The PackageManagerService is responsible
    for the creation of ApplicationInfo objects. The OverlayManagerService
    is responsible for informing the PackageManagerService in advance about
    what resourceDirs to use.
    
    When launching an application, the ApplicationInfo is already populated
    with up-to-date information about overlays.
    
    When enabling or disabling an overlay for a running application, the
    OverlayManagerService first notifies the OverlayManagerService about the
    updated resourceDirs. It then tells the ActivityManagerService to push
    the new ApplicationInfo object to the application's ActivityThread.
    Finally the application requests its ResourcesManager to create new
    ResourcesImpl objects based on the updated paths.
    
    Change-Id: If0b1eaa690c38f9c33f7c8dc981314205a73fa9c

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -21416,14 +21416,13 @@ Slog.v(TAG, ":: stepped forward, applying functor at tag " + parser.getName());
 
         @Override
         public void setResourceDirs(int userId, String packageName, String[] resourceDirs) {
-            // TODO: uncomment when we integrate OMS properly
-            // synchronized (mPackages) {
-            //     PackageSetting ps = mSettings.mPackages.get(packageName);
-            //     if (ps == null) {
-            //         return;
-            //     }
-            //     ps.setResourceDirs(resourceDirs, userId);
-            // }
+            synchronized (mPackages) {
+                final PackageSetting ps = mSettings.mPackages.get(packageName);
+                if (ps == null) {
+                    return;
+                }
+                ps.setResourceDirs(resourceDirs, userId);
+            }
         }
 
         @Override
