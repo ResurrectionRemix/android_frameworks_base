3f4cbd06aa759cb5fe4d9f3253198f181b016b05
Adnan Begovic <adnan@cyngn.com>
2016-06-02 15:29:16 -0700

    SystemUI: Add support for persistent usb drive notification.
      This option can be enabled via config_persistUsbDriveNotification
    
    Change-Id: I331a759aeb914e46e3ef4842896d3465d894707a
    TICKET: PAELLA-225
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- packages/SystemUI/src/com/android/systemui/usb/StorageNotification.java
+++ packages/SystemUI/src/com/android/systemui/usb/StorageNotification.java
@@ -393,6 +393,8 @@ public class StorageNotification extends SystemUI {
                             mContext.getString(R.string.ext_media_unmount_action),
                             buildUnmountPendingIntent(vol)))
                     .setContentIntent(browseIntent)
+                    .setOngoing(mContext.getResources().getBoolean(
+                            R.bool.config_persistUsbDriveNotification))
                     .setCategory(Notification.CATEGORY_SYSTEM);
             // USB disks notification can be persistent
             if (disk.isUsb()) {
