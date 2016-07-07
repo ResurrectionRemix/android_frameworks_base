a1e19e80a69b2ba7e093ff03ace596f5f32d2936
Jessica Wagantall <jwagantall@cyngn.com>
2016-07-07 14:19:06 -0700

    Merge remote-tracking branch 'remotes/android-6.0.1_r52' into HEAD Ticket: CYNGNOS-3020
    Change-Id: Ia14b6d0120de0b458c7c249a11041ff121389cfa

--- services/core/java/com/android/server/LockSettingsService.java
+++ services/core/java/com/android/server/LockSettingsService.java
@@ -468,6 +468,7 @@ public class LockSettingsService extends ILockSettings.Stub {
     @Override
     public void setLockPattern(String pattern, String savedCredential, int userId)
             throws RemoteException {
+        checkWritePermission(userId);
         byte[] currentHandle = getCurrentHandle(userId);
 
         if (pattern == null) {
@@ -496,6 +497,7 @@ public class LockSettingsService extends ILockSettings.Stub {
     @Override
     public void setLockPassword(String password, String savedCredential, int userId)
             throws RemoteException {
+        checkWritePermission(userId);
         byte[] currentHandle = getCurrentHandle(userId);
 
         if (password == null) {
