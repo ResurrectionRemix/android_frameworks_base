0f660c414f8c91ac33976225c080cde640d066d2
Jessica Wagantall <jwagantall@cyngn.com>
2016-08-02 11:09:55 -0700

    Merge tag 'android-6.0.1_r61' into HEAD
    Android 6.0.1 Release 61 (MOB30Z)
    
    Change-Id: Ib003ccb606e0d77209291b757ea36399d3b65814

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -3937,7 +3937,9 @@ public final class ActivityStackSupervisor implements DisplayListener {
     }
 
     void showLockTaskToast() {
-        mLockTaskNotify.showToast(mLockTaskModeState);
+        if (mLockTaskNotify != null) {
+            mLockTaskNotify.showToast(mLockTaskModeState);
+        }
     }
 
     void showLockTaskEscapeMessageLocked(TaskRecord task) {
