6304326f89e74e08202ad28bf9417dbed5869b4d
Randall Rushing <randall.rushing@gmail.com>
2016-08-06 23:38:55 -0700

    Fling: update android-trail-drawing from Orange Open Source to 1.2.6
    https://github.com/Orange-OpenSource/android-trail-drawing
    
    Change-Id: I44c5bb7bae086f3f17b1433e8b520de068a2a1c8

--- packages/SystemUI/libs/Android.mk
+++ packages/SystemUI/libs/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    trail-drawing:trail-core-lib-1.0.4-SNAPSHOT.jar \
+    trail-drawing:trail-core-lib-1.2.6-SNAPSHOT.jar \
     rebound:rebound-0.3.8.jar
 
 include $(BUILD_MULTI_PREBUILT)
