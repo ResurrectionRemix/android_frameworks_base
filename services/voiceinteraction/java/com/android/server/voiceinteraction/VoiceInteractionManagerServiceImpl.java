b9f39951f0047414473b6288b58bfcc0edd5b7e3
gaoshang <gaoshang@xiaomi.com>
2016-08-18 14:25:10 +0800

    Framework: Do not turn off voice interaction when close system dialogs reason is assist
    In PhoneWindowManager, before the start assist
    it sendCloseSystemWindows to call ActivityManagerNative CloseSystemDialog as reason  assist，
    and then AMS send the Intent.ACTION_CLOSE_SYSTEM_DIALOGS broadcast
    then PhoneWindowManager call statusbar to startAssist
    
    VoiceInteractionManagerServiceImpl register the broadcast and check the reason to close itself
    but the broadcast and the start assist is disordered,
    so maybe VoiceInteraction start before the broadcast receive,
    and then it close itself by the reason start it
    @see #launchAssistAction(String hint, int deviceId) in PhoneWindowManager
    
    Change-Id: I80cafab41362c5573e49ff0ce1eb3bb36a9f48f8
    Signed-off-by: gaoshang <gaoshang@xiaomi.com>

--- services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerServiceImpl.java
+++ services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerServiceImpl.java
@@ -73,7 +73,7 @@ class VoiceInteractionManagerServiceImpl implements VoiceInteractionSessionConne
         public void onReceive(Context context, Intent intent) {
             if (Intent.ACTION_CLOSE_SYSTEM_DIALOGS.equals(intent.getAction())) {
                 String reason = intent.getStringExtra("reason");
-                if (!CLOSE_REASON_VOICE_INTERACTION.equals(reason) && !"dream".equals(reason)) {
+                if (!CLOSE_REASON_VOICE_INTERACTION.equals(reason) && !"dream".equals(reason) && !"assist".equals(reason)) {
                     synchronized (mLock) {
                         if (mActiveSession != null && mActiveSession.mSession != null) {
                             try {
