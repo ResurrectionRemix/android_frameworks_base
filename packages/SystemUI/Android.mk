4c209747cff60344b61364d4c19b3baf16f98abd
Hendrik Hagendorn <git@finnq.de>
2016-09-19 22:18:04 +0200

    SystemUI: Use AndroidManifest_cm.xml
    * Fixes navigation bar settings crash
    
    Change-Id: I3834b6fe4e2865964d2bcfcd583ca30a3c5b76d0

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -34,6 +34,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4
 
 LOCAL_JAVA_LIBRARIES := telephony-common
+LOCAL_FULL_LIBS_MANIFEST_FILES := $(LOCAL_PATH)/AndroidManifest_cm.xml
 
 LOCAL_PACKAGE_NAME := SystemUI
 LOCAL_CERTIFICATE := platform
