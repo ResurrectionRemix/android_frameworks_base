1bdded7749919fb177d6743ae88df310ca3a499f
ezio84 <brabus84@gmail.com>
2016-09-20 14:07:03 +0200

    Data tile: make it customizable by the user [1/2]
    if advanced tile enabled: one tap to switch on/off data, long press to show details view
    if disabled: stock behavior, one tap for details view and long press for Cellular settings
    
    Added secondary click to show details view
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6954,6 +6954,12 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Whether to use one tap tile action to enable or disable data 
+         * @hide
+         */
+        public static final String QS_DATA_ADVANCED = "qs_data_advanced";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
