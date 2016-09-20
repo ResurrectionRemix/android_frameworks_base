7e037ebc15925cc2a8ab42f3c10bacbfd8ab8728
yuanhuihui <yuanhuihui@xiaomi.com>
2016-09-20 21:36:28 +0800

    [system_server] fix crash issues by IndexOutOfBoundsException in ProcessStats
    when ActivityManager setProcessTrackerStateLocked, then process is dead.
    ensureNotDead() only print warn log, could not resovle problem,
    so  should return directly to avoid system_server crash.
    
    https://code.google.com/p/android/issues/detail?id=223017
    
    Change-Id: I4eb67ea18172a62d8fe5cecf253de346e2a4d1f3
    Signed-off-by: yuanhuihui <yuanhuihui@xiaomi.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/com/android/internal/app/procstats/ProcessState.java
+++ core/java/com/android/internal/app/procstats/ProcessState.java
@@ -273,12 +273,13 @@ public final class ProcessState {
         mDead = true;
     }
 
-    private void ensureNotDead() {
+    private boolean ensureNotDead() {
         if (!mDead) {
-            return;
+            return true;
         }
         Slog.w(TAG, "ProcessState dead: name=" + mName
                 + " pkg=" + mPackage + " uid=" + mUid + " common.name=" + mCommonProcess.mName);
+        return false;
     }
 
     public void writeToParcel(Parcel out, long now) {
@@ -322,7 +323,9 @@ public final class ProcessState {
     }
 
     public void makeActive() {
-        ensureNotDead();
+        if(!ensureNotDead()) {
+            return;
+        }
         mActive = true;
     }
 
@@ -378,7 +381,9 @@ public final class ProcessState {
     }
 
     public void setState(int state, long now) {
-        ensureNotDead();
+        if(!ensureNotDead()) {
+            return;
+        }
         if (mCurState != state) {
             //Slog.i(TAG, "Setting state in " + mName + "/" + mPackage + ": " + state);
             commitStateTime(now);
@@ -465,7 +470,9 @@ public final class ProcessState {
 
     public void addPss(long pss, long uss, boolean always,
             ArrayMap<String, ProcessStateHolder> pkgList) {
-        ensureNotDead();
+        if(!ensureNotDead()) {
+            return;
+        }
         if (!always) {
             if (mLastPssState == mCurState && SystemClock.uptimeMillis()
                     < (mLastPssTime+(30*1000))) {
@@ -493,7 +500,9 @@ public final class ProcessState {
     }
 
     public void reportExcessiveWake(ArrayMap<String, ProcessStateHolder> pkgList) {
-        ensureNotDead();
+        if(!ensureNotDead()) {
+            return;
+        }
         mCommonProcess.mNumExcessiveWake++;
         if (!mCommonProcess.mMultiPackage) {
             return;
@@ -505,7 +514,9 @@ public final class ProcessState {
     }
 
     public void reportExcessiveCpu(ArrayMap<String, ProcessStateHolder> pkgList) {
-        ensureNotDead();
+        if(!ensureNotDead()) {
+            return;
+        }
         mCommonProcess.mNumExcessiveCpu++;
         if (!mCommonProcess.mMultiPackage) {
             return;
@@ -536,7 +547,9 @@ public final class ProcessState {
     }
 
     public void reportCachedKill(ArrayMap<String, ProcessStateHolder> pkgList, long pss) {
-        ensureNotDead();
+        if(!ensureNotDead()) {
+            return;
+        }
         mCommonProcess.addCachedKill(1, pss, pss, pss);
         if (!mCommonProcess.mMultiPackage) {
             return;
