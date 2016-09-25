cb5d0c900ae854a89852d4461a83b16795075f81
Varun Date <date.varun123@gmail.com>
2016-09-25 18:47:10 +0530

    SysUI:Fix Membar Updating on Task Clear
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -167,6 +167,7 @@ public class RecentsView extends FrameLayout {
                 @Override
                 public void onClick(View v) {
                     EventBus.getDefault().send(new DismissAllTaskViewsEvent());
+					updateMemoryStatus();
                 }
             });
             addView(mStackActionButton);
