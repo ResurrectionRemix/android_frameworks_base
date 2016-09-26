af1d165729b6b1720879c8e19bf78977c8424115
liulvping <liulvping@xiaomi.com>
2016-09-26 20:00:15 +0800

    Clear pending activity launches when force stop package
    https://code.google.com/p/android/issues/detail?id=223731
    
    It is possible that an activity in pending launches be resumed while
    it's package was stopped by some other reason, like uninstall pkg.
    
    Change-Id: I130ded89cee162aa59ac2bf330f026e49e16ef69
    Signed-off-by: liulvping <liulvping@xiaomi.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -6309,6 +6309,8 @@ public final class ActivityManagerService extends ActivityManagerNative
                 ProcessList.INVALID_ADJ, callerWillRestart, true, doit, evenPersistent,
                 packageName == null ? ("stop user " + userId) : ("stop " + packageName));
 
+        didSomething |= mActivityStarter.clearPendingActivityLaunchesLocked(packageName);
+
         if (mStackSupervisor.finishDisabledPackageActivitiesLocked(
                 packageName, null, doit, evenPersistent, userId)) {
             if (!doit) {
