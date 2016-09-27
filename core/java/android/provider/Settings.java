8d965ee8a57d282b17f57093da49f924caf03a45
Daniel Koman <dankoman30@gmail.com>
2016-09-27 21:44:42 +0200

    Proximity speaker [1/2]
    Change-Id: Ib6bce7e6b92339da1a80504dc4801ed9badb597e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4425,6 +4425,25 @@ public final class Settings {
         public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
 
         /**
+         * Whether the proximity sensor will adjust call to speaker
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER = "proximity_auto_speaker";
+
+        /**
+         * Time delay to activate speaker after proximity sensor triggered
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_DELAY = "proximity_auto_speaker_delay";
+
+        /**
+         * Whether the proximity sensor will adjust call to speaker,
+         * only while in call (not while ringing on outgoing call)
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_INCALL_ONLY = "proximity_auto_speaker_incall_only";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
