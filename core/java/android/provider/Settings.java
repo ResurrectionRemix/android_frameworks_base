f96ea9b517f9fad7e9c126071aa992fb7042a0c4
Alex Cruz <mazdarider23@gmail.com>
2016-11-28 05:11:24 +0100

    QS tile titles visibility [1/2]
    Pretty simple mod to remove tile titles if and when the titles become
    something that you don't want. A good case for this is if you set 7 or more
    columns across, the titles move down to 2 lines and is just looks awful IMO
    
    Before - https://i.imgur.com/G3ICaqb.png
    After - https://i.imgur.com/Hb0J1XW.png
    
    PS2 - Thanks to @maxwen for the know how to adjust the gap when titles
    are removed
    
    Change-Id: I10c534c5383397ce777f3ab0bdc0fa82fad6a7e1
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4798,6 +4798,12 @@ public static final String PHONE_BLACKLIST_REGEX_ENABLED = "phone_blacklist_rege
         public static final String TRANSPARENT_VOLUME_DIALOG = "transparent_volume_dialog";
 
         /**
+         * Whether to display qs tile titles in the qs panel
+         * @hide
+         */
+        public static final String QS_TILE_TITLE_VISIBILITY = "qs_tile_title_visibility";
+
+        /**
          * Volume dialog stroke
          * 0 = disabled
          * 1 = use accent color (default)
