078d7c6fcf315cce186842cb04510401d5450c5e
Varun Date <date.varun123@gmail.com>
2016-11-29 22:18:45 +0530

    Revert "statusbar header date and clock click targets"
    This reverts commit 8fb582c61a8c71be22467b3d6be237c9d8d3e475.
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
@@ -65,8 +65,6 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
 
     private TextView mAlarmStatus;
     private View mAlarmStatusCollapsed;
-    private View mClock;
-    private View mDate;
 
     private QSPanel mQsPanel;
 
@@ -115,10 +113,7 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
         mDateTimeGroup = (ViewGroup) findViewById(R.id.date_time_group);
         mDateTimeGroup.setPivotX(0);
         mDateTimeGroup.setPivotY(0);
-        mClock = findViewById(R.id.clock);
-        mClock.setOnClickListener(this);
-        mDate = findViewById(R.id.date);
-        mDate.setOnClickListener(this);
+
         mDateTimeTranslation = getResources().getDimension(R.dimen.qs_date_time_translation);
         mShowFullAlarm = getResources().getBoolean(R.bool.quick_settings_show_full_alarm);
 
@@ -354,10 +349,6 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
             if (showIntent != null && showIntent.isActivity()) {
                 mActivityStarter.startActivity(showIntent.getIntent(), true /* dismissShade */);
             }
-        } else if (v == mClock) {
-            startAlarmsActivity();
-        } else if (v == mDate) {
-            startCalendarActivity();
         }
     }
 
@@ -379,17 +370,6 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
         duIntent.setClassName("com.android.settings",
             "com.android.settings.Settings$MainSettingsLayoutActivity");
         mActivityStarter.startActivity(duIntent, true /* dismissShade */);
-    }
-
-    private void startCalendarActivity() {
-        Intent calIntent = new Intent(Intent.ACTION_MAIN);
-        calIntent.addCategory(Intent.CATEGORY_APP_CALENDAR);
-        mActivityStarter.startActivity(calIntent, true /* dismissShade */);
-    }
-
-    private void startAlarmsActivity() {
-        mActivityStarter.startActivity(new Intent(android.provider.AlarmClock.ACTION_SHOW_ALARMS),
-                true /* dismissShade */);
     }
 
     @Override
