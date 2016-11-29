d9a1263a3f8711d1fe5c8a19b1dd583acaf56cf3
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2016-11-29 08:00:41 -0500

    Battery: allow set a custom symbol near text pct on charging [1/2]
    default (disabled)
    flash
    tilde
    
    PS2 - OCD things
    
    Change-Id: Ibadd63f2b5b1369a02984381ba7a2a2ca37c0dbb
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8294,11 +8294,21 @@ public static final String PHONE_BLACKLIST_REGEX_ENABLED = "phone_blacklist_rege
          public static final String STATUS_BAR_CHARGE_COLOR = "status_bar_charge_color";
 
         /**
-          * Whether to force percentage text out of the battery icon when charging
-          *
-          * @hide
-        */
-         public static final String FORCE_CHARGE_BATTERY_TEXT = "force_charge_battery_text";
+         * Whether to force percentage text out of the battery icon while charging
+         *
+         * @hide
+         */
+        public static final String FORCE_CHARGE_BATTERY_TEXT = "force_charge_battery_text";
+
+        /**
+         * Charging symbol near battery text percentage
+         * 0: no symbol
+         * 1: flash symbol
+         * 2: tilde symbol
+         * default: 0
+         * @hide
+         */
+        public static final String TEXT_CHARGING_SYMBOL = "text_charging_symbol";
 
         /**
          * Whether the camera double twist gesture to flip between front and back mode should be
