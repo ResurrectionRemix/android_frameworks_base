0a5c95f3ba42baa7278b410b643f45769d26b57d
Blackjubel <blackjubels@gmail.com>
2016-12-10 02:28:29 +0100

    fixed

--- packages/SystemUI/src/com/android/systemui/ViewInvertHelper.java
+++ packages/SystemUI/src/com/android/systemui/ViewInvertHelper.java
@@ -37,7 +37,6 @@ public class ViewInvertHelper {
     private final ColorMatrix mMatrix = new ColorMatrix();
     private final ColorMatrix mGrayscaleMatrix = new ColorMatrix();
     private final long mFadeDuration;
-    private final boolean mThemeInvert;
     private final ArrayList<View> mTargets = new ArrayList<>();
 
     public ViewInvertHelper(View v, long fadeDuration) {
@@ -46,7 +45,6 @@ public class ViewInvertHelper {
     }
     public ViewInvertHelper(Context context, long fadeDuration) {
         mFadeDuration = fadeDuration;
-        mThemeInvert = context.getResources().getBoolean(com.android.internal.R.bool.config_invert_colors_on_doze);
     }
 
     private static ArrayList<View> constructArray(View target) {
@@ -93,7 +91,6 @@ public class ViewInvertHelper {
     }
 
     public void update(boolean invert) {
-        if (invert && mThemeInvert) {
         if (invert && Resources.getSystem().getBoolean(
                 com.android.internal.R.bool.config_invert_colors_on_doze)) {
             updateInvertPaint(1f);
