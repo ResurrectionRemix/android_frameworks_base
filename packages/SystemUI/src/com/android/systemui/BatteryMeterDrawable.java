cce9663f58ef463c1b8cfa097abd50446411b2f3
ezio84 <brabus84@gmail.com>
2016-12-27 14:31:13 +0100

    Battery icon: fix getDrawable for new Android API
    Teamwork as usual with awesome substratum guys
    
    this fixes:
    
    12-23 01:08:01.060  6026  6026 W Resources: Drawable com.android.systemui:drawable/ic_battery_portrait has unresolved theme attributes! Consider using Resources.getDrawable(int, Theme) or Context.getDrawable(int).
    12-23 01:08:01.060  6026  6026 W Resources: java.lang.RuntimeException
    12-23 01:08:01.060  6026  6026 W Resources: 	at android.content.res.Resources.getDrawable(Resources.java:746)
    12-23 01:08:01.060  6026  6026 W Resources: 	at com.android.systemui.BatteryMeterDrawable.loadBatteryDrawables(BatteryMeterDrawable.java:450)
    12-23 01:08:01.060  6026  6026 W Resources: 	at com.android.systemui.BatteryMeterDrawable.<init>(BatteryMeterDrawable.java:184)
    12-23 01:08:01.060  6026  6026 W Resources: 	at com.android.systemui.BatteryMeterDrawable.<init>(BatteryMeterDrawable.java:146)
    12-23 01:08:01.060  6026  6026 W Resources: 	at com.android.systemui.BatteryMeterView.<init>(BatteryMeterView.java:59)
    12-23 01:08:01.060  6026  6026 W Resources: 	at com.android.systemui.BatteryMeterView.<init>(BatteryMeterView.java:49)
    
    Change-Id: I251b2a9ae71c4b337843c347c07dc24c5b8a64c6

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -470,7 +470,7 @@ public class BatteryMeterDrawable extends Drawable implements
         }
 
         final int drawableResId = getBatteryDrawableResourceForStyle(style);
-        mBatteryDrawable = (LayerDrawable) res.getDrawable(drawableResId);
+        mBatteryDrawable = (LayerDrawable) res.getDrawable(drawableResId, null);
         mFrameDrawable = mBatteryDrawable.findDrawableByLayerId(R.id.battery_frame);
         mFrameDrawable.setTint(mCurrentBackgroundColor != 0
                 ? mCurrentBackgroundColor : res.getColor(R.color.batterymeter_frame_color));
@@ -490,7 +490,7 @@ public class BatteryMeterDrawable extends Drawable implements
         final int resId = getBatteryDrawableResourceForStyle(style);
         final Drawable batteryDrawable;
         try {
-            batteryDrawable = res.getDrawable(resId);
+            batteryDrawable = res.getDrawable(resId, null);
         } catch (Resources.NotFoundException e) {
             throw new BatteryMeterDrawableException(res.getResourceName(resId) + " is an " +
                     "invalid drawable", e);
