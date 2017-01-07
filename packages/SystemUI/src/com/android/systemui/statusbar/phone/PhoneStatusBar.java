d5b0da139f74dae6c0df2537301f9be6bb1e6a00
Altaf-Mahdi <altaf.mahdi@gmail.com>
2017-01-07 11:54:55 +0530

    TaskManager: Clean up calls in PSB
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -466,8 +466,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private TaskManager mTaskManager;
     private LinearLayout mTaskManagerPanel;
     private ImageButton mTaskManagerButton;
+    // task manager enabled
     private boolean mShowTaskManager;
-    private boolean showTaskList = false;
+    // task manager click state
+    private boolean mShowTaskList = false;
 
     // top bar
     BaseStatusBarHeader mHeader;
@@ -947,8 +949,21 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mCLogoright = (ImageView) mStatusBarView.findViewById(R.id.custom_right);
             mCLogoleft = (ImageView) mStatusBarView.findViewById(R.id.custom_left);
 	    showmCustomlogo(mCustomlogo,mCustomlogoColor,mCustomLogoPos);
-            mShowTaskManager = Settings.System.getIntForUser(resolver,
+            boolean showTaskManager = Settings.System.getIntForUser(resolver,
                     Settings.System.ENABLE_TASK_MANAGER, 0, UserHandle.USER_CURRENT) == 1;
+            if (mShowTaskManager != showTaskManager) {
+                if (!mShowTaskManager) {
+                    // explicitly reset click state when disabled
+                    mShowTaskList = false;
+                }
+                mShowTaskManager = showTaskManager;
+                if (mHeader != null) {
+                    mHeader.setTaskManagerEnabled(showTaskManager);
+                }
+                if (mNotificationPanel != null) {
+                    mNotificationPanel.setTaskManagerEnabled(showTaskManager);
+                }
+            }
 
 
             mDt2lCameraVibrateConfig = Settings.System.getIntForUser(resolver,
@@ -1705,19 +1720,22 @@ mWeatherTempSize, mWeatherTempFontStyle, mWeatherTempColor);
             });
         }
 
-        // Task manager
         mTaskManagerPanel =
-                (LinearLayout) mStatusBarWindowContent.findViewById(R.id.task_manager_panel);
+                (LinearLayout) mStatusBarWindow.findViewById(R.id.task_manager_panel);
         mTaskManager = new TaskManager(mContext, mTaskManagerPanel);
         mTaskManager.setActivityStarter(this);
         mTaskManagerButton = (ImageButton) mHeader.findViewById(R.id.task_manager_button);
         mTaskManagerButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                showTaskList = !showTaskList;
-                mNotificationPanel.setTaskManagerVisibility(showTaskList);
+                mShowTaskList = !mShowTaskList;
+                mNotificationPanel.setTaskManagerVisibility(mShowTaskList);
             }
         });
+        /* Hide this for now. We dont recreate statusbar yet
+        mHeader.setTaskManagerEnabled(mShowTaskManager);
+        mNotificationPanel.setTaskManagerEnabled(mShowTaskManager);
+        mShowTaskList = false;*/
 
 
         // User info. Trigger first load.
