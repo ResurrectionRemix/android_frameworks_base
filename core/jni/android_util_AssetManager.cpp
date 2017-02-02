6d90eb7c0a5479637bc61d8d9bf84c273f6884db
Varun Date <date.varun123@gmail.com>
2017-02-02 11:28:15 +0530

    Revert "Revert "Support AOSP service to scan carrier resources""
    This reverts commit 99dee2220e69bff9d732a6cddfa5aaf6e7346c08.
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/jni/android_util_AssetManager.cpp
+++ core/jni/android_util_AssetManager.cpp
@@ -27,6 +27,7 @@
 #include <sys/stat.h>
 
 #include <private/android_filesystem_config.h> // for AID_SYSTEM
+#include <private/regionalization/Environment.h>
 
 #include "androidfw/Asset.h"
 #include "androidfw/AssetManager.h"
