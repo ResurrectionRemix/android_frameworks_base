f8bfe83072e89ca8ab81ffed01505fa9e04f9d7e
PMS22 <prathams99@rediff.com>
2017-02-07 16:43:13 +0000

    Hide Lockscreen emergency button [1/2]
    *DU edits/notes*
    
    - Hiding the emergency button is something that some users wish to have.
    I don't get it but it can be done while keeping the stock behavior by default
    
    - Removed unnecessary imports
    - Removed unnecessary void used in the original patch
    - Kept the original argument for 'IsInCall' which was removed in original patch
    
    Honestly this was a train wreck when we got it but it works good now :-)
    
    Change-Id: I908e5b16faf2912842d7a8a3c657ecca9b76baaf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5683,6 +5683,12 @@ public static final String PHONE_BLACKLIST_REGEX_ENABLED = "phone_blacklist_rege
         public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
 
         /**
+         * Show/Hide Emergency Button on Lockscreen
+         * @hide
+         */
+        public static final String SHOW_EMERGENCY_BUTTON = "show_emergency_button";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/Keyguard/src/com/android/keyguard/EmergencyButton.java
+++ packages/Keyguard/src/com/android/keyguard/EmergencyButton.java
@@ -26,6 +26,7 @@ import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.telecom.TelecomManager;
 import android.telephony.ServiceState;
 import android.util.AttributeSet;
@@ -220,18 +221,22 @@ public class EmergencyButton extends Button {
                 }
             }
         }
-        if (visible) {
-            setVisibility(View.VISIBLE);
 
+        if (visible) {
             int textId;
-            if (isInCall()) {
-                textId = com.android.internal.R.string.lockscreen_return_to_call;
+            boolean showEmergencyButton = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.SHOW_EMERGENCY_BUTTON, 1, UserHandle.USER_CURRENT) != 1;
+            if (!showEmergencyButton) {
+                setVisibility(View.VISIBLE);
+                if (isInCall()) {
+                    textId = com.android.internal.R.string.lockscreen_return_to_call;
+                } else {
+                    textId = com.android.internal.R.string.lockscreen_emergency_call;
+                }
+                setText(textId);
             } else {
-                textId = com.android.internal.R.string.lockscreen_emergency_call;
+                setVisibility(View.GONE);
             }
-            setText(textId);
-        } else {
-            setVisibility(View.GONE);
         }
     }
 
