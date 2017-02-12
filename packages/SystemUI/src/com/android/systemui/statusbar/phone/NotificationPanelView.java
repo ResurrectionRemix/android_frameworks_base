c26d67174893b13556c356a6fb9bb7631e6aec4c
Gabriele M <moto.falcon.git@gmail.com>
2017-02-12 12:01:21 +0100

    Don't reposition the clock when pulling down the notification bar
    positionClockAndNotifications() is causing the digital clock to
    abruptly fade and shrink when pulling down the notification bar,
    while it should slowly fade away and only while swiping up to
    unlock the screen.
    
    Change-Id: I455798c1dfc880353334dbe9d1b8ac0c6b5bc87c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2586,9 +2586,6 @@ public class NotificationPanelView extends PanelView implements
 
         // Hide "No notifications" in QS.
         mNotificationStackScroller.updateEmptyShadeView(mShadeEmpty && !mQsExpanded);
-        if (mStatusBarState == StatusBarState.KEYGUARD) {
-            positionClockAndNotifications();
-        }
     }
 
     public void setQsScrimEnabled(boolean qsScrimEnabled) {
