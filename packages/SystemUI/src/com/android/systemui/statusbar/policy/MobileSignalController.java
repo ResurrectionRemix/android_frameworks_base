835fcfb2b634020943580f0313a1549030c6fd81
xyyx <xyyx@mail.ru>
2017-02-12 12:37:45 +0800

    Add VOLTE icon
    Extracted from CAF and adapted to AOSP by @xyyx
    Added switch to statusbar icon blacklist by @xyyx
    
    Original commits:
    https://github.com/codeaurora-unofficial/platform-frameworks-base/commit/8c968e8d64abca36987d90d95e6dd1cc951c0e0a
    https://github.com/AOSPA/android_frameworks_base/commit/4dabbbe7df5f51f517c13da98809c22b34175964
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -48,7 +48,7 @@ import android.provider.Settings;
 import java.io.PrintWriter;
 import java.util.BitSet;
 import java.util.Objects;
-
+import java.util.List;
 
 public class MobileSignalController extends SignalController<
         MobileSignalController.MobileState, MobileSignalController.MobileIconGroup> {
@@ -281,7 +281,25 @@ public class MobileSignalController extends SignalController<
         int typeIcon = showDataIcon ? icons.mDataType : 0;
         callback.setMobileDataIndicators(statusIcon, qsIcon, typeIcon, qsTypeIcon,
                 activityIn, activityOut, dataContentDescription, description, icons.mIsWide,
-                mSubscriptionInfo.getSubscriptionId());
+                mSubscriptionInfo.getSubscriptionId(), isMobileIms());
+    }
+
+    private boolean isMobileIms() {
+
+        List<SubscriptionInfo> subInfos = SubscriptionManager.from(mContext)
+                        .getActiveSubscriptionInfoList();
+        if (subInfos != null) {
+            for (SubscriptionInfo subInfo: subInfos) {
+                int subId = subInfo.getSubscriptionId();
+                if (mPhone != null
+                        && mPhone.isImsRegisteredForSubscriber(subId)) {
+                    return true;
+                }
+            }
+        } else {
+            Log.e(mTag, "Invalid SubscriptionInfo");
+        }
+        return false;
     }
 
     @Override
