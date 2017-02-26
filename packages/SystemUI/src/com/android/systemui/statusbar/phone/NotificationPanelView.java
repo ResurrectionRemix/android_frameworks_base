95bc5a22f8ce837a482714815f34c7401d883cbd
Gabriele M <moto.falcon.git@gmail.com>
2017-02-26 13:00:44 +0100

    Disable quick pulldown by default
    With Android N this feature became less relevant since a subset of
    the toggles is available without fully expanding the notification
    panel. This gives better access to the notifications with almost no
    functionality loss.
    
    Change-Id: Idb66472b77e60f4ae753c5dd00f6588566cc3c63
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2949,7 +2949,7 @@ public class NotificationPanelView extends PanelView implements
         public void update() {
             ContentResolver resolver = mContext.getContentResolver();
             mOneFingerQuickSettingsIntercept = CMSettings.System.getInt(
-                    resolver, CMSettings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1);
+                    resolver, CMSettings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0);
             mDoubleTapToSleepEnabled = CMSettings.System.getInt(
                     resolver, CMSettings.System.DOUBLE_TAP_SLEEP_GESTURE, 1) == 1;
             mDoubleTapToSleepAnywhere = Settings.System.getIntForUser(resolver,
