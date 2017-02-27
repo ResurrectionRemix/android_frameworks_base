1c53b76f9ce3f1de76ae8f0cb7356cd51e6f1de9
ezio84 <brabus84@gmail.com>
2017-02-27 14:04:59 +0100

    Slim Recent: pinch-in to clear all: pixelblue warning color, 0.7f opacity
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
@@ -315,7 +315,8 @@ public class RecentController implements RecentPanelView.OnExitListener,
         mEmptyRecentView.setImageResource(0);
 
         // Set correct backgrounds based on calculated main gravity.
-        mRecentWarningContent.setBackgroundColor(Color.RED);
+        int warningColor = mContext.getResources().getColor(R.color.recent_warning_background);
+        mRecentWarningContent.setBackgroundColor(warningColor);
 
         int tintColor = getEmptyRecentColor();
         int backgroundColor = mPanelColor;
@@ -699,7 +700,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
 
             // Update colors in RecentPanelView
             mPanelColor = Settings.System.getIntForUser(resolver,
-                    Settings.System.RECENT_PANEL_BG_COLOR, 0x00ffffff, UserHandle.USER_CURRENT);
+                    Settings.System.RECENT_PANEL_BG_COLOR, 0x763367d6, UserHandle.USER_CURRENT);
 
             // Set main gravity and background images.
             setGravityAndImageResources();
@@ -836,7 +837,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
                 final boolean hasFavorite = mRecentPanelView.hasFavorite();
 
                 // Setup animation for warning content - fade out.
-                ValueAnimator animation1 = ValueAnimator.ofFloat(1.0f, 0.0f);
+                ValueAnimator animation1 = ValueAnimator.ofFloat(0.7f, 0.0f);
                 animation1.setDuration(ANIMATION_FADE_OUT_DURATION);
                 animation1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                     @Override
@@ -892,7 +893,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
                     public void onAnimationEnd(Animator animation) {
                         // Animation is finished. Prepare warning content for next call.
                         mRecentWarningContent.setVisibility(View.GONE);
-                        mRecentWarningContent.setAlpha(1.0f);
+                        mRecentWarningContent.setAlpha(0.7f);
                         // Remove all tasks now.
                         if (mRecentPanelView.removeAllApplications()) {
                             // Prepare listview for next recent call.
