AICP/frameworks_base

Slim recents app sidebar [1/2]
Based on the app sidebar by 0xD34D <clark@scheffsblend.com>.

Changes to the original sidebar include:
- No trigger areas, but show/hide with the SlimRecent screen
- Adapted look and animation to SlimRecent
- Longpress actions
- Slimer settings using ActionListViewSettings, also added possibility to configure sidebar size
  (roughly, as the size is adapted to the screen height in order to show full icons) and colors
- Removed possibility to add folders to the sidebar
- Queue apps to launch on recents close by swiping icons left/right

Bring back the required ActionHelper utils we had in mm, thanks to Lars Greiss <kufikugel@googlemail.com> for those.
Contains vector support for ImageHelper commit by Daniel Koman <dankoman30@gmail.com>.

Change-Id: I09d169876dc67ee41cf5d9636f6e73c84f475853

core/java/com/android/internal/util/aicp/ActionConfig.java
@@ -1,0 +1,81 @@
+/*
+* Copyright (C) 2014 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.aicp;
+
+public class ActionConfig {
+
+    private String mClickAction;
+    private String mClickActionDescription;
+    private String mLongpressAction;
+    private String mLongpressActionDescription;
+    private String mIconUri;
+
+    public ActionConfig(String clickAction, String clickActionDescription,
+                    String longpressAction, String longpressActionDescription, String iconUri) {
+        mClickAction = clickAction;
+        mClickActionDescription = clickActionDescription;
+        mLongpressAction = longpressAction;
+        mLongpressActionDescription = longpressActionDescription;
+        mIconUri = iconUri;
+    }
+
+    @Override
+    public String toString() {
+        return mClickActionDescription;
+    }
+
+    public String getClickAction() {
+        return mClickAction;
+    }
+
+    public String getClickActionDescription() {
+        return mClickActionDescription;
+    }
+
+    public String getLongpressAction() {
+        return mLongpressAction;
+    }
+
+    public String getLongpressActionDescription() {
+        return mLongpressActionDescription;
+    }
+
+    public String getIcon() {
+        return mIconUri;
+    }
+
+    public void setClickAction(String action) {
+        mClickAction = action;
+    }
+
+    public void setClickActionDescription(String description) {
+        mClickActionDescription = description;
+    }
+
+    public void setLongpressAction(String action) {
+        mLongpressAction = action;
+    }
+
+    public void setLongpressActionDescription(String description) {
+        mLongpressActionDescription = description;
+    }
+
+    public void setIcon(String iconUri) {
+        mIconUri = iconUri;
+    }
+
+}

core/java/com/android/internal/util/aicp/ActionConstants.java
@@ -1,0 +1,102 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.aicp;
+
+public class ActionConstants {
+
+    // key must fit with the values arrays from Settings to use
+    // SlimActions.java actions
+    public static final String ACTION_HOME                 = "**home**";
+    public static final String ACTION_BACK                 = "**back**";
+    public static final String ACTION_SEARCH               = "**search**";
+    public static final String ACTION_VOICE_SEARCH         = "**voice_search**";
+    public static final String ACTION_MENU                 = "**menu**";
+    public static final String ACTION_MENU_BIG             = "**menu_big**";
+    public static final String ACTION_POWER                = "**power**";
+    public static final String ACTION_NOTIFICATIONS        = "**notifications**";
+    public static final String ACTION_RECENTS              = "**recents**";
+    public static final String ACTION_SCREENSHOT           = "**screenshot**";
+    public static final String ACTION_IME                  = "**ime**";
+    public static final String ACTION_LAST_APP             = "**lastapp**";
+    public static final String ACTION_KILL                 = "**kill**";
+    public static final String ACTION_ASSIST               = "**assist**";
+    public static final String ACTION_VIB                  = "**ring_vib**";
+    public static final String ACTION_SILENT               = "**ring_silent**";
+    public static final String ACTION_VIB_SILENT           = "**ring_vib_silent**";
+    public static final String ACTION_POWER_MENU           = "**power_menu**";
+    public static final String ACTION_TORCH                = "**torch**";
+    public static final String ACTION_EXPANDED_DESKTOP     = "**expanded_desktop**";
+    public static final String ACTION_THEME_SWITCH         = "**theme_switch**";
+    public static final String ACTION_KEYGUARD_SEARCH      = "**keyguard_search**";
+    public static final String ACTION_PIE                  = "**pie**";
+    public static final String ACTION_NAVBAR               = "**nav_bar**";
+    public static final String ACTION_IME_NAVIGATION_LEFT  = "**ime_nav_left**";
+    public static final String ACTION_IME_NAVIGATION_RIGHT = "**ime_nav_right**";
+    public static final String ACTION_IME_NAVIGATION_UP    = "**ime_nav_up**";
+    public static final String ACTION_IME_NAVIGATION_DOWN  = "**ime_nav_down**";
+    public static final String ACTION_CAMERA               = "**camera**";
+    public static final String ACTION_MEDIA_PREVIOUS       = "**media_previous**";
+    public static final String ACTION_MEDIA_NEXT           = "**media_next**";
+    public static final String ACTION_MEDIA_PLAY_PAUSE     = "**media_play_pause**";
+    public static final String ACTION_WAKE_DEVICE          = "**wake_device**";
+
+    // no action
+    public static final String ACTION_NULL            = "**null**";
+
+    // this shorcut constant is only used to identify if the user
+    // selected in settings a custom app...after it is choosed intent uri
+    // is saved in the ButtonConfig object
+    public static final String ACTION_APP          = "**app**";
+
+    public static final String ICON_EMPTY = "empty";
+    public static final String SYSTEM_ICON_IDENTIFIER = "system_shortcut=";
+    public static final String ACTION_DELIMITER = "|";
+
+    public static final String NAVIGATION_CONFIG_DEFAULT =
+          ACTION_BACK    + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY     + ACTION_DELIMITER
+        + ACTION_HOME    + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY     + ACTION_DELIMITER
+        + ACTION_RECENTS + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+    public static final String NAV_RING_CONFIG_DEFAULT =
+          ACTION_ASSIST + ACTION_DELIMITER
+        + ACTION_NULL   + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+    public static final String PIE_SECOND_LAYER_CONFIG_DEFAULT =
+          ACTION_POWER_MENU    + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_NOTIFICATIONS + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_SEARCH        + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_SCREENSHOT    + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_IME           + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+}

core/java/com/android/internal/util/aicp/AppHelper.java
@@ -1,0 +1,138 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.aicp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.util.Log;
+
+import java.net.URISyntaxException;
+
+public class AppHelper {
+
+    private static final String SETTINGS_METADATA_NAME = "com.android.settings";
+
+    public static String getProperSummary(Context context, PackageManager pm,
+            Resources settingsResources, String action, String values, String entries) {
+
+        if (pm == null || settingsResources == null || action == null) {
+            return context.getResources().getString(
+                com.android.internal.R.string.error_message_title);
+        }
+
+        if (values != null && entries != null) {
+            int resIdEntries = -1;
+            int resIdValues = -1;
+
+            resIdEntries = settingsResources.getIdentifier(
+                        SETTINGS_METADATA_NAME + ":array/" + entries, null, null);
+
+            resIdValues = settingsResources.getIdentifier(
+                        SETTINGS_METADATA_NAME + ":array/" + values, null, null);
+
+            if (resIdEntries > 0 && resIdValues > 0) {
+                try {
+                    String[] entriesArray = settingsResources.getStringArray(resIdEntries);
+                    String[] valuesArray = settingsResources.getStringArray(resIdValues);
+                    for (int i = 0; i < valuesArray.length; i++) {
+                        if (action.equals(valuesArray[i])) {
+                            return entriesArray[i];
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return getFriendlyNameForUri(context, pm, action);
+    }
+
+    public static String getFriendlyActivityName(Context context,
+            PackageManager pm, Intent intent, boolean labelOnly) {
+        ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+        String friendlyName = null;
+
+        if (ai != null) {
+            friendlyName = ai.loadLabel(pm).toString();
+            if (friendlyName == null && !labelOnly) {
+                friendlyName = ai.name;
+            }
+        }
+
+        if (friendlyName == null || friendlyName.startsWith("#Intent;")) {
+            return context.getResources().getString(
+                com.android.internal.R.string.error_message_title);
+        }
+        return friendlyName != null || labelOnly ? friendlyName : intent.toUri(0);
+    }
+
+    public static String getFriendlyShortcutName(
+                Context context, PackageManager pm, Intent intent) {
+        String activityName = getFriendlyActivityName(context, pm, intent, true);
+        String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (activityName == null || activityName.startsWith("#Intent;")) {
+            return context.getResources().getString(
+                com.android.internal.R.string.error_message_title);
+        }
+        if (activityName != null && name != null) {
+            return activityName + ": " + name;
+        }
+        return name != null ? name : intent.toUri(0);
+    }
+
+    public static String getFriendlyNameForUri(
+                Context context, PackageManager pm, String uri) {
+        if (uri == null || uri.startsWith("**")) {
+            return null;
+        }
+
+        try {
+            Intent intent = Intent.parseUri(uri, 0);
+            if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+                return getFriendlyActivityName(context, pm, intent, false);
+            }
+            return getFriendlyShortcutName(context, pm, intent);
+        } catch (URISyntaxException e) {
+        }
+
+        return uri;
+    }
+
+    public static String getShortcutPreferred(
+            Context context, PackageManager pm, String uri) {
+        if (uri == null || uri.startsWith("**")) {
+            return null;
+        }
+        try {
+            Intent intent = Intent.parseUri(uri, 0);
+            String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+            if (name == null || name.startsWith("#Intent;")) {
+                return getFriendlyActivityName(context, pm, intent, false);
+            }
+            return name;
+        } catch (URISyntaxException e) {
+        }
+
+        return uri;
+    }
+
+}

ResurrectionRemix/android_frameworks_base

Signed-off-by: Varun Date <date.varun123@gmail.com>

core/java/com/android/internal/util/rr/ActionConfig.java
@@ -1,0 +1,81 @@
+/*
+* Copyright (C) 2014 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.rr;
+
+public class ActionConfig {
+
+    private String mClickAction;
+    private String mClickActionDescription;
+    private String mLongpressAction;
+    private String mLongpressActionDescription;
+    private String mIconUri;
+
+    public ActionConfig(String clickAction, String clickActionDescription,
+                    String longpressAction, String longpressActionDescription, String iconUri) {
+        mClickAction = clickAction;
+        mClickActionDescription = clickActionDescription;
+        mLongpressAction = longpressAction;
+        mLongpressActionDescription = longpressActionDescription;
+        mIconUri = iconUri;
+    }
+
+    @Override
+    public String toString() {
+        return mClickActionDescription;
+    }
+
+    public String getClickAction() {
+        return mClickAction;
+    }
+
+    public String getClickActionDescription() {
+        return mClickActionDescription;
+    }
+
+    public String getLongpressAction() {
+        return mLongpressAction;
+    }
+
+    public String getLongpressActionDescription() {
+        return mLongpressActionDescription;
+    }
+
+    public String getIcon() {
+        return mIconUri;
+    }
+
+    public void setClickAction(String action) {
+        mClickAction = action;
+    }
+
+    public void setClickActionDescription(String description) {
+        mClickActionDescription = description;
+    }
+
+    public void setLongpressAction(String action) {
+        mLongpressAction = action;
+    }
+
+    public void setLongpressActionDescription(String description) {
+        mLongpressActionDescription = description;
+    }
+
+    public void setIcon(String iconUri) {
+        mIconUri = iconUri;
+    }
+
+}

core/java/com/android/internal/util/rr/ActionConstants.java
@@ -1,0 +1,102 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.rr;
+
+public class ActionConstants {
+
+    // key must fit with the values arrays from Settings to use
+    // SlimActions.java actions
+    public static final String ACTION_HOME                 = "**home**";
+    public static final String ACTION_BACK                 = "**back**";
+    public static final String ACTION_SEARCH               = "**search**";
+    public static final String ACTION_VOICE_SEARCH         = "**voice_search**";
+    public static final String ACTION_MENU                 = "**menu**";
+    public static final String ACTION_MENU_BIG             = "**menu_big**";
+    public static final String ACTION_POWER                = "**power**";
+    public static final String ACTION_NOTIFICATIONS        = "**notifications**";
+    public static final String ACTION_RECENTS              = "**recents**";
+    public static final String ACTION_SCREENSHOT           = "**screenshot**";
+    public static final String ACTION_IME                  = "**ime**";
+    public static final String ACTION_LAST_APP             = "**lastapp**";
+    public static final String ACTION_KILL                 = "**kill**";
+    public static final String ACTION_ASSIST               = "**assist**";
+    public static final String ACTION_VIB                  = "**ring_vib**";
+    public static final String ACTION_SILENT               = "**ring_silent**";
+    public static final String ACTION_VIB_SILENT           = "**ring_vib_silent**";
+    public static final String ACTION_POWER_MENU           = "**power_menu**";
+    public static final String ACTION_TORCH                = "**torch**";
+    public static final String ACTION_EXPANDED_DESKTOP     = "**expanded_desktop**";
+    public static final String ACTION_THEME_SWITCH         = "**theme_switch**";
+    public static final String ACTION_KEYGUARD_SEARCH      = "**keyguard_search**";
+    public static final String ACTION_PIE                  = "**pie**";
+    public static final String ACTION_NAVBAR               = "**nav_bar**";
+    public static final String ACTION_IME_NAVIGATION_LEFT  = "**ime_nav_left**";
+    public static final String ACTION_IME_NAVIGATION_RIGHT = "**ime_nav_right**";
+    public static final String ACTION_IME_NAVIGATION_UP    = "**ime_nav_up**";
+    public static final String ACTION_IME_NAVIGATION_DOWN  = "**ime_nav_down**";
+    public static final String ACTION_CAMERA               = "**camera**";
+    public static final String ACTION_MEDIA_PREVIOUS       = "**media_previous**";
+    public static final String ACTION_MEDIA_NEXT           = "**media_next**";
+    public static final String ACTION_MEDIA_PLAY_PAUSE     = "**media_play_pause**";
+    public static final String ACTION_WAKE_DEVICE          = "**wake_device**";
+
+    // no action
+    public static final String ACTION_NULL            = "**null**";
+
+    // this shorcut constant is only used to identify if the user
+    // selected in settings a custom app...after it is choosed intent uri
+    // is saved in the ButtonConfig object
+    public static final String ACTION_APP          = "**app**";
+
+    public static final String ICON_EMPTY = "empty";
+    public static final String SYSTEM_ICON_IDENTIFIER = "system_shortcut=";
+    public static final String ACTION_DELIMITER = "|";
+
+    public static final String NAVIGATION_CONFIG_DEFAULT =
+          ACTION_BACK    + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY     + ACTION_DELIMITER
+        + ACTION_HOME    + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY     + ACTION_DELIMITER
+        + ACTION_RECENTS + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+    public static final String NAV_RING_CONFIG_DEFAULT =
+          ACTION_ASSIST + ACTION_DELIMITER
+        + ACTION_NULL   + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+    public static final String PIE_SECOND_LAYER_CONFIG_DEFAULT =
+          ACTION_POWER_MENU    + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_NOTIFICATIONS + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_SEARCH        + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_SCREENSHOT    + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_IME           + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+}

core/java/com/android/internal/util/rr/AppHelper.java
@@ -1,0 +1,138 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.rr;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.util.Log;
+
+import java.net.URISyntaxException;
+
+public class AppHelper {
+
+    private static final String SETTINGS_METADATA_NAME = "com.android.settings";
+
+    public static String getProperSummary(Context context, PackageManager pm,
+            Resources settingsResources, String action, String values, String entries) {
+
+        if (pm == null || settingsResources == null || action == null) {
+            return context.getResources().getString(
+                com.android.internal.R.string.error_message_title);
+        }
+
+        if (values != null && entries != null) {
+            int resIdEntries = -1;
+            int resIdValues = -1;
+
+            resIdEntries = settingsResources.getIdentifier(
+                        SETTINGS_METADATA_NAME + ":array/" + entries, null, null);
+
+            resIdValues = settingsResources.getIdentifier(
+                        SETTINGS_METADATA_NAME + ":array/" + values, null, null);
+
+            if (resIdEntries > 0 && resIdValues > 0) {
+                try {
+                    String[] entriesArray = settingsResources.getStringArray(resIdEntries);
+                    String[] valuesArray = settingsResources.getStringArray(resIdValues);
+                    for (int i = 0; i < valuesArray.length; i++) {
+                        if (action.equals(valuesArray[i])) {
+                            return entriesArray[i];
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return getFriendlyNameForUri(context, pm, action);
+    }
+
+    public static String getFriendlyActivityName(Context context,
+            PackageManager pm, Intent intent, boolean labelOnly) {
+        ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+        String friendlyName = null;
+
+        if (ai != null) {
+            friendlyName = ai.loadLabel(pm).toString();
+            if (friendlyName == null && !labelOnly) {
+                friendlyName = ai.name;
+            }
+        }
+
+        if (friendlyName == null || friendlyName.startsWith("#Intent;")) {
+            return context.getResources().getString(
+                com.android.internal.R.string.error_message_title);
+        }
+        return friendlyName != null || labelOnly ? friendlyName : intent.toUri(0);
+    }
+
+    public static String getFriendlyShortcutName(
+                Context context, PackageManager pm, Intent intent) {
+        String activityName = getFriendlyActivityName(context, pm, intent, true);
+        String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (activityName == null || activityName.startsWith("#Intent;")) {
+            return context.getResources().getString(
+                com.android.internal.R.string.error_message_title);
+        }
+        if (activityName != null && name != null) {
+            return activityName + ": " + name;
+        }
+        return name != null ? name : intent.toUri(0);
+    }
+
+    public static String getFriendlyNameForUri(
+                Context context, PackageManager pm, String uri) {
+        if (uri == null || uri.startsWith("**")) {
+            return null;
+        }
+
+        try {
+            Intent intent = Intent.parseUri(uri, 0);
+            if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+                return getFriendlyActivityName(context, pm, intent, false);
+            }
+            return getFriendlyShortcutName(context, pm, intent);
+        } catch (URISyntaxException e) {
+        }
+
+        return uri;
+    }
+
+    public static String getShortcutPreferred(
+            Context context, PackageManager pm, String uri) {
+        if (uri == null || uri.startsWith("**")) {
+            return null;
+        }
+        try {
+            Intent intent = Intent.parseUri(uri, 0);
+            String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+            if (name == null || name.startsWith("#Intent;")) {
+                return getFriendlyActivityName(context, pm, intent, false);
+            }
+            return name;
+        } catch (URISyntaxException e) {
+        }
+
+        return uri;
+    }
+
+}