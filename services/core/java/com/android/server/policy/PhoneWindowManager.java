b69db5898659418e6cfce502990d2b0ecf6f9062
Alex Naidis <alex.naidis@linux.com>
2017-03-03 22:18:47 +0100

    Introduce Keypress Boost
    Common cases of jank are button and key presses.
    So it is useful to introduce a boost which intercepts key
    presses and applies fine tuned boost patterns based on the key/button
    type.
    
    A boost pattern consists of a boost duration and device/chipset
    specific boost params.
    
    Keypress Boost allows specifying a weak and a strong boost pattern.
    The weak boost pattern is applied to common button presses.
    Soft buttons and hardware buttons are handled in this implementation.
    Additionally we send a weak keypress boost for the media play event
    to minimize the media startup latency and for the "enter" event from
    a keyboard.
    
    A strong Keypress Boost is dispatched for the power button press and
    for the camera.
    
    The defined boost patterns for Keypress Boost can be used for other
    boosting features which would be dependant on Keypress Boost.
    
    Change-Id: I3554e38dc4bed68ce189c86c493c3aad9fa801d2
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -206,6 +206,7 @@ import android.service.vr.IPersistentVrStateCallbacks;
 import android.service.gesture.EdgeGestureManager;
 import android.speech.RecognizerIntent;
 import android.telecom.TelecomManager;
+import android.util.BoostFramework;
 import android.util.DisplayMetrics;
 import android.util.EventLog;
 import android.util.Log;
@@ -529,6 +530,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     Handler mHandler;
     WindowState mLastInputMethodWindow = null;
     WindowState mLastInputMethodTargetWindow = null;
+
+    private BoostFramework mPerf = null;
+    private boolean lIsPerfBoostEnabled;
+    private int[] mBoostParamValWeak;
+    private int[] mBoostParamValStrong;
 
     // FIXME This state is shared between the input reader and handler thread.
     // Technically it's broken and buggy but it has been like this for many years
@@ -2256,6 +2262,17 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mAccessibilityShortcutController =
                 new AccessibilityShortcutController(mContext, new Handler(), mCurrentUserId);
         mAlarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+
+        // Initialise Keypress Boost
+        lIsPerfBoostEnabled = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_enableKeypressBoost);
+        mBoostParamValWeak = context.getResources().getIntArray(
+                com.android.internal.R.array.keypressboost_weak_param_value);
+        mBoostParamValStrong = context.getResources().getIntArray(
+                com.android.internal.R.array.keypressboost_strong_param_value);
+        if (lIsPerfBoostEnabled) {
+            mPerf = new BoostFramework();
+        }
 
         // Init display burn-in protection
         boolean burnInProtectionEnabled = context.getResources().getBoolean(
@@ -4789,6 +4806,39 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         return mSearchManager;
     }
 
+    private void dispatchKeypressBoost(int keyCode) {
+        int mBoostDuration = 0;
+        int[] mBoostParamVal = mBoostParamValWeak;
+
+        // Calculate the duration of the boost
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_UNKNOWN:
+                return;
+            case KeyEvent.KEYCODE_APP_SWITCH:
+            case KeyEvent.KEYCODE_BACK:
+            case KeyEvent.KEYCODE_ENTER:
+            case KeyEvent.KEYCODE_HOME:
+            case KeyEvent.KEYCODE_SOFT_LEFT:
+            case KeyEvent.KEYCODE_SOFT_RIGHT:
+                mBoostDuration = 300;
+                break;
+            case KeyEvent.KEYCODE_CAMERA:
+            case KeyEvent.KEYCODE_POWER:
+                mBoostDuration = 500;
+                mBoostParamVal = mBoostParamValStrong;
+                break;
+            case KeyEvent.KEYCODE_MEDIA_PLAY:
+                mBoostDuration = 650;
+                break;
+        }
+
+        // Dispatch the boost
+        if (mBoostDuration != 0) {
+            Slog.i(TAG, "Dispatching Keypress boost for " + mBoostDuration + " ms.");
+            mPerf.perfLockAcquire(mBoostDuration, mBoostParamVal);
+        }
+    }
+
     private void preloadRecentApps() {
         mPreloadedRecentApps = true;
         StatusBarManagerInternal statusbar = getStatusBarManagerInternal();
@@ -6844,6 +6894,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 /*                && !navBarKey */
                 && (appSwitchKey || homeKey || menuKey || backKey)) {
             return 0;
+        }
+
+        // Intercept the Keypress event for Keypress boost
+        if (lIsPerfBoostEnabled) {
+            dispatchKeypressBoost(keyCode);
         }
 
         // Basic policy based on interactive state.
