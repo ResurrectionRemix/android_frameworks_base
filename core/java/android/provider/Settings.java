cf23b7b0403b2e380b60b284dbdf7bd82d02be04
George G <kreach3r@users.noreply.github.com>
2017-03-07 09:56:04 -0500

    base: Force authorize all calling packages by Masquerade (2/3)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8988,6 +8988,12 @@ public static final String PHONE_BLACKLIST_REGEX_ENABLED = "phone_blacklist_rege
         public static final String WEB_ACTION_ENABLED = "web_action_enabled";
 
         /**
+         * Force authorize Substratum (or equivalent) frontend calling packages by Masquerade
+         * @hide
+         */
+        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
