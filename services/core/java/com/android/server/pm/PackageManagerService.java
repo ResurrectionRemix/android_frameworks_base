4c40eacef965b1f40773d292b207146d0ad30aa6
Brian <bdogg718k@gmail.com>
2017-03-08 10:13:05 -0500

    Fix derp

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4580,7 +4580,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         || actionName.startsWith("com.android.internal.telephony.data-reconnect")
                         || actionName.startsWith("android.net.netmon.launchCaptivePortalApp")
                         || actionName.startsWith("intent_navbar_edit")
-                        || actionName.startsWith("org.omnirom.omniswitch") {
+                        || actionName.startsWith("org.omnirom.omniswitch")) {
                     return true;
                 }
             }
