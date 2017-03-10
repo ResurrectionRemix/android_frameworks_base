669850aa34c8e6be05b3e54b6028218ba2aa9255
maxwen <max.weninger@gmail.com>
2017-03-10 03:20:33 +0100

    base: notifications: DEFAULT_LIGHTS and FLAG_SHOW_LIGHTS must count as noisy
    else LED notifications will not correctly work without
    force bypass dnd mode since they are not passing canInterrupt
    in buzzBeepBlinkLocked in NotificationManagerService
    
    Change-Id: I89360adc86426a6d2865a6347a09a0ab233057ea

--- services/core/java/com/android/server/notification/NotificationRecord.java
+++ services/core/java/com/android/server/notification/NotificationRecord.java
@@ -145,8 +145,10 @@ public final class NotificationRecord {
 
         boolean isNoisy = (n.defaults & Notification.DEFAULT_SOUND) != 0
                 || (n.defaults & Notification.DEFAULT_VIBRATE) != 0
+                || (n.defaults & Notification.DEFAULT_LIGHTS) != 0
                 || n.sound != null
-                || n.vibrate != null;
+                || n.vibrate != null
+                || (n.flags & Notification.FLAG_SHOW_LIGHTS) != 0;
         stats.isNoisy = isNoisy;
 
         int importanceLevel = importanceToLevel(importance);
