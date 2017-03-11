7c586a99105c9c1de39a0a6a18df08f41bcf52f3
George G <kreach3r@users.noreply.github.com>
2017-03-11 14:11:54 +0200

    Hold "volume up" during boot to disable all overlays
    Following the way "safe mode" was coded, you can now long press
    "volume up" during boot to automatically disable all overlays
    (from the current system/owner user).
    
    This should come in handy as a global "reset" mechanism.
    
    Example output:
    03-12 03:22:07.090   678   678 D TEST    : Disabling overlay android.GalaxyEvolution
    03-12 03:22:07.176   678   678 D TEST    : Disabling overlay com.android.launcher3.GalaxyEvolution
    03-12 03:22:07.267   678   678 D TEST    : Disabling overlay com.android.server.telecom.GalaxyEvolution
    
    Caveats:
    SystemServer seems to have already loaded a context based on the
    overlays by the time "disableOverlays" is finished, so SystemUI
    turns up themed, even if the overlay is correctly disabled.
    
    In the case of a user using this reset mode to "fix" a SystemUI FC,
    this means that the user will experience one more FC after boot
    (which then would trigger a SystemUI restart, loading the default
    theme this time, and thus ending the FC loop).
    
    Change-Id: I64fb769ea175d37a90a0804f916926b63e5b93ca
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -253,6 +253,8 @@ import android.content.IIntentReceiver;
 import android.content.IIntentSender;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.ConfigurationInfo;
@@ -13805,6 +13807,33 @@ public class ActivityManagerService extends IActivityManager.Stub
         lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_SHOW_FOR_ALL_USERS;
         ((WindowManager)mContext.getSystemService(
                 Context.WINDOW_SERVICE)).addView(v, lp);
+    }
+
+    public final void disableOverlays() {
+        try {
+            IOverlayManager iom = IOverlayManager.Stub.asInterface(
+                    ServiceManager.getService("overlay"));
+            if (iom == null) {
+                return;
+            }
+            Log.d(TAG, "Contacting the Overlay Manager Service for the list of enabled overlays");
+            Map<String, List<OverlayInfo>> allOverlays = iom.getAllOverlays(UserHandle.USER_SYSTEM);
+            if (allOverlays != null) {
+                Log.d(TAG, "The Overlay Manager Service provided the list of enabled overlays");
+                Set<String> set = allOverlays.keySet();
+                for (String targetPackageName : set) {
+                    for (OverlayInfo oi : allOverlays.get(targetPackageName)) {
+                        if (oi.isEnabled()) {
+                            iom.setEnabled(oi.packageName, false, UserHandle.USER_SYSTEM);
+                            Log.d(TAG, "Now disabling \'" + oi.packageName + "\'");
+                        }
+                    }
+                }
+            }
+        } catch (RemoteException re) {
+            re.printStackTrace();
+            Log.d(TAG, "RemoteException while trying to contact the Overlay Manager Service!");
+        }
     }
 
     public void noteWakeupAlarm(IIntentSender sender, int sourceUid, String sourcePkg, String tag) {
