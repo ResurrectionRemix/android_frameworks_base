953680827dad66c376dcd7b137b4bdc9514f6c80
Alex Naidis <alex.naidis@linux.com>
2017-04-01 18:00:50 +0200

    Introduce Rotation Boost
    AOSP dispatches "POWER_HINT_INTERACTION" as part of
    a rotation.
    Since we ignore that power hint in the Qualcomm power
    HAL, we introduce Rotation Boost.
    
    Rotation Boost gets activated before the change of
    rotation gets executed. Once it is reported that
    the rotation is completed, we start the FADE process
    which takes 300ms. After the FADE process is completed,
    we release the acquired perflock.
    As a security timeout (which should never be hit),
    we use 5000ms.
    
    Since a device rotation is heavy, because we need a full
    redraw, we use our generic strong boost parameter
    definition.
    
    We also create a seperate BoostFramework instance,
    rather than reusing the instance from Keypress Boost.
    
    Ticket: NOUGAT-43
    
    Change-Id: I174fde1a158504ac8c52c8dbbc248cb6c696302f
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Nick <nickgunale@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -531,13 +531,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     WindowState mLastInputMethodWindow = null;
     WindowState mLastInputMethodTargetWindow = null;
 
-    private BoostFramework mPerf = null;
+    private BoostFramework mPerfKey = null;
+    private BoostFramework mPerfRotation = null;
     private boolean lIsPerfBoostEnabled;
     private int[] mBoostParamValWeak;
     private int[] mBoostParamValStrong;
     private boolean mKeypressBoostBlocked;
     private long mBoostEventTime = 0L;
     private int mLastBoostDuration = 0;
+    private final int ROTATION_BOOST_TIMEOUT = 5000 /*ms*/;
+    private final int ROTATION_BOOST_FADE = 300 /*ms*/;
 
     // FIXME This state is shared between the input reader and handler thread.
     // Technically it's broken and buggy but it has been like this for many years
@@ -981,6 +984,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     // BoostFramework constants
     private static final int MSG_DISPATCH_KEYPRESS_BOOST_UNBLOCK = 100;
+    private static final int MSG_RELEASE_ROTATION_LOCK = 101;
 
     private class PolicyHandler extends Handler {
         @Override
@@ -1094,6 +1098,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
                 case MSG_DISPATCH_KEYPRESS_BOOST_UNBLOCK:
                     mKeypressBoostBlocked = false;
+                    break;
+                case MSG_RELEASE_ROTATION_LOCK:
+                    mPerfRotation.perfLockRelease();
                     break;
             }
         }
@@ -1251,7 +1258,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             public void run() {
                 // send interaction hint to improve redraw performance
                 mPowerManagerInternal.powerHint(PowerHint.INTERACTION, 0);
+                if (lIsPerfBoostEnabled) {
+                    mPerfRotation.perfLockAcquire(ROTATION_BOOST_TIMEOUT, mBoostParamValStrong);
+                }
                 updateRotation(false);
+                if (lIsPerfBoostEnabled) {
+                    mHandler.sendEmptyMessageDelayed(MSG_RELEASE_ROTATION_LOCK, ROTATION_BOOST_FADE);
+                }
             }
         };
 
@@ -2280,7 +2293,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         lIsPerfBoostEnabled = mBoostParamValWeak.length != 0
                 && mBoostParamValStrong.length != 0;
         if (lIsPerfBoostEnabled) {
-            mPerf = new BoostFramework();
+            mPerfKey = new BoostFramework();
+            mPerfRotation = new BoostFramework();
         }
 
         // Init display burn-in protection
@@ -4846,7 +4860,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (boostDuration != 0) {
             mLastBoostDuration = boostDuration;
             Slog.i(TAG, "Dispatching Keypress boost for " + boostDuration + " ms.");
-            mPerf.perfLockAcquire(boostDuration, boostParamVal);
+            mPerfKey.perfLockAcquire(boostDuration, boostParamVal);
 
             // Block Keypress boost
             mKeypressBoostBlocked = true;
@@ -6929,7 +6943,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     if (mKeypressBoostBlocked && mLastBoostDuration != 0 && reBoostByDiff) {
                         // We have a few milliseconds remaining from our previous boost, release current boost before triggering next one.
                         mHandler.removeMessages(MSG_DISPATCH_KEYPRESS_BOOST_UNBLOCK);
-                        mPerf.perfLockRelease();
+                        mPerfKey.perfLockRelease();
                         mKeypressBoostBlocked = false;
                     }
                 }
