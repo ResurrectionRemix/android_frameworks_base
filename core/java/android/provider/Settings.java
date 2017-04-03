e640ec998f4db204b2c9ca2173ec472631759f57
Varun Date <date.varun123@gmail.com>
2017-04-04 00:55:50 +0530

    Smartbar pixel animation: Ability to customize the dots [1/3]
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4108,6 +4108,36 @@ public final class Settings {
          */
         public static final String HOME_RESIZE_DURATION = "home_resize_anim_duration";
 
+        /**
+         * Pixel Anim Color
+         * @hide
+         */
+        public static final String DOT_TOP_COLOR = "dot_top_color";
+
+        /**
+         * Pixel Anim Color
+         * @hide
+         */
+        public static final String DOT_BOTTOM_COLOR = "dot_bottom_color";
+
+        /**
+         * Pixel Anim Color
+         * @hide
+         */
+        public static final String DOT_LEFT_COLOR = "dot_left_color";
+
+        /**
+         * Pixel Anim Color
+         * @hide
+         */
+        public static final String DOT_RIGHT_COLOR = "dot_right_color";
+
+        /**
+         * Pixel Anim Color
+         * @hide
+         */
+        public static final String DOT_COLOR_SWITCH = "dot_color_switch";
+
  	    /**
          * show clear all recents button
          * @hide
