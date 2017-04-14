904ce140095c4b516f7e24f55e8d82bdea540e61
Anderson Mendes <lge500meu@gmail.com>
2017-04-14 13:27:19 -0300

    fix build

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -326,8 +326,8 @@ status_t BootAnimation::readyToRun() {
     else if (access(THEME_BOOTANIMATION_FILE, R_OK) == 0) {
         mZipFileName = THEME_BOOTANIMATION_FILE;
     }
-    else if (access(getAnimationFileName(IMG_OEM), R_OK) == 0) {
-        mZipFileName = getAnimationFileName(IMG_OEM);
+    else if (access(OEM_BOOTANIMATION_FILE, R_OK) == 0) {
+        mZipFileName = OEM_BOOTANIMATION_FILE;
     }
     else if (access(SYSTEM_BOOTANIMATION_FILE, R_OK) == 0) {
         mZipFileName = SYSTEM_BOOTANIMATION_FILE;
