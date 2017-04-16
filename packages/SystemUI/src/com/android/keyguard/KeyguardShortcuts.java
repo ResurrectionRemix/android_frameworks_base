bdc0aa941a8911cc7eb02bf8f41148de68ced485
SpiritCroc <spiritcroc@gmail.com>
2017-04-16 16:44:44 +0200

    Fix missing lockscreen shortcut icons after reboot
    Change-Id: I8755fab4fd3ee754a3507d695bb32cfa6deb415e
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardShortcuts.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardShortcuts.java
@@ -15,11 +15,15 @@
  */
 package com.android.keyguard;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff.Mode;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.UserHandle;
@@ -55,6 +59,19 @@ public class KeyguardShortcuts extends LinearLayout {
 
     public KeyguardShortcuts(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        // On boot, keyguard is inflated before icons can be loaded.
+        // To work around that, recreate shorcuts on boot completed
+        context.registerReceiver(
+            new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    recreateShortcuts();
+                }
+            },
+            new IntentFilter(Intent.ACTION_LOCKED_BOOT_COMPLETED)
+        );
+
         mContext = context;
         mPackageManager = mContext.getPackageManager();
         mLockPatternUtils = new LockPatternUtils(mContext);
@@ -169,7 +186,7 @@ public class KeyguardShortcuts extends LinearLayout {
             mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS),
                     false, this, UserHandle.USER_ALL);
-            update();
+            recreateShortcuts();
         }
 
         void unobserve() {
@@ -178,15 +195,15 @@ public class KeyguardShortcuts extends LinearLayout {
 
         @Override
         public void onChange(boolean selfChange) {
-            update();
+            recreateShortcuts();
         }
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            update();
+            recreateShortcuts();
         }
 
-        public void update() {
+        public void recreateShortcuts() {
             removeAllViews();
             createShortcuts();
         }
