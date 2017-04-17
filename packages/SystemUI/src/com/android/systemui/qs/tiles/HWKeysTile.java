88921e111b0b9e825ea434bf94b2623167b10966
xyyx <xyyx@mail.ru>
2017-04-17 21:28:41 +0800

    SystemUI: Add tile for enable/disable HW keys
    Signed-off-by: Varun Date <date.varun123@gmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/HWKeysTile.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2017 The Nitrogen Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Intent;
+import android.provider.Settings;
+
+import com.android.systemui.qs.QSTile;
+import com.android.systemui.R;
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+/** Quick settings tile: HWKeys Actions **/
+public class HWKeysTile extends QSTile<QSTile.BooleanState> {
+
+    // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+
+    public HWKeysTile(Host host) {
+        super(host);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        setEnabled(!mState.value);
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_hwkeys_label);
+    }
+
+
+    @Override
+    protected void handleLongClick() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setClassName("com.android.settings",
+            "com.android.settings.Settings$HwKeysSettingsActivity");
+        mHost.startActivityDismissingKeyguard(intent);
+    }
+
+    private void setEnabled(boolean enabled) {
+        Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.HARDWARE_KEYS_DISABLE,
+                enabled ? 0 : 1);
+    }
+
+   private boolean isHWKeysEnabled() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.HARDWARE_KEYS_DISABLE, 0) == 0;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.value = isHWKeysEnabled();
+        if (state.value) {
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_hwkeys_on);
+            state.label =  mContext.getString(
+                    R.string.accessibility_quick_settings_hwkeys_on);
+        } else {
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_hwkeys_off);
+            state.label =  mContext.getString(
+                    R.string.accessibility_quick_settings_hwkeys_off);
+        }
+    }
+
+    @Override
+    public boolean isAvailable() {
+        final int deviceKeys = mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+
+        // read bits for present hardware keys
+        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
+        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
+        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+        final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
+
+        return (hasHomeKey || hasBackKey || hasMenuKey || hasAppSwitchKey);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.QUICK_SETTINGS;
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        if (mState.value) {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_hwkeys_on);
+        } else {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_hwkeys_off);
+        }
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        // Do nothing
+    }
+}
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -62,6 +62,7 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.ImeTile;
+import com.android.systemui.qs.tiles.HWKeysTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocaleTile;
 import com.android.systemui.qs.tiles.LocationTile;
@@ -507,6 +508,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("weather")) return new WeatherTile(this);
         else if (tileSpec.equals("suspend_actions")) return new SuspendActionsTile(this);
         else if (tileSpec.equals("performance") && mNumPerfProfiles > 0) return new PerfProfileTile(this);
+        else if (tileSpec.equals("hwkeys")) return  new HWKeysTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
