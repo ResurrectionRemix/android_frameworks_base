03a9a2ed77ef22134c6436b014f19c2f200b4aa9
Varun Date <date.varun123@gmail.com>
2017-05-03 14:54:50 +0530

    Base:Dont do anything on weather errors for header & keyguard
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
@@ -361,9 +361,7 @@ public class KeyguardStatusView extends GridLayout implements
 
     @Override
     public void weatherError(int errorReason) {
-        if (mWeatherData != null) {
-            setVisibility(View.GONE);
-        }
+             //do nothing
     }
 
     private void refreshBatteryInfo() {
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
@@ -388,9 +388,7 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
 
     @Override
     public void weatherError(int errorReason) {
-        if (mWeatherData != null) {
-            setVisibility(View.GONE);
-        }
+        //do nothing
     }
 
     @Override
