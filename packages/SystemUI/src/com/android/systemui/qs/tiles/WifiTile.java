be8ceccff569e86b03577f7a77ce5f200ec0c3d6
Alex Cruz <du.alexcruz@gmail.com>
2017-05-15 04:08:02 +0200

    WiFiTile: Fix in the off state while entering the detail view
    Steps to duplicate the issue
    
    - Make sure WiFi is off
    - Enable Advanced QS easy toggle
    - Pull down the quick settings panel
    - Long press on the WiFi tile
    
    WiFi should be in the on state just like the location tile because
    you can see wifi scanning in detail view but since the switch is not
    on, you can't select your connection.
    
    Change-Id: Iad472d2b3799c5ca04f5fa980b5dad5e0a2288fd

--- packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
@@ -17,7 +17,6 @@
 package com.android.systemui.qs.tiles;
 
 import android.content.Context;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
@@ -107,7 +106,6 @@ public class WifiTile extends QSTile<QSTile.SignalState> {
         mState.copyTo(mStateBeforeClick);
         MetricsLogger.action(mContext, getMetricsCategory(), !mState.value);
         mController.setWifiEnabled(!mState.value);
-
     }
 
     @Override
@@ -137,6 +135,10 @@ public class WifiTile extends QSTile<QSTile.SignalState> {
             if (!mWifiController.canConfigWifi()) {
                 mHost.startActivityDismissingKeyguard(new Intent(Settings.ACTION_WIFI_SETTINGS));
             } else {
+                if (!mState.value) {
+                    mController.setWifiEnabled(true);
+                    mState.value = true;
+                }
                 showDetail(true);
             }
         } else {
