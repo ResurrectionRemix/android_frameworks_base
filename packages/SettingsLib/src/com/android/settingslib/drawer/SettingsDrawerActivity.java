6afb2ae75bfe3ef8807f3acc86bde6098c0ead3e
ezio84 <brabus84@gmail.com>
2017-05-19 01:04:03 +0200

    Fix lag when opening an entry from the Settings left drawer
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/drawer/SettingsDrawerActivity.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/SettingsDrawerActivity.java
@@ -76,6 +76,7 @@ public class SettingsDrawerActivity extends Activity {
     private SettingsDrawerAdapter mDrawerAdapter;
     private FrameLayout mContentHeaderContainer;
     private DrawerLayout mDrawerLayout;
+    private boolean mOpenTileFromLeftDrawer = false;
     private boolean mShowingMenu;
     private UserManager mUserManager;
 
@@ -120,6 +121,7 @@ public class SettingsDrawerActivity extends Activity {
                     long id) {
                 if(isHeaderEnabled()) {
                    position = position - 1;
+                   mOpenTileFromLeftDrawer = true;
                 }
                 onTileClicked(mDrawerAdapter.getTile(position));
             }
@@ -326,7 +328,13 @@ public class SettingsDrawerActivity extends Activity {
     }
 
     public boolean openTile(Tile tile) {
-        closeDrawer();
+        if (mOpenTileFromLeftDrawer) {
+            // if we open a tile from the left drawer, don't close/animate the drawer
+            // so it will go away with the old activity animation, avoiding lags
+            mOpenTileFromLeftDrawer = false;
+        } else {
+            closeDrawer();
+        }
         if (tile == null) {
             startActivity(new Intent(Settings.ACTION_SETTINGS).addFlags(
                     Intent.FLAG_ACTIVITY_CLEAR_TASK));
