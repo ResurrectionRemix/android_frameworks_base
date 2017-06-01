6d2489cf5e87f15d5ee61fb05aa6e968442d6210
huiwan <huiwan@codeaurora.org>
2017-06-01 17:35:40 +0800

    SystemUI: move time tick to bg handler
    When system is heavy, the updateClock time tick will result in
    ANR. Move it to bg handler to avoid it.
    
    Change-Id: I8390edb8831c88077bf4e7a512b225a12f9482e0
    CRs-Fixed: 2055053

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -247,7 +247,7 @@ public class Clock extends TextView implements DemoMode {
             }
 
             if (mScreenOn) {
-                updateClock();
+                getHandler().post(() -> updateClock());
             }
         }
     };
