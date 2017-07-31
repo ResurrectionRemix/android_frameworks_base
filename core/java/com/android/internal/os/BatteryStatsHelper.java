8c844a1d93d330ad06c8dedf1b114e52ea745590
Libo Jin <liboj@codeaurora.org>
2017-07-31 08:27:03 +0800

    Settings:Bugfix for NPE, Only valid screen power consumption need to smear
    Change-Id: I75622e3f0ad689c6391e3a333959d811a46223ea
    CRs-Fixed: 2083242
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- core/java/com/android/internal/os/BatteryStatsHelper.java
+++ core/java/com/android/internal/os/BatteryStatsHelper.java
@@ -880,7 +880,10 @@ public class BatteryStatsHelper {
             }
         }
 
-        smearScreenBatterySipper(sippers, screenSipper);
+        //Only valid screen power consumption need to smear
+        if(screenSipper != null){
+           smearScreenBatterySipper(sippers, screenSipper);
+        }
 
         return proportionalSmearPowerMah;
     }
