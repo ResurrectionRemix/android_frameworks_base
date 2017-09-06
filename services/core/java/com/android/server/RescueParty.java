efbed50171ca3ba31fe78c7f5de09b9dc97878be
maxwen <max.weninger@gmail.com>
2017-09-07 00:58:11 +0200

    base: rescuce party: check isDisabled on all public API
    Change-Id: Iad73bbbe20c1e9c138c262729d7f3c8309727b83

--- services/core/java/com/android/server/RescueParty.java
+++ services/core/java/com/android/server/RescueParty.java
@@ -164,6 +164,7 @@ public class RescueParty {
      * opportunity to reset any settings depending on our rescue level.
      */
     public static void onSettingsProviderPublished(Context context) {
+        if (isDisabled()) return;
         handleNativeRescuePartyResets();
         executeRescueLevel(context);
     }
