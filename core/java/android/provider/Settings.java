b8e08c9690b2ff9a62a2174bd505ea48fdecd493
xyyx <xyyx@mail.ru>
2017-09-07 11:16:04 +0800

    Ability to disable dashboards conditions (1/2)
    Change-Id: I3e2ca14d83564cc0201cf0f1b9acddd6e75ceef2
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>
    
     Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4393,6 +4393,12 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
         /**
+         * Disable dashboard conditions in settings
+         * @hide
+         */
+        public static final String ENABLE_CONDITIONS = "enable_conditions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
