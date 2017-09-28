aacb3287ed5f6167d48c2bb5d9a75664d9d49f58
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2017-09-28 17:29:54 +0200

    Add Sound tile
    Add Sound tile to make user easier to change sound mode to manner mode.
    
    Sound Tile improvement (beanstown106 <nbenis106@gmail.com>)
    
    *uncoupled zen mode from sound tile let users use dnd for that
    *cycles through ring/vibrate/silent
    *added more descriptive labels for each state instead of always saying sound
    *added longpress action
    
    AICPfy:
    - Adapt to Nougat
    - Bring it to Oreo - @LorDClockaN
    
    Change-Id: I9934e72758f162bd70ca578f6077fcd231fe7a6a
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -47,6 +47,7 @@ import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SyncTile;
+import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -97,6 +98,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("ime")) return new ImeTile(mHost);
         else if (tileSpec.equals("locale")) return new LocaleTile(mHost);
         else if (tileSpec.equals("music")) return new MusicTile(mHost);
+        else if (tileSpec.equals("sound")) return new SoundTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
