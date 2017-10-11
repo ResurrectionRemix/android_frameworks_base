1c6799c6b44f1e507fca8a46a9d669783dfb129a
LorDClockaN <lordclockan@gmail.com>
2017-10-11 14:30:25 +0200

    FWB: Toast icon switch [1/2]
    Change-Id: I59ffd10f20a2ae9491f76380ec9af641e0844928
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4245,6 +4245,12 @@ public final class Settings {
         public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
 
         /**
+         * Toast icon switch
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
