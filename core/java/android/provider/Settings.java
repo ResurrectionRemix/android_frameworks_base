d673b9a5159e554a7d10256e45b846e1ee94b248
ezio84 <brabus84@gmail.com>
2017-11-30 17:30:29 +0100

    DUI Smartbar double tap to sleep [1/3]
    Change-Id: I2f67f43937c4d17fe89e866568bf299337a3b83c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5797,6 +5797,12 @@ public final class Settings {
         public static final String FLING_KEYBOARD_CURSORS = "fling_keyboard_cursors";
 
         /**
+         * Whether to enable double tap to sleep for smartbar
+         * @hide
+         */
+        public static final String SMARTBAR_DOUBLETAP_SLEEP = "smartbar_doubletap_sleep";
+
+        /**
          * A flag containing settings used for biometric weak
          * @hide
          */
