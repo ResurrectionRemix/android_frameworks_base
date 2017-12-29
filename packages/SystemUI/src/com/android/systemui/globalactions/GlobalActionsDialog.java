1ba1d2f9e6fff685038f562c702cb67bfa07dd6e
ezio84 <brabus84@gmail.com>
2017-12-29 02:52:27 -0500

    Don't add the emergency button (e.g. with some indians carriers)
    Change-Id: I4687639bfd6d6f71ea1864836f2ab27d8a10320c

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -411,9 +411,9 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
             addedKeys.add(actionKey);
         }
 
-        if (mEmergencyAffordanceManager.needsEmergencyAffordance()) {
+        /*if (mEmergencyAffordanceManager.needsEmergencyAffordance()) {
             mItems.add(getEmergencyAction());
-        }
+        }*/
 
         mAdapter = new MyAdapter();
 
