1bd30823aa8d2324c99a95efe3c7ec6d7b117f73
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-03 19:20:36 -0200

    GlobalActions: Remove custom strings in dialogs
    Some translations looks ugly with new power menu style, so make it look like AOSP

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
@@ -97,12 +97,14 @@ public class GlobalActionsImpl implements GlobalActions {
         bar.getIndeterminateDrawable().setTint(color);
         TextView message = d.findViewById(R.id.text1);
         message.setTextColor(color);
-        if (reason != null && PowerManager.REBOOT_BOOTLOADER.equals(reason)) {
-            message.setText(com.android.internal.R.string.reboot_to_bootloader_message);
-        } else if (reason != null && PowerManager.REBOOT_RECOVERY.equals(reason)) {
-            message.setText(com.android.internal.R.string.reboot_to_recovery_message);
-        } else if (isReboot) {
-            message.setText(R.string.reboot_message);
+
+        boolean mIsReboot = isReboot;
+        if (reason != null && (PowerManager.REBOOT_BOOTLOADER.equals(reason) || PowerManager.REBOOT_RECOVERY.equals(reason))) {
+            mIsReboot = true;
+        }
+
+        if (mIsReboot){
+             message.setText(R.string.reboot_to_reset_message);
         }
 
         Point displaySize = new Point();
