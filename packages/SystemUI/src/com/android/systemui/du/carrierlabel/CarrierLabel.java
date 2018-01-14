49505ac960a6e740232cc1927898cc9eb2e9999b
Ali B <abittin@gmail.com>
2018-01-14 07:16:21 -0500

    Adapt carrier label to DarkController
    - Parts of the code adapted from http://gerrit.aicp-rom.com/#/c/53587/
    
    Change-Id: Ifc8f61e8f490510e9e14e4e07f643b94f45e2a47

--- packages/SystemUI/src/com/android/systemui/du/carrierlabel/CarrierLabel.java
+++ packages/SystemUI/src/com/android/systemui/du/carrierlabel/CarrierLabel.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.ContentObserver;
+import android.graphics.Rect;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -36,13 +37,16 @@ import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.util.du.Utils;
 
 import com.android.systemui.R;
+import com.android.systemui.Dependency;
 import com.android.systemui.du.carrierlabel.SpnOverride;
+import com.android.systemui.statusbar.policy.DarkIconDispatcher;
+import com.android.systemui.statusbar.policy.DarkIconDispatcher.DarkReceiver;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
 
-public class CarrierLabel extends TextView {
+public class CarrierLabel extends TextView implements DarkReceiver {
 
     private Context mContext;
     private boolean mAttached;
@@ -65,7 +69,7 @@ public class CarrierLabel extends TextView {
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-
+        Dependency.get(DarkIconDispatcher.class).addDarkReceiver(this);
         if (!mAttached) {
             mAttached = true;
             IntentFilter filter = new IntentFilter();
@@ -78,10 +82,16 @@ public class CarrierLabel extends TextView {
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
+        Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(this);
         if (mAttached) {
             mContext.unregisterReceiver(mIntentReceiver);
             mAttached = false;
         }
+    }
+
+    @Override
+    public void onDarkChanged(Rect area, float darkIntensity, int tint) {
+        setTextColor(DarkIconDispatcher.getTint(area, this, tint));
     }
 
     private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
@@ -136,6 +146,6 @@ public class CarrierLabel extends TextView {
         if (TextUtils.isEmpty(operatorName)) {
             operatorName = telephonyManager.getSimOperatorName();
         }
-        return operatorName.toUpperCase();
+        return operatorName;
     }
 }
