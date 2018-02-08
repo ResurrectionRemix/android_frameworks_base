45bc8e63402e71e32966b6a5bd7f318b75d04a69
Alex Cruz <du.alexcruz@gmail.com>
2018-02-08 15:15:14 +0100

    Power menu: improve longpress action on some buttons
    trigger a single press action if the longpress one
    is not available
    also don't dismiss the dialog if not needed
    
    Change-Id: I60b4a2b58e75c1e0bfca0d271b4cdd4c83e5633d

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -347,6 +347,10 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             public boolean showBeforeProvisioning() {
                 return false;
             }
+
+            public boolean onLongPress() {
+                return false;
+            }
         };
         onAirplaneModeChanged();
 
@@ -547,10 +551,11 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         @Override
         public boolean onLongPress() {
             UserManager um = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
-            if (!um.hasUserRestriction(UserManager.DISALLOW_SAFE_BOOT)) {
+            /*if (!um.hasUserRestriction(UserManager.DISALLOW_SAFE_BOOT)) {
+                mDialog.dismiss();
                 mWindowManagerFuncs.reboot(true);
                 return true;
-            }
+            }*/
             return false;
         }
 
@@ -652,6 +657,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         public boolean onLongPress() {
             UserManager um = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
             if (!um.hasUserRestriction(UserManager.DISALLOW_SAFE_BOOT)) {
+                mDialog.dismiss();
                 mWindowManagerFuncs.reboot(true);
                 return true;
             }
@@ -708,6 +714,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         @Override
         public boolean onLongPress() {
             //if (FeatureFlagUtils.isEnabled(mContext, FeatureFlagUtils.SCREENRECORD_LONG_PRESS)) {
+                mDialog.dismiss();
                 mScreenRecordHelper.launchRecordPrompt();
             /*} else {
                 onPress();
@@ -1096,7 +1103,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         public boolean onLongClickItem(int position) {
             final Action action = mAdapter.getItem(position);
             if (action instanceof LongPressAction) {
-                mDialog.dismiss();
+                //mDialog.dismiss();
                 return ((LongPressAction) action).onLongPress();
             }
             return false;
@@ -1443,7 +1450,8 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
 
         @Override
         public boolean onLongPress() {
-            return true;
+            // pass to onClick listener (onPress action)
+            return false;
         }
 
         public boolean isEnabled() {
