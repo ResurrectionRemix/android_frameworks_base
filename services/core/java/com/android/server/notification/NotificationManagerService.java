614016c202c901a4ec2ebb8693346b8cd927bdd6
Sam Mortimer <sam@mortimer.me.uk>
2018-02-16 00:51:38 -0800

    frameworks/base lights: Let Lineage lights decide if notification led is on/off
    *) Allows us to cater for the case where the notification led is turned off
       and yet the user wants to adjust battery light settings (which requires
       posting led notifications to illustrate the change).
    
    Change-Id: Iea52feb907f99ecf7925aff1fc1baf008642f7d7

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -5116,8 +5116,7 @@ public class NotificationManagerService extends SystemService {
 
         NotificationRecord.Light light = ledNotification != null ?
                 ledNotification.getLight() : null;
-        if (ledNotification == null || mLineageNotificationLights == null ||
-                light == null || !mNotificationPulseEnabled) {
+        if (ledNotification == null || mLineageNotificationLights == null || light == null) {
             mNotificationLight.turnOff();
             return;
         }
