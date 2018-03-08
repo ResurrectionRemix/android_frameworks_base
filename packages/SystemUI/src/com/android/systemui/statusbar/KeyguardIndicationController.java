961e6bdb1c28260f3c00a2178001f700f89a07d7
ezio84 <brabus84@gmail.com>
2018-03-08 13:02:48 +0100

    Ambient battery level: use a ⚡ symbol when charging
    Change-Id: Ib36060e9a3b0ab54d0bcfa725979e2c145039836

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -299,7 +299,10 @@ public class KeyguardIndicationController {
                     mTextView.setTextColor(Color.WHITE);
                     mTextView.switchIndication(mTransientIndication);
                 } else {
-                    CharSequence chargeIndicator = (mPowerPluggedIn ? "~" : "") +
+                    // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
+                    // to load its emoji colored variant with the uFE0E flag
+                    String bolt = "\u26A1\uFE0E";
+                    CharSequence chargeIndicator = (mPowerPluggedIn ? (bolt + " ") : "") +
                             NumberFormat.getPercentInstance().format(mLevel / 100f);
                     mTextView.switchIndication(chargeIndicator);
                 }
