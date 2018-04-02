a748ba0f9d923cb51e7dbd73d3909d34f20da452
Daniel Micay <danielmicay@gmail.com>
2018-04-02 12:55:28 -0400

    Use Cloudflare DNS servers as defaults for tethering
    Cloudflare DNS has a better privacy policy than Google Public DNS and
    has DNS-over-TLS and DNS-over-HTTPS so it won't be a downgrade when
    Android ships one of them.
    
    Change-Id: I29511960f286a49cf8c9899fa373690458940d87
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- services/core/java/com/android/server/connectivity/tethering/TetheringConfiguration.java
+++ services/core/java/com/android/server/connectivity/tethering/TetheringConfiguration.java
@@ -80,7 +80,7 @@ public class TetheringConfiguration {
         "192.168.48.2", "192.168.48.254", "192.168.49.2", "192.168.49.254",
     };
 
-    private final String[] DEFAULT_IPV4_DNS = {"8.8.4.4", "8.8.8.8"};
+    private final String[] DEFAULT_IPV4_DNS = {"1.0.0.1", "1.1.1.1"};
 
     public final String[] tetherableUsbRegexs;
     public final String[] tetherableWifiRegexs;
