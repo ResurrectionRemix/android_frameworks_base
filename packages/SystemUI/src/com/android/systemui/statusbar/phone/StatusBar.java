6a23c0356a6d2cc2ec6e80f817959341814e4306
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2018-04-08 12:19:15 -0400

    Fix some refresh issues when changing an overlay
    e.g. switch from light theme to dark or viceversa: bottom lockscreen
    elements like charging info, aosp fingerprint unlock hints, or some
    notifications text colors are broken.
    
    This will fix above issues.
    
    TODO: reinflateViews gets called twice (under updateTheme() and
    onOverlayChanged(), we can probably clean this up in the future
    
    Change-Id: I9e31b41ef72fa988261009595f488b49051ac309

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1666,12 +1666,12 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     @Override
     public void onOverlayChanged() {
-        reinflateViews();
         updateNotificationsOnOverlayChanged();
         mStackScroller.onOverlayChanged();
         mNotificationShelf.onOverlayChanged();
         mNotificationPanel.onOverlayChanged();
         Dependency.get(DarkIconDispatcher.class).onOverlayChanged(mContext);
+        reinflateViews();
     }
 
     private void reinflateViews() {
