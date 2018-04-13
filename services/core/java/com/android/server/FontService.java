acf8c5872e93dd2f32690fb1c061ba87250d4375
maxwen <max.weninger@gmail.com>
2018-04-14 00:06:05 +0200

    Add progress for font apply [1/2]
    since this can take a few seconds its nice to give
    user some feedback that things are going on.
    so add a broadcast for change font. This is still
    not exactly sent when font apply is read but gives
    some approx information
    
    Edit by Invictrix: protect the broadcast
    
    Change-Id: I86d31b7a0652261560b87bd17b2e106648e3a62c

--- services/core/java/com/android/server/FontService.java
+++ services/core/java/com/android/server/FontService.java
@@ -466,6 +466,10 @@ public class FontService extends IFontService.Stub {
             } else {
                 copyFonts(info);
             }
+            Intent intent = new Intent("com.android.server.ACTION_FONT_CHANGED");
+            intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY
+                        | Intent.FLAG_RECEIVER_FOREGROUND);
+            mContext.sendBroadcastAsUser(intent, UserHandle.CURRENT);
         } finally {
             Binder.restoreCallingIdentity(ident);
         }
