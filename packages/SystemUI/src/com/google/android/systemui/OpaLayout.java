610e89d57fe0c56aba275f57e1229f513bae8a8f
Harsh Shandilya <harsh@prjkt.io>
2018-04-24 00:36:02 +0530

    OpaLayout: Set PorterDuff mode to SRC_ATOP
    Should fix home button not taking on accent tint
    with some themes.
    
    Change-Id: Ibf478d5b01c46ff3ca314ff365db60dcbb9b63e8
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
+++ packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
@@ -702,7 +702,7 @@ public class OpaLayout extends FrameLayout implements ButtonInterface, Tunable {
 
         Drawable drawHomeIcon = res.getDrawable(intHomeDrawable);
         drawHomeIcon.setColorFilter(null);
-        drawHomeIcon.setColorFilter(homeColor, PorterDuff.Mode.SRC_IN);
+        drawHomeIcon.setColorFilter(homeColor, PorterDuff.Mode.SRC_ATOP);
 
         if (isLightColor(homeColor)) {
             ShadowKeyDrawable withShadow = new ShadowKeyDrawable(drawHomeIcon.mutate());
