f59967cf36dd79123a51034cf20509f3c6169dbe
zhenchao <zhenchao@codeaurora.org>
2018-05-28 18:20:45 +0800

    Bluetooth: Serialize sending sendBluetoothServiceUpCallback
    In some corner case, Register Adpater and bluetooth service up happen
    simultaneously. In such scenario it is possible that bluetooth service
    connected message is sent earlier than Register adpater message leading
    to callback not sent to client whose registration is still in progress.
    
    Change-Id: I129f841ce28d943c37011932d1b0dbcb58c96422
    CRs-Fixed: 2616935
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -114,6 +114,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
     private static final int MESSAGE_DISABLE = 2;
     private static final int MESSAGE_REGISTER_ADAPTER = 20;
     private static final int MESSAGE_UNREGISTER_ADAPTER = 21;
+    private static final int MESSAGE_INFORM_ADAPTER_SERVICE_UP = 22;
     private static final int MESSAGE_REGISTER_STATE_CHANGE_CALLBACK = 30;
     private static final int MESSAGE_UNREGISTER_STATE_CHANGE_CALLBACK = 31;
     private static final int MESSAGE_BLUETOOTH_SERVICE_CONNECTED = 40;
@@ -1480,6 +1481,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
      */
     private void sendBluetoothServiceUpCallback() {
         try {
+            mBluetoothLock.writeLock().lock();
             int n = mCallbacks.beginBroadcast();
             Slog.d(TAG, "Broadcasting onBluetoothServiceUp() to " + n + " receivers.");
             for (int i = 0; i < n; i++) {
@@ -1491,6 +1493,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
             }
         } finally {
             mCallbacks.finishBroadcast();
+            mBluetoothLock.writeLock().unlock();
         }
     }
 
@@ -1757,13 +1760,20 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                     break;
 
                 case MESSAGE_REGISTER_ADAPTER: {
+                    if (DBG) Slog.d(TAG,"MESSAGE_REGISTER_ADAPTER");
                     IBluetoothManagerCallback callback = (IBluetoothManagerCallback) msg.obj;
                     mCallbacks.register(callback);
                     break;
                 }
                 case MESSAGE_UNREGISTER_ADAPTER: {
+                    if (DBG) Slog.d(TAG,"MESSAGE_UNREGISTER_ADAPTER");
                     IBluetoothManagerCallback callback = (IBluetoothManagerCallback) msg.obj;
                     mCallbacks.unregister(callback);
+                    break;
+                }
+                case MESSAGE_INFORM_ADAPTER_SERVICE_UP: {
+                    if (DBG) Slog.d(TAG,"MESSAGE_INFORM_ADAPTER_SERVICE_UP");
+                    sendBluetoothServiceUpCallback();
                     break;
                 }
                 case MESSAGE_REGISTER_STATE_CHANGE_CALLBACK: {
@@ -1835,7 +1845,9 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                             Slog.e(TAG, "Unable to register BluetoothCallback", re);
                         }
                         //Inform BluetoothAdapter instances that service is up
-                        sendBluetoothServiceUpCallback();
+                        Message informMsg =
+                                    mHandler.obtainMessage(MESSAGE_INFORM_ADAPTER_SERVICE_UP);
+                        mHandler.sendMessage(informMsg);
 
                         //Do enable request
                         try {
