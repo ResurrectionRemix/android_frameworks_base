c3190d5b772b70571b0e5839f009936298419d3d
LuK1337 <priv.luk@gmail.com>
2018-06-23 13:18:27 +0200

    SystemUI: CellularTile: Don't call showDetail() when device is locked
    * We recently enabled dualTarget for CellularTile which allowed us
      to toggle mobile data without unlocking device. Moving showDetail
      call to postQSRunnableDismissingKeyguard when the device is locked
      fixes this issue.
    
    Change-Id: I16dc766166a33a309a2fc945e81fa15b5d194196

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -129,6 +129,12 @@ public class CellularTile extends QSTileImpl<SignalState> {
     @Override
     protected void handleSecondaryClick() {
         if (mDataController.isMobileDataSupported()) {
+            if (mKeyguardMonitor.isSecure() && !mKeyguardMonitor.canSkipBouncer()) {
+                mActivityStarter.postQSRunnableDismissingKeyguard(() -> {
+                    showDetail(true);
+                });
+                return;
+            }
             showDetail(true);
         } else {
             mActivityStarter
