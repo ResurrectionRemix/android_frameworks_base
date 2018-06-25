a7880108fb1f314b1bd36207a95b562486b449de
10011283 <zhang.pengbo@zte.com.cn>
2018-06-25 09:25:54 +0800

    InputMethodManager: Fixed memory leak when windowDismissed
    Release reference to rootview when window dismissed
    to avoid memory leak.
    
    Test: N/A
    
    Change-Id: I19c7da85f7551adaf6427885fc60105c5202824e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- core/java/android/view/inputmethod/InputMethodManager.java
+++ core/java/android/view/inputmethod/InputMethodManager.java
@@ -1395,6 +1395,10 @@ public final class InputMethodManager {
     public void windowDismissed(IBinder appWindowToken) {
         checkFocus();
         synchronized (mH) {
+            if (mCurRootView != null
+                    && mCurRootView.getWindowToken() == appWindowToken) {
+                mCurRootView = null;
+            }
             if (mServedView != null &&
                     mServedView.getWindowToken() == appWindowToken) {
                 finishInputLocked();
