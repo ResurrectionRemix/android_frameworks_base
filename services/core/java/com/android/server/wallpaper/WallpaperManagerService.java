791a57e236f0891dda75f7fc1d075e0dda93aa65
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-08-14 21:54:51 -0400

    WallpaperManagerService: Fix theme switching
    * When default wallpaper is applied (first boot case), the WallpaperColors is null, and theme switching doesn't work until user applies a new wallpaper, so create a new instance of WallpaperColors and never return null.
    
    Change-Id: I77f3b112dec91cfb4984980dcedf451518847134
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- services/core/java/com/android/server/wallpaper/WallpaperManagerService.java
+++ services/core/java/com/android/server/wallpaper/WallpaperManagerService.java
@@ -376,7 +376,8 @@ public class WallpaperManagerService extends IWallpaperManager.Stub
      */
     private boolean needUpdateLocked(WallpaperColors colors, int themeMode) {
         if (colors == null) {
-            return false;
+            colors = new WallpaperColors(Color.valueOf(Color.WHITE), null, null, 0);
+            //return false;
         }
 
         if (themeMode == mThemeMode) {
@@ -589,8 +590,9 @@ public class WallpaperManagerService extends IWallpaperManager.Stub
      */
     private WallpaperColors getThemeColorsLocked(WallpaperColors colors) {
         if (colors == null) {
-            Slog.w(TAG, "Cannot get theme colors because WallpaperColors is null.");
-            return null;
+            colors = new WallpaperColors(Color.valueOf(Color.WHITE), null, null, 0);
+            //Slog.w(TAG, "Cannot get theme colors because WallpaperColors is null.");
+            //return null;
         }
 
         int colorHints = colors.getColorHints();
