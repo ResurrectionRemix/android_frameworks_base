d4e16e7435d20a1559bf18d9acee09091a7091b7
xyyx <xyyx@mail.ru>
2018-08-29 18:19:06 +0800

    Disable suggestions (1/2)
    Change-Id: I0556f3508347536f6e06c67bf3d7f333724113a5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4399,6 +4399,12 @@ public final class Settings {
         public static final String ENABLE_CONDITIONS = "enable_conditions";
 
         /**
+         * Disable dashboard suggestions in settings
+         * @hide
+         */
+        public static final String ENABLE_SUGGESTIONS = "enable_suggestions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
