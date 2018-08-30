dff0f7a2ac2c7b305d33a889e90a35694168d59a
xyyx <xyyx@mail.ru>
2018-08-30 23:34:46 +0800

    BatteryMeter: set mForceShowPercent to false when battery styles changed
    Change-Id: I6c19f235577b7e33df158e1f18c7fa361ad7272b
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -236,7 +236,7 @@ public class BatteryMeterView extends LinearLayout implements
     @Override
     public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
 
-        if (isCircleBattery()) {
+        if (isCircleBattery()  || mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT) {
             setForceShowPercent(pluggedIn && !isBatteryPercentHidden);
         }
 
@@ -418,6 +418,12 @@ public class BatteryMeterView extends LinearLayout implements
                     addView(mBatteryIconView, mlp);
                 }
                 break;
+        }
+
+        if (style == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT) {
+            mForceShowPercent = true;
+        } else {
+            mForceShowPercent = false;
         }
         updateShowPercent();
         onDensityOrFontScaleChanged();
