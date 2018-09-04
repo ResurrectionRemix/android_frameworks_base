6b1545c656f471b24c41fbf95e54c95e62032c72
Joe Maples <joe@frap129.org>
2018-09-04 11:45:58 -0500

    AppStandbyController: Only check user standby setting
    APP_STANDBY_ENABLED isn't a user-facing toggle, it's controlled by
    Turbo (or a similar app for other OEMs). Remove the check for it to
    actually enable doze/battery manager on user request.
    
    Change-Id: I933a26032e48761f517b2a4c4b3fe86181811dd5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- services/usage/java/com/android/server/usage/AppStandbyController.java
+++ services/usage/java/com/android/server/usage/AppStandbyController.java
@@ -1541,8 +1541,8 @@ public class AppStandbyController {
             final boolean buildFlag = mContext.getResources().getBoolean(
                     com.android.internal.R.bool.config_enableAutoPowerModes);
             final boolean runtimeFlag = Global.getInt(mContext.getContentResolver(),
-                    Global.APP_STANDBY_ENABLED, 1) == 1
-                    && Global.getInt(mContext.getContentResolver(),
+                    /*Global.APP_STANDBY_ENABLED, 1) == 1
+                    && Global.getInt(mContext.getContentResolver(),*/
                     Global.ADAPTIVE_BATTERY_MANAGEMENT_ENABLED, 1) == 1;
             return buildFlag && runtimeFlag;
         }
@@ -1784,7 +1784,7 @@ public class AppStandbyController {
         void registerObserver() {
             final ContentResolver cr = mContext.getContentResolver();
             cr.registerContentObserver(Global.getUriFor(Global.APP_IDLE_CONSTANTS), false, this);
-            cr.registerContentObserver(Global.getUriFor(Global.APP_STANDBY_ENABLED), false, this);
+            /*cr.registerContentObserver(Global.getUriFor(Global.APP_STANDBY_ENABLED), false, this);*/
             cr.registerContentObserver(Global.getUriFor(Global.ADAPTIVE_BATTERY_MANAGEMENT_ENABLED),
                     false, this);
         }
@@ -1797,9 +1797,9 @@ public class AppStandbyController {
 
         void updateSettings() {
             if (DEBUG) {
-                Slog.d(TAG,
+                /*Slog.d(TAG,
                         "appidle=" + Global.getString(mContext.getContentResolver(),
-                                Global.APP_STANDBY_ENABLED));
+                                Global.APP_STANDBY_ENABLED));*/
                 Slog.d(TAG,
                         "adaptivebat=" + Global.getString(mContext.getContentResolver(),
                                 Global.ADAPTIVE_BATTERY_MANAGEMENT_ENABLED));
