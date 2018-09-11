69aca03995db5404fd448c5eb4079a6a0bd25585
xyyx <xyyx@mail.ru>
2018-09-11 17:04:31 +0800

    BatteryStatsImpl.java: Stop resetting battery stats after reboot with battery level >= 90
    Change-Id: I77d3426c1af4abef05ed924c0859a9d71d1c77ec
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- core/java/com/android/internal/os/BatteryStatsImpl.java
+++ core/java/com/android/internal/os/BatteryStatsImpl.java
@@ -12374,7 +12374,7 @@ public class BatteryStatsImpl extends BatteryStats {
             // level to a now very high level).
             boolean reset = false;
             if (!mNoAutoReset && (oldStatus == BatteryManager.BATTERY_STATUS_FULL
-                    || level >= 90
+                    || level >= 99
                     || (mDischargeCurrentLevel < 20 && level >= 80)
                     || (getHighDischargeAmountSinceCharge() >= 200
                             && mHistoryBuffer.dataSize() >= MAX_HISTORY_BUFFER))) {
