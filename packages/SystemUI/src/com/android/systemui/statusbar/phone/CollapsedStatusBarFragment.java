c1633a21d1ae71a4288165645b30550612925ec3
maxwen <max.weninger@gmail.com>
2018-09-13 14:47:41 +0200

    base: SystemUI: tuner: fix hiding of statusbar clock
    clock in expanded quick settings will not be hidden
    from tuner setting
    
    Change-Id: Ic5db96fba79e5bc77282b70398e7f8055b998892
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -37,6 +37,7 @@ import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.StatusBarState;
 import com.android.systemui.statusbar.phone.StatusBarIconController.DarkIconManager;
+import com.android.systemui.statusbar.policy.Clock;
 import com.android.systemui.statusbar.policy.EncryptionHelper;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
 import com.android.systemui.statusbar.policy.NetworkController;
@@ -60,7 +61,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private KeyguardMonitor mKeyguardMonitor;
     private NetworkController mNetworkController;
     private LinearLayout mSystemIconArea;
-    private View mClockView;
+    private Clock mClockView;
     private View mNotificationIconAreaInner;
     private View mNetworkTrafficHolder;
     private View mCenteredIconArea;
@@ -109,7 +110,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         Dependency.get(StatusBarIconController.class).addIconGroup(mDarkIconManager);
         mSystemIconArea = mStatusBar.findViewById(R.id.system_icon_area);
         mNetworkTrafficHolder = mStatusBar.findViewById(R.id.network_traffic_holder);
-        mClockView = mStatusBar.findViewById(R.id.clock);
+        mClockView = (Clock) mStatusBar.findViewById(R.id.clock);
         mBatteryBars[0] = mStatusBar.findViewById(R.id.battery_bar);
         mBatteryBars[1] = mStatusBar.findViewById(R.id.battery_bar_1);
         showSystemIconArea(false);
@@ -271,11 +272,15 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     }
 
     public void hideClock(boolean animate) {
-        animateHiddenState(mClockView, clockHiddenMode(), animate);
+        if (mClockView.isClockVisible()) {
+            animateHiddenState(mClockView, clockHiddenMode(), animate);
+        }
     }
 
     public void showClock(boolean animate) {
-        animateShow(mClockView, animate);
+        if (mClockView.isClockVisible()) {
+            animateShow(mClockView, animate);
+        }
     }
 
     /**
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -79,6 +79,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
 
     private boolean mClockVisibleByPolicy = true;
     private boolean mClockVisibleByUser = true;
+    private boolean mClockHideableByUser = true;
 
     private boolean mAttached;
     private Calendar mCalendar;
@@ -271,8 +272,10 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
     }
 
     public void setClockVisibleByUser(boolean visible) {
-        mClockVisibleByUser = visible;
-        updateClockVisibility();
+        if (mClockHideableByUser) {
+            mClockVisibleByUser = visible;
+            updateClockVisibility();
+        }
     }
 
     public void setClockVisibilityByPolicy(boolean visible) {
@@ -288,6 +291,14 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
         boolean visible = shouldBeVisible();
         int visibility = visible ? View.VISIBLE : View.GONE;
         super.setVisibility(visibility);
+    }
+
+    public boolean isClockVisible() {
+        return mClockVisibleByPolicy && mClockVisibleByUser;
+    }
+
+    public void setClockHideableByUser(boolean value) {
+        mClockHideableByUser = value;
     }
 
     final void updateClock() {
