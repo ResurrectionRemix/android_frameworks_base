b7de6623b85cad09c5253c0a9c91d2e20897dfdc
Terigele <terigele@huawei.com>
2018-09-21 17:28:23 +0800

    PackageManagerService: update client app info when related shared library updated.
    Once a shared library is updated, the client apps which announce
    using the library will not be able to find this library anymore,
    because the PackageParser.Package.usesLibraryFiles of client app
    is not updated.
    
    Test: manual:
    1. install lib and client from /frameworks/base/tests/SharedLibrary/
    2. install -r lib(reinstall the lib), the client will not be automatically removed.
    3. client can launch successfully, which is the same as Android N version
    
    Change-Id: I8fb7e03cd039c6d9f4fb4cbe13d8538585fd7186
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -9870,6 +9870,15 @@ public class PackageManagerService extends IPackageManager.Stub
                     }
 
                     PackageParser.Package libPkg = mPackages.get(libEntry.apk);
+                    if (changingLib != null && changingLib.packageName.equals(libEntry.apk)) {
+                        // If we are doing this while in the middle of updating a library apk,
+                        // then we need to make sure to use that new apk for determining the
+                        // dependencies here.  (We haven't yet finished committing the new apk
+                        // to the package manager state.)
+                        if (libPkg == null || libPkg.packageName.equals(changingLib.packageName)) {
+                            libPkg = changingLib;
+                        }
+                    }
                     if (libPkg == null) {
                         throw new PackageManagerException(INSTALL_FAILED_MISSING_SHARED_LIBRARY,
                                 "Package " + packageName + " requires unavailable static shared"
@@ -9937,7 +9946,7 @@ public class PackageManagerService extends IPackageManager.Stub
     }
 
     private static boolean hasString(List<String> list, List<String> which) {
-        if (list == null) {
+        if (list == null || which == null) {
             return false;
         }
         for (int i=list.size()-1; i>=0; i--) {
@@ -9959,7 +9968,7 @@ public class PackageManagerService extends IPackageManager.Stub
                     && !hasString(pkg.usesOptionalLibraries, changingPkg.libraryNames)
                     && !ArrayUtils.contains(pkg.usesStaticLibraries,
                             changingPkg.staticSharedLibName)) {
-                return null;
+                continue;
             }
             if (res == null) {
                 res = new ArrayList<>();
