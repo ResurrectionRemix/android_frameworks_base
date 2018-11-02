3edf037e9462b728d028ea9b0248ccb8c15a7b3e
Bill Peckham <bpeckham@google.com>
2018-11-02 16:46:03 -0700

    Adapt to IMS registration changes.
    The upstream change
    https://keystone-qcom.googlesource.com/platform/frameworks/base/+/941c0a5cfd5cdfe8a19c2928486fab9efa489ada
    modified the class to use when registering with ImsManager from
    android.telephony.ims.stub.ImsRegistrationImplBase.Callback to
    android.telephony.ims.ImsMmTelManager.RegistrationCallback. This keystone
    change adapts a Qualcomm addition to the upstream change.
    
    Change-Id: Ib93bb72797d5f87d35acd16156fc2aa2a073df4a

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -30,9 +30,10 @@ import android.os.Looper;
 import android.os.Message;
 import android.provider.Settings;
 import android.provider.Settings.Global;
-import android.telephony.NetworkRegistrationInfo;
+import android.telephony.ims.ImsMmTelManager;
 import android.telephony.ims.ImsReasonInfo;
 import android.telephony.ims.stub.ImsRegistrationImplBase;
+import android.telephony.NetworkRegistrationInfo;
 import android.telephony.PhoneStateListener;
 import android.telephony.ServiceState;
 import android.telephony.SignalStrength;
@@ -276,11 +277,7 @@ public class MobileSignalController extends SignalController<
     public void unregisterListener() {
         mPhone.listen(mPhoneStateListener, 0);
         mContext.getContentResolver().unregisterContentObserver(mObserver);
-        try {
-            mImsManager.removeRegistrationListener(mImsRegistrationCallback);
-        }catch(ImsException e){
-            Log.d(mTag, "exception:" + e);
-        }
+        mImsManager.removeRegistrationListener(mImsRegistrationCallback);
         mContext.unregisterReceiver(mVolteSwitchObserver);
     }
 
@@ -863,8 +860,8 @@ public class MobileSignalController extends SignalController<
         }
     };
 
-    private final ImsRegistrationImplBase.Callback mImsRegistrationCallback =
-            new ImsRegistrationImplBase.Callback() {
+    private final ImsMmTelManager.RegistrationCallback mImsRegistrationCallback =
+            new ImsMmTelManager.RegistrationCallback() {
                 @Override
                 public void onRegistered(
                         @ImsRegistrationImplBase.ImsRegistrationTech int imsRadioTech) {
