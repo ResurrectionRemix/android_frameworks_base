aae3645cb2bfb84dfae4339c3d43690dfa8ea260
Mazda-- <mazdarider23@gmail.com>
2018-11-14 19:20:56 -0700

    Optional screenshot type [1/2]
    Change-Id: Idf3ff47623b02f0c639606c95b2b76896482264f
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>
    
     Conflicts:
    	core/java/android/provider/Settings.java
    	services/core/java/com/android/server/policy/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4985,6 +4985,12 @@ public final class Settings {
          */
         public static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
 
+	    /**
+	     * Whether to use partial screenshot when using volume keys
+	     * @hide
+	     */
+        public static final String SCREENSHOT_TYPE = "screenshot_type";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
