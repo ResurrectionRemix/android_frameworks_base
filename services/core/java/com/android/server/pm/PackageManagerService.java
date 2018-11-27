b88ce90fc5e1a6048992b3cd3f43c15d8f254ec5
Terigele <terigele@huawei.com>
2018-11-27 20:29:03 +0800

    PMS: Improve code stability and avoid vm reboot
    Test: NA.
    Change-Id: Iceddfbe8c4b33c5ff829f2599f7429676047c334
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4108,6 +4108,7 @@ public class PackageManagerService extends IPackageManager.Stub
     @Override
     public PackageInfo getPackageInfoVersioned(VersionedPackage versionedPackage,
             int flags, int userId) {
+        if(versionedPackage == null) return null;
         return getPackageInfoInternal(versionedPackage.getPackageName(),
                 versionedPackage.getLongVersionCode(), flags, Binder.getCallingUid(), userId);
     }
@@ -6646,6 +6647,7 @@ public class PackageManagerService extends IPackageManager.Stub
         mPermissionManager.enforceCrossUserPermission(Binder.getCallingUid(), userId,
                 false /* requireFullPermission */, false /* checkShell */,
                 "query intent activities");
+        if (intent == null) return Collections.emptyList();
         final String pkgName = intent.getPackage();
         ComponentName comp = intent.getComponent();
         if (comp == null) {
@@ -8422,6 +8424,7 @@ public class PackageManagerService extends IPackageManager.Stub
         synchronized (mPackages) {
             final int callingUid = Binder.getCallingUid();
             final int callingUserId = UserHandle.getUserId(callingUid);
+            if (component == null) return null;
             final PackageSetting ps = mSettings.mPackages.get(component.getPackageName());
             if (ps == null) return null;
             if (filterAppAccessLPr(ps, callingUid, component, TYPE_UNKNOWN, callingUserId)) {
@@ -14328,7 +14331,7 @@ public class PackageManagerService extends IPackageManager.Stub
             synchronized (mPackages) {
                 for (int i = 0; i < packageNames.length; i++) {
                     final String packageName = packageNames[i];
-                    if (callingPackage.equals(packageName)) {
+                    if (callingPackage !=null && callingPackage.equals(packageName)) {
                         Slog.w(TAG, "Calling package: " + callingPackage + " trying to "
                                 + (suspended ? "" : "un") + "suspend itself. Ignoring");
                         unactionedPackages.add(packageName);
@@ -15265,6 +15268,12 @@ public class PackageManagerService extends IPackageManager.Stub
                 mHandler.sendEmptyMessage(MCS_RECONNECT);
                 res = false;
             }
+            catch (Exception e) {
+                if (DEBUG_INSTALL) Slog.i(TAG, "Posting install MCS_GIVE_UP");
+                mHandler.sendEmptyMessage(MCS_GIVE_UP);
+                res = false;
+            }
+
             handleReturnCode();
             return res;
         }
@@ -19270,6 +19279,10 @@ public class PackageManagerService extends IPackageManager.Stub
     }
 
     private void clearExternalStorageDataSync(String packageName, int userId, boolean allData) {
+        if (packageName == null){
+            Slog.w(TAG, "clearExternalStorageDataSync packageName is null!");
+            return;
+        }
         if (DEFAULT_CONTAINER_PACKAGE.equals(packageName)) return;
 
         final boolean mounted;
@@ -19615,6 +19628,10 @@ public class PackageManagerService extends IPackageManager.Stub
     @Override
     public void deleteApplicationCacheFiles(final String packageName,
             final IPackageDataObserver observer) {
+        if (null == packageName) {
+            Slog.w(TAG, "Failed to delete cache files, for packageName is null!");
+            return ;
+        }
         final int userId = UserHandle.getCallingUserId();
         deleteApplicationCacheFilesAsUser(packageName, userId, observer);
     }
@@ -20803,6 +20820,7 @@ Slog.v(TAG, ":: stepped forward, applying functor at tag " + parser.getName());
     public void setComponentEnabledSetting(ComponentName componentName,
             int newState, int flags, int userId) {
         if (!sUserManager.exists(userId)) return;
+        if (componentName == null) return;
         // Don't allow to enable components marked for disabling at build-time
         if (mDisabledComponentsList.contains(componentName)) {
             Slog.d(TAG, "Ignoring attempt to set enabled state of disabled component "
@@ -21220,6 +21238,7 @@ Slog.v(TAG, ":: stepped forward, applying functor at tag " + parser.getName());
         mPermissionManager.enforceCrossUserPermission(callingUid, userId,
                 false /*requireFullPermission*/, false /*checkShell*/, "getComponentEnabled");
         synchronized (mPackages) {
+            if (component == null) return COMPONENT_ENABLED_STATE_DISABLED;
             if (filterAppAccessLPr(mSettings.getPackageLPr(component.getPackageName()), callingUid,
                     component, TYPE_UNKNOWN, userId)) {
                 return COMPONENT_ENABLED_STATE_DISABLED;
