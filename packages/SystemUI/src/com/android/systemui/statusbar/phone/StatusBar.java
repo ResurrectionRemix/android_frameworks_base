ea5a8b79198c4ce87c1c423273eedb6454713561
Anay Wadhera <anay1018@gmail.com>
2019-01-11 15:48:50 -0800

    base: add Cookie QS style [2/3]
    Change-Id: I3eec3e2f124aaa3cd697ede809c9a055a2bd8a35

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -411,6 +411,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         "com.android.systemui.qstile.oreocircletrim", // 13
         "com.android.systemui.qstile.oreosquircletrim", // 14
         "com.android.systemui.qstile.inkdrop", // 15
+        "com.android.systemui.qstile.cookie", // 16
     };
 
     /** If true, the system is in the half-boot-to-decryption-screen state.
