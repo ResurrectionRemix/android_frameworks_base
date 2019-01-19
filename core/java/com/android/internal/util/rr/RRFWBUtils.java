ff478238c862acfdbfc1aa5fe5f8c8b02317da6a
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 17:30:24 -0600

    Utils: Add method to determine battery temp
    Change-Id: Id0b8addc460afae030eac0a2bf064fb21aa2c4dc
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- core/java/com/android/internal/util/rr/RRFWBUtils.java
+++ core/java/com/android/internal/util/rr/RRFWBUtils.java
@@ -105,6 +105,18 @@ public class RRFWBUtils {
         }
     }
 
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
+    }
 
     /**
      * @hide
