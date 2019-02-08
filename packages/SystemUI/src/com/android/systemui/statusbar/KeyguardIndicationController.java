38f358a4df02d6add66fbfd8c2cbe1bf843ec203
Shreyansh Lodha <slodha96@gmail.com>
2019-02-08 06:43:21 +0530

    Animate visibility change for Keyguard Inidication Area
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>
    Change-Id: I5e14a47971e44410145ac7c8d6fbb89d7fe1c602
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -330,6 +330,9 @@ public class KeyguardIndicationController {
             int userId = KeyguardUpdateMonitor.getCurrentUser();
             String trustGrantedIndication = getTrustGrantedIndication();
             String trustManagedIndication = getTrustManagedIndication();
+
+            updateChargingIndication();
+
             if (!mUserManager.isUserUnlocked(userId)) {
                 mTextView.switchIndication(com.android.internal.R.string.lockscreen_storage_locked);
                 mTextView.setTextColor(mInitialTextColor);
@@ -360,7 +363,6 @@ public class KeyguardIndicationController {
                 mTextView.switchIndication(mRestingIndication);
                 mTextView.setTextColor(mInitialTextColor);
             }
-            updateChargingIndication();
         }
     }
 
