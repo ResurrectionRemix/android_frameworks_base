420239ee18b0d28b7bd005112cd3859647d53d6c
maxwen <max.weninger@gmail.com>
2019-02-13 00:13:00 +0100

    SystemUI: keyguard show charging watt
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I401134f992fe0630c2f28e827ef15c649ede581b

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -119,6 +119,7 @@ public class KeyguardIndicationController implements StateListener,
     private boolean mPowerPluggedInWired;
     private boolean mPowerCharged;
     private int mChargingSpeed;
+    private double mChargingWattage;
     private int mChargingCurrent;
     private double mChargingVoltage;
     private int mChargingWattage;
@@ -591,6 +592,10 @@ public class KeyguardIndicationController implements StateListener,
                 batteryInfo = batteryInfo + (mChargingCurrent < 5 ?
                           (mChargingCurrent * 1000) : (mChargingCurrent < 4000 ?
                           mChargingCurrent : (mChargingCurrent / 1000))) + "mA" ;
+            }
+            if (mChargingWattage > 0) {
+                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
+                        String.format("%.1f" , (mChargingWattage / 1000 / 1000)) + "W";
             }
             if (mChargingVoltage > 0) {
                 batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
