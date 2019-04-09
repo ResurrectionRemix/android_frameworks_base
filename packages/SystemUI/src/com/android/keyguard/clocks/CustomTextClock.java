663b00ced7ad3cf51f965857fe63037b56b411f0
Ovenoboyo <ovenoboyo@gmail.com>
2019-04-09 14:05:42 +0530

    CustomTextClock: Improve UI and optimisations
    - Kill the massive switch loop cause I was high when I wrote it.
    - Make the clock look somewhat alike to android Q's.
    - Let the clock font be in accordance with the system font.
    - And ofcourse add a dead overwatch meme at 12:00PM
    
    Change-Id: I6891a2d6f717127f73ed9d5e4979fb6974a0b45c

--- packages/SystemUI/src/com/android/keyguard/clocks/CustomTextClock.java
+++ packages/SystemUI/src/com/android/keyguard/clocks/CustomTextClock.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Typeface;
 import android.text.format.DateUtils;
 import android.text.format.DateFormat;
 import android.text.format.Time;
@@ -23,7 +22,9 @@ import com.android.systemui.R;
 public class CustomTextClock extends TextView {
 
     private final String[] TensString = {"", "", "Twenty","Thirty","Forty", "Fifty", "Sixty"};
-    private final String[] UnitsString = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private final String[] UnitsString = {"Clock", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+    private final String[] TensStringH = {"", "", "Twenty","Thirty","Forty", "Fifty", "Sixty"};
+    private final String[] UnitsStringH = {"Twelve", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
 
     private Time mCalendar;
 
@@ -43,10 +44,7 @@ public class CustomTextClock extends TextView {
         final TypedArray a = context.obtainStyledAttributes(
                 attrs, R.styleable.CustomTextClock);
 
-        handType = a.getInteger(R.styleable.CustomTextClock_HandType, 0);
-
-        Typeface myTypeface = Typeface.createFromAsset(getContext().getAssets(), "Rounded_Elegance.ttf");
-        setTypeface(myTypeface);
+        handType = a.getInteger(R.styleable.CustomTextClock_HandType, 2);
 
         mCalendar = new Time();
 
@@ -117,15 +115,22 @@ public class CustomTextClock extends TextView {
 
         switch(handType){
             case 0:
-                setText(getIntString(hour));
+                if (hour == 12 && minute == 0) {
+                setText("High");
+                } else {
+                setText(getIntStringHour(hour));
+                }
                 break;
             case 1:
-                setText(getIntString(minute));
+                if (hour == 12 && minute == 0) {
+                setText("Noon");
+                } else {
+                setText(getIntStringMin(minute));
+                }
                 break;
             default:
                 break;
         }
-
 
         updateContentDescription(mCalendar, getContext());
     }
@@ -151,7 +156,25 @@ public class CustomTextClock extends TextView {
         setContentDescription(contentDescription);
     }
 
-    private String getIntString (int num) {
+    private String getIntStringHour (int num) {
+        int tens, units;
+        String NumString = "";
+        if(num >= 20) {
+            units = num % 10 ;
+            tens =  num / 10;
+            if ( units == 0 ) {
+                NumString = TensStringH[tens];
+            } else {
+                NumString = TensStringH[tens]+" "+UnitsStringH[units];
+            }
+        } else if (num < 20 ) {
+            NumString = UnitsStringH[num];
+        }
+
+        return NumString;
+    }
+
+    private String getIntStringMin (int num) {
         int tens, units;
         String NumString = "";
         if(num >= 20) {
@@ -163,48 +186,9 @@ public class CustomTextClock extends TextView {
                 NumString = TensString[tens]+" "+UnitsString[units];
             }
         } else if (num < 10 ) {
-            NumString = UnitsString[num];
+            NumString = "O\'"+UnitsString[num];
         } else if (num >= 10 && num < 20) {
-                switch (num) {
-                    case 10:
-                        NumString = "Ten";
-                        break;
-                    case 11:
-                        NumString = "Eleven";
-                        break;
-
-                    case 12:
-                        NumString = "Twelve";
-                        break;
-
-                    case 13:
-                        NumString = "Thirteen";
-                        break;
-                    case 14:
-                        NumString = "Fourteen";
-                        break;
-
-                    case 15:
-                        NumString = "Fifteen";
-                        break;
-
-                    case 16:
-                        NumString = "Sixteen";
-                        break;
-                    case 17:
-                        NumString = "Seventeen";
-                        break;
-
-                    case 18:
-                        NumString = "Eighteen";
-                        break;
-
-                    case 19:
-                        NumString = "Nineteen";
-                        break;
-                    default:
-                        break;
-            }
+            NumString = UnitsString[num];
         }
 
         return NumString;
