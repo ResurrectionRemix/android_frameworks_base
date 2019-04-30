29911abe259ddf8b595ac43aa8492e606eb66677
fessmm <furkaaan28@live.de>
2019-04-30 09:00:46 +0000

    Revert "Keyguard: Avoid starting FP authentication right after cancelling"
    breaks fp on some devices
    This reverts commit 12630233e64fa082c094447907b9e1d836bfcd03.

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1349,7 +1349,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener,
         if (mFingerprintRunningState == FINGERPRINT_STATE_RUNNING && !shouldListenForFingerprint) {
             stopListeningForFingerprint();
         } else if (mFingerprintRunningState != FINGERPRINT_STATE_RUNNING
-                && mFingerprintRunningState != FINGERPRINT_STATE_CANCELLING
                 && shouldListenForFingerprint) {
             startListeningForFingerprint();
         }
