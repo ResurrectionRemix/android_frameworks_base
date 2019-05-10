ccce75364b9e06a66756bd72d3c4bb1ea5037119
mracar07 <umutcan@umutcanacar.me>
2019-05-10 16:07:02 +0000

    Revert "Revert "ViewRoot: Ensure we release surface from setWindowStopped""
    This reverts commit a6e20d215b9495686fd1a8972306c9c7e419e4ad.

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -1370,6 +1370,10 @@ public final class ViewRootImpl implements ViewParent,
             for (int i = 0; i < mWindowStoppedCallbacks.size(); i++) {
                 mWindowStoppedCallbacks.get(i).windowStopped(stopped);
             }
+
+            if (mStopped) {
+                mSurface.release();
+            }
         }
     }
 
