59b0fb36cd75430911a34c6a8f7f35322bcad30e
Anushek Prasal <anushekprasal@gmail.com>
2019-06-06 14:18:24 +0530

    base: Add toggle to disable charging animation [1/2]
    Change-Id: I8982623ce5d231d1a29dfa934179214a6745faae

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7696,6 +7696,12 @@ public final class Settings {
         public static final String SWAP_NAVIGATION_KEYS = "swap_navigation_keys";
 
         /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
