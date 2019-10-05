447012050b26bc237a26b2554c4a77c333d45f18
Chris Crump <chris@cypheros.co>
2019-10-05 00:13:58 -0400

    SystemUI: Update keyguard indication margin when fod is in use
    Decrease the bottom margin when fingerprint in display is active
    to avoid obstructing the view
    
    Change-Id: Iec87c5ca67f0f6081209fd685e7a8621b8e8b1d7
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -614,7 +614,6 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, OnCo
     }
 
     private void setScrimAlpha(ScrimView scrim, float alpha) {
-        if (DEBUG) Log.d(TAG, "setScrimAlpha " + getScrimName(scrim) + " " + alpha);
         if (alpha == 0f) {
             scrim.setClickable(false);
         } else {
