3f291e7f144e57f55da68e6691453199e0849517
Anushek Prasal <anushekprasal@gmail.com>
2019-10-17 21:13:04 +0530

    SystemUI: Fix statusbar padding and layout for system icons
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: Id16075bdfa35c8d50a8b36b2315c5bc17a37316d

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -76,12 +76,9 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
     /**
      * Status icons are currently drawn with the intention of being 17dp tall, but we
      * want to scale them (in a way that doesn't require an asset dump) down 2dp. So
-     * 17dp * (15 / 17) = 15dp, the new height. After the first call to {@link #reloadDimens} all
-     * values will be in px.
+     * 17dp * (15 / 17) = 15dp, the new height.
      */
-    private float mSystemIconDesiredHeight = 15f;
-    private float mSystemIconIntrinsicHeight = 17f;
-    private float mSystemIconDefaultScale = mSystemIconDesiredHeight / mSystemIconIntrinsicHeight;
+    private static final float SYSTEM_ICON_SCALE = 15.f / 17.f;
     private final int ANIMATION_DURATION_FAST = 100;
 
     public static final int STATE_ICON = 0;
@@ -217,24 +214,8 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
         updatePivot();
     }
 
-    // Makes sure that all icons are scaled to the same height (15dp). If we cannot get a height
-    // for the icon, it uses the default SCALE (15f / 17f) which is the old behavior
     private void updateIconScaleForSystemIcons() {
-        float iconHeight = getIconHeight();
-        if (iconHeight != 0) {
-            mIconScale = mSystemIconDesiredHeight / iconHeight;
-        } else {
-            mIconScale = mSystemIconDefaultScale;
-        }
-    }
-
-    private float getIconHeight() {
-        Drawable d = getDrawable();
-        if (d != null) {
-            return (float) getDrawable().getIntrinsicHeight();
-        } else {
-            return mSystemIconIntrinsicHeight;
-        }
+        mIconScale = SYSTEM_ICON_SCALE;
     }
 
     public float getIconScaleIncreased() {
@@ -252,8 +233,8 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
         if (density != mDensity) {
             mDensity = density;
             reloadDimens();
-            updateDrawable();
             maybeUpdateIconScaleDimens();
+            updateDrawable();
         }
         boolean nightMode = (newConfig.uiMode & Configuration.UI_MODE_NIGHT_MASK)
                 == Configuration.UI_MODE_NIGHT_YES;
@@ -275,11 +256,6 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
         if (applyRadius) {
             mDotRadius = mStaticDotRadius;
         }
-        mSystemIconDesiredHeight = res.getDimension(
-                com.android.internal.R.dimen.status_bar_system_icon_size);
-        mSystemIconIntrinsicHeight = res.getDimension(
-                com.android.internal.R.dimen.status_bar_system_icon_intrinsic_size);
-        mSystemIconDefaultScale = mSystemIconDesiredHeight / mSystemIconIntrinsicHeight;
     }
 
     public void setNotification(StatusBarNotification notification) {
@@ -289,7 +265,6 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
         if (notification != null) {
             setContentDescription(notification.getNotification());
         }
-        maybeUpdateIconScaleDimens();
     }
 
     public StatusBarIconView(Context context, AttributeSet attrs) {
@@ -298,7 +273,7 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
         mBlocked = false;
         mAlwaysScaleIcon = true;
         reloadDimens();
-        maybeUpdateIconScaleDimens();
+        updateIconScaleForNotifications();
         mDensity = context.getResources().getDisplayMetrics().densityDpi;
     }
 
@@ -351,8 +326,6 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
             if (!updateDrawable(false /* no clear */)) return false;
             // we have to clear the grayscale tag since it may have changed
             setTag(R.id.icon_is_grayscale, null);
-            // Maybe set scale based on icon height
-            maybeUpdateIconScaleDimens();
         }
         if (!levelEquals || force) {
             setImageLevel(icon.iconLevel);
