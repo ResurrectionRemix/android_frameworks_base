adc35c260b719d2e0124ac44483373835d3dce5a
Anushek Prasal <anushekprasal@gmail.com>
2019-10-24 20:59:30 +0530

    base: Use ro.build.date.utc to signal upgrades
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -65,7 +65,7 @@ public class Build {
     /** The build date
      * @hide
      */
-    public static final String DATE = getString("ro.build.date");
+    public static final String DATE = getString("ro.build.date.utc");
 
     /**
      * The build date
