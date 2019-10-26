0b1024044e361fa051d2c1902227f7079fb7df6c
Lars Greiss <kufikugel@googlemail.com>
2019-10-26 12:28:15 +0200

    Disable Quick Settings on secure lockscreen (1/2)
    This commit gets the following ones uptodate with marshmallow.
    Adapted KreAch3R
    
    - Moved the necessary bits into their new functions
    - isSecure() now explicity needs a userhandle (followed google's way because USER_CURRENT didn't work)
    - Implemented a mKeyguardOrShadeShowing boolean to stop the bug of notification drawer (which reappeared in marshmallow even after the fix because keyguardShowing changed.
    
    Frameworks: Disable access to quick settings on secure lockscreen (1/2)
    
    We have a lot security guys as user due of using the phones as business phones...that
    said a lot complains go into the direction of google how it is possible to have
    access to quick settings on a secure lockscreen which is absolutely understandable
    due that you are able to control Wifi BT AirplaneMode data etc.
    
    This patch takes care on this problem and adds an option to control this behaviour.
    
    ATTENTION: Due that this is really an UX derp for an average user this feature is
    enabled by default now. User need to disable it explicit in settings to get on secure
    locksreen the original android behaviour back.
    
    SystemUI: fix quick settings on secure lockscreen feature
    by Altaf-Mahdi
    
    With the current implementation there was a bug were if you expand a notification
    you will be able to access quick settings like normal.
    
    A more simpler solution is to use the existing function setQsExpansionEnabled() which works
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I1454528233f353854e33b04a537ef555f685a28e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -16023,6 +16023,12 @@ public final class Settings {
         public static final String RINGTONE_FOCUS_MODE = "ringtone_focus_mode";
 
         /**
+         * Whether to show quick settings on secure LockScreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_ENABLE_QS = "lockscreen_enable_qs";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -63,6 +63,7 @@ import android.widget.FrameLayout;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardClockSwitch;
 import com.android.keyguard.KeyguardStatusView;
 import com.android.keyguard.KeyguardUpdateMonitor;
@@ -171,6 +172,8 @@ public class NotificationPanelView extends PanelView implements
             "lineagesystem:" + LineageSettings.System.STATUS_BAR_QUICK_QS_PULLDOWN;
     public static final String DOUBLE_TAP_SLEEP_GESTURE =
             "lineagesystem:" + LineageSettings.System.DOUBLE_TAP_SLEEP_GESTURE;
+    private static final String LOCKSCREEN_ENABLE_QS =
+            "global:" + Settings.Global.LOCKSCREEN_ENABLE_QS;
 
     private static final Rect mDummyDirtyRect = new Rect(0, 0, 1, 1);
     private static final Rect mEmptyRect = new Rect();
@@ -271,6 +274,7 @@ public class NotificationPanelView extends PanelView implements
     private boolean mQsExpandedWhenExpandingStarted;
     private boolean mQsFullyExpanded;
     private boolean mKeyguardShowing;
+    private boolean mKeyguardOrShadeShowing;
     private boolean mDozing;
     private boolean mDozingOnDown;
     protected int mBarState;
@@ -345,6 +349,9 @@ public class NotificationPanelView extends PanelView implements
     private String mLastCameraLaunchSource = KeyguardBottomAreaView.CAMERA_LAUNCH_SOURCE_AFFORDANCE;
 
     private NotificationLightsView mPulseLightsView;
+    private boolean mStatusBarShownOnSecureKeyguard;
+
+    private LockPatternUtils mLockPatternUtils;
 
     private Runnable mHeadsUpExistenceChangedRunnable = new Runnable() {
         @Override
@@ -510,6 +517,7 @@ public class NotificationPanelView extends PanelView implements
         });
         mBottomAreaShadeAlphaAnimator.setDuration(160);
         mBottomAreaShadeAlphaAnimator.setInterpolator(Interpolators.ALPHA_OUT);
+        mLockPatternUtils = new LockPatternUtils(mContext);
         mDoubleTapGesture = new GestureDetector(mContext,
                 new GestureDetector.SimpleOnGestureListener() {
             @Override
@@ -589,6 +597,7 @@ public class NotificationPanelView extends PanelView implements
         Dependency.get(ConfigurationController.class).addCallback(this);
         Dependency.get(TunerService.class).addTunable(this, STATUS_BAR_QUICK_QS_PULLDOWN);
         Dependency.get(TunerService.class).addTunable(this, DOUBLE_TAP_SLEEP_GESTURE);
+        Dependency.get(TunerService.class).addTunable(this, LOCKSCREEN_ENABLE_QS);
         mUpdateMonitor.registerCallback(mKeyguardUpdateCallback);
         // Theme might have changed between inflating this view and attaching it to the window, so
         // force a call to onThemeChanged
@@ -612,6 +621,9 @@ public class NotificationPanelView extends PanelView implements
             mOneFingerQuickSettingsIntercept = newValue == null ? 1 : Integer.parseInt(newValue);
         } else if (DOUBLE_TAP_SLEEP_GESTURE.equals(key)) {
             mDoubleTapToSleepEnabled = newValue == null || Integer.parseInt(newValue) == 1;
+        } else if (LOCKSCREEN_ENABLE_QS.equals(key)) {
+            mStatusBarShownOnSecureKeyguard =
+                        TunerService.parseIntegerSwitch(newValue, true);
         }
     }
 
@@ -988,10 +1000,15 @@ public class NotificationPanelView extends PanelView implements
         mAnimateNextPositionUpdate = true;
     }
 
+    private boolean isQSEventBlocked() {
+        return mLockPatternUtils.isSecure(KeyguardUpdateMonitor.getCurrentUser())
+            && !mStatusBarShownOnSecureKeyguard && mKeyguardOrShadeShowing;
+    }
+
     public void setQsExpansionEnabled(boolean qsExpansionEnabled) {
-        mQsExpansionEnabled = qsExpansionEnabled;
+        mQsExpansionEnabled = qsExpansionEnabled && !isQSEventBlocked();
         if (mQs == null) return;
-        mQs.setHeaderClickable(qsExpansionEnabled);
+        mQs.setHeaderClickable(mQsExpansionEnabled);
     }
 
     @Override
@@ -1661,11 +1678,14 @@ public class NotificationPanelView extends PanelView implements
         boolean keyguardFadingAway = mKeyguardMonitor.isKeyguardFadingAway();
         int oldState = mBarState;
         boolean keyguardShowing = statusBarState == StatusBarState.KEYGUARD;
+        boolean keyguardOrShadeShowing = statusBarState == StatusBarState.KEYGUARD
+                || statusBarState == StatusBarState.SHADE_LOCKED;
         setKeyguardStatusViewVisibility(statusBarState, keyguardFadingAway, goingToFullShade);
         setKeyguardBottomAreaVisibility(statusBarState, goingToFullShade);
 
         mBarState = statusBarState;
         mKeyguardShowing = keyguardShowing;
+        mKeyguardOrShadeShowing = keyguardOrShadeShowing;
 
         if (oldState == StatusBarState.KEYGUARD
                 && (goingToFullShade || statusBarState == StatusBarState.SHADE_LOCKED)) {
