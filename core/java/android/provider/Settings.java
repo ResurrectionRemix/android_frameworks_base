e166169c0246c48e76e373384673c3dcdcfc9533
Michael Bestas <mkbestas@lineageos.org>
2019-11-17 14:28:32 +0200

    Fix VOLUME_LINK_NOTIFICATION javadoc
    * Fixes checkapi:
      frameworks/base/core/java/android/provider/Settings.java:8920: error:
      Added field android.provider.Settings.Secure.VOLUME_LINK_NOTIFICATION [AddedField]
    
    Change-Id: I4da34de53ce23e90d5f2eff72ef0a15034f08ec1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8948,7 +8948,7 @@ public final class Settings {
 
         private static final Validator AWARE_LOCK_ENABLED_VALIDATOR = BOOLEAN_VALIDATOR;
 
-        /*
+        /**
          * Boolean value whether to link ringtone and notification volume
          * @hide
          */
