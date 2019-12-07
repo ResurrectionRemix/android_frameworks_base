e9f18e0e98ba2671c66960f96c882da73fd546ca
SKULSHADY <anushekprasal@gmail.com>
2019-12-07 15:02:45 +0200

    base: Add refresh rate setting [1/2]
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I737a5410340049a9d366de9a25434bda6f090e7c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5802,6 +5802,11 @@ public final class Settings {
         private static final Validator QS_BACKGROUND_BLUR_INTENSITY_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * @hide
+         */
+        public static final String REFRESH_RATE_SETTING = "refresh_rate_setting";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
