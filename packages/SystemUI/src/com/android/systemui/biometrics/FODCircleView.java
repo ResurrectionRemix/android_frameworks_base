16550cff91930d9f879499f1cabd1abf776afca5
TheScarastic <warabhishek@gmail.com>
2019-12-16 12:17:10 +0000

    FODCircleView: Add partial wakelock on aod
    Change-Id: I5d19aab7e577c5a431afc94edd9b4cc019ae9adf
    Signed-off-by: Rohan Hasabe <rohanhasabe8@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -29,6 +29,7 @@ import android.hardware.display.DisplayManager;
 import android.os.Handler;
 import android.os.IHwBinder;
 import android.os.Looper;
+import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.provider.Settings;
@@ -81,6 +82,9 @@ public class FODCircleView extends ImageView implements OnTouchListener {
 
     private Handler mHandler;
 
+    private PowerManager mPowerManager;
+    private PowerManager.WakeLock mWakeLock;
+
     private Timer mBurnInProtectionTimer;
 
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =
@@ -121,8 +125,14 @@ public class FODCircleView extends ImageView implements OnTouchListener {
             if (dreaming) {
                 mBurnInProtectionTimer = new Timer();
                 mBurnInProtectionTimer.schedule(new BurnInProtectionTask(), 0, 60 * 1000);
+                if (!mWakeLock.isHeld()) {
+                    mWakeLock.acquire();
+                }
             } else if (mBurnInProtectionTimer != null) {
                 mBurnInProtectionTimer.cancel();
+                if (mWakeLock.isHeld()) {
+                    mWakeLock.release();
+                }
             }
             if (mIsViewAdded) {
                 resetPosition();
@@ -253,7 +263,12 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         mUpdateMonitor = KeyguardUpdateMonitor.getInstance(context);
         mUpdateMonitor.registerCallback(mMonitorCallback);
 
-        mDisplayManager = context.getSystemService(DisplayManager.class);
+
+        updateCutoutFlags();
+
+        Dependency.get(ConfigurationController.class).addCallback(this);
+        mPowerManager = context.getSystemService(PowerManager.class);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "FODCircleView");
     }
 
     @Override
