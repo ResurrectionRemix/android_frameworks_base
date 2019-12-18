ffa39bb5fea4a8a11593d32a0f63a1f900d654f4
mracar07 <umutcan@umutcanacar.me>
2019-12-18 18:26:42 +0000

    Revert "SystemUI: Don't dismiss keyguard if user key isn't unlocked"
    This reverts commit c8df7611a3ceb5a7d24963fe36495c849f58d3ae.

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -54,7 +54,6 @@ import android.os.SystemProperties;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.UserManager;
-import android.os.storage.StorageManager;
 import android.provider.Settings;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
