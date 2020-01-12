c6f33e31ba6124ea4108a49be38b8982b1293745
rituj <ritujbeniwal@gmail.com>
2020-01-12 22:50:49 +0900

    VibratorService: update constant values for OnePlus HAL
    Change-Id: I6c8b4b73995dcd1a114b863f944b1de5254b5a0d
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: AnierinB <anierinb@evolution-x.org>

--- services/core/java/com/android/server/VibratorService.java
+++ services/core/java/com/android/server/VibratorService.java
@@ -113,11 +113,11 @@ public class VibratorService extends IVibratorService.Stub
     private static final int ONEPLUS_SCALE = 100000;
     private static final int ONEPLUS_BREAK_CONSTANT = 9990;
     private static final int ONEPLUS_EFFECT_THRESHOLD = 100;
-    private static final long ONEPLUS_EFFECT_CLICK = 5909995;
+    private static final long ONEPLUS_EFFECT_CLICK = 1600051;
     private static final long[] ONEPLUS_DOUBLE_CLICK_EFFECT_FALLBACK_TIMINGS = { 0, 80, 25, 75 };
-    private static final long ONEPLUS_EFFECT_HEAVY_CLICK = 1600051;
-    private static final long ONEPLUS_EFFECT_TEXTURE_TICK = 1100111;
-    private static final long ONEPLUS_EFFECT_TICK = 1100031;
+    private static final long ONEPLUS_EFFECT_HEAVY_CLICK = 5909995;
+    private static final long ONEPLUS_EFFECT_TEXTURE_TICK = 900021;
+    private static final long ONEPLUS_EFFECT_TICK = 1100111;
     private static final long ONEPLUS_EFFECT_POP = 1100041;
     private static final long ONEPLUS_EFFECT_THUD = 3000003;
 
