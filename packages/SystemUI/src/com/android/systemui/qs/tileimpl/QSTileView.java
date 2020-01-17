4d30890ce028c3c29dc7cb8551716466ab13b74c
DennySPB <dennyspb@gmail.com>
2020-01-17 12:37:46 +0300

    SystemUI: Tint active qs tile expand indicator with accent color
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I2dad0d8c46d6763c0ff1374de985de96d771778c

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileView.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileView.java
@@ -46,7 +46,7 @@ public class QSTileView extends QSTileBaseView {
     private ImageView mPadLock;
     private int mState;
     private ViewGroup mLabelContainer;
-    private View mExpandIndicator;
+    private ImageView mExpandIndicator;
     private View mExpandSpace;
     private ColorStateList mColorLabelDefault;
     private ColorStateList mColorLabelActive;
@@ -147,8 +147,10 @@ public class QSTileView extends QSTileBaseView {
 
         if (state.state == Tile.STATE_ACTIVE) {
             mLabel.setTextColor(mColorLabelActive);
+            mExpandIndicator.setImageTintList(mColorLabelActive);
         } else if (state.state == Tile.STATE_INACTIVE) {
             mLabel.setTextColor(mColorLabelDefault);
+            mExpandIndicator.setImageTintList(mColorLabelDefault);
         }
         boolean dualTarget = DUAL_TARGET_ALLOWED && state.dualTarget;
         mExpandIndicator.setVisibility(View.GONE);
