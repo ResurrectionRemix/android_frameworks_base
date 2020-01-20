8d1491ace832c9a9269b85dce1d56fe77345be0c
rohan <purohit.rohan@gmail.com>
2020-01-20 23:58:39 +0530

    Add Custom Sneeky Analog Clock
    Thanks to mohancm
    Thanks to ROGERdotT for thumbnail.
    
    Change-Id: I704a991ee5301b3ab3a4514b542b44406a9d71ce

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -155,6 +155,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SpectrumClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new DotClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new SneekyClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new BinaryClockController(res, layoutInflater, colorExtractor));
 
