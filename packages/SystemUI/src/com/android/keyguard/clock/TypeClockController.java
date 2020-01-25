b2ad396f5a262fb8f472a7df39a63246b81fadff
rohan <purohit.rohan@gmail.com>
2020-01-25 14:14:09 -0500

    TypeClock: adapt to changes in keyguard
    * fixes type clock not showing on lockscreen
    * Still using typographic layout instead of clocklayout
    * fix preview being oversized
    
    Change-Id: Id92b4e978be24559c31d3f70a5f0e4efe14453a7
    Signed-off-by: rohan <purohit.rohan@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/TypeClockController.java
@@ -61,11 +61,7 @@ public class TypeClockController implements ClockPlugin {
      */
     private View mView;
     private TypographicClock mTypeClock;
-
-    /**
-     * Small clock shown on lock screen above stack scroller.
-     */
-    private TypographicClock mLockClock;
+    private TypographicClock mBigClockView;
 
     /**
      * Controller for transition into dark state.
@@ -88,21 +84,16 @@ public class TypeClockController implements ClockPlugin {
 
     private void createViews() {
         mView = mLayoutInflater.inflate(R.layout.type_aod_clock, null);
-        mTypeClock = mView.findViewById(R.id.type_clock);
 
-        // For now, this view is used to hide the default digital clock.
-        // Need better transition to lock screen.
-        mLockClock = (TypographicClock) mLayoutInflater.inflate(R.layout.typographic_clock, null);
-        mLockClock.setVisibility(View.GONE);
-
-        mDarkController = new CrossFadeDarkController(mView, mLockClock);
+        mBigClockView  = (TypographicClock) mLayoutInflater.inflate(R.layout.typographic_clock, null);
+        mTypeClock = mBigClockView.findViewById(R.id.type_clock);
     }
 
     @Override
     public void onDestroyView() {
         mView = null;
+        mBigClockView = null;
         mTypeClock = null;
-        mLockClock = null;
         mDarkController = null;
     }
 
@@ -140,27 +131,32 @@ public class TypeClockController implements ClockPlugin {
 
     @Override
     public View getView() {
-        if (mLockClock == null) {
+        if (mBigClockView == null) {
             createViews();
         }
-        return mLockClock;
+        return mBigClockView;
     }
 
     @Override
     public View getBigClockView() {
-        if (mView == null) {
+        if (mBigClockView  == null) {
             createViews();
         }
-        return mView;
+        return mBigClockView ;
     }
 
     @Override
     public void setStyle(Style style) {}
 
     @Override
+    public int getPreferredY(int totalHeight) {
+        return totalHeight / 2;
+    }
+
+
+    @Override
     public void setTextColor(int color) {
         mTypeClock.setTextColor(color);
-        mLockClock.setTextColor(color);
     }
 
     @Override
@@ -170,13 +166,11 @@ public class TypeClockController implements ClockPlugin {
         }
         final int color = colorPalette[Math.max(0, colorPalette.length - 5)];
         mTypeClock.setClockColor(color);
-        mLockClock.setClockColor(color);
     }
 
     @Override
     public void onTimeTick() {
         mTypeClock.onTimeChanged();
-        mLockClock.onTimeChanged();
     }
 
     @Override
@@ -189,7 +183,6 @@ public class TypeClockController implements ClockPlugin {
     @Override
     public void onTimeZoneChanged(TimeZone timeZone) {
         mTypeClock.onTimeZoneChanged(timeZone);
-        mLockClock.onTimeZoneChanged(timeZone);
     }
 
     @Override
