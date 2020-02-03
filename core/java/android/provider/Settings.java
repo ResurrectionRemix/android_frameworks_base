544f5e265cc01bf50a6a6ab0230c2a67c8250c63
rituj <ritujbeniwal@gmail.com>
2020-02-03 09:26:45 +0530

    base: Add vibration patterns from OOS
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I83906f892371a014465c93aff3318c21833883bf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5725,6 +5725,17 @@ public final class Settings {
         public static final String INCALL_FEEDBACK_VIBRATE = "incall_feeedback_vibrate";
 
         /**
+         * Which Vibration Pattern to use
+         * 0: dzzz-dzzz
+         * 1: dzzz-da
+         * 2: mm-mm-mm
+         * 3: da-da-dzzz
+         * 4: da-dzzz-da
+         * @hide
+         */
+        public static final String RINGTONE_VIBRATION_PATTERN = "ringtone_vibration_pattern";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
