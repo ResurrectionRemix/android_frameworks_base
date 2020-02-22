282e92db7633045d301c0cbcc673fb5ce4af5e36
ezio84 <brabus84@gmail.com>
2020-02-21 22:30:46 -0500

    Navbar: Add Expand notifications action
    Change-Id: I70878ba8815af82edbe856ef39c832e0b8206a8b
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- services/core/java/com/android/server/policy/NavbarUtilities.java
+++ services/core/java/com/android/server/policy/NavbarUtilities.java
@@ -41,6 +41,7 @@ public class NavbarUtilities {
     public static final int KEY_ACTION_VOLUME_PANEL = 10;
     public static final int KEY_ACTION_SCREEN_OFF = 11;
     public static final int KEY_ACTION_SCREENSHOT = 12;
+    public static final int KEY_ACTION_NOTIFICATIONS = 13;
 
     // Special values, used internal only.
     public static final int KEY_ACTION_HOME = 100;
@@ -126,6 +127,7 @@ public class NavbarUtilities {
             KEY_ACTION_VOLUME_PANEL,
             KEY_ACTION_SCREEN_OFF,
             KEY_ACTION_SCREENSHOT,
+            KEY_ACTION_NOTIFICATIONS,
     };
 
     /**
--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6906,6 +6906,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NavbarUtilities.KEY_ACTION_SCREENSHOT:
                 toggleScreenshot();
                 break;
+            case NavbarUtilities.KEY_ACTION_NOTIFICATIONS:
+                toggleNotifications();
+                break;
         }
     }
 
@@ -6991,5 +6994,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private void toggleScreenshot() {
         performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Screenshot");
         ActionUtils.takeScreenshot(true);
+    }
+
+    // Notifications
+    private void toggleNotifications() {
+        performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, true, "Notifications");
+        ActionUtils.toggleNotifications();
     }
 }
