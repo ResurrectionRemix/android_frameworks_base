6cfe2e3467147de5b64e8587bf258d41c651300b
Sonalika Paul <psonalik@codeaurora.org>
2020-03-03 21:52:36 +0530

    IMS: Add new carrier config.
    Add new carrier config to determine
    default RTT mode.
    
    Change-Id: Idb7b238529cff02f952a8ff82da7c1604a80a1e6
    CRs-Fixed: 2631830
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -3329,6 +3329,14 @@ public class CarrierConfigManager {
     public static final String KEY_CARRIER_SUPPORTS_MULTIANCHOR_CONFERENCE =
             "carrier_supports_multianchor_conference";
 
+    /**
+     * Determines the default RTT mode.
+     *
+     * @hide
+     */
+    public static final String KEY_DEFAULT_RTT_MODE_INT =
+            "default_rtt_mode_int";
+
     /** The default value for every variable. */
     private final static PersistableBundle sDefaults;
 
@@ -3784,6 +3792,7 @@ public class CarrierConfigManager {
         sDefaults.putString(KEY_EMERGENCY_NOTIFICATION_SUMMARY_STRING, "");
         sDefaults.putString(KEY_EMERGENCY_NOTIFICATION_TITLE_STRING, "");
         sDefaults.putBoolean(KEY_CARRIER_SUPPORTS_MULTIANCHOR_CONFERENCE, false);
+        sDefaults.putInt(KEY_DEFAULT_RTT_MODE_INT, 0);
     }
 
     /**
