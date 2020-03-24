747f6b7444267ec0e5304a4f7fae09b753683cbf
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-23 23:44:52 -0300

    FODCircleView: Also hide circle on auth error
    Change-Id: I6957d36a84efbef18caf34fc1ef1031bef4a28b8

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -155,6 +155,15 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         public void onScreenTurnedOff() {
             hideCircle();
         }
+
+        @Override
+        public void onBiometricHelp(int msgId, String helpString,
+                BiometricSourceType biometricSourceType) {
+            if (msgId == -1){ // Auth error
+                hideCircle();
+                mHandler.post(() -> mFODAnimation.hideFODanimation());
+            }
+        }
     };
 
     private boolean mCutoutMasked;
