4eb576c8413ffccba51c72f43b258efb95226f1a
Varun Date <date.varun123@gmail.com>
2020-04-03 10:47:59 +1100

    Revert "SystemUI: Implement Smart space"
    This reverts commit 6eab60768b48c1544fcf40579165ccc4a8b95e96.
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I18c170cee02e5ed72fa5fdb5f148e306b2a1c06a

--- packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleFactory.java
+++ packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleFactory.java
@@ -1,7 +1,6 @@
 package com.google.android.systemui;
 
 import com.android.systemui.SystemUIFactory;
-import com.android.systemui.statusbar.NotificationLockscreenUserManager;
 import com.android.systemui.statusbar.ScrimView;
 import com.android.systemui.statusbar.phone.DozeParameters;
 import com.android.systemui.statusbar.phone.ScrimController;
@@ -13,4 +12,5 @@ import java.util.function.Consumer;
 import com.android.systemui.statusbar.notification.NotificationEntryManager;
 
 public class SystemUIGoogleFactory extends SystemUIFactory {
+
 }
