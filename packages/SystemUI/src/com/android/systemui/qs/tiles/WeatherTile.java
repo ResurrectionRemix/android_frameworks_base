ce213c9f256a2724cdf1ac11294e68ed0e062b84
Varun Date <date.varun123@gmail.com>
2020-04-05 17:33:57 +1000

    WOw amazing cherry-pick derp
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I6a2753b879d3c19adb0a39ad6746d68d7f8d501c

--- packages/SystemUI/src/com/android/systemui/qs/tiles/WeatherTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/WeatherTile.java
@@ -80,7 +80,6 @@ public class WeatherTile extends QSTileImpl<BooleanState> implements OmniJawsCli
         mDetailAdapter = (WeatherDetailAdapter) createDetailAdapter();
     }
 
-    @Override
     public boolean isDualTarget() {
         return true;
     }
