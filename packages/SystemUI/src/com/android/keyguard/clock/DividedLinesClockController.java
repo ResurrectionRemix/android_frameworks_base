1dd1143174fcbd1b3f03f04c04064eabe9eba1e9
rohan <purohit.rohan@gmail.com>
2020-04-16 06:43:03 -0400

    SystemUI: DividedLinesClock: Fix NPE on setTextColor
    * not sure if its the best way.
    
    Change-Id: I0ecdb7df9a221251a42cb076de82d56c61f29564
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/keyguard/clock/DividedLinesClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/DividedLinesClockController.java
@@ -82,6 +82,11 @@ public class DividedLinesClockController implements ClockPlugin {
     private View mBottomLine;
 
     /**
+     * Helper to extract colors from wallpaper palette for clock face.
+     */
+    private final ClockPalette mPalette = new ClockPalette();
+
+    /**
      * Create a DefaultClockController instance.
      *
      * @param res Resources contains title and thumbnail.
@@ -172,11 +177,19 @@ public class DividedLinesClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mClock.setTextColor(color);
+        updateColor();
     }
 
     @Override
-    public void setColorPalette(boolean supportsDarkText, int[] colorPalette) {}
+    public void setColorPalette(boolean supportsDarkText, int[] colorPalette) {
+        mPalette.setColorPalette(supportsDarkText, colorPalette);
+        updateColor();
+    }
+
+    private void updateColor() {
+        final int primary = mPalette.getPrimaryColor();
+        final int secondary = mPalette.getSecondaryColor();
+    }
 
     @Override
     public void onTimeTick() {
