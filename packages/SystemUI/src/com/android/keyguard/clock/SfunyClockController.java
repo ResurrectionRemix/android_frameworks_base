25aa8eb189693aef5d4bf68d6ae98bbebcac0689
rohan <purohit.rohan@gmail.com>
2020-04-16 05:56:35 -0400

    SystemUI: Sfuny: Fix NPE on setDarkAmount.
    Change-Id: I40a94a893f89a2af4787e0e08795b524a4d6c0aa
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
@@ -70,6 +70,11 @@ public class SfunyClockController implements ClockPlugin {
     private TextClock mMinuteClock;
 
     /**
+     * Controller for transition into dark state.
+     */
+    private CrossFadeDarkController mDarkController;
+
+    /**
      * Create a DefaultClockController instance.
      *
      * @param res Resources contains title and thumbnail.
@@ -168,7 +173,9 @@ public class SfunyClockController implements ClockPlugin {
 
     @Override
     public void setDarkAmount(float darkAmount) {
-        mBigClockView.setDarkAmount(darkAmount);
+        if (mDarkController != null) {
+            mBigClockView.setDarkAmount(darkAmount);
+        }
     }
 
     @Override
