4473fff250ca17e686a28cf89379591de00adfee
Varun Date <date.varun123@gmail.com>
2020-04-21 18:00:11 +1000

    Statusbar:Add proper resources for corner radius
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I675ec19a32ee93b54d9050db7bea102e42d59948

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3927,7 +3927,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     public boolean isCurrentRoundedSameAsFw() {
         float density = Resources.getSystem().getDisplayMetrics().density;
         // Resource IDs for framework properties
-        int resourceIdRadius = (int) mContext.getResources().getDimension(R.dimen.rounded_corner_radius);
+        int resourceIdRadius = (int) mContext.getResources().getDimension(com.android.internal.R.dimen.rounded_corner_radius);
         int resourceIdPadding = (int) mContext.getResources().getDimension(R.dimen.rounded_corner_content_padding);
         int resourceIdSBPadding = (int) mContext.getResources().getDimension(R.dimen.status_bar_extra_padding);
 
@@ -3968,7 +3968,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     private void updateCorners() {
         if (mSysuiRoundedFwvals && !isCurrentRoundedSameAsFw()) {
             float density = Resources.getSystem().getDisplayMetrics().density;
-            int resourceIdRadius = (int) mContext.getResources().getDimension(R.dimen.rounded_corner_radius);
+            int resourceIdRadius = (int) mContext.getResources().getDimension(com.android.internal.R.dimen.rounded_corner_radius);
             Settings.Secure.putIntForUser(mContext.getContentResolver(),
                 Settings.Secure.SYSUI_ROUNDED_SIZE, (int) (resourceIdRadius / density), UserHandle.USER_CURRENT);
             int resourceIdPadding = (int) mContext.getResources().getDimension(R.dimen.rounded_corner_content_padding);
