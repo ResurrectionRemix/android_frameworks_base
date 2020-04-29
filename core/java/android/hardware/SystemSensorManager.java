56701c75e097d611a6a3959332ebcfba226089c2
SuperDroidBond <superdroidbond@yahoo.com>
2020-04-29 23:16:36 +0530

    Add Accelerometer and Linear Acceleration sensors in blocking list
    **make it default on & add 2 more apps in the list
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: If084e4642d888ba410c05bbabace1b89ce3429fd

--- core/java/android/hardware/SystemSensorManager.java
+++ core/java/android/hardware/SystemSensorManager.java
@@ -165,7 +165,7 @@ public class SystemSensorManager extends SensorManager {
                 + MAX_LISTENER_COUNT);
         }
         if (Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SENSOR_BLOCK, 0) == 1) {
+                Settings.System.SENSOR_BLOCK, 1) == 1) {
             if (sensor.getType() == Sensor.TYPE_SIGNIFICANT_MOTION) {
                 String pkgName = mContext.getPackageName();
                 for (String blockedPkgName : mContext.getResources().getStringArray(
@@ -173,6 +173,34 @@ public class SystemSensorManager extends SensorManager {
                     if (pkgName.equals(blockedPkgName)) {
                         Log.w(TAG, "Preventing " + pkgName + "from draining battery using " +
                                 "significant motion sensor");
+                        return false;
+                    }
+                }
+            }
+        }
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SENSOR_BLOCK, 1) == 1) {
+            if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                String pkgName = mContext.getPackageName();
+                for (String blockedPkgName : mContext.getResources().getStringArray(
+                        com.android.internal.R.array.config_blockPackagesSensorDrain)) {
+                    if (pkgName.equals(blockedPkgName)) {
+                        Log.w(TAG, "Preventing " + pkgName + "from draining battery using " +
+                                "accelerometer sensor");
+                        return false;
+                    }
+                }
+            }
+        }
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SENSOR_BLOCK, 1) == 1) {
+            if (sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+                String pkgName = mContext.getPackageName();
+                for (String blockedPkgName : mContext.getResources().getStringArray(
+                        com.android.internal.R.array.config_blockPackagesSensorDrain)) {
+                    if (pkgName.equals(blockedPkgName)) {
+                        Log.w(TAG, "Preventing " + pkgName + "from draining battery using " +
+                                "linear acceleration sensor");
                         return false;
                     }
                 }
