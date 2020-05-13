05e8596e6837b7aa46f30f8f3f120564ecc75fa2
Ali B <abittin@gmail.com>
2020-05-13 23:10:10 +0300

    Revert "base: overlays: Hide pill in full gestural nav mode [1/2]"
    This reverts commit feb7daddb2b1f4b9fe3b283b74e25a5168aedd77.
    
    Change-Id: I66d725b6fae04a97a1a8869c64893197557e74b3
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -81,11 +81,7 @@ LOCAL_REQUIRED_MODULES := \
 	NavigationBarModeGesturalOverlay \
 	NavigationBarModeGesturalOverlayNarrowBack \
 	NavigationBarModeGesturalOverlayWideBack \
-	NavigationBarModeGesturalOverlayExtraWideBack \
-	NavigationBarModeGesturalOverlayNoPill \
-	NavigationBarModeGesturalOverlayNarrowBackNoPill \
-	NavigationBarModeGesturalOverlayWideBackNoPill \
-	NavigationBarModeGesturalOverlayExtraWideBackNoPill
+	NavigationBarModeGesturalOverlayExtraWideBack
 
 include $(BUILD_PHONY_PACKAGE)
 include $(CLEAR_VARS)
