AICP/frameworks_base

Allow to hide navigation bar gesture completely [1/2]
abc: adapt to our code

reminder: we don't update user setting values here with an observer
because we trigger an overlay update from the Settings part, so all
views here get refreshed accordingly (while DisplayPolicy gets updated
by onConfigurationChanged on overlay changes).

Change-Id: I9c990994b7ec9a14a9f3c081feb8a56f009dcdb0

services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -165,6 +165,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.policy.ScreenDecorationsUtils;
 import com.android.internal.util.ScreenShapeHelper;
 import com.android.internal.util.ScreenshotHelper;
+import com.android.internal.util.aicp.AicpUtils;
 import com.android.internal.util.aicp.DeviceUtils;
 import com.android.internal.util.function.TriConsumer;
 import com.android.internal.widget.PointerLocationView;
@@ -2743,6 +2744,17 @@ public class DisplayPolicy {
         final int landscapeRotation = displayRotation.getLandscapeRotation();
         final int seascapeRotation = displayRotation.getSeascapeRotation();
         final int uiMode = mService.mPolicy.getUiMode();
+        final boolean showNavbar = AicpUtils.shouldShowGestureNav(mContext);
+        final int navbarHeight = showNavbar ?
+                res.getDimensionPixelSize(R.dimen.navigation_bar_height) : 0;
+        final int navbarHeightLandscape = showNavbar ?
+                res.getDimensionPixelSize(R.dimen.navigation_bar_height_landscape) : 0;
+        final int navbarFrameHeight = showNavbar ?
+                res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height) : 0;
+        final int navbarFrameHeightLandscape = showNavbar ?
+                res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height_landscape) : 0;
+        final int navbarWidth = showNavbar ?
+                res.getDimensionPixelSize(R.dimen.navigation_bar_width) : 0;
 
         if (hasStatusBar()) {
             mStatusBarHeightForRotation[portraitRotation] =
@@ -2761,25 +2773,25 @@ public class DisplayPolicy {
         // Height of the navigation bar when presented horizontally at bottom
         mNavigationBarHeightForRotationDefault[portraitRotation] =
         mNavigationBarHeightForRotationDefault[upsideDownRotation] =
-                res.getDimensionPixelSize(R.dimen.navigation_bar_height);
+                navbarHeight;
         mNavigationBarHeightForRotationDefault[landscapeRotation] =
         mNavigationBarHeightForRotationDefault[seascapeRotation] =
-                res.getDimensionPixelSize(R.dimen.navigation_bar_height_landscape);
+                navbarHeightLandscape;
 
         // Height of the navigation bar frame when presented horizontally at bottom
         mNavigationBarFrameHeightForRotationDefault[portraitRotation] =
         mNavigationBarFrameHeightForRotationDefault[upsideDownRotation] =
-                res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height);
+                navbarFrameHeight;
         mNavigationBarFrameHeightForRotationDefault[landscapeRotation] =
         mNavigationBarFrameHeightForRotationDefault[seascapeRotation] =
-                res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height_landscape);
+                navbarFrameHeightLandscape;
 
         // Width of the navigation bar when presented vertically along one side
         mNavigationBarWidthForRotationDefault[portraitRotation] =
         mNavigationBarWidthForRotationDefault[upsideDownRotation] =
         mNavigationBarWidthForRotationDefault[landscapeRotation] =
         mNavigationBarWidthForRotationDefault[seascapeRotation] =
-                res.getDimensionPixelSize(R.dimen.navigation_bar_width);
+                navbarWidth;
 
         if (ALTERNATE_CAR_MODE_NAV_SIZE) {
             // Height of the navigation bar when presented horizontally at bottom

ResurrectionRemix/android_frameworks_base

Signed-off-by: Varun Date <date.varun123@gmail.com>

services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -167,6 +167,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.policy.ScreenDecorationsUtils;
 import com.android.internal.util.ScreenShapeHelper;
 import com.android.internal.util.ScreenshotHelper;
+import com.android.internal.util.rr.RRUtils;
 import com.android.internal.util.function.TriConsumer;
 import com.android.internal.widget.PointerLocationView;
 import com.android.server.LocalServices;
@@ -2773,6 +2774,17 @@ public class DisplayPolicy {
         final int landscapeRotation = displayRotation.getLandscapeRotation();
         final int seascapeRotation = displayRotation.getSeascapeRotation();
         final int uiMode = mService.mPolicy.getUiMode();
+        final boolean showNavbar = RRUtils.shouldShowGestureNav(mContext);
+        final int navbarHeight = showNavbar ?
+                res.getDimensionPixelSize(R.dimen.navigation_bar_height) : 0;
+        final int navbarHeightLandscape = showNavbar ?
+                res.getDimensionPixelSize(R.dimen.navigation_bar_height_landscape) : 0;
+        final int navbarFrameHeight = showNavbar ?
+                res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height) : 0;
+        final int navbarFrameHeightLandscape = showNavbar ?
+                res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height_landscape) : 0;
+        final int navbarWidth = showNavbar ?
+                res.getDimensionPixelSize(R.dimen.navigation_bar_width) : 0;
 
         if (hasStatusBar()) {
             mStatusBarHeightForRotation[portraitRotation] =
@@ -2791,25 +2803,25 @@ public class DisplayPolicy {
         // Height of the navigation bar when presented horizontally at bottom
         mNavigationBarHeightForRotationDefault[portraitRotation] =
         mNavigationBarHeightForRotationDefault[upsideDownRotation] =
-                res.getDimensionPixelSize(R.dimen.navigation_bar_height);
+                navbarHeight;
         mNavigationBarHeightForRotationDefault[landscapeRotation] =
         mNavigationBarHeightForRotationDefault[seascapeRotation] =
-                res.getDimensionPixelSize(R.dimen.navigation_bar_height_landscape);
+                navbarHeightLandscape;
 
         // Height of the navigation bar frame when presented horizontally at bottom
         mNavigationBarFrameHeightForRotationDefault[portraitRotation] =
         mNavigationBarFrameHeightForRotationDefault[upsideDownRotation] =
-                res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height);
+                navbarFrameHeight;
         mNavigationBarFrameHeightForRotationDefault[landscapeRotation] =
         mNavigationBarFrameHeightForRotationDefault[seascapeRotation] =
-                res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height_landscape);
+                navbarFrameHeightLandscape;
 
         // Width of the navigation bar when presented vertically along one side
         mNavigationBarWidthForRotationDefault[portraitRotation] =
         mNavigationBarWidthForRotationDefault[upsideDownRotation] =
         mNavigationBarWidthForRotationDefault[landscapeRotation] =
         mNavigationBarWidthForRotationDefault[seascapeRotation] =
-                res.getDimensionPixelSize(R.dimen.navigation_bar_width);
+                navbarWidth;
 
         if (ALTERNATE_CAR_MODE_NAV_SIZE) {
             // Height of the navigation bar when presented horizontally at bottom