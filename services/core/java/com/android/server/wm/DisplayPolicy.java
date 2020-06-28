b92ab6fed1f8c40b6e906108026eeb882be5fd54
Anushek Prasal <anushekprasal@gmail.com>
2020-06-28 23:27:20 +0530

    base: Fix navbar when IME space is hidden
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -2781,9 +2781,9 @@ public class DisplayPolicy {
                 res.getDimensionPixelSize(R.dimen.navigation_bar_height) : 0;
         final int navbarHeightLandscape = showNavbar ?
                 res.getDimensionPixelSize(R.dimen.navigation_bar_height_landscape) : 0;
-        final int navbarFrameHeight = showIMESpace ?
+        final int navbarFrameHeight = showIMESpace || !gestureNavbarHidden() ?
                 res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height) : 0;
-        final int navbarFrameHeightLandscape = showIMESpace ?
+        final int navbarFrameHeightLandscape = showIMESpace || !gestureNavbarHidden() ?
                 res.getDimensionPixelSize(R.dimen.navigation_bar_frame_height_landscape) : 0;
         final int navbarWidth = showNavbar ?
                 res.getDimensionPixelSize(R.dimen.navigation_bar_width) : 0;
@@ -3909,5 +3909,15 @@ public class DisplayPolicy {
         final WindowManager wm = mContext.getSystemService(WindowManager.class);
         wm.removeView(mPointerLocationView);
         mPointerLocationView = null;
+    }
+
+    private boolean gestureNavbarHidden() {
+        boolean isGestureNavbar = (Utils.isThemeEnabled("com.android.internal.systemui.navbar.gestural")
+                || Utils.isThemeEnabled("com.android.internal.systemui.navbar.gestural_wide_back")
+                || Utils.isThemeEnabled("com.android.internal.systemui.navbar.gestural_extra_wide_back")
+                || Utils.isThemeEnabled("com.android.internal.systemui.navbar.gestural_narrow_back"));
+        boolean isNavbarHidden = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.NAVIGATION_HANDLE_WIDTH, 1) == 0;
+        return isGestureNavbar && isNavbarHidden;
     }
 }
