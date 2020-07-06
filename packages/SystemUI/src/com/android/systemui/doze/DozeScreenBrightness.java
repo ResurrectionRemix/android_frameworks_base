4f1d936ccd13aad933654511605dc246f50d740d
Varun Date <date.varun123@gmail.com>
2020-07-06 22:11:51 +1000

    Revert "SystemUI: fix black screen after AOD_PAUSED"
    This reverts commit b54c4f75d0a30b9a00e80b27700055b0147289dd.
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: Ib3df0d688ddbe5e0097371a947598ce689187db9

--- packages/SystemUI/src/com/android/systemui/doze/DozeScreenBrightness.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeScreenBrightness.java
@@ -114,12 +114,6 @@ public class DozeScreenBrightness extends BroadcastReceiver implements DozeMachi
                 break;
             case DOZE_AOD:
                 setBrightnessToValue(getDozeBrightnessValue());
-                setLightSensorEnabled(true);
-                // we dont have a brightness sensor so remove any font scrim
-                // set from prepareForGentleWakeUp right away
-                if (!mRegistered) {
-                    mDozeHost.setAodDimmingScrim(0f);
-                }
                 break;
             case DOZE_REQUEST_PULSE:
                 setBrightnessToValue(getPuleBrightnessValue());
