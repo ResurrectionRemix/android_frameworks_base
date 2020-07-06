0f12e6cb4e97d20f68e0d462e39f09642d935bf6
AnkitGourav <Ankitgourav035@gmail.com>
2020-07-06 23:58:14 +0530

    SettingsLib: Add method to get daily data usage
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/net/DataUsageUtils.java
+++ packages/SettingsLib/src/com/android/settingslib/net/DataUsageUtils.java
@@ -20,9 +20,11 @@ import android.content.Context;
 import android.net.NetworkTemplate;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
+import android.text.format.Time;
 import android.util.Log;
 
 import com.android.internal.util.ArrayUtils;
+
 /**
  * Utils class for data usage
  */
@@ -54,5 +56,16 @@ public class DataUsageUtils {
         }
 
         return NetworkTemplate.normalize(mobileAll, mergedSubscriberIds);
+    }
+
+    /**
+     * Returns today's passed time in Millisecond
+     */
+    public static long getTodayMillis() {
+        final long passedMillis;
+        Time time = new Time();
+        time.set(System.currentTimeMillis());
+        passedMillis = ((time.hour * 60 * 60) + (time.minute * 60) + time.second) * 1000;
+        return passedMillis;
     }
 }
