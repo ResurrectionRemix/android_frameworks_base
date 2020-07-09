af95ff24f3da05ee31b8ba0cbf6cd0d2239aeb50
Varun Date <date.varun123@gmail.com>
2020-07-09 12:52:31 +1000

    Revert "base: overlays: Add Pebble icon shape"
    This reverts commit 031cd9e2799fbdd9e475e77a6f10435e8f198b40.

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -70,7 +70,6 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedThemePickerOverlay \
 	IconShapeCylinderOverlay \
 	IconShapeHexagonOverlay \
-	IconShapePebbleOverlay \
 	IconShapeRoundedHexagonOverlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
