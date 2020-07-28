3618ebd594395191c9b8ed58c9e5505e703ac519
SuperDroidBond <superdroidbond@yahoo.com>
2020-07-28 14:35:02 +1000

    Clock style font, size and color [1/2]
    Port to RR @varund7726
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: If91434b5cd230b7e20acf0975de01e567c90b517

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6232,6 +6232,25 @@ public final class Settings {
         public static final String LOCKSCREEN_LOCK_ICON = "lockscreen_lock_icon";
 
         /**
+         * Clock font size
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SIZE = "status_bar_clock_size";
+
+        /**
+         * Clock font color
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_COLOR = "status_bar_clock_color";
+
+        /**
+         * Clock font style
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_FONT_STYLE = "status_bar_clock_font_style";
+
+
+        /**
          * =-
          * @hide
          */
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -45,7 +45,7 @@ import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.widget.TextView;
-
+import android.graphics.Typeface;
 import com.android.settingslib.Utils;
 import com.android.systemui.DemoMode;
 import com.android.systemui.Dependency;
@@ -133,12 +133,28 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
     private int mHideDuration = HIDE_DURATION, mShowDuration = SHOW_DURATION;
     private Handler mHandler = new Handler();
 
+    private int mClockFontStyle = FONT_NORMAL;
+    public int DEFAULT_CLOCK_SIZE = 14;
+    public int DEFAULT_CLOCK_COLOR = 0xffffffff;
+    private int mClockColor = 0xffffffff;
+    protected int mClockDateDisplay = CLOCK_DATE_DISPLAY_GONE;
+    protected int mClockDateStyle = CLOCK_DATE_STYLE_REGULAR;
+    protected int mClockStyle = STYLE_CLOCK_LEFT;
+    private int mClockSize = 14;
+
     public static final String STATUS_BAR_CLOCK_AUTO_HIDE =
             "system:" + Settings.System.STATUS_BAR_CLOCK_AUTO_HIDE;
     public static final String STATUS_BAR_CLOCK_AUTO_HIDE_HDURATION =
             "system:" + Settings.System.STATUS_BAR_CLOCK_AUTO_HIDE_HDURATION;
     public static final String STATUS_BAR_CLOCK_AUTO_HIDE_SDURATION =
             "system:" + Settings.System.STATUS_BAR_CLOCK_AUTO_HIDE_SDURATION;
+
+    public static final String STATUS_BAR_CLOCK_COLOR =
+            "system:" + Settings.System.STATUS_BAR_CLOCK_COLOR;
+    public static final String STATUS_BAR_CLOCK_SIZE =
+            "system:" + Settings.System.STATUS_BAR_CLOCK_SIZE;
+    public static final String STATUS_BAR_CLOCK_FONT_STYLE =
+            "system:" + Settings.System.STATUS_BAR_CLOCK_FONT_STYLE;
 
     /**
      * Whether we should use colors that adapt based on wallpaper/the scrim behind quick settings
@@ -238,6 +254,9 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
                     STATUSBAR_CLOCK_AM_PM_STYLE, STATUSBAR_CLOCK_DATE_DISPLAY,
                     STATUSBAR_CLOCK_DATE_STYLE, STATUSBAR_CLOCK_DATE_FORMAT, STATUSBAR_CLOCK_DATE_POSITION,
                     STATUS_BAR_CLOCK_AUTO_HIDE,
+                    STATUS_BAR_CLOCK_COLOR,
+                    STATUS_BAR_CLOCK_FONT_STYLE,
+                    STATUS_BAR_CLOCK_SIZE,
                     STATUS_BAR_CLOCK_AUTO_HIDE_HDURATION,
                     STATUS_BAR_CLOCK_AUTO_HIDE_SDURATION);
             if (mShowDark) {
@@ -394,6 +413,202 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
         } else if (STATUS_BAR_CLOCK_AUTO_HIDE_SDURATION.equals(key)) {
             mShowDuration =
                     TunerService.parseInteger(newValue, SHOW_DURATION);
+        } else if (STATUS_BAR_CLOCK_COLOR.equals(key)) {
+           mClockColor =
+                    TunerService.parseInteger(newValue, DEFAULT_CLOCK_COLOR);
+           updateClockColor();
+        } else if (STATUS_BAR_CLOCK_FONT_STYLE.equals(key)) {
+            mClockFontStyle  =
+                    TunerService.parseInteger(newValue, 0);
+            updateClockFontStyle();
+        } else if (STATUS_BAR_CLOCK_SIZE.equals(key)) {
+            mClockSize =
+                    TunerService.parseInteger(newValue, DEFAULT_CLOCK_SIZE);
+            updateClockSize();
+        }
+    }
+
+    public void updateClockSize() {
+            setTextSize(mClockSize);
+            updateClock();
+    }
+
+    private void updateClockColor() {
+            if (mClockColor == 0xFFFFFFFF) {
+                    setTextColor(mNonAdaptedColor);
+            } else {
+                    setTextColor(mClockColor);
+            }
+   	    updateClock();
+    }
+
+
+    private void updateClockFontStyle() {
+        getClockFontStyle(mClockFontStyle);
+        updateClock();
+    }
+
+    public void getClockFontStyle(int font) {
+     if (font == 0) {
+            setTypeface(Typeface.create("sans-serif", Typeface.NORMAL));
+        }
+        if (font == 1) {
+            setTypeface(Typeface.create("sans-serif", Typeface.BOLD));
+        }
+        if (font == 2) {
+            setTypeface(Typeface.create("sans-serif", Typeface.ITALIC));
+        }
+        if (font == 3) {
+            setTypeface(Typeface.create("sans-serif", Typeface.BOLD_ITALIC));
+        }
+        if (font == 4) {
+            setTypeface(Typeface.create("sans-serif-light", Typeface.ITALIC));
+        }
+        if (font == 5) {
+            setTypeface(Typeface.create("sans-serif-light", Typeface.NORMAL));
+        }
+        if (font == 6) {
+            setTypeface(Typeface.create("sans-serif-thin", Typeface.ITALIC));
+        }
+        if (font == 7) {
+            setTypeface(Typeface.create("sans-serif-thin", Typeface.NORMAL));
+        }
+        if (font == 8) {
+            setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
+        }
+        if (font == 9) {
+            setTypeface(Typeface.create("sans-serif-condensed", Typeface.ITALIC));
+        }
+        if (font == 10) {
+            setTypeface(Typeface.create("sans-serif-condensed", Typeface.BOLD));
+        }
+        if (font == 11) {
+            setTypeface(Typeface.create("sans-serif-condensed", Typeface.BOLD_ITALIC));
+        }
+        if (font == 12) {
+            setTypeface(Typeface.create("sans-serif-medium", Typeface.NORMAL));
+        }
+        if (font == 13) {
+            setTypeface(Typeface.create("sans-serif-medium", Typeface.ITALIC));
+        }
+        if (font == 14) {
+                setTypeface(Typeface.create("sans-serif-condensed-light", Typeface.NORMAL));
+        }
+        if (font == 15) {
+                setTypeface(Typeface.create("sans-serif-condensed-light", Typeface.ITALIC));
+        }
+        if (font == 16) {
+                setTypeface(Typeface.create("sans-serif-black", Typeface.NORMAL));
+        }
+        if (font == 17) {
+                setTypeface(Typeface.create("sans-serif-black", Typeface.ITALIC));
+        }
+        if (font == 18) {
+                setTypeface(Typeface.create("cursive", Typeface.NORMAL));
+        }
+        if (font == 19) {
+                setTypeface(Typeface.create("cursive", Typeface.BOLD));
+        }
+        if (font == 20) {
+                setTypeface(Typeface.create("casual", Typeface.NORMAL));
+        }
+        if (font == 21) {
+                setTypeface(Typeface.create("serif", Typeface.NORMAL));
+        }
+        if (font == 22) {
+                setTypeface(Typeface.create("serif", Typeface.ITALIC));
+        }
+        if (font == 23) {
+                setTypeface(Typeface.create("serif", Typeface.BOLD));
+        }
+        if (font == 24) {
+                setTypeface(Typeface.create("serif", Typeface.BOLD_ITALIC));
+        }
+        if (font == 25) {
+            setTypeface(Typeface.create("accuratist", Typeface.NORMAL));
+        }
+        if (font == 26) {
+            setTypeface(Typeface.create("aclonica", Typeface.NORMAL));
+        }
+        if (font == 27) {
+            setTypeface(Typeface.create("amarante", Typeface.NORMAL));
+        }
+        if (font == 28) {
+            setTypeface(Typeface.create("bariol", Typeface.NORMAL));
+        }
+        if (font == 29) {
+            setTypeface(Typeface.create("cagliostro", Typeface.NORMAL));
+        }
+        if (font == 30) {
+            setTypeface(Typeface.create("cocon", Typeface.NORMAL));
+        }
+        if (font == 31) {
+            setTypeface(Typeface.create("comfortaa", Typeface.NORMAL));
+        }
+
+        if (font == 32) {
+                setTypeface(Typeface.create("comicsans", Typeface.NORMAL));
+        }
+        if (font == 33) {
+                setTypeface(Typeface.create("coolstory", Typeface.NORMAL));
+        }
+        if (font == 34) {
+                setTypeface(Typeface.create("exotwo", Typeface.NORMAL));
+        }
+        if (font == 35) {
+                setTypeface(Typeface.create("fifa2018", Typeface.NORMAL));
+        }
+        if (font == 36) {
+            setTypeface(Typeface.create("googlesans", Typeface.NORMAL));
+        }
+        if (font == 37) {
+            setTypeface(Typeface.create("grandhotel", Typeface.NORMAL));
+        }
+        if (font == 38) {
+            setTypeface(Typeface.create("lato", Typeface.NORMAL));
+        }
+        if (font == 39) {
+            setTypeface(Typeface.create("lgsmartgothic", Typeface.NORMAL));
+        }
+        if (font == 40) {
+            setTypeface(Typeface.create("nokiapure", Typeface.NORMAL));
+        }
+        if (font == 41) {
+            setTypeface(Typeface.create("nunito", Typeface.NORMAL));
+        }
+        if (font == 42) {
+            setTypeface(Typeface.create("quando", Typeface.NORMAL));
+        }
+
+        if (font == 43) {
+                setTypeface(Typeface.create("redressed", Typeface.NORMAL));
+        }
+        if (font == 44) {
+            setTypeface(Typeface.create("reemkufi", Typeface.NORMAL));
+        }
+        if (font == 45) {
+            setTypeface(Typeface.create("robotocondensed", Typeface.NORMAL));
+        }
+        if (font == 46) {
+            setTypeface(Typeface.create("rosemary", Typeface.NORMAL));
+        }
+        if (font == 47) {
+            setTypeface(Typeface.create("samsungone", Typeface.NORMAL));
+        }
+        if (font == 48) {
+            setTypeface(Typeface.create("oneplusslate", Typeface.NORMAL));
+        }
+        if (font == 49) {
+            setTypeface(Typeface.create("sonysketch", Typeface.NORMAL));
+        }
+        if (font == 50) {
+            setTypeface(Typeface.create("storopia", Typeface.NORMAL));
+        }
+        if (font == 51) {
+            setTypeface(Typeface.create("surfer", Typeface.NORMAL));
+        }
+        if (font == 52) {
+            setTypeface(Typeface.create("ubuntu", Typeface.NORMAL));
         }
     }
 
