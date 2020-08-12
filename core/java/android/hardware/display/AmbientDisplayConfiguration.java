a65edad5dfb51c874c1ecd60a38301c74bb9482d
ezio84 <brabus84@gmail.com>
2020-08-12 12:18:05 +1000

    DozeTriggers: Ensure we pass double tap as gesture
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I27f5ad137dfd3c3453f7c5ececb1b82bb20d5b51

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -73,7 +73,8 @@ public class AmbientDisplayConfiguration {
     private boolean pulseOnCustomDozeEventEnabled(int user) {
         return (Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_TILT_GESTURE, 0) != 0
                 || Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_POCKETMODE_GESTURE, 0) != 0
-                || Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_HANDWAVE_GESTURE, 0) != 0)
+                || Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_HANDWAVE_GESTURE, 0) != 0
+                || Settings.System.getInt(mContext.getContentResolver(), Settings.System.DOZE_TRIGGER_DOUBLETAP, 0) != 0)
                 && pulseOnNotificationAvailable();
     }
 
