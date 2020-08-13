AICP/frameworks_base

Fix crash due to missing preference
SystemUI Tuner was crashing due to call to a non-existent preference in the current preferenceScreen.
Move the call to the proper fragment with all the references.

Change-Id: Ibaeb0778442b0130a543ebb99aaece3c588578d1

packages/SystemUI/src/com/android/systemui/tuner/StatusBarTuner.java
@@ -24,7 +24,11 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.systemui.R;
 
+import com.android.internal.util.aicp.DeviceUtils;
+
 public class StatusBarTuner extends PreferenceFragment {
+
+    private static final String KEY_NFC = "nfc";
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -36,6 +40,10 @@ public class StatusBarTuner extends PreferenceFragment {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.status_bar_prefs);
+
+        if (!DeviceUtils.deviceSupportsNfc(getContext())) {
+            getPreferenceScreen().removePreference(findPreference(KEY_NFC));
+        }
     }
 
     @Override

ResurrectionRemix/android_frameworks_base

Signed-off-by: Varun Date <date.varun123@gmail.com>

packages/SystemUI/src/com/android/systemui/tuner/StatusBarTuner.java
@@ -24,7 +24,11 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.systemui.R;
 
+import com.android.internal.util.rr.DeviceUtils;
+
 public class StatusBarTuner extends PreferenceFragment {
+
+    private static final String KEY_NFC = "nfc";
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -36,6 +40,10 @@ public class StatusBarTuner extends PreferenceFragment {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.status_bar_prefs);
+
+        if (!DeviceUtils.deviceSupportsNfc(getContext())) {
+            getPreferenceScreen().removePreference(findPreference(KEY_NFC));
+        }
     }
 
     @Override