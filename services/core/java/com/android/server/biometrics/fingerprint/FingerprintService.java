c3cc3a8ac9cbfe28e0634de575f104fd23e6f752
Jesse Chan <jc@lineageos.org>
2020-08-15 15:40:27 +0800

    FingerprintService: start FOD after fingerprint HAL success
    In some cases, fingerprint HAL operation may fail. If that happens,
    it is useless to show FOD icon which will then stuck on screen.
    
    There also exists another scenario under which fingerprint sensor might
    need time to prepare (eg. calibrate) before it can handle requests.
    Showing FOD icon prematurely can mislead users to think the FOD is ready
    to use. In some cases, preparation procedures can fail if user touches
    the sensor before it is ready.
    
    This change makes sure that FOD is only shown after the fingerprint HAL
    success and improves the UX by making FOD icon a more accurate indicator
    of fingerprint sensor readiness.
    
    Change-Id: I76992ff51bfb2030e786220e825d3e9347454261

--- services/core/java/com/android/server/biometrics/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/biometrics/fingerprint/FingerprintService.java
@@ -711,6 +711,10 @@ public class FingerprintService extends BiometricServiceBase {
                 Slog.w(TAG, "authenticate(): no fingerprint HAL!");
                 return ERROR_ESRCH;
             }
+            int ret = daemon.authenticate(operationId, groupId);
+            if (ret < 0) {
+                return ret;
+            }
             if (mHasFod) {
                 IFingerprintInscreen fodDaemon = getFingerprintInScreenDaemon();
                 if (fodDaemon != null) {
@@ -726,7 +730,7 @@ public class FingerprintService extends BiometricServiceBase {
                     Slog.e(TAG, "showInDisplayFingerprintView failed", e);
                 }
             }
-            return daemon.authenticate(operationId, groupId);
+            return ret;
         }
 
         @Override
@@ -774,6 +778,10 @@ public class FingerprintService extends BiometricServiceBase {
                 Slog.w(TAG, "enroll(): no fingerprint HAL!");
                 return ERROR_ESRCH;
             }
+            int ret = daemon.enroll(cryptoToken, groupId, timeout);
+            if (ret < 0) {
+                return ret;
+            }
             if (mHasFod) {
                 IFingerprintInscreen fodDaemon = getFingerprintInScreenDaemon();
                 if (fodDaemon != null) {
@@ -789,7 +797,7 @@ public class FingerprintService extends BiometricServiceBase {
                     Slog.e(TAG, "showInDisplayFingerprintView failed", e);
                 }
             }
-            return daemon.enroll(cryptoToken, groupId, timeout);
+            return ret;
         }
 
         @Override
