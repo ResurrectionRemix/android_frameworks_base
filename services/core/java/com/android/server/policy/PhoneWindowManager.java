38d410f8c9fc2677f0ee254f2ce3513f37445020
Varun Date <date.varun123@gmail.com>
2020-08-22 01:24:45 +1000

    PhoneWindowManager: Force 3 finger gesture to take full screen screenshots
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: Ice410acb043e279882c1d5fd54545ef982fea88e

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2214,7 +2214,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mSwipeToScreenshot = new SwipeToScreenshotListener(context, new SwipeToScreenshotListener.Callbacks() {
             @Override
             public void onSwipeThreeFinger() {
-                mHandler.post(mScreenshotRunnable);
+                RRActionUtils.takeScreenshot(true);
             }
         });
 
