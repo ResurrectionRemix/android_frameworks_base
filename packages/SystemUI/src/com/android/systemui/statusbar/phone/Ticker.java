0f99447b48bc48f3853f7114e95235d64f5deccf
Zhao Dong <zhao.dong20@zte.com.cn>
2020-08-27 12:39:31 +0800

    Apply theme by using notification context
    Apply theme by using notification context instead of
    SystemUI context when loading notification icon drawable.
    
    When loading notification icon for StatusBarIconView, use
    notification's context as parameter, then applyTheme will
    find theme resource in notification package and avoid
    finding wrong theme resource in SystemUI for other apply's
    drawable resources.
    
    Bug: 158126029
    
    Change-Id: Iaf8d8c6ff4af988233a1a331b255f6a4b44ca6cb
    [@neobuddy89: Update ticker code to fix compilation.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
@@ -276,7 +276,7 @@ public abstract class Ticker implements DarkReceiver {
             }
         }
 
-        final Drawable icon = StatusBarIconView.getIcon(mContext,
+        final Drawable icon = StatusBarIconView.getIcon(mContext, mContext,
                 new StatusBarIcon(n.getPackageName(), n.getUser(), n.getNotification().icon, n.getNotification().iconLevel, 0,
                         n.getNotification().tickerText));
         final CharSequence text = n.getNotification().tickerText;
