f773073ce2210b374f9ef576be6871f034f3fa28
Julia Reynolds <juliacr@google.com>
2022-01-07 16:28:52 -0500

    Check group channels for FGSes
    Before allowing the group to be deleted, by updating
    the current check to the method that populates the channel
    list
    
    Test: NotificationManagerServiceTest
    Bug: 209965481
    Change-Id: I9db781c300e96e9c80bd5d21585b8be9b4db08c8
    Merged-In: I9db781c300e96e9c80bd5d21585b8be9b4db08c8
    (cherry picked from commit 331b61794982778acd140eeae5047730072c93d5)
    (cherry picked from commit adf4be190e39691c99578ee01fc3b19ca0dd16d1)
    Merged-In:I9db781c300e96e9c80bd5d21585b8be9b4db08c8

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2130,7 +2130,7 @@ public class NotificationManagerService extends SystemService {
         }
     }
 
-    private void createNotificationChannelGroup(String pkg, int uid, NotificationChannelGroup group,
+    void createNotificationChannelGroup(String pkg, int uid, NotificationChannelGroup group,
             boolean fromApp, boolean fromListener) {
         Preconditions.checkNotNull(group);
         Preconditions.checkNotNull(pkg);
@@ -2924,7 +2924,8 @@ public class NotificationManagerService extends SystemService {
 
             final int callingUid = Binder.getCallingUid();
             NotificationChannelGroup groupToDelete =
-                    mPreferencesHelper.getNotificationChannelGroup(groupId, pkg, callingUid);
+                    mPreferencesHelper.getNotificationChannelGroupWithChannels(
+                            pkg, callingUid, groupId, false);
             if (groupToDelete != null) {
                 // Preflight for allowability
                 final int userId = UserHandle.getUserId(callingUid);
